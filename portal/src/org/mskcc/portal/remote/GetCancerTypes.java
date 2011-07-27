78ada85205654dcef766395b9825e1a712e473ad
Ethan Cerami <cerami@gmail.com>
2011-07-27 07:07:32 -0400

    Cancer studies are now alphabetized;  fixes issue #9

--- portal/src/org/mskcc/portal/remote/GetCancerTypes.java
+++ portal/src/org/mskcc/portal/remote/GetCancerTypes.java
@@ -7,6 +7,8 @@ import org.mskcc.portal.util.XDebug;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 
 /**
  * Gets all Cancer Types stored in Remote CGDS Server.
@@ -46,6 +48,27 @@ public class GetCancerTypes {
         } catch (IOException e) {
             throw new RemoteException("Remote Access Error", e);
         }
+
+        //  Before returning the list, sort it alphabetically
+        Collections.sort(cancerTypeList, new CancerTypeComparator());
         return cancerTypeList;
+    }
+}
+
+/**
+ * Compares Cancer Studies, so that we can sort them alphabetically.
+ */
+class CancerTypeComparator implements Comparator {
+
+    /**
+     * Compare two cancer studies.
+     * @param o  First Cancer Study.
+     * @param o1 Second Cancer Study.
+     * @return int indicating name sort order.
+     */
+    public int compare(Object o, Object o1) {
+        CancerType study0 = (CancerType) o;
+        CancerType study1 = (CancerType) o1;
+        return study0.getCancerName().compareTo(study1.getCancerName());
     }
 }

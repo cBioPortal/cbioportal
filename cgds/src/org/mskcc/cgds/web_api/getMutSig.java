edbc4bb4cd9206162ac51c00c28f9daf35fe1277
lennartbastian <lennartbastian@gmail.com>
2011-08-02 11:51:11 -0400

    added getMutSig to Web API

---
+++ cgds/src/org/mskcc/cgds/web_api/getMutSig.java
@@ -0,0 +1,11 @@
+package org.mskcc.cgds.web_api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lennartbastian
+ * Date: 02/08/2011
+ * Time: 11:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class getMutSig {
+}

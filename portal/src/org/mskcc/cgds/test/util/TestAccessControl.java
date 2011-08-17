5ec034cf81bdc5bcc41d2cc7e90ec8bd53f218d0
Benjamin Gross <benjamin.gross@gmail.com>
2011-08-17 18:27:58 -0400

    Removed args to User constructor no longer required.  This test will need updating soon.

--- portal/src/org/mskcc/cgds/test/util/TestAccessControl.java
+++ portal/src/org/mskcc/cgds/test/util/TestAccessControl.java
@@ -89,9 +89,9 @@ public class TestAccessControl extends TestCase {
     private void setUpDBMS() throws DaoException, IOException {
         ResetDatabase.resetDatabase();
 
-        user1 = new User("artg@gmail.com", "Arthur", true, "not-used");
+        user1 = new User("artg@gmail.com", "Arthur", true);
         DaoUser.addUser(user1);
-        user2 = new User("joe@gmail.com", "J", true, "not-used");
+        user2 = new User("joe@gmail.com", "J", true);
         DaoUser.addUser(user2);
 
         // load cancers

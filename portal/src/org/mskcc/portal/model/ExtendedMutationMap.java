99fda0ad2bbad8ca47da81ecf662f34baf8e0fff
Ethan Cerami <cerami@gmail.com>
2011-09-18 16:02:14 -0400

    Added unit tests for ExtendedMutationMap

--- portal/src/org/mskcc/portal/model/ExtendedMutationMap.java
+++ portal/src/org/mskcc/portal/model/ExtendedMutationMap.java
@@ -4,29 +4,47 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import org.mskcc.cgds.model.ExtendedMutation;
 
+/**
+ * A HashMap of ExtendedMutation Objects, indexed by Gene and Case ID.
+ *
+ * @author Ethan Cerami.
+ */
 public class ExtendedMutationMap {
+    private static final String DELIMITER = ":";
     HashMap <String, ArrayList<ExtendedMutation>> mutationMap =
             new HashMap<String, ArrayList<ExtendedMutation>>();
 
     public ExtendedMutationMap (ArrayList<ExtendedMutation> mutationList) {
         for (ExtendedMutation mutation:  mutationList) {
-            String key = mutation.getGeneSymbol() + ":" + mutation.getCaseId();
-            if (mutationMap.containsKey(key)) {
-                ArrayList <ExtendedMutation> currentList = mutationMap.get(key);
-                currentList.add(mutation);
-            } else {
-                ArrayList <ExtendedMutation> currentList = new ArrayList <ExtendedMutation> ();
-                currentList.add(mutation);
-                mutationMap.put(key, currentList);
-            }
+            String key = getKey(mutation.getGeneSymbol(), mutation.getCaseId());
+            appendToMap(key, mutation);
         }
     }
 
+    /**
+     * Gets all Mutations, associated with the specified Gene / Case ID combination.
+     * @param geneSymbol    Gene Symbol.
+     * @param caseId        Case ID.
+     * @return ArrayList of ExtendedMutation Objects.
+     */
     public ArrayList <ExtendedMutation> getMutations (String geneSymbol, String caseId) {
-        return mutationMap.get(geneSymbol + ":" + caseId);
+        String key = getKey(geneSymbol.toUpperCase(), caseId);
+        return mutationMap.get(key);
     }
 
-    public int getSize() {
-        return mutationMap.size();
+    private void appendToMap(String key, ExtendedMutation mutation) {
+        if (mutationMap.containsKey(key)) {
+            ArrayList<ExtendedMutation> currentList = mutationMap.get(key);
+            currentList.add(mutation);
+        } else {
+            ArrayList <ExtendedMutation> currentList = new ArrayList <ExtendedMutation> ();
+            currentList.add(mutation);
+            mutationMap.put(key, currentList);
+        }
     }
+
+    private String getKey(String geneSymbol, String caseId) {
+        return geneSymbol + DELIMITER + caseId;
+    }
+
 }

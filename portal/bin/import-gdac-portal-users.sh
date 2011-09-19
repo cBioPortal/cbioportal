1ec25d21f020cca3ecd4b6f9f4eb978acfc700d2
Benjamin Gross <benjamin.gross@gmail.com>
2011-09-19 14:58:24 -0400

    bash script that wraps importUsers.py for using in cron.

--- portal/bin/import-gdac-portal-users.sh
+++ portal/bin/import-gdac-portal-users.sh
@@ -1,4 +1,9 @@
-#!/bin/bash -l
+#!/bin/bash
+
+# setup the enviroment vars (maybe easier to source bashrc, etc, like . /home/user/.bashrc)
+PYTHONHOME=
+PYTHONPATH=
+PORTAL_HOME=
 
 # Note, this script was intended to be run via cron.  Please ensure that the path to build.properties.GDAC
 # points to an unchanging location and version of the file.  The path below is only meant for illustrative purposes.

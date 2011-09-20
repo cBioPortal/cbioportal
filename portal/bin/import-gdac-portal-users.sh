8f638d9c11d2a7dd98ba00d8b2da584c6456488c
Benjamin Gross <benjamin.gross@gmail.com>
2011-09-20 13:04:39 -0400

    Added send-email-confirm argument

--- portal/bin/import-gdac-portal-users.sh
+++ portal/bin/import-gdac-portal-users.sh
@@ -7,4 +7,4 @@ PORTAL_HOME=
 
 # Note, this script was intended to be run via cron.  Please ensure that the path to build.properties.GDAC
 # points to an unchanging location and version of the file.  The path below is only meant for illustrative purposes.
-$PORTAL_HOME/scripts/importUsers.py --properties-file $PORTAL_HOME/build.properties.GDAC 2>&1 > /dev/null
+$PORTAL_HOME/scripts/importUsers.py --properties-file $PORTAL_HOME/build.properties.GDAC --send-email-confirm true 2>&1 > /dev/null

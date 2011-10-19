f122c7d57faad35e1aa2db2e7e067b8bcc2e5fb3
Ethan Cerami <cerami@gmail.com>
2011-10-19 16:35:51 -0400

    Fixed typo in cell-map.sif

--- portal/scripts/loadNetwork.sh
+++ portal/scripts/loadNetwork.sh
@@ -1,5 +1,5 @@
 
 ./importHprd.pl $CGDS_DATA_HOME/network/HPRD.tsv
-./importSif.pl $CGDS_DATA_HOME/network/cellmap.sif CELLMAP
+./importSif.pl $CGDS_DATA_HOME/network/cell-map.sif CELLMAP
 ./importSif.pl $CGDS_DATA_HOME/network/nci-nature.sif NCI_NATURE
 ./importSif.pl $CGDS_DATA_HOME/network/reactome.sif REACTOME

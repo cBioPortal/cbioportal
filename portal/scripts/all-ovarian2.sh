27a77b0f48c881f2475818558b69c058256a2db2
Ethan Cerami <cerami@gmail.com>
2011-10-26 13:55:40 -0400

    Continued to work on ovarian2.sh

--- portal/scripts/all-ovarian2.sh
+++ portal/scripts/all-ovarian2.sh
@@ -1,15 +1,11 @@
-# Delete all preprocessed files
-rm -v $CGDS_DATA_HOME/ovarian/processed_*
-
 # Load up the Ovarian Meta Data File
 ./importCancerStudy.pl $CGDS_DATA_HOME/ovarian/ovarian.txt
 
 # Imports All Case Lists
-#./importCaseList.pl $CBIO_BRCA_HOME/data/case_list.txt
+./importCaseList.pl $CBIO_BRCA_HOME/out/4way_case_set.txt
 
 # Imports Clinical Data
-#./importClinicalData.pl $CBIO_BRCA_HOME/data/clinical_final.txt
+./importClinicalData.pl $CBIO_BRCA_HOME/data/clinical/clinical_final.txt
 
 # Imports Mutation Data
-./importProfileData.pl --data $CGDS_DATA_HOME/gdac-override/mafs/OV.maf --meta $CGDS_DATA_HOME/ovarian/meta_mutations_extended.txt --dbmsAction clobber --germlineWhiteList $CGDS_DATA_HOME/ovarian/ovarianGermlineWhiteList.txt --acceptRemainingMutations
-
+./importProfileData.pl --data $CBIO_BRCA_HOME/data/genomic/OV.maf --meta $CBIO_BRCA_HOME/data/genomic/meta_mutations_extended.txt --dbmsAction clobber --germlineWhiteList $CBIO_BRCA_HOME/data/genomic/ovarianGermlineWhiteList.txt

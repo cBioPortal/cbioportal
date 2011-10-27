122ecdf2866e7ef2045cb4315b8a860194935ca9
Ethan Cerami <cerami@gmail.com>
2011-10-27 11:58:41 -0400

    Updated ovarian script

--- portal/scripts/all-ovarian2.sh
+++ portal/scripts/all-ovarian2.sh
@@ -2,10 +2,13 @@
 ./importCancerStudy.pl $CGDS_DATA_HOME/ovarian/ovarian.txt
 
 # Imports All Case Lists
-./importCaseList.pl $CBIO_BRCA_HOME/out/4way_case_set.txt
+./importCaseList.pl $CBIO_BRCA_HOME/out/cases
 
 # Imports Clinical Data
 ./importClinicalData.pl $CBIO_BRCA_HOME/data/clinical/clinical_final.txt
 
 # Imports Mutation Data
 ./importProfileData.pl --data $CBIO_BRCA_HOME/data/genomic/OV.maf --meta $CBIO_BRCA_HOME/data/genomic/meta_mutations_extended.txt --dbmsAction clobber --germlineWhiteList $CBIO_BRCA_HOME/data/genomic/ovarianGermlineWhiteList.txt
+
+# Import Methylation Data
+./importProfileData.pl --data $CBIO_BRCA_HOME/data/genomic/data_brca1_binary_methylation.txt --meta $CBIO_BRCA_HOME/data/genomic/meta_brca1_binary_methylation.txt --dbmsAction clobber

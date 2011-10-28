319622186af30a73eee7c9296da83f80e5ec9c1f
Michael Heuer <heuermh@gmail.com>
2011-10-27 23:53:43 -0500

    adding alt implementation of mutation service

---
+++ portal/src/org/mskcc/portal/mutation/diagram/ListMultimapMutationService.java
@@ -0,0 +1,32 @@
+package org.mskcc.portal.mutation.diagram;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ListMultimap;
+
+/**
+ * Implementation of MutationService backed by a ListMultimap.
+ */
+public final class ListMultimapMutationService implements MutationService {
+    private static final long serialVersionUID = 1L;
+    private final ListMultimap<String, Mutation> mutations;
+
+    /**
+     * Create a new list multimap mutation service with the specified mutations.  The
+     * mutations will be copied defensively into this class.
+     *
+     * @param mutations mutations, must not be null
+     */
+    public ListMultimapMutationService(final ListMultimap<String, Mutation> mutations) {
+        this.mutations = ImmutableListMultimap.copyOf(mutations);
+    }
+
+    /** {@inheritDoc} */
+    public List<Mutation> getMutations(final String hugoGeneSymbol) {
+        checkNotNull(hugoGeneSymbol);
+        return mutations.get(hugoGeneSymbol);
+    }
+}

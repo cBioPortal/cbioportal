d047de011be5c9ea73da2381f60f0a6523f5d1ef
heuermh <devnull@localhost>
2011-10-28 00:18:27 -0500

    minor checksyle fixes

--- portal/test/org/mskcc/portal/mutation/diagram/ListMultimapMutationServiceTest.java
+++ portal/test/org/mskcc/portal/mutation/diagram/ListMultimapMutationServiceTest.java
@@ -3,7 +3,6 @@ package org.mskcc.portal.mutation.diagram;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;

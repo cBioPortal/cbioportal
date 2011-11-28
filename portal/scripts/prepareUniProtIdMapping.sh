1d2d05f51ab8fed968a19bdb40d27a56ecdcb68a
Michael Heuer <heuermh@gmail.com>
2011-11-27 22:56:19 -0600

    fix path

--- portal/scripts/prepareUniProtIdMapping.sh
+++ portal/scripts/prepareUniProtIdMapping.sh
@@ -9,7 +9,7 @@ grep GeneID HUMAN_9606_idmapping.dat > uniprot_to_geneId.dat
 awk '{print $3"\t"$1}' uniprot_to_geneId.dat | sort -n > uniprot_id_mapping.txt
 #echo "found " `wc -l uniprot_id_mapping.txt` " mappings for " `cut -f 1 uniprot_id_mapping.txt | sort -u | wc -l` " entrez gene ids"
 
-echo "copying to ../sample_data/uniprot_id_mapping.txt..."
-cp uniprot_id_mapping.txt ../sample_data
+echo "copying to ../sample_data/genes/uniprot_id_mapping.txt..."
+cp uniprot_id_mapping.txt ../sample_data/genes/
 
 echo "done."

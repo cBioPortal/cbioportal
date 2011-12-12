385bfb6705724b5750456b807eeb69a0cd104fa7
Benjamin Gross <benjamin.gross@gmail.com>
2011-12-12 13:31:12 -0500

    added dropped RNA-Seq and Log2CNA data

--- portal/scripts/all-coadread.sh
+++ portal/scripts/all-coadread.sh
@@ -8,12 +8,15 @@
 # Load Mutation Data
 ./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_mutations_extended.txt --meta $CGDS_DATA_HOME/coadread/meta_mutations_extended.txt --dbmsAction clobber
 
-# Load CNA Data
+# Load CNA/Log2CNA Data
 ./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_CNA.txt --meta $CGDS_DATA_HOME/coadread/meta_CNA.txt --dbmsAction clobber
+./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_log2CNA.txt --meta $CGDS_DATA_HOME/coadread/meta_log2CNA.txt --dbmsAction clobber
 
-# Load mRNA and microRNA Data
+# Load mRNA and RNA-Seq Data
 ./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_expression_median.txt --meta $CGDS_DATA_HOME/coadread/meta_expression_median.txt --dbmsAction clobber
 ./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_mRNA_median_Zscores.txt --meta $CGDS_DATA_HOME/coadread/meta_mRNA_median_Zscores.txt --dbmsAction clobber
+./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_RNA_Seq_expression_median.txt --meta $CGDS_DATA_HOME/coadread/meta_RNA_Seq_expression_median.txt --dbmsAction clobber
+./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_RNA_Seq_mRNA_median_Zscores.txt --meta $CGDS_DATA_HOME/coadread/meta_RNA_Seq_mRNA_median_Zscores.txt --dbmsAction clobber
 
 # Load Methylation Data
 ./importProfileData.pl --data $CGDS_DATA_HOME/coadread/data_methylation.txt --meta $CGDS_DATA_HOME/coadread/meta_methylation.txt --dbmsAction clobber

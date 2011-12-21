d93aee979b75db64a314895b98270420722804f3
Nikolaus Schultz <nschultz@gmail.com>
2011-12-21 18:28:40 -0500

    latest run

--- portal/content/public_data_table.pl
+++ portal/content/public_data_table.pl
@@ -102,7 +102,7 @@ while ($line = <IN3>) {
 		print OUT1 "\t\t<td style=\"text-align: center;\">$data[1]</td>\n";
 		if ($data[0] eq "PRAD_MSKCC") { print OUT1 "\t\t<td style=\"text-align: center;\"><b>85</b></td>\n"; }
 		elsif ($data[0] eq "SARC_MSKCC") { print OUT1 "\t\t<td style=\"text-align: center;\"><b>149</b></td>\n"; }
-		else { print OUT1 "\t\t<td style=\"text-align: center;\"><b>$data[7]</b></td>\n";
+		else { print OUT1 "\t\t<td style=\"text-align: center;\"><b>$data[7]</b></td>\n"; }
 		print OUT1 "\t</tr>\n\n";
 		$all_count = $all_count + $data[11];
 	}

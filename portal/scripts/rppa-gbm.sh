eef0bab5568f41ca11daece00d5bc2362d3a4956
B. Arman Aksoy <arman@cbio.mskcc.org>
2012-01-26 18:26:26 -0500

    updated the GBM importing pipeline. Might need more generalizations.

--- portal/scripts/rppa-gbm.sh
+++ portal/scripts/rppa-gbm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Import RPPA data
-./importProteinArrayData.pl $CGDS_DATA_HOME/RPPA/GBM215_RPPA_Median_Centered.txt $1
+./importProteinArrayData.pl $CGDS_DATA_HOME/gbm/GBM215_RPPA_Median_Centered.txt $1

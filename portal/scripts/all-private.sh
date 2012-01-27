577b27d771ef23763fc79131e1fc39ee4d87e627
Benjamin Gross <benjamin.gross@gmail.com>
2012-01-27 17:13:32 -0500

    renamed scripts used for private portal import

--- portal/scripts/all-private.sh
+++ portal/scripts/all-private.sh
@@ -1,5 +1,5 @@
 ./init.sh
-./all-breast.sh
-./all-bladder.sh
+./all-breast-private.sh
+./all-bladder-private.sh
 ./loadNetwork.sh
 ./importUsers.py --properties-file ../build.properties --send-email-confirm false

40c384f9753a30e7c04744306059fdfa87499614
Benjamin Gross <benjamin.gross@gmail.com>
2012-02-17 13:05:21 -0500

    script now puts mapping file in CGDS_DATA_HOME/reference-data

--- portal/scripts/prepareUniProtIdMapping.sh
+++ portal/scripts/prepareUniProtIdMapping.sh
@@ -9,7 +9,7 @@ grep GeneID /tmp/HUMAN_9606_idmapping.dat > /tmp/uniprot_to_geneId.dat
 awk '{print $3"\t"$1}' /tmp/uniprot_to_geneId.dat | sort -n > /tmp/uniprot_id_mapping.txt
 #echo "found " `wc -l uniprot_id_mapping.txt` " mappings for " `cut -f 1 uniprot_id_mapping.txt | sort -u | wc -l` " entrez gene ids"
 
-echo "copying to $CGDS_DATA_HOME/reference-data../sample_data/genes/uniprot_id_mapping.txt..."
+echo "copying to $CGDS_DATA_HOME/reference-data../tmp/uniprot_id_mapping.txt..."
 cp /tmp/uniprot_id_mapping.txt $CGDS_DATA_HOME/reference-data/
 
 echo "cleaning up /tmp..."

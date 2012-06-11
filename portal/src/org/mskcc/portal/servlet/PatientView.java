5d8a3aa579113dcadbd491f494a2c6c7605447ff
Jianjiong Gao <jianjiong.gao@gmail.com>
2012-06-11 12:00:00 -0400

    patient-view: validate

--- portal/src/org/mskcc/portal/servlet/PatientView.java
+++ portal/src/org/mskcc/portal/servlet/PatientView.java
@@ -2,6 +2,9 @@
 package org.mskcc.portal.servlet;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import javax.servlet.RequestDispatcher;
 
 import javax.servlet.ServletException;
@@ -10,7 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.mskcc.cgds.dao.DaoException;
+import org.mskcc.cgds.model.GeneticProfile;
 import org.mskcc.cgds.util.AccessControl;
+import org.mskcc.cgds.web_api.ProtocolException;
+import org.mskcc.portal.util.XDebug;
 import org.owasp.validator.html.PolicyException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -22,7 +29,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class PatientView extends HttpServlet {
     private static Logger logger = Logger.getLogger(PatientView.class);
-
+    public static final String ERROR = "error";
     public static final String PATIENT_ID = "patient";
     private ServletXssUtil servletXssUtil;
 
@@ -56,15 +63,55 @@ public class PatientView extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        XDebug xdebug = new XDebug( request );
+        request.setAttribute(QueryBuilder.XDEBUG_OBJECT, xdebug);
+        
         //  Get patient ID
-        request.setAttribute(QueryBuilder.HTML_TITLE, "Patient View");
         String patientID = servletXssUtil.getCleanInput (request, PATIENT_ID);
+
+        request.setAttribute(QueryBuilder.HTML_TITLE, "Patient "+patientID);
         request.setAttribute(PATIENT_ID, patientID);
         
-        RequestDispatcher dispatcher =
-                getServletContext().getRequestDispatcher("/WEB-INF/jsp/patient_view.jsp");
-        dispatcher.forward(request, response);
+        try {
+            if (validate(request)) {
+                forwardToErrorPage(request, response,
+                               (String)request.getAttribute(QueryBuilder.USER_ERROR_MESSAGE), xdebug);
+            }
+
+            RequestDispatcher dispatcher =
+                    getServletContext().getRequestDispatcher("/WEB-INF/jsp/patient_view.jsp");
+            dispatcher.forward(request, response);
         
+        } catch (DaoException e) {
+            xdebug.logMsg(this, "Got Database Exception:  " + e.getMessage());
+            forwardToErrorPage(request, response,
+                               "An error occurred while trying to connect to the database.", xdebug);
+        } 
+    }
+    
+    /**
+     * validate the portal web input form.
+     */
+    private boolean validate(HttpServletRequest request) throws DaoException {
+        boolean errorsExist = false;
+        String cancerStudyIdentifier = (String)request.getAttribute(QueryBuilder.CANCER_STUDY_ID);
+        if (accessControl.isAccessibleCancerStudy(cancerStudyIdentifier).size() != 1) {
+            request.setAttribute(ERROR,
+                    "You are not authorized to view the cancer study with id: '" +
+                    cancerStudyIdentifier + "'. ");
+            errorsExist = true;
+        }
+        return errorsExist;
+    }
+    
+    private void forwardToErrorPage(HttpServletRequest request, HttpServletResponse response,
+                                    String userMessage, XDebug xdebug)
+            throws ServletException, IOException {
+        request.setAttribute("xdebug_object", xdebug);
+        request.setAttribute(QueryBuilder.USER_ERROR_MESSAGE, userMessage);
+        RequestDispatcher dispatcher =
+                getServletContext().getRequestDispatcher("/WEB-INF/jsp/error.jsp");
+        dispatcher.forward(request, response);
     }
     
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">

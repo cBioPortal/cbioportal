23841ed08b95cafeb252e53600a49cd6c1149740
Gideon Dresdner <gideond@gmail.com>
2012-06-22 19:24:03 -0400

    mutsig preserves gene_list. improved user-interface (javascript)

--- portal/web/js/mutsig.js
+++ portal/web/js/mutsig.js
@@ -87,7 +87,7 @@ var studySelected = function() {
                                     + '<th>Gene Symbol</th>'
                                     + '<th>Num Mutations</th>'
                                     + '<th>Q-Value</th>'
-                                    + '<td><input class="checkall" type="checkbox">&nbspSelect All</td>'
+                                    + '<td><input class="checkall" type="checkbox"></td>'
                                     + '</thead></tr>');
 
                 // append MutSig data to table
@@ -101,13 +101,13 @@ var studySelected = function() {
 
                     $('.MutSig').dataTable( {
                         "sScrollY": "200px",
+                        "aaSorting": [],     // disable autosorting
                         "aoColumns": [
                             null,
                             null,
                             { "bSortable" : true, "sType" : "scientific" },
-                            { "bSortable" : false }
+                            { "bSortable" : false, "sWidth": "5%" }
                         ],
-                        "bSort": false,     // disable autosorting
                         "bPaginate": false,
                         "bFilter": false,
                         "iDisplayLength": 5,
@@ -124,42 +124,111 @@ var studySelected = function() {
             });
 };
 
-
 var updateGeneList = function() {
     "use strict";
-    var genes = [];
-    $('.MutSig :not(.checkall):checked').each(function(i) {
-        genes.push((i !== 0 ? " " : "") + $(this).val());
-    });
 
-    var gene_list_updated = $('#gene_list').text() + genes;
-    //$('#gene_list').val((genes.toString()).replace(/,/g,''));
-    $('#gene_list').text(gene_list_updated);
+    // push all the genes in the gene_list onto a list
+    var gene_list = $('#gene_list').val();
+
+    // remove spaces in gene_list
+    gene_list = gene_list.replace(/ {2,}/, "");                // delete 2 or more spaces in a row
+    gene_list = gene_list.replace(/^ /ig, "");  // delete leading space
+
+    // if gene_list is currently empty put all the checked mutsig genes into it.
+    // don't put in a leading space in the gene_list
+    if (gene_list === "") {
+
+        gene_list = [];
+        $('.MutSig :not(.checkall):checked').each(function(i) {     // don't select the Select All checkbox
+                gene_list.push((i !== 0 ? " " : "") + $(this).val());
+                });
+        gene_list = gene_list.join("");
+    }
+
+    else {
+        // look for the selected mutsigs in gene_list
+        // if they're not there, append them
+        $('.MutSig :not(.checkall):checked').each(function() {
+                    var checked = $(this).val();
+
+                    if ( gene_list.search(new RegExp(checked, "i")) === -1 ) {
+                    checked = " " + checked;
+                    gene_list += checked;
+
+                    }
+            });
+
+        // look for the unselected mutsigs in the gene_list
+        // if they're there, delete them
+        // you should be forced to know that your gene is recurrently mutated
+        $('.MutSig input:not(:checked)').each(function() {
+                var unchecked = $(this).val();
+                if ( gene_list.search(new RegExp(unchecked, "i")) !== -1) {
+                    gene_list = gene_list.replace(new RegExp(unchecked, "ig"), "");
+                }
+            });
+        }
+
+    $('#gene_list').val(gene_list);
 };
 
+var updateMutSigTable = function() {
+    var gene_list = $('#gene_list').val().split(" ");
+    var gene_list_len = gene_list.length;
+
+    // clear all checks
+    $('.MutSig :checkbox').attr('checked', false);
+
+    // if genes in the gene_list are added
+    // check them in the mutsig table
+    var i;
+    for (i = 0; i < gene_list_len; i += 1) {
+        // select mutsig checkboxes that are in the gene list
+        $('.MutSig :checkbox:[value=' +  gene_list[i].toUpperCase() + ']').
+            attr('checked', true);
+    }
+}
 
 $(document).ready( function () {
     "use strict";
 
-// initialize
+// -- initialize --
 // bind handlers to events
     $('#MutSig_view').hide();   // MutSig toggle and table is initialized as hidden
     $('.MutSig_wrapper').hide();
 
+    // make checkall check all
     $('.checkall').live('click', function() {
         $(this).parents().find('.MutSig input').attr('checked', this.checked);
     });
+
     $('.MutSig input').live('click', updateGeneList);
     updateGeneList();
+
+    $('#gene_list').change(function () {
+        if ( $('.MutSig').is(':visible') ) {
+            updateMutSigTable();
+            console.log("updated MutSigTable");
+        }
+    });
 
     $('#toggle_mutsig').click(function() {
         $($('#MutSig_view > .ui-icon')[0]).toggle();
         $($('#MutSig_view > .ui-icon')[1]).toggle();
-        $('.MutSig_wrapper').toggle();
+        updateMutSigTable();
+        $('.MutSig_wrapper').slideToggle('slow');
         return false;
     });
-// end initialize
 
+    // make sure that the ui-icon is in the right place
+    $('#gene_list').keyup(function() {
+        if ( $('.MutSig').is(':visible') ) {
+            $('.MutSig_wrapper').slideUp('slow');
+            $($('#MutSig_view > .ui-icon')[0]).toggle();
+            $($('#MutSig_view > .ui-icon')[1]).toggle();
+        }
+    });
+// -- end initialize --
 
     $('#select_cancer_type').change( function() {
         studySelected();

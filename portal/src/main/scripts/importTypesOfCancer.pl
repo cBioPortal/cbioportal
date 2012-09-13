fe1ef240c02675e693521ef868e430da7378659e
Gideon Dresdner <gideond@gmail.com>
2012-09-13 13:26:40 -0400

    de-mavenized

--- portal/src/main/scripts/importTypesOfCancer.pl
+++ portal/src/main/scripts/importTypesOfCancer.pl
@@ -2,6 +2,4 @@
 # author: Arthur Goldberg, goldberg@cbio.mskcc.org
 require "../scripts/env.pl";
 
-$cmd = join(' ', @ARGV);
-$cmd = 'mvn -f $PORTAL_HOME/pom.xml exec:java -Dexec.mainClass="org.mskcc.cbio.cgds.scripts.ImportTypesOfCancers" -Dexec.args="' . $cmd . '"';
-system($cmd);
+system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DCGDS_HOME='$cgdsHome' org.mskcc.cbio.cgds.scripts.ImportTypesOfCancers @ARGV");
--- portal/src/main/scripts/loadSampleData.sh
+++ portal/src/main/scripts/loadSampleData.sh
@@ -5,40 +5,34 @@
 # portal/sample_data
 #################################################
 
-SAMPLE_DATA=$PORTAL_HOME/src/main/resources/sample_data
-TEST_DATA=$PORTAL_HOME/src/test/resources
-
 # Clear the Database
 ./resetDb.pl
 
 # Load up Entrez Genes
-./importGenes.pl $SAMPLE_DATA/genes/human_genes.txt
+./importGenes.pl ../sample_data/genes/human_genes.txt
 
 # Load Entrez Gene ID --> UniProt ID mapping
-./importUniProtIdMapping.pl $SAMPLE_DATA/genes/uniprot_id_mapping.txt
+./importUniProtIdMapping.pl ../sample_data/genes/uniprot_id_mapping.txt
 
 # Load up All Cancer Types
-./importTypesOfCancer.pl $SAMPLE_DATA/cancers.txt
+./importTypesOfCancer.pl ../sample_data/cancers.txt
 
 # Load up the GBM Meta Data File
-./importCancerStudy.pl $SAMPLE_DATA/gbm/gbm.txt
+./importCancerStudy.pl ../sample_data/gbm/gbm.txt
 
 # Load Cases and Clinical Data
-./importCaseList.pl $SAMPLE_DATA/gbm/case_lists
-./importClinicalData.pl $SAMPLE_DATA/gbm/GBM_clinical_portal_20110210.txt
+./importCaseList.pl ../sample_data/gbm/case_lists
+./importClinicalData.pl ../sample_data/gbm/GBM_clinical_portal_20110210.txt
 
 # Load Mutation Data
-./importProfileData.pl --data $SAMPLE_DATA/gbm/data_mutations_MAF.txt --meta $SAMPLE_DATA/gbm/meta_mutations_MAF.txt --dbmsAction clobber 
+./importProfileData.pl --data ../sample_data/gbm/data_mutations_MAF.txt --meta ../sample_data/gbm/meta_mutations_MAF.txt --dbmsAction clobber 
 
 # Load CNA Data
-./importProfileData.pl --data $SAMPLE_DATA/gbm/data_CNA_consensus.txt --meta $SAMPLE_DATA/gbm/meta_CNA_consensus.txt --dbmsAction clobber
+./importProfileData.pl --data ../sample_data/gbm/data_CNA_consensus.txt --meta ../sample_data/gbm/meta_CNA_consensus.txt --dbmsAction clobber
 
 # Load network data
-./importSif.pl $SAMPLE_DATA/network/cell-map.sif CELLMAP
-./importSif.pl $SAMPLE_DATA/network/nci-nature.sif NCI_NATURE
+./importSif.pl ../sample_data/network/cell-map.sif CELLMAP
+./importSif.pl ../sample_data/network/nci-nature.sif NCI_NATURE
 
 # Load MutSig data
-./importMutSig.pl $TEST_DATA/test_mut_sig_data.txt $TEST_DATA/testCancerStudy.txt
-
-# Load Gistic data
-./importGistic.pl $TEST_DATA/testCancerStudy.txt $TEST_DATA/test-gistic-amp.txt $TEST_DATA/test-gistic-table-amp.txt 
+./importMutSig.pl ../test_data/test_mut_sig_data.txt ../test_data/testCancerStudy.txt

181f95f27af4c09e141d6a769ddd6fe0604f95c6
Gideon Dresdner <gideond@gmail.com>
2012-12-05 17:59:24 -0500

    clean up some things, added hugo symbol labels

--- portal/src/main/webapp/WEB-INF/jsp/oncoprint.jsp
+++ portal/src/main/webapp/WEB-INF/jsp/oncoprint.jsp
@@ -4,6 +4,15 @@
     <script type="text/javascript" src="js/oncoprint.js"></script>
     <script type="text/javascript" src="js/d3.v2.min.js"></script>
     <%--todo: we may want to import d3 globally but for now, it's just here--%>
+
+    <button type='button' onclick='oncoprint.defaultSort()'>default sort</button>
+    <button type='button' onclick='oncoprint.memoSort()'>memo sort</button>
+    <button type='button' onclick='oncoprint.toggleWhiteSpace()'>toggle white space</button>
+    <button type='button' onclick='oncoprint.scaleWidth(3)'>scale width 3</button>
+    <button type='button' onclick='oncoprint.scaleWidth(1.5)'>scale width 1.5</button>
+    <button type='button' onclick='oncoprint.scaleWidth(1)'>scale width 1</button>
+    <button type='button' onclick='oncoprint.scaleWidth(.5)'>scale width .5</button>
+
     <script type="text/javascript">
         var oncoPrintParams = {
             cancer_study_id: "<%=cancerTypeId%>",
--- portal/src/main/webapp/js/oncoprint.js
+++ portal/src/main/webapp/js/oncoprint.js
@@ -1,50 +1,60 @@
 var Oncoprint = function(wrapper, params) {
     var that = {};
 
+    // constants
+    var RECT_HEIGHT = 23;
+    var LITTLE_RECT_HEIGHT = RECT_HEIGHT / 3;
+    var LABEL_PADDING = 130;
+
+    // global state variables
+    var PADDING = true;
+    var WIDTH_SCALAR = 1;
+
+    // functions that get state
+    var getRectWidth = function() {
+        var unscaled = 5.5;
+        return WIDTH_SCALAR * unscaled;
+    };
+
+    var getRectPadding = function() {
+        var unscaled = 3;
+        return PADDING ? (WIDTH_SCALAR * unscaled) : 0;
+    };
+
+    var getXScale = function() {
+        return (getRectWidth() + getRectPadding()) * no_samples;
+    };
+
+    var getWidth = function() {
+        return getXScale() + LABEL_PADDING + (2 * (getRectWidth() + getRectPadding()));
+    };
+
+    var getHeight = function() {
+        return (RECT_HEIGHT + 10) * no_genes;
+    };
+
+    // functions that echo
+    d3.select(wrapper)
+        .attr('width', 600)
+        .attr('overflow-x', 'auto')
+        .attr('overflow-y', 'hidden');
+
     that.wrapper = wrapper;
 
     that.getData = function() {
         return params.data;
     };
 
+    // useful variables
     var query = QueryGeneData(params.data);
     var samples_list = query.getSampleList();
     var sorted_samples;
+    var no_samples = samples_list.length;
     var genes_list = query.getGeneList();
+    var gene_data = params.data.gene_data;
+    var no_genes = gene_data.length;
 
-    var no_samples = samples_list.length,
-        gene_data = params.data.gene_data,
-        no_genes = gene_data.length;
-
-    var rectHeight = 23;
-    var littleRectHeight = rectHeight / 3;
-
-    // global state variables
-    var padding = true;
-    var width_scalar = 1;
-
-    var getRectWidth = function() {
-        var unscaled = 5.5;
-
-        return width_scalar * unscaled;
-    };
-
-    var getRectPadding = function() {
-        var unscaled = 3;
-
-        return padding ? (width_scalar * unscaled) : 0;
-    };
-
-    var getWidth = function() {
-
-        return (getRectWidth() + getRectPadding()) * no_samples;
-    };
-
-    var getHeight = function() {
-        return (rectHeight + 10) * no_genes;
-    };
-
-    var x = d3.scale.ordinal().rangeBands([0, getWidth()], 0);
+    var x = d3.scale.ordinal().rangeBands([0, getXScale()], 0);
 
     var y = d3.scale.ordinal().rangeBands([0, getHeight()], 0)
         .domain(genes_list);
@@ -61,7 +71,7 @@ var Oncoprint = function(wrapper, params) {
     that.draw = function() {
 
         var svg = d3.select(wrapper).append('svg')
-            .attr('width', getWidth() + (2 * (getRectWidth() + getRectPadding())))
+            .attr('width', getWidth())
             .attr('height', getHeight());
         that.svg = svg;
 
@@ -71,32 +81,46 @@ var Oncoprint = function(wrapper, params) {
 
         gene_data.forEach(function(gene_obj) {
 
-            var cleaned_hugo = cleanHugo(gene_obj.hugo);
+            var hugo = gene_obj.hugo;
+            var cleaned_hugo = cleanHugo(hugo);
 
-            var sample = svg.selectAll('.sample ' + cleaned_hugo)
+            // N.B. there is no data bound to g,
+            // is this bad form?
+            var track = svg.append('g')
+                .attr('transform', translate(LABEL_PADDING, 0))
+                .attr('class', 'track');
+
+            track.append('text')
+                .attr('x', -LABEL_PADDING)
+                .attr('y', y(hugo) + .5 * RECT_HEIGHT)
+//                .attr('dy', '.32em')
+                .attr('text-anchor', 'start')
+                .text(gene_obj.hugo);
+
+            var sample = track.selectAll('.sample')
                 .data(samples_list, function(d) { return d;});
 
             var sample_enter = sample.enter().append('g')
-                .attr('class', 'sample ' + cleaned_hugo)
+                .attr('class', 'sample')
                 .attr('transform', function(d) {
-                    return translate(x(d), y(gene_obj.hugo));
+                    return translate(x(d), y(hugo));
                 });
 
             var cna = sample_enter.append('rect')
                 .attr('class', function(d) {
-                    var cna = query.data(d, gene_obj.hugo, 'cna');
+                    var cna = query.data(d, hugo, 'cna');
                     return 'cna ' + (cna === null ? 'none' : cna);
                 })
                 .attr('width', getRectWidth())
-                .attr('height', rectHeight);
+                .attr('height', RECT_HEIGHT);
 
             var mut = sample.append('rect')
                 .attr('class', function(d) {
-                    var mutation = query.data(d, gene_obj.hugo, 'mutation');
+                    var mutation = query.data(d, hugo, 'mutation');
                     return 'mutation ' + (mutation === null ? 'none' : 'mut');
                 })
                 .attr('width', getRectWidth())
-                .attr('height', littleRectHeight);
+                .attr('height', LITTLE_RECT_HEIGHT);
 
 //        // ... mrna, rppa
         });
@@ -107,16 +131,17 @@ var Oncoprint = function(wrapper, params) {
     var transition = function() {
         // helper function
 
-        var rect_width = getRectWidth();
+        that.svg.transition().duration(1000).attr('width', getWidth());
 
-        gene_data.forEach(function(gene_obj) {
-            that.svg.selectAll('.sample.' + cleanHugo(gene_obj.hugo))
+        that.svg.selectAll('.track')[0].forEach(function(val, i) {
+            d3.select(val).selectAll('.sample')
                 .transition()
                 .duration(1000)
                 .attr('transform', function(d) {
-                    return translate(x(d), y(gene_obj.hugo));
+                    return translate(x(d), y(genes_list[i]));
                 });
 
+            var rect_width = getRectWidth();
             that.svg.selectAll('rect')
                 .transition()
                 .duration(1000)
@@ -125,10 +150,11 @@ var Oncoprint = function(wrapper, params) {
     };
 
     that.memoSort = function() {
+        // todo: precompute this
         var memoSort = MemoSort(params.data, genes_list);
         sorted_samples = memoSort.sort();
 
-        x.domain(sorted_samples);       // reset global variable
+        x.domain(sorted_samples);
         transition();
 
         return sorted_samples;
@@ -140,15 +166,15 @@ var Oncoprint = function(wrapper, params) {
     };
 
     that.toggleWhiteSpace = function() {
-        padding = !padding;
-        x.rangeBands([0, getWidth()]);
+        PADDING = !PADDING;
+        x.rangeBands([0, getXScale()]);
         transition();
     };
 
     that.scaleWidth = function(scalar) {
-        width_scalar = scalar;
+        WIDTH_SCALAR = scalar;
 
-        x.rangeBands([0, getWidth()]);
+        x.rangeBands([0, getXScale()]);
 
         transition();
     };

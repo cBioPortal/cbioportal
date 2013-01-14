acd6ac7e6f94a57f7e35e27216c8191dd1ea2e65
Benjamin Gross <benjamin.gross@gmail.com>
2013-01-14 13:18:43 -0500

    minor updates, including replacement of CGDS_DATA_HOME w/PORTAL_DATA_HOME

--- core/src/main/scripts/prepareUniProtIdMapping.sh
+++ core/src/main/scripts/prepareUniProtIdMapping.sh
@@ -5,16 +5,15 @@ wget -P /tmp/ ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowle
 
 echo "extracting UniProt --> GeneID mapping..."
 gunzip /tmp/HUMAN_9606_idmapping.dat.gz
-grep GeneID /tmp/HUMAN_9606_idmapping.dat > /tmp/uniprot_to_geneId.dat
-awk '{print $3"\t"$1}' /tmp/uniprot_to_geneId.dat | sort -n > /tmp/uniprot_id_mapping.txt
-#echo "found " `wc -l uniprot_id_mapping.txt` " mappings for " `cut -f 1 uniprot_id_mapping.txt | sort -u | wc -l` " entrez gene ids"
+grep GeneID /tmp/HUMAN_9606_idmapping.dat > /tmp/uniprot-to-geneId.dat
+awk '{print $3"\t"$1}' /tmp/uniprot-to-geneId.dat | sort -n > /tmp/uniprot-id-mapping.txt
 
-echo "copying to $CGDS_DATA_HOME/reference-data../tmp/uniprot_id_mapping.txt..."
-cp /tmp/uniprot_id_mapping.txt $CGDS_DATA_HOME/reference-data/
+echo "copying to $PORTAL_DATA_HOME/reference-data../tmp/uniprot-id-mapping.txt..."
+cp /tmp/uniprot-id-mapping.txt $PORTAL_DATA_HOME/reference-data/
 
 echo "cleaning up /tmp..."
 rm -f /tmp/HUMAN_9606_idmapping.dat
-rm -f /tmp/uniprot_to_geneId.dat
-rm -f /tmp/uniprot_id_mapping.txt
+rm -f /tmp/uniprot-to-geneId.dat
+rm -f /tmp/uniprot-id-mapping.txt
 
 echo "done."

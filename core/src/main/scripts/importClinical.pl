c9665d284f6f51b6be87b4c0ee5fabc5c8eea574
Gideon Dresdner <gideond@gmail.com>
2013-02-04 10:53:11 -0500

    just run the class directly from a jar

--- core/src/main/scripts/importClinical.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-require "../scripts/env.pl";
-
-$cp="/Users/dresdneg/dev/cbio-cancer-genomics-portal/core/target/classes";
-
-system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DCGDS_HOME='$cgdsHome' org.mskcc.cbio.cgds.scripts.ImportClinical @ARGV");

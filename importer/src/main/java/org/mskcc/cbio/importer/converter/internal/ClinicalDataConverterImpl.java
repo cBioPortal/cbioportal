8be81224e0d618bb8bb451de79ac28c7cce70131
Gideon Dresdner <gideond@gmail.com>
2013-02-08 18:22:28 -0500

    started on importing NCI clinical attribute metadata

--- importer/src/main/java/org/mskcc/cbio/importer/converter/internal/ClinicalDataConverterImpl.java
+++ importer/src/main/java/org/mskcc/cbio/importer/converter/internal/ClinicalDataConverterImpl.java
@@ -29,8 +29,8 @@
 package org.mskcc.cbio.importer.converter.internal;
 
 // imports
-import com.google.gdata.util.common.base.StringUtil;
-import static org.mskcc.cbio.cgds.scripts.ImportClinical.IGNORE_LINE_PREFIX;
+//import static org.mskcc.cbio.cgds.scripts.ImportClinical.IGNORE_LINE_PREFIX;
+import org.mskcc.cbio.cgds.scripts.ImportClinical;
 import org.mskcc.cbio.importer.Config;
 import org.mskcc.cbio.importer.CaseIDs;
 import org.mskcc.cbio.importer.IDMapper;
@@ -284,7 +284,7 @@ public class ClinicalDataConverterImpl implements Converter {
             colNames.add(normalName);
 
             // case id is always the first column
-            String prefix = normalName.equals(CASE_ID) ? IGNORE_LINE_PREFIX : "";
+            String prefix = normalName.equals(CASE_ID) ? ImportClinical.IGNORE_LINE_PREFIX : "";
 
             String displayName = metaData.getDisplayName();
             displayNames.add(prefix + displayName);

78404789fe56783f0368e2e0dec60a11b0f84b60
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-02-13 15:39:54 -0500

    change cgds_home to portal_home in script

--- core/src/main/scripts/env.pl
+++ core/src/main/scripts/env.pl
@@ -1,4 +1,4 @@
-#  Set up Environment for Running CGDS Java Tools
+#  Set up Environment for Running cBio Portal Java Tools
 
 # check for JAVA_HOME
 $JAVA_HOME = $ENV{JAVA_HOME};
@@ -6,18 +6,17 @@ if ($JAVA_HOME eq "") {
 	die "JAVA_HOME Environment Variable is not set.  Please set, and try again.\n";
 }
 
-# Check to see if CGDS_HOME is set via command line arguments
+# Check to see if PORTAL_HOME is set via command line arguments
 if ($#ARGV >= 0) {
 	$arg0 = $ARGV[0];
-	$index = index($arg0, "CGDS_HOME");
+	$index = index($arg0, "PORTAL_HOME");
 	if ($index >= 0) {
 		$home = substr($arg0, 11);
-		$ENV{CGDS_HOME}=$home;
+		$ENV{PORTAL_HOME}=$home;
 	}
 }
 
-$cgdsHome = $ENV{PORTAL_HOME}."/core";
-$cgdsDataHome = $ENV{CGDS_DATA_HOME};
+$portalHome = $ENV{PORTAL_HOME};
 $portalDataHome = $ENV{PORTAL_DATA_HOME};
 my $osCheck = $ENV{OS};
 my $pathDelim;
@@ -28,12 +27,8 @@ if( $osCheck =~ /win/i){
     $pathDelim=":";
 }
 
-if ($cgdsHome eq "") {
+if ($portalHome eq "") {
 	die "CGDS_HOME Environment Variable is not set.  Please set, and try again.\n";
-}
-
-if ($cgdsDataHome eq "") {
-	die "CGDS_DATA_HOME Environment Variable is not set.  Please set, and try again.\n";
 }
 
 if ($portalDataHome eq "") {
@@ -41,9 +36,8 @@ if ($portalDataHome eq "") {
 }
 
 # Set up Classpath to use all JAR files in lib dir.
-# print "Using CGDS_HOME $cgdsHome\n";
-$cp="$cgdsHome/target/portal/WEB-INF/classes";
-@jar_files = glob ("$cgdsHome/target/portal/WEB-INF/lib/*.jar");
+$cp="$portalHome/core/target/portal/WEB-INF/classes";
+@jar_files = glob ("$portalHome/core/target/portal/WEB-INF/lib/*.jar");
 foreach my $jar (@jar_files) {
   $cp="$cp$pathDelim$jar"
 }

e41e8b1d853c086bf93fc9afbd83946381f95485
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-02-20 21:38:59 -0500

    update cgds_home to portal_home

--- core/src/main/scripts/importGistic.pl
+++ core/src/main/scripts/importGistic.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 require "../scripts/env.pl";
 
-system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DCGDS_HOME='$cgdsHome' org.mskcc.cbio.cgds.scripts.ImportGisticData @ARGV");
+system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp PORTAL_HOME='$portalHome' org.mskcc.cbio.cgds.scripts.ImportGisticData @ARGV");
--- core/src/main/scripts/importMicroRna.pl
+++ core/src/main/scripts/importMicroRna.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 require "../scripts/env.pl";
 
-system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DCGDS_HOME='$cgdsHome' org.mskcc.cbio.cgds.scripts.ImportMicroRnaData @ARGV");
+system ("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DPORTAL_HOME='$portalHome' org.mskcc.cbio.cgds.scripts.ImportMicroRnaData @ARGV");

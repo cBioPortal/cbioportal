ac9baa0918dde42ec080b3012e48e20ed93cc0a7
B. Arman Aksoy <arman@aksoy.org>
2013-03-28 15:14:06 -0400

    fixed a bug in PiHelperImporter -- it was causing some of the drug targets to be skipped.

--- core/src/main/java/org/mskcc/cbio/cgds/scripts/drug/internal/PiHelperImporter.java
+++ core/src/main/java/org/mskcc/cbio/cgds/scripts/drug/internal/PiHelperImporter.java
@@ -98,7 +98,7 @@ public class PiHelperImporter extends AbstractDrugInfoImporter {
             if(line.startsWith("#")) continue;
             if((++lineNo) == 1) continue;
 
-            String[] tokens = line.split(separator);
+            String[] tokens = line.split(separator, -1);
             assert tokens.length ==  5;
             if(tokens.length < 5) continue;
             /*
@@ -148,7 +148,7 @@ public class PiHelperImporter extends AbstractDrugInfoImporter {
             if(line.startsWith("#")) continue;
             if((++lineNo) == 1) continue;
 
-            String[] t = line.split(separator);
+            String[] t = line.split(separator, -1);
             assert t.length ==  12;
             if(t.length < 12) continue;
             /*

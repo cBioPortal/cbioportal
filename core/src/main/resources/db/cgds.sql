edc18d75f9334e925cf7187e1d22a76d5a5681af
Gideon Dresdner <gideond@gmail.com>
2013-05-31 17:59:25 -0400

    small change (added foreign key)

--- core/src/main/resources/db/cgds.sql
+++ core/src/main/resources/db/cgds.sql
@@ -317,20 +317,6 @@ CREATE TABLE `case_profile` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clinical`
---
-drop table IF EXISTS clinical;
-CREATE TABLE `clinical` (
-  `CANCER_STUDY_ID` int(11) NOT NULL,
-  `CASE_ID` varchar(255) NOT NULL,
-  `ATTR_ID` varchar(255) NOT NULL,
-  `ATTR_VALUE` varchar(255) NOT NULL,
-  PRIMARY KEY (`CANCER_STUDY_ID`, `CASE_ID`, `ATTR_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `clinical_attribute`
 --
 drop table IF EXISTS clinical_attribute;
@@ -341,6 +327,23 @@ CREATE TABLE `clinical_attribute` (
   `DATATYPE` varchar(255) NOT NULL,
   PRIMARY KEY (`ATTR_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='DATATYPE can be NUMBER, BOOLEAN, STRING';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clinical`
+--
+drop table IF EXISTS clinical;
+CREATE TABLE `clinical` (
+  `CANCER_STUDY_ID` int(11) NOT NULL,
+  `CASE_ID` varchar(255) NOT NULL,
+  `ATTR_ID` varchar(255) NOT NULL,
+  `ATTR_VALUE` varchar(255) NOT NULL,
+  PRIMARY KEY (`CANCER_STUDY_ID`, `CASE_ID`, `ATTR_ID`),
+  FOREIGN KEY (`ATTR_ID`) REFERENCES `clinical_attribute` (`ATTR_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 drop table IF EXISTS clinical_free_form;
 CREATE TABLE `clinical_free_form` (

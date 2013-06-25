034fe458878ebe938c633e2a9e39e06cc60a39db
yichaoS <devnull@localhost>
2013-06-25 16:24:43 -0400

    Lastest Pull.

--- core/src/main/java/org/mskcc/cbio/portal/servlet/SvgConverter.java
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/SvgConverter.java
@@ -140,16 +140,7 @@ public class SvgConverter extends HttpServlet {
     private void convertToPDF(HttpServletResponse response, String xml) throws ServletException, IOException {
         OutputStream out = response.getOutputStream();
         try {
-<<<<<<< local
             InputStream is = new ByteArrayInputStream(xml.getBytes());
-=======
-            String docRoot = "<?xml version='1.0' encoding='utf-8'?>";
-            String docRoot2 = "<svg version='1.1' width='700px' height='600px' xml:space='preserve'>";
-            svgContent = docRoot + docRoot2 + svgContent + "</svg>";
-	    svgContent = svgContent.replaceAll("<text y=\"9\" x=\"0\" dy=\".71em\"", "<text y=\"19\" x=\"0\" dy=\".71em\"");
-	    svgContent = svgContent.replaceAll("<text x=\"-9\" y=\"0\" dy=\".32em\"", "<text x=\"-9\" y=\"3\" dy=\".32em\"");
-            InputStream is = new ByteArrayInputStream(svgContent.getBytes());
->>>>>>> other
             TranscoderInput input = new TranscoderInput(is);
             TranscoderOutput output = new TranscoderOutput(out);
             Transcoder transcoder = new PDFTranscoder();

ee4040574b16eacaf14e6437a4cc3db66ca3db85
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-08-02 16:09:46 -0400

    added tooltips for each sample

--- portal/src/main/webapp/js/src/patient-view/genomic-overview.js
+++ portal/src/main/webapp/js/src/patient-view/genomic-overview.js
@@ -168,9 +168,11 @@ function plotMuts(p,config,chmInfo,row,mutations,caseId) {
     }
         
     if (caseId!==null) {
-        var label = mapCaseLabels[caseId];
-        p.circle(8,yRow-config.rowHeight/2,6).attr({'stroke':mapCaseColor[caseId], 'fill':mapCaseColor[caseId]});
-        p.text(8,yRow-config.rowHeight/2,label).attr({'text-anchor': 'center', 'fill':'white'});
+        var label = caseMetaData.label[caseId];
+        var c = p.circle(8,yRow-config.rowHeight/2,6).attr({'stroke':caseMetaData.color[caseId], 'fill':caseMetaData.color[caseId]});
+        var t = p.text(8,yRow-config.rowHeight/2,label).attr({'text-anchor': 'center', 'fill':'white'});
+        addToolTip(c.node, caseMetaData.tooltip[caseId],false,{my:'middle left',at:'middle right'});
+        addToolTip(t.node, caseMetaData.tooltip[caseId],false,{my:'middle left',at:'middle right'});
     } else {
         p.text(0,yRow-config.rowHeight/2,'MUT').attr({'text-anchor': 'start'});
     }
@@ -220,9 +222,11 @@ function plotCnSegs(p,config,chmInfo,row,segs,chrCol,startCol,endCol,segCol,case
         var tip = "Mean copy number log2 value: "+segMean+"<br/>from "+loc2string([chm,start],chmInfo)+"<br/>to "+loc2string([chm,end],chmInfo);
         addToolTip(r.node,tip);
     }if (caseId!==null) {
-        var label = mapCaseLabels[caseId];
-        p.circle(8,yRow+config.rowHeight/2,6).attr({'stroke':mapCaseColor[caseId], 'fill':mapCaseColor[caseId]});
-        p.text(8,yRow+config.rowHeight/2,label).attr({'text-anchor': 'center', 'fill':'white'});
+        var label = caseMetaData.label[caseId];
+        var c = p.circle(8,yRow+config.rowHeight/2,6).attr({'stroke':caseMetaData.color[caseId], 'fill':caseMetaData.color[caseId]});
+        var t = p.text(8,yRow+config.rowHeight/2,label).attr({'text-anchor': 'center', 'fill':'white'});
+        addToolTip(c.node, caseMetaData.tooltip[caseId],false,{my:'middle left',at:'middle right'});
+        addToolTip(t.node, caseMetaData.tooltip[caseId],false,{my:'middle left',at:'middle right'});
     } else {
         p.text(0,yRow+config.rowHeight/2,'CNA').attr({'text-anchor': 'start'});
     }

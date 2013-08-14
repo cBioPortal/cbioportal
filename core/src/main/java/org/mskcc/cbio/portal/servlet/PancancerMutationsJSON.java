5a1a51a090196187e6488d9ab3f7349488c2d132
Gideon Dresdner <gideond@gmail.com>
2013-08-14 17:11:28 -0400

    boilerplate servlet for pancancer mutations in place

---
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/PancancerMutationsJSON.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2012 Memorial Sloan-Kettering Cancer Center.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ * documentation provided hereunder is on an "as is" basis, and
+ * Memorial Sloan-Kettering Cancer Center
+ * has no obligations to provide maintenance, support,
+ * updates, enhancements or modifications.  In no event shall
+ * Memorial Sloan-Kettering Cancer Center
+ * be liable to any party for direct, indirect, special,
+ * incidental or consequential damages, including lost profits, arising
+ * out of the use of this software and its documentation, even if
+ * Memorial Sloan-Kettering Cancer Center
+ * has been advised of the possibility of such damage.  See
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+package org.mskcc.cbio.portal.servlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.owasp.validator.html.PolicyException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Gideon Dresdner <dresdnerg@cbio.mskcc.org>
+ *
+ */
+public class PancancerMutationsJSON extends HttpServlet {
+    private ServletXssUtil servletXssUtil;
+    private static Log log = LogFactory.getLog(PancancerMutationsJSON.class);
+
+    /**
+     * Initializes the servlet.
+     *
+     * @throws ServletException
+     */
+    public void init() throws ServletException {
+        super.init();
+        try {
+            servletXssUtil = ServletXssUtil.getInstance();
+        } catch (PolicyException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String mutationKeysRawJson = request.getParameter("mutation_keys");
+        System.out.println(mutationKeysRawJson);
+
+        if (mutationKeysRawJson == null || mutationKeysRawJson.equals("")) {
+            throw new ServletException("no mutation_keys parameter provided");
+        }
+
+        JSONArray mutationKeys = (JSONArray) JSONValue.parse(mutationKeysRawJson);
+
+        PrintWriter writer = response.getWriter();
+        JSONArray.writeJSONString(mutationKeys, writer);
+    }
+}

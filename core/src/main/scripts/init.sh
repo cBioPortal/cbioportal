e64f8adf30772ffbfac4b023c3e5e911893749ba
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-08-23 15:48:45 -0400

    update init.sh

--- core/src/main/scripts/init.sh
+++ core/src/main/scripts/init.sh
@@ -5,10 +5,7 @@
 ./resetDb.pl
 
 # Load up Entrez Genes
-./importGenes.pl $PORTAL_DATA_HOME/reference-data/human-genes.txt $PORTAL_DATA_HOME/reference-data/all_exon_loci.bed
-
-# Load up MicroRNA IDs
-./importMicroRNAIDs.pl $PORTAL_DATA_HOME/reference-data/id_mapping_mirbase.txt
+./importGenes.pl $PORTAL_DATA_HOME/reference-data/human-genes.txt $PORTAL_DATA_HOME/reference-data/id_mapping_mirbase.txt $PORTAL_DATA_HOME/reference-data/all_exon_loci.bed
 
 # Load up Cancer Types
 ./importTypesOfCancer.pl $PORTAL_DATA_HOME/reference-data/public-cancers.txt

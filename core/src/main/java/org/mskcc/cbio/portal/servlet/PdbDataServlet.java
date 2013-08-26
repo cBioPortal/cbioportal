ef988d8972037baa716927a2c1c706c9b3667a37
Onur Sumer <s.onur.sumer@gmail.com>
2013-08-26 13:44:20 -0400

    renamed Mutation3dServlet to PdbDataServlet

--- core/src/main/java/org/mskcc/cbio/portal/servlet/PdbDataServlet.java
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/PdbDataServlet.java
@@ -1,12 +1,36 @@
+/** Copyright (c) 2012 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
+
 package org.mskcc.cbio.portal.servlet;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.mskcc.cbio.cgds.dao.DaoException;
-import org.mskcc.cbio.cgds.dao.DaoGeneOptimized;
 import org.mskcc.cbio.cgds.dao.DaoPdbUniprotResidueMapping;
-import org.mskcc.cbio.portal.mut_diagram.IdMappingService;
-import org.mskcc.cbio.portal.mut_diagram.impl.CgdsIdMappingService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,14 +39,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
+ * PDB Data Servlet designed to provide PDB data for a specific uniprot id.
  *
+ * @author Selcuk Onur Sumer
  */
-public class Mutation3dServlet extends HttpServlet
+public class PdbDataServlet extends HttpServlet
 {
 	@Override
 	protected void doGet(final HttpServletRequest request,

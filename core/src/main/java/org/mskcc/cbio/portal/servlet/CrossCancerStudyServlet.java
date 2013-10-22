3d795e7bfee5ddc4ae5839ba7d59391bf50e8610
Onur Sumer <s.onur.sumer@gmail.com>
2013-10-22 17:35:27 -0400

    patched suspicious request parameters in PatientView and CrossCancerStudy servlets

--- core/src/main/java/org/mskcc/cbio/portal/servlet/CrossCancerStudyServlet.java
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/CrossCancerStudyServlet.java
@@ -96,7 +96,7 @@ public class CrossCancerStudyServlet extends HttpServlet {
         XDebug xdebug = new XDebug();
         xdebug.startTimer();
         try {
-            String geneList = servletXssUtil.getCleanInput(httpServletRequest,
+            String geneList = servletXssUtil.getCleanerInput(httpServletRequest,
                     QueryBuilder.GENE_LIST);
             ArrayList<CancerStudy> cancerStudyList = getCancerStudiesWithData();
 
@@ -110,7 +110,7 @@ public class CrossCancerStudyServlet extends HttpServlet {
             httpServletRequest.setAttribute(QueryBuilder.CANCER_TYPES_INTERNAL, cancerStudyList);
             httpServletRequest.setAttribute(QueryBuilder.XDEBUG_OBJECT, xdebug);
 
-            String action = servletXssUtil.getCleanInput(httpServletRequest,
+            String action = servletXssUtil.getCleanerInput(httpServletRequest,
                     QueryBuilder.ACTION_NAME);
             if (action != null && action.equals(QueryBuilder.ACTION_SUBMIT)) {
                 GeneValidator geneValidator = new GeneValidator(geneList);

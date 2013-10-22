768bcce5489127238043e7230700d10af30242c1
Jianjiong Gao <jianjiong.gao@gmail.com>
2013-10-22 18:46:07 -0400

    rm after tabs all loaded

--- portal/src/main/webapp/js/src/survival_curve.js
+++ portal/src/main/webapp/js/src/survival_curve.js
@@ -258,7 +258,7 @@ var survivalCurves = (function() {
                 setDFSGroups(result, caseLists);
                 if (os_altered_group.length === 0 && os_unaltered_group.length === 0 &&
                     dfs_altered_group.length === 0 && dfs_unaltered_group.length === 0) {
-                    setTimeout(removeTag, 200);
+                    removeTag();
                 } else {
                     if (os_altered_group.length !== 0 || os_unaltered_group.length !== 0) {
                         calcOS();
@@ -951,15 +951,17 @@ function loadSurvivalCurveSVG(svgId) {
     return xmlString;
 }
 function removeTag() {
-    var _index = 0;
-    var _survival_index = 0;
-    $('#tabs .ui-tabs-nav a').each(function() {
-        if ($(this).attr('href') === "#survival") {
-            _survival_index = _index;
-            return false;
-        } else {
-            _index += 1;
-        }
+    $("#tabs").load(function(){
+        var _index = 0;
+        var _survival_index = 0;
+        $('#tabs .ui-tabs-nav a').each(function() {
+            if ($(this).attr('href') === "#survival") {
+                _survival_index = _index;
+                return false;
+            } else {
+                _index += 1;
+            }
+        });
+        $("#tabs").tabs("remove", _survival_index);
     });
-    $("#tabs").tabs("remove", _survival_index);
 }

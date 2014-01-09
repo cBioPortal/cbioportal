016019a1c1360218d22ae543ac6239f074cdc4ec
Jianjiong Gao <jianjiong.gao@gmail.com>
2014-01-09 10:37:44 -0500

    import only -2/2 for cna event

--- core/src/main/java/org/mskcc/cbio/portal/scripts/ImportTabDelimData.java
+++ core/src/main/java/org/mskcc/cbio/portal/scripts/ImportTabDelimData.java
@@ -258,9 +258,9 @@ public class ImportTabDelimData {
                                     int i = values[0].equals(""+entrezGeneId) ? 1:0;
                                     for (; i<n; i++) {
                                         if (values[i].equals(GeneticAlterationType.AMPLIFICATION) 
-                                                || values[i].equals(GeneticAlterationType.GAIN)
-                                                || values[i].equals(GeneticAlterationType.ZERO)
-                                                || values[i].equals(GeneticAlterationType.HEMIZYGOUS_DELETION)
+                                               // || values[i].equals(GeneticAlterationType.GAIN)
+                                               // || values[i].equals(GeneticAlterationType.ZERO)
+                                               // || values[i].equals(GeneticAlterationType.HEMIZYGOUS_DELETION)
                                                 || values[i].equals(GeneticAlterationType.HOMOZYGOUS_DELETION)) {
                                             CnaEvent cnaEvent = new CnaEvent(orderedCaseList.get(i), geneticProfileId, entrezGeneId, Short.parseShort(values[i]));
                                             

51a1e91567452f5adc1e560e34a39ea5b789819a
yichaoS <devnull@localhost>
2014-02-17 14:32:19 -0500

    changed simple json to jackson json

--- core/src/main/java/org/mskcc/cbio/portal/util/CoExpUtil.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/CoExpUtil.java
@@ -6,28 +6,32 @@ import org.mskcc.cbio.portal.model.*;
 import org.mskcc.cbio.portal.util.*;
 import org.mskcc.cbio.portal.dao.*;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.map.ObjectMapper;
+
 
 public class CoExpUtil {
 
-    //Sort json objects array by propName
-    public static ArrayList<JSONObject> sortJsonArr(ArrayList<JSONObject> jsonArr, String propName) {
+    // //Sort json objects array by propName
+    // public static ArrayList<JsonNode> sortJsonArr(ArrayList<JsonNode> jsonArr, String propName) {
 
-        for (int mainIndex = 0 ; mainIndex < jsonArr.size(); mainIndex++) {
-            for (int comparedIndex = mainIndex + 1; comparedIndex < jsonArr.size(); comparedIndex++) {
-                JSONObject mainObj = jsonArr.get(mainIndex);
-                JSONObject comparedObj = jsonArr.get(comparedIndex);
-                double mainScore = (Double)mainObj.get(propName);
-                double comparedScore = (Double)comparedObj.get(propName);
-                if (Math.abs(mainScore) > Math.abs(comparedScore)) {
-                    jsonArr.add(mainIndex, comparedObj);
-                    jsonArr.remove(mainIndex + 1);
-                    jsonArr.add(comparedIndex, mainObj);
-                    jsonArr.remove(comparedIndex + 1);
-                }
-            }
-        }
-        return jsonArr;
-    }
+    //     for (int mainIndex = 0 ; mainIndex < jsonArr.size(); mainIndex++) {
+    //         for (int comparedIndex = mainIndex + 1; comparedIndex < jsonArr.size(); comparedIndex++) {
+    //             JsonNode mainObj = jsonArr.get(mainIndex);
+    //             JsonNode comparedObj = jsonArr.get(comparedIndex);
+    //             double mainScore = (Double)mainObj.get(propName);
+    //             double comparedScore = (Double)comparedObj.get(propName);
+    //             if (Math.abs(mainScore) > Math.abs(comparedScore)) {
+    //                 jsonArr.add(mainIndex, comparedObj);
+    //                 jsonArr.remove(mainIndex + 1);
+    //                 jsonArr.add(comparedIndex, mainObj);
+    //                 jsonArr.remove(comparedIndex + 1);
+    //             }
+    //         }
+    //     }
+    //     return jsonArr;
+    // }
 
     public static ArrayList<String> getCaseIds(String caseSetId, String caseIdsKey) {
 		try {

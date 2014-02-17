4c4f1aaf8a42ac12916eecb041bba9ce4fdeb8ed
Jianjiong Gao <jianjiong.gao@gmail.com>
2014-02-17 14:31:18 -0500

    fixed path

--- core/src/main/scripts/prepareEbiSiftsPdbUniprotMappingData.sh
+++ core/src/main/scripts/prepareEbiSiftsPdbUniprotMappingData.sh
@@ -4,6 +4,6 @@ echo "downloading..."
 wget -P $PORTAL_DATA_HOME/reference-data/ ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv/pdb_chain_uniprot.tsv.gz
 
 echo "extracting..."
-gunzip $PORTAL_DATA_HOME/reference-data/sifts_tmp/pdb_chain_uniprot.tsv.gz
+gunzip $PORTAL_DATA_HOME/reference-data/pdb_chain_uniprot.tsv.gz
 
 echo "done."

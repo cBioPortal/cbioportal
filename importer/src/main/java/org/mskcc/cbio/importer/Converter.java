b3e9a3f20b3540037fd6e99fd69cfb7fd86c36f0
n1zea144 <devnull@localhost>
2014-03-04 12:37:21 -0500

    Added restricted rppa col headers to restricted list

--- importer/src/main/java/org/mskcc/cbio/importer/Converter.java
+++ importer/src/main/java/org/mskcc/cbio/importer/Converter.java
@@ -47,8 +47,8 @@ public interface Converter {
 	public static final String GENE_ID_COLUMN_HEADER_NAME = "Entrez_Gene_Id";
 	public static final String GENE_SYMBOL_COLUMN_HEADER_NAME = "Hugo_Symbol";
 	public static final String MUTATION_CASE_ID_COLUMN_HEADER = "Tumor_Sample_Barcode";
-        public static final Set<String> NON_CASE_IDS = new HashSet<String>(
-                Arrays.asList("ENTREZ_GENE_ID","HUGO_SYMBOL","LOCUS ID","CYTOBAND"));
+    public static final Set<String> NON_CASE_IDS = new HashSet<String>(
+        Arrays.asList("ENTREZ_GENE_ID","HUGO_SYMBOL","LOCUS ID","CYTOBAND", "Composite.Element.REF"));
 
 	/**
 	 * Converts data for the given portal.

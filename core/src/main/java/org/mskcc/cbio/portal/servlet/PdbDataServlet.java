49738fcec3723b52260bc1e335563995823ebde7
Onur Sumer <s.onur.sumer@gmail.com>
2014-03-10 15:09:16 -0400

    moved parse methods from PDBDataServlet to a new class

--- core/src/main/java/org/mskcc/cbio/portal/servlet/PdbDataServlet.java
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/PdbDataServlet.java
@@ -37,6 +37,7 @@ import org.mskcc.cbio.portal.dao.DaoPdbUniprotResidueMapping;
 import org.mskcc.cbio.portal.dao.DaoTextCache;
 import org.mskcc.cbio.portal.model.PdbUniprotAlignment;
 import org.mskcc.cbio.portal.model.PdbUniprotResidueMapping;
+import org.mskcc.cbio.portal.util.PdbFileParser;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -166,13 +167,14 @@ public class PdbDataServlet extends HttpServlet
 		// not cached yet, request from service and parse raw data
 		if (jsonString == null)
 		{
-			info = new JSONObject();
+			PdbFileParser pdbParser = new PdbFileParser();
 			String rawData = this.makeRequest(pdbId);
-			Map<String, String> content = this.parsePdbFile(rawData);
+			Map<String, String> content = pdbParser.parsePdbFile(rawData);
 
-			info.put("title", this.parseTitle(content.get("title")));
-			info.put("compound", this.parseCompound(content.get("compnd")));
-			info.put("source", this.parseCompound(content.get("source")));
+			info = new JSONObject();
+			info.put("title", pdbParser.parseTitle(content.get("title")));
+			info.put("compound", pdbParser.parseCompound(content.get("compnd")));
+			info.put("source", pdbParser.parseCompound(content.get("source")));
 
 			// cache json data as a string
 			cache.cacheText(key, info.toJSONString());
@@ -180,185 +182,11 @@ public class PdbDataServlet extends HttpServlet
 		// already cached, just parse the JSON string
 		else
 		{
-			JSONParser parser = new JSONParser();
-			info = (JSONObject) parser.parse(jsonString);
+			JSONParser jsonParser = new JSONParser();
+			info = (JSONObject) jsonParser.parse(jsonString);
 		}
 
 		return info;
-	}
-
-	/**
-	 * Parses the raw PDB file retrieved from the server and
-	 * creates a mapping for each main identifier.
-	 *
-	 * @param rawInput  raw file contents as a String
-	 * @return  data mapped on the main identifier names
-	 */
-	protected Map<String, String> parsePdbFile(String rawInput)
-	{
-		String[] lines = rawInput.toLowerCase().split("\n");
-
-		// count for distinct identifiers
-		Map<String, Integer> countMap = new HashMap<String, Integer>();
-
-		// map of builders to build content for each identifier
-		Map<String, StringBuilder> contentMap = new HashMap<String, StringBuilder>();
-
-		// actual content map to return
-		Map<String, String> content = new HashMap<String, String>();
-
-		for (String line: lines)
-		{
-			String[] tokens = line.split("[\\s]+");
-
-			// first token is the identifier
-			if (tokens.length == 0)
-			{
-				// empty line, just skip
-				continue;
-			}
-
-			String identifier = tokens[0];
-
-			// get the corresponding count
-			Integer count = countMap.get(identifier);
-
-			if (count == null)
-			{
-				count = 0;
-			}
-
-			// update count
-			countMap.put(identifier, ++count);
-
-			String str = line;
-
-			// if there is more than one identifier lines,
-			// than the line starts with the line number
-			// we should get rid of the line number as well
-			if (count > 1)
-			{
-				str = str.replaceFirst(count.toString(), "");
-			}
-
-			// get the corresponding string builder
-			StringBuilder sb = contentMap.get(identifier);
-
-			if (sb == null)
-			{
-				sb = new StringBuilder();
-				contentMap.put(identifier, sb);
-			}
-
-			// get rid of the identifier itself
-			str = str.replaceFirst(identifier, "").trim();
-
-			// append the parsed line
-			sb.append(str);
-			sb.append("\n");
-		}
-
-		for (String identifier: contentMap.keySet())
-		{
-			String value = contentMap.get(identifier).toString().trim();
-			content.put(identifier, value);
-		}
-
-		return content;
-	}
-
-	protected Map<String, Object> parseCompound(String rawInput)
-	{
-		String[] lines = rawInput.split("\n");
-		Map<String, Object> content = new HashMap<String, Object>();
-		Map<String, Object> mol = null;
-
-		// buffering lines (just in case if an entity consists of multiple lines)
-		StringBuilder buffer = new StringBuilder();
-
-		for (String line: lines)
-		{
-			buffer.append(line);
-
-			// process the buffer if line end with a semicolon
-			if (line.trim().endsWith(";"))
-			{
-				String[] tokens = buffer.toString().split(":");
-
-				if (tokens.length > 1)
-				{
-					String field = tokens[0].trim();
-					String value = tokens[1].trim();
-					// remove the semicolon
-					value = value.substring(0, value.length() - 1);
-					JSONArray list = null;
-
-					// create a new mapping object for each mol_id
-					if (field.equals("mol_id"))
-					{
-						mol = new HashMap<String, Object>();
-						content.put(value, mol);
-					}
-					// convert comma separated chain and gene lists into an array
-					else if (field.equals("chain") ||
-					         field.equals("gene"))
-					{
-						String[] values = value.split("[\\s,]+");
-						list = new JSONArray();
-						list.addAll(Arrays.asList(values));
-					}
-
-					// add the field for the current mol
-					if (mol != null)
-					{
-						if (list != null)
-						{
-							mol.put(field, list);
-						}
-						else
-						{
-							mol.put(field, value);
-						}
-					}
-				}
-
-				// reset buffer for the next entity
-				buffer = new StringBuilder();
-			}
-			else
-			{
-				// add a whitespace before adding the next line
-				buffer.append(" ");
-			}
-		}
-
-		return content;
-	}
-
-	/**
-	 * Parses the raw PDB info returned by the service, and creates
-	 * a human readable info string.
-	 *
-	 * @param rawTitle   data lines to process
-	 * @return  a human readable info string
-	 */
-	protected String parseTitle(String rawTitle)
-	{
-		String[] lines = rawTitle.split("\n");
-		StringBuilder sb = new StringBuilder();
-
-		for (String line: lines)
-		{
-			sb.append(line);
-
-			// whether to add a space at the end or not
-			if (!line.endsWith("-"))
-			{
-				sb.append(" ");
-			}
-		}
-
-		return sb.toString().trim();
 	}
 
 	/**

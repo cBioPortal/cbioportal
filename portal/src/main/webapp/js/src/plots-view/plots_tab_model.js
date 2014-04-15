bc2866ec8a02b083b6e872ec28745dfd8e42456d
Benjamin Gross <benjamin.gross@gmail.com>
2014-04-15 12:41:52 -0400

    Refactoring to reflect changes in backend code

--- portal/src/main/webapp/js/src/plots-view/plots_tab_model.js
+++ portal/src/main/webapp/js/src/plots-view/plots_tab_model.js
@@ -171,8 +171,8 @@ var Plots = (function(){
         init: function() {
             var paramsGetProfiles = {
                 cancer_study_id: cancer_study_id,
-                case_set_id: case_set_id,
-                case_ids_key: case_ids_key,
+                case_set_id: patient_set_id,
+                case_ids_key: patient_ids_key,
                 gene_list: gene_list_str
             };
             $.post("getGeneticProfile.json", paramsGetProfiles, getGeneticProfileCallback, "json");

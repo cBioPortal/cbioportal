eff5f9ddbdd5ef7d28681f0d895e571607d00e61
JJ Gao <jgao@cbio.mskcc.org>
2014-06-13 15:14:53 -0400

    trim clinical data

--- core/src/main/java/org/mskcc/cbio/portal/scripts/ImportClinicalData.java
+++ core/src/main/java/org/mskcc/cbio/portal/scripts/ImportClinicalData.java
@@ -85,7 +85,7 @@ public class ImportClinicalData {
             String caseId = fields[iCaseId];
             for (int i = 0; i < fields.length; i++) {
                 if (i!=iCaseId && !fields[i].isEmpty()) {
-                    DaoClinicalData.addDatum(cancerStudy.getInternalId(), caseId, columnAttrs.get(i).getAttrId(), fields[i]);
+                    DaoClinicalData.addDatum(cancerStudy.getInternalId(), caseId, columnAttrs.get(i).getAttrId(), fields[i].trim());
                 }
             }
         }

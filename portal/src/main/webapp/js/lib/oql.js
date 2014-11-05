4983447b7a918a7481d0803f9782896915be7b6d
Adam Abeshouse <adama@cbio.mskcc.org>
2014-11-05 16:21:14 -0500

    adding dummy files to integrate datamanager into

---
+++ portal/src/main/webapp/js/lib/oql.js
@@ -0,0 +1,241 @@
+// NOTE: Documentation can be generated from these annotations with jsdoc (http://usejsdoc.org/)
+/**
+ * @fileOverview Library for OQL functionality
+ * @author <a href="mailto:adama@cbio.mskcc.org">Adam Abeshouse</a>
+ * @namespace oql
+ * @example
+ * You probably want to call 
+ * filter(parseQuery(query), samples)
+ */
+oql = (function () {
+    /**
+     * @memberOf oql
+     * @param {string} query The query we wish to extract genes ids from
+     * @returns {Array.<string>} A list of genes found in the query
+     */
+    function getGeneList(query) {
+        // isolate gene ids from query
+        var splitq = query.split(/[\n;]+/);
+        var lines = [];
+        for (var i in splitq) {
+            if ($.trim(splitq[i]) !== "") {
+                lines.push(splitq[i]);
+            }
+        }
+        var genes = [];
+        for (var i in lines) {
+            var gene = $.trim(lines[i].split(/[:]/)[0]);
+            if (gene !== "") {
+                genes.push(gene);
+            }
+        }
+        return genes;
+    }
+
+    /* PARSING */
+    // main method is parseQuery(query)
+    /**
+     * @memberOf oql
+     * @param {string} query The query we wish to sanitize
+     * @returns {string} A 'sanitized' query, ie transformed from user input so that
+     * our code can process it as valid oncoquery language.
+     */
+    function sanitizeQuery(query) {
+        // IN: text query, as from user
+        // OUT: "sanitized", i.e. with a few adjustments made to put
+        //		into valid OQL
+        // These adjustments are: - capitalize everything except case-sensitive strings like mutation names (TODO)
+        //						  - insert defaults from cbioportal interface (TODO)
+        var ret = query;
+        //ret = ret.toUpperCase();
+        return ret;
+    }
+
+    /**
+     * @memberOf oql
+     * @param {string} query The multiline query we wish to parse
+     * @returns {Object} {result: resultCode, return: array}
+     * On success, resultCode = 0, array = a list of maps containing parsed query objects, one per line
+     * On failure, resultCode = 1, array = a list of maps {line:lineNumber, msg:errorMessage} corresponding to the syntax errors.
+     */
+    function parseQuery(query) {
+        var lines = sanitizeQuery(query).split(/[\n;]/);
+        var ret = [];
+        var errors = [];
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            if (line.length == 0) {
+                continue;
+            }
+            try {
+                ret.push(oqlParser.parse(line));
+            } catch (err) {
+                errors.push({"line": i, "msg": err});
+            }
+        }
+        if (errors.length > 0) {
+            return {"result": 1, "return": errors};
+        }
+        return {"result": 0, "return": ret};
+    }
+
+    /* FILTERING */
+    /**
+     * @memberOf oql
+     * @description Helper function for createFilterTree
+     * @see createFilterTree
+     * @param {Object} test Either,e.g., {type:'class', value:'MISSENSE'} or,e.g., {type:'name', value:'V600E'}
+     * @param {Array.<Object>} targets An array of Objects that look like {name:'V600E', class:'MISSENSE'}
+     * @returns {boolean} Whether there is some element of 'targets' that matches 'test'
+     */
+    function mutationMatch(test, targets) {
+           if (test.type === "class") {
+            //TODO: get mutation type info to make this work
+            for (var i = 0; i < targets.length; i++) {
+                if (targets[i].class === test.value) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            // TODO: how to match mutations by position, not specific name
+            for (var i = 0; i < targets.length; i++) {
+                if (targets[i].name === test.value) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
+     * @memberOf oql
+     * @description Helper function for filterSample. Takes in a tree created by createFilterTree, and
+     * reduces it recursively into a single boolean value.
+     * @see createFilterTree
+     * @see filterSample
+     * @param tree A "filter tree" produced by createFilterTree
+     * @returns {boolean} The single boolean value that 'tree' reduces to.
+     */
+    function reduceFilterTree(tree) {
+        // Recursive
+        if (tree.type === "LEAF") {
+            return tree.value;
+        } else {
+            if (tree.type === "NOT") {
+                return !reduceFilterTree(tree.child);
+            } else if (tree.type === "AND") {
+                return reduceFilterTree(tree.left) && reduceFilterTree(tree.right);
+            } else if (tree.type === "OR") {
+                return reduceFilterTree(tree.left) || reduceFilterTree(tree.right);
+            }
+        }
+    }
+
+    /**
+     * @memberOf oql
+     * @description Helper function for filterSample. Creates a "filter tree", which is basically
+     * the result of visiting a query's parse tree and converting each command into a boolean 
+     * of whether the given genotype fulfills that requirement.
+     * @param {Object} cmds The attribute commands portion of the result of parseQuery. This is a parse tree.
+     * @param {Object} gene_attrs The genotype of a particular gene in a given sample.
+     * @see filterSample
+     * @returns {Object} A nested object that represents a tree in the following way:
+     * Where T is a subtree and B is a boolean literal value, a leaf looks like
+     * {type:'LEAF', value:B}
+     * and a node looks like one of the following three:
+     * • {type: 'NOT', child: T}
+     * • {type: 'AND', left: T1, right: T2}
+     * • {type: 'OR', left:T1, right:T2}
+     */
+    function createFilterTree(cmds, gene_attrs) {
+        // IN: a query's command tree, a sample's gene attributes
+        // OUT: a tree as described in 'reduceBooleanTree'
+        //		in which each command is converted to the boolean
+        //		value representing whether the given gene attributes complies with it
+        if (cmds.type === "AND" || cmds.type === "OR") {
+            return {"type": cmds.type, "left": createFilterTree(cmds["left"], gene_attrs), "right": createFilterTree(cmds["right"], gene_attrs)};
+        } else if (cmds.type === "NOT") {
+            return {"type": "NOT", "child": createFilterTree(cmds["child"], gene_attrs)};
+        } else {
+            // non-logical type
+            var ret = false;
+            if (cmds.type === "AMP" || cmds.type === "HOMDEL" || cmds.type === "GAIN" || cmds.type === "HETLOSS") {
+                ret = gene_attrs[cmds.type] && (gene_attrs[cmds.type] === true);
+            } else if (cmds.type === "EXP" || cmds.type === "PROT") {
+                if (cmds.constrType === "<") {
+                    ret = gene_attrs[cmds.type] && (gene_attrs[cmds.type] !== false) && (gene_attrs[cmds.type] < cmds.constrVal);
+                } else if (cmds.constrType === "<=") {
+                    ret = gene_attrs[cmds.type] && (gene_attrs[cmds.type] !== false) && (gene_attrs[cmds.type] <= cmds.constrVal);
+                } else if (cmds.constrType === ">") {
+                    ret = gene_attrs[cmds.type] && (gene_attrs[cmds.type] !== false) && (gene_attrs[cmds.type] > cmds.constrVal);
+                } else if (cmds.constrType === ">=") {
+                    ret = gene_attrs[cmds.type] && (gene_attrs[cmds.type] !== false) && (gene_attrs[cmds.type] >= cmds.constrVal);
+                }
+            } else if (cmds.type === "MUT") {
+                if (cmds.constrType === "=") {
+                    ret = gene_attrs[cmds.type] && mutationMatch(cmds.constrVal, gene_attrs["MUT"]);
+                } else if (cmds.constrType === "!=") {
+                    ret = gene_attrs[cmds.type] && !(mutationMatch(cmds.constrVal, gene_attrs["MUT"]));
+                } else if (cmds.constrType === false) {
+                    // match any mutation
+                    ret = gene_attrs[cmds.type] && (gene_attrs["MUT"].length > 0);
+                }
+            }
+            return {"type": "LEAF", "value": ret};
+        }
+    }
+
+    /**
+     * @memberOf oql
+     * @description Helper function for filter
+     * @see filter
+     * @param {Object} single_query One-line query, i.e. one element of the array returned by parseQuery
+     * @param {Object} sample A sample
+     * @see angular.query-page-module.DataManager._samples
+     * @returns {boolean} Whether the given sample passes the given one-line query
+     */
+    function filterSample(single_query, sample) {
+        if (!(single_query.gene in sample)) {
+            // no records on this gene, return false by default
+            return false;
+        }
+        var filterTree = createFilterTree(single_query.cmds, sample[single_query.gene]);
+        return reduceFilterTree(filterTree);
+    }
+
+    /**
+     * @memberOf oql
+     * @param {Array.<Object>} query The result of parseQuery
+     * @param {Object.<string, Object>} samples
+     * @see angular.query-page-module.DataManager._samples
+     * @param {Array.<string>} subset An optional argument which restricts the
+     * samples in consideration to the given subset of samples, specified by their ids.
+     * @returns {Array.<string>} A list of ids of samples (which are in subset, if specified) which pass at least one of the lines of the given query.
+     */
+    function filter(query, samples, subset) {
+        // IN: Javascript object representing an OQL query, a list of samples
+        // OUT: The names of samples in 'samples' for which at least one
+        //		line of query returns true
+        var ret = [];
+        subset = subset || Object.keys(samples);
+        for (var i = 0; i < subset.length; i++) {
+            var s = subset[i];
+            if (s in samples) {
+                var matchesOne = false;
+                for (var j = 0; j < query.length; j++) {
+                    if (filterSample(query[j], samples[s])) {
+                        matchesOne = true;
+                        break;
+                    }
+                }
+                if (matchesOne) {
+                    ret.push(s);
+                }
+            }
+        }
+        return ret;
+    }
+    
+    return {getGeneList: getGeneList, parseQuery: parseQuery, filter: filter};
+})();
---
+++ portal/src/main/webapp/query_page2.jsp
@@ -0,0 +1,125 @@
+<%-- 
+    Document   : query_page
+    Created on : Sep 26, 2014, 1:19:14 PM
+    Author     : abeshoua
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html ng-app="query-page-module" ng-controller="mainController">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-chosen.css"/>
+        <link type="text/css" rel="stylesheet" href="css/chosen-spinner.css"/>
+        <link type="text/css" rel="stylesheet" href="css/query_page.css"/>
+    </head>
+    <body>
+        <jsp:include page="header.html" /> 
+        <!-- STEP 1 -->
+        <div id="mainForm" class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"><span>Select Cancer Study:</span></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <select chosen ng-model="formVars.cancer_study_id"
+                            ng-options="csId as csObj.name group by appVars.vars.types_of_cancer[csObj.type_of_cancer] for (csId, csObj) in appVars.vars.cancer_study_stubs"
+                    >
+                    </select>
+                </div>
+            </div>
+            <div class="row hidden" ng-hide="formVars.cancer_study_id === 'all'">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <p class="leftFloat" ng-bind-html="appVars.vars.cancer_studies[formVars.cancer_study_id].description | to_trusted_html"></p>
+                    <button class="btn btn-sm leftFloat">Study Summary</button>
+                </div>
+            </div>
+            <!-- STEP 2 (CROSS-STUDY) -->
+            <div class="row" ng-show="formVars.cancer_study_id === 'all'">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"><span>Select Data Type Priority:</span></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <div ng-repeat="datap in appVars.vars.data_priorities">
+                        <input type="radio" name="data_priority" ng-model="formVars.data_priority" value="{{datap.id}}">{{datap.label}}
+                    </div>
+                </div>
+            </div>
+            <!-- STEP 2 (SINGLE STUDY) -->
+            <div class="row" ng-hide="formVars.cancer_study_id === 'all'">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"><span>Select Genomic Profiles:</span></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <!-- we have to do (index, profgp) instead of just track by index because otherwise angular doesnt register the change -->
+                    <!-- , even though we don't actually use 'index' -->
+                    <div ng-repeat="(index, profgp) in appVars.vars.ordered_profile_groups">
+                        <div profile-group>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row"  ng-hide="formVars.cancer_study_id === 'all'">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"><span>Select Patient/Case Set:</span></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <select chosen ng-model="formVars.case_set_id"
+                            ng-options="cs.id as cs.label for cs in appVars.vars.case_sets"
+                            >
+                    </select>
+                    <div ng-show="formVars.case_set_id === '-1'">
+                        <textarea class="form-control" ng-model="formVars.custom_case_list" rows="6" cols="80" placeholder="Enter case IDs"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"><span>Enter Gene Set:</span><br/><a target="_blank" href="http://www.cbioportal.org/public-portal/onco_query_lang_desc.jsp">Advanced: Onco Query Language (OQL)</a></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <select chosen ng-model="appVars.vars.gene_set_id"
+                    ng-options="id as gs.name for (id,gs) in appVars.vars.gene_set_stubs"
+                    ng-change="insertGeneList()"
+                    >
+                    </select>
+                    <textarea class="form-control" ng-model="formVars.oql_query" rows="6" cols="80" placeholder="Enter HUGO Gene Symbols or Gene Aliases"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 leftColumn"></div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <button type="button" class="btn btn-mskcc btn-sm" ng-click="submitForm()">Submit</button>
+                    &nbsp;&nbsp;
+                    <button type="button" class="btn btn-mskcc btn-sm" ng-click="formVars.clear()">Clear</button>
+                    <!--<button type="button" class="btn btn-mskcc btn-sm" ng-click="syncToUrl()">Update URL</button>
+                    <button type="button" class="btn btn-mskcc btn-sm" ng-click="syncFromUrl()">Get From URL</button>-->
+                </div>
+            </div>
+            <div class="row">
+                <p ng-show="appVars.vars.error_msg.length > 0" style="color:red">{{appVars.vars.error_msg}}</p>
+            </div>
+            <div class="row">
+                <table ng-show="!isEmpty(appVars.vars.query_result.samples)"  class="table table-responsive table-striped">
+                    <tbody>
+                        <tr>
+                            <td rowspan="2">Sample\ Gene</td>
+                            <td ng-repeat="gene in appVars.vars.query_result.genes" colspan="7">{{gene}}</td>
+                        </tr>
+                        <tr>
+                            <!-- 7 is magic number: the number of event types (AMP, GAIN, HOMDEL, HETLOSS, MUT, EXP, PROT) -->
+                            <td ng-repeat="i in range(7 * appVars.vars.query_result.genes.length) track by $index">
+                                {{appVars.vars.event_types[$index % 7]}}
+                            </td>
+                        </tr>
+                        <tr ng-repeat="(id, samp) in appVars.vars.query_result.samples">
+                            <td>{{id}}</td>
+                            <td ng-repeat="i in range(7 * appVars.vars.query_result.genes.length) track by $index">
+                                {{samp[appVars.vars.query_result.genes[Math.floor($index / 7)]][appVars.vars.event_types[$index % 7]]}}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <script type="text/javascript" src="js/lib/jquery.min.js"></script>
+        <script type="text/javascript" src="js/src/query-page/angular.min.js"></script>
+        <script type="text/javascript" src="js/src/query-page/ui-bootstrap-tpls-0.11.2.min.js"></script>
+        <script type="text/javascript" src="js/lib/chosen.jquery.angular.js"></script>
+        <script type="text/javascript" src="js/lib/chosen-angular.js"></script>
+        
+        <script type="text/javascript" src="js/lib/oql-parser.js"></script>
+        <script type="text/javascript" src="js/lib/oql.js"></script>
+        <script type="text/javascript" src="js/src/query-page/query_page.js"></script>
+    </body>
+</html>

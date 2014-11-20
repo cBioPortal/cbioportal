f4f3ca3941ec8adb24d78923d69e85e67ca6c59b
yichaoS <devnull@localhost>
2014-11-20 11:44:38 -0500

    added dao, model, util, importer files

---
+++ importer/src/main/java/org/mskcc/cbio/importer/cvr/darwin/dao/IdMapperDAO.java
@@ -0,0 +1,37 @@
+package org.mskcc.cbio.importer.cvr.darwin.dao;
+
+import org.mskcc.cbio.importer.cvr.darwin.model.IdMapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mskcc.cbio.importer.cvr.darwin.model.IdMapper;
+
+public class IdMapperDAO {
+    
+    private SqlSessionFactory sqlSessionFactory = null;
+
+    public IdMapperDAO(SqlSessionFactory sqlSessionFactory){
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+    
+    public IdMapper selectByDmpId(String dmpId){
+        IdMapper idMapper = null;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            idMapper = session.selectOne("IdMapperMapper.selectByDmpId", dmpId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return idMapper;
+    }
+    
+    public IdMapper selectByDarwinId(String darwinId){
+        IdMapper idMapper = null;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            idMapper = session.selectOne("IdMapperMapper.selectByDarwinId", darwinId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return idMapper;
+    }
+    
+}

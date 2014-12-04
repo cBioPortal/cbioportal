7caee66508782c7bfa72b44bcfaffa3fef9596a0
yichaoS <devnull@localhost>
2014-12-04 13:04:05 -0500

    revert back session manager retrieving config file path from importer properties files

--- importer/src/main/java/org/mskcc/cbio/importer/cvr/darwin/util/DarwinSessionManager.java
+++ importer/src/main/java/org/mskcc/cbio/importer/cvr/darwin/util/DarwinSessionManager.java
@@ -2,10 +2,6 @@ package org.mskcc.cbio.importer.cvr.darwin.util;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -13,11 +9,6 @@ import org.apache.log4j.Logger;
 
 import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Properties;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.mskcc.cbio.importer.fetcher.internal.DmpDarwinFetcherImpl;
-import org.mskcc.cbio.portal.util.GlobalProperties;
 
 /**
  * Copyright (c) 2014 Memorial Sloan-Kettering Cancer Center.
@@ -39,7 +30,6 @@ import org.mskcc.cbio.portal.util.GlobalProperties;
  * Created by criscuof on 11/20/14.
  */
 public enum DarwinSessionManager {
-    
 
     /*
     A Singleton implemented as an enum to provide access to a Darwin SQL
@@ -48,14 +38,9 @@ public enum DarwinSessionManager {
     INSTANCE;
     static final Logger logger = Logger.getLogger(DarwinSessionManager.class);
     private SqlSession session = Suppliers.memoize(new DarwinSessionSupplier()).get();
-    
-    private static final String HOME_DIR = "PORTAL_HOME";
-    private static final String PORTAL_PROPERTIES_FILENAME = "importer.properties";
-    private static final String MYBATIS_CONFIG_FILE = "darwin.mybatis_config_file";
-    private static final Log LOG = LogFactory.getLog(DarwinSessionManager.class);
-    
+
     public SqlSession getDarwinSession(){
-        return this.session;
+        return  this.session;
     }
 
     public void closeSession(){
@@ -64,10 +49,11 @@ public enum DarwinSessionManager {
     }
 
     private class DarwinSessionSupplier implements Supplier<SqlSession> {
+        //TODO: make this a property
+        private final String configFileName = "/mybatis-config.xml";
+
         @Override
         public SqlSession get() {
-            Properties properties = loadProperties(getResourcesStream());
-            String configFileName = properties.getProperty(MYBATIS_CONFIG_FILE);
             InputStream inputStream =DarwinSessionSupplier.class.getResourceAsStream(configFileName);
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
            return sqlSessionFactory.openSession();
@@ -84,61 +70,6 @@ public enum DarwinSessionManager {
         }
         DarwinSessionManager.INSTANCE.closeSession();
     }
-    
-    private InputStream getResourcesStream() {
-        String resourceFilename = null;
-        InputStream resourceFIS = null;
-
-        try {
-            String home = System.getenv(HOME_DIR);
-            if (home != null) {
-                 resourceFilename =
-                    home + File.separator + PORTAL_PROPERTIES_FILENAME;
-                if (LOG.isInfoEnabled()) {
-                    LOG.info("Attempting to read properties file: " + resourceFilename);
-                }
-                resourceFIS = new FileInputStream(resourceFilename);
-                if (LOG.isInfoEnabled()) {
-                    LOG.info("Successfully read properties file");
-                }
-            }
-        }
-        catch (FileNotFoundException e) {
-            if (LOG.isInfoEnabled()) {
-                LOG.info("Failed to read properties file: " + resourceFilename);
-            }
-        }
-
-        if (resourceFIS == null) {
-            if (LOG.isInfoEnabled()) {
-                LOG.info("Attempting to read properties file from classpath");
-            }
-            resourceFIS = GlobalProperties.class.getClassLoader().
-                getResourceAsStream(PORTAL_PROPERTIES_FILENAME);
-            if (LOG.isInfoEnabled()) {
-                LOG.info("Successfully read properties file");
-            }
-        }
-        
-        return resourceFIS;
-    }
-    
-    private static Properties loadProperties(InputStream resourceInputStream) {
-
-        Properties _properties = new Properties();
-        try {
-            _properties.load(resourceInputStream);
-            resourceInputStream.close();
-        }
-        catch (IOException e) {
-            if (LOG.isErrorEnabled()) {
-                LOG.error("Error loading properties file: " + e.getMessage());
-            }
-        }
-
-        return _properties;
-    }
-    
 
 
 }

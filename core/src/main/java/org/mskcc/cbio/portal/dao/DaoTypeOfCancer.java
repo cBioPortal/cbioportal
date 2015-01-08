996880e60df4a738a174487f54f6bc365f0e2f57
JJ Gao <jgao@cbio.mskcc.org>
2015-01-08 11:39:32 -0500

    short name into db

--- core/src/main/java/org/mskcc/cbio/portal/dao/DaoTypeOfCancer.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoTypeOfCancer.java
@@ -41,7 +41,7 @@ public class DaoTypeOfCancer {
       ResultSet rs = null;
       try {
          con = JdbcUtil.getDbConnection(DaoTypeOfCancer.class);
-         pstmt = con.prepareStatement("INSERT INTO type_of_cancer ( `TYPE_OF_CANCER_ID`, `NAME`, `CLINICAL_TRIAL_KEYWORDS`, `DEDICATED_COLOR`, `SHORT_NAME`, `PARENT` ) VALUES (?,?,?,?,?)");
+         pstmt = con.prepareStatement("INSERT INTO type_of_cancer ( `TYPE_OF_CANCER_ID`, `NAME`, `CLINICAL_TRIAL_KEYWORDS`, `DEDICATED_COLOR`, `SHORT_NAME`, `PARENT` ) VALUES (?,?,?,?,?,?)");
          pstmt.setString(1, typeOfCancer.getTypeOfCancerId());
          pstmt.setString(2, typeOfCancer.getName());
          pstmt.setString(3, typeOfCancer.getClinicalTrialKeywords());

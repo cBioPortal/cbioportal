2dbea508eb664ec58e2047b0a45df2cf64002aa4
yichaoS <devnull@localhost>
2015-02-25 16:15:25 -0500

    fixed the download help icon problem

--- portal/src/main/webapp/js/src/plots-tab/view/components/optSpec.js
+++ portal/src/main/webapp/js/src/plots-tab/view/components/optSpec.js
@@ -30,31 +30,77 @@ var optSpec = (function() {
         //SVG & PDF buttons
         $("#" + ids.sidebar.util.download_buttons).append("<button id='svg_download'>SVG</button>");
         $("#svg_download").click(function() {
-              var xmlSerializer = new XMLSerializer();
-              var download_str = cbio.download.addSvgHeader(xmlSerializer.serializeToString($("#" + ids.main_view.div + " svg")[0]));
-              cbio.download.clientSideDownload([download_str], "result.svg", "application/svg+xml");
+            //remove the help icon
+            var elem_x_txt = $("." + d3_class.x.title_help).qtip('api').options.content.text;
+            var elem_y_txt = $("." + d3_class.y.title_help).qtip('api').options.content.text;
+            var elem_x = $("." + d3_class.x.title_help).remove();
+            var elem_y = $("." + d3_class.y.title_help).remove(); 
+            //send download request
+            var xmlSerializer = new XMLSerializer();
+            var download_str = cbio.download.addSvgHeader(xmlSerializer.serializeToString($("#" + ids.main_view.div + " svg")[0]));
+            cbio.download.clientSideDownload([download_str], "result.svg", "application/svg+xml");
+            //add back on the help icons
+            $(".axis").append(elem_x);
+            $(".axis").append(elem_y);
+            $("." + d3_class.x.title_help).qtip({
+                content: {text: "<font size=2>" + elem_x_txt + "</font>" },
+                style: { classes: 'qtip-light qtip-rounded qtip-shadow qtip-lightyellow' },
+                show: {event: "mouseover"},
+                hide: {fixed:true, delay: 100, event: "mouseout"},
+                position: {my:'left bottom',at:'top right', viewport: $(window)}
+            });
+            $("." + d3_class.y.title_help).qtip({
+                content: {text: "<font size=2>" + elem_y_txt + "</font>" },
+                style: { classes: 'qtip-light qtip-rounded qtip-shadow qtip-lightyellow' },
+                show: {event: "mouseover"},
+                hide: {fixed:true, delay: 100, event: "mouseout"},
+                position: {my:'left bottom',at:'top right', viewport: $(window)}
+            });
         });
         $("#" + ids.sidebar.util.download_buttons).append("<button id='pdf_download'>PDF</button>");
         $("#pdf_download").click(function() {
+            //remove the help icon
+            var elem_x_txt = $("." + d3_class.x.title_help).qtip('api').options.content.text;
+            var elem_y_txt = $("." + d3_class.y.title_help).qtip('api').options.content.text;
+            var elem_x = $("." + d3_class.x.title_help).remove();
+            var elem_y = $("." + d3_class.y.title_help).remove();
+            //send download request
             var downloadOptions = {
                 filename: "result.pdf",
                 contentType: "application/pdf",
-                servletName: "svgtopdf.do",
-                preProcess: trimHelpIcon 
+                servletName: "svgtopdf.do"
             };
-            function trimHelpIcon(svg_str) {
-                //<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="images/help.png" class="plots-y-axis-title-help" x="44" y="79" width="16" height="16" data-hasqtip="1312"/>
-                var _begin_index_1 = svg_str.indexOf("<image");
-                var _end_index_1 = svg_str.indexOf("data-hasqtip") + 21;
-                var _trimmed_str_1 = svg_str.substring(0, _begin_index_1) + svg_str.substring(_end_index_1, svg_str.length);
-                var _begin_index_2 = _trimmed_str_1.indexOf("<image");
-                var _end_index_2 = _trimmed_str_1.indexOf("data-hasqtip") + 21;
-                var _trimmed_str_2 = _trimmed_str_1.substring(0, _begin_index_2) + _trimmed_str_1.substring(_end_index_2, svg_str.length);
-                return _trimmed_str_2;
-            }
             cbio.download.initDownload(
                     $("#" + ids.main_view.div + " svg")[0], downloadOptions);
+            //add back on the help icons
+            $(".axis").append(elem_x);
+            $(".axis").append(elem_y);
+            $("." + d3_class.x.title_help).qtip({
+                content: {text: "<font size=2>" + elem_x_txt + "</font>" },
+                style: { classes: 'qtip-light qtip-rounded qtip-shadow qtip-lightyellow' },
+                show: {event: "mouseover"},
+                hide: {fixed:true, delay: 100, event: "mouseout"},
+                position: {my:'left bottom',at:'top right', viewport: $(window)}
+            });
+            $("." + d3_class.y.title_help).qtip({
+                content: {text: "<font size=2>" + elem_y_txt + "</font>" },
+                style: { classes: 'qtip-light qtip-rounded qtip-shadow qtip-lightyellow' },
+                show: {event: "mouseover"},
+                hide: {fixed:true, delay: 100, event: "mouseout"},
+                position: {my:'left bottom',at:'top right', viewport: $(window)}
+            });
         });
+        
+        function trimHelpIcon(svg_str) {
+            //<image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="images/help.png" class="plots-y-axis-title-help" x="44" y="79" width="16" height="16" data-hasqtip="1312"/>
+            var _begin_index_1 = svg_str.indexOf("<image");
+            var _end_index_1 = svg_str.indexOf("data-hasqtip") + 21;
+            var _trimmed_str_1 = svg_str.substring(0, _begin_index_1) + svg_str.substring(_end_index_1, svg_str.length);
+            var _begin_index_2 = _trimmed_str_1.indexOf("<image");
+            var _end_index_2 = _trimmed_str_1.indexOf("data-hasqtip") + 21;
+            var _trimmed_str_2 = _trimmed_str_1.substring(0, _begin_index_2) + _trimmed_str_1.substring(_end_index_2, svg_str.length);
+            return _trimmed_str_2;
+        }
         
         //Data download button
         $("#" + ids.sidebar.util.download_buttons).append("<button id='data_download'>Data</button>");

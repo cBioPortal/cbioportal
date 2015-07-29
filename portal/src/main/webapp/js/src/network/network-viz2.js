d6bdd237abfa332faf3e227898e6df6cb448758a
Mehmet Furkan ŞAHİN <sahinffurkan@gmail.com>
2015-07-29 18:11:38 +0300

    panzoom added

--- portal/src/main/webapp/js/src/network/network-viz2.js
+++ portal/src/main/webapp/js/src/network/network-viz2.js
@@ -266,8 +266,28 @@ function send2cytoscapeweb(elements, cytoscapeDivId, networkDivId)
 
 			//to hide drugs initially
 			netVis._changeListener();
+			var panProps = ({
+					zoomFactor: 0.05, // zoom factor per zoom tick
+					zoomDelay: 45, // how many ms between zoom ticks
+					minZoom: 0.1, // min zoom level
+					maxZoom: 10, // max zoom level
+					fitPadding: 50, // padding when fitting
+					panSpeed: 10, // how many ms in between pan ticks
+					panDistance: 10, // max pan distance per tick
+					panDragAreaSize: 75, // the length of the pan drag box in which the vector for panning is calculated (bigger = finer control of pan speed and direction)
+					panMinPercentSpeed: 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
+					panInactiveArea: 3, // radius of inactive area in pan drag box
+					panIndicatorMinOpacity: 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
+					autodisableForMobile: true, // disable the panzoom completely for mobile (since we don't really need it with gestures like pinch to zoom)
 
+					// icon class names
+					sliderHandleIcon: 'fa fa-minus',
+					zoomInIcon: 'fa fa-plus',
+					zoomOutIcon: 'fa fa-minus',
+					resetIcon: 'fa fa-expand'    });
+			cy.panzoom(panProps);
 		}});
+
 }
 
 var rgb2hex = function(r, g, b)

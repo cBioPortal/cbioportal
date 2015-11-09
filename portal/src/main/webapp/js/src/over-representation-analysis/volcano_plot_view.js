a08a02e8c0d0a1bb17f9bdbd70ecc59c861b6bf6
pieter <pieter@thehyve.nl>
2015-11-09 11:42:45 +0100

    code now using only Scatterplots.js

--- portal/src/main/webapp/js/src/over-representation-analysis/volcano_plot_view.js
+++ portal/src/main/webapp/js/src/over-representation-analysis/volcano_plot_view.js
@@ -78,16 +78,15 @@ function VolcanoPlot()
 	this.render = function(plotElName, model, dmPresenter, data){
 		this.model = model;
 		
-    	//get data via presenter layer:
+    	//how the code should be if we get the data via presenter layer:
 		//this.plotPresenter = new PlotPresenter(model, dmPresenter, geneId);
 		//Get data:
 	    //this.plotPresenter.getDataForPlot(function (plotData) {
 			//var plotData = null;
 	    	//draw plot
-			//var plotEl = $("#" + plotElName)[0];
-	    	//drawPlot(plotData, model, plotEl);
+	    	//drawPlot(plotData, model, plotElName);
 	    //});
-		var scatterPlotDataAttr = {
+		var plotDataAttr = {
 			    min_x: 0,
 			    max_x: 0,
 			    min_y: 0,
@@ -95,19 +94,19 @@ function VolcanoPlot()
 			};
 
 		
-		var scatterPlotArr = [];
+		var plotData = [];
 		for (var i = 0; i < data.length; i++){
 			
 			var xValue = parseFloat(data[i][3].replace("<","").replace(">","")); //remove < and > from <-10 or >10 scenarios
 			var yValue = -Math.log10(data[i][4]);
-			if (xValue < scatterPlotDataAttr.min_x)
-				scatterPlotDataAttr.min_x = xValue;
-			if (xValue > scatterPlotDataAttr.max_x)
-				scatterPlotDataAttr.max_x = xValue;
-			if (yValue < scatterPlotDataAttr.min_y)
-				scatterPlotDataAttr.min_y = yValue;
-			if (yValue > scatterPlotDataAttr.max_y)
-				scatterPlotDataAttr.max_y = yValue;
+			if (xValue < plotDataAttr.min_x)
+				plotDataAttr.min_x = xValue;
+			if (xValue > plotDataAttr.max_x)
+				plotDataAttr.max_x = xValue;
+			if (yValue < plotDataAttr.min_y)
+				plotDataAttr.min_y = yValue;
+			if (yValue > plotDataAttr.max_y)
+				plotDataAttr.max_y = yValue;
 			
 			var _scatterPlotItem = {
 					x_val : xValue,
@@ -115,16 +114,9 @@ function VolcanoPlot()
 					case_id : xValue + "_" + yValue,
 					qtip : "p-value: " + data[i][4] + ", log ratio: " + data[i][3],
 			};
-	        scatterPlotArr.push(_scatterPlotItem);
+			plotData.push(_scatterPlotItem);
 		}
-		
-		var brushOn = true;
-		var scatterPlot = new ScatterPlots();
-		scatterPlotOptions.names.body = plotElName;
-        scatterPlot.init(scatterPlotOptions, scatterPlotArr, scatterPlotDataAttr, brushOn, false);            
-        //scatterPlot.jointBrushCallback(scatterPlotBrushCallBack);
-        //scatterPlot.jointClickCallback(scatterPlotClickCallBack);
-        $("#study-view-scatter-plot-header").css('display', 'none');
+		drawPlot(plotData, null, plotElName, plotDataAttr);
 		
 	}
 	    
@@ -134,73 +126,16 @@ function VolcanoPlot()
 	 * @param plotData: the plot data formated by PlotPresenter
 	 * @param model: the model with the parameters that can influence part of the look/feel
 	 * @param plotEl: el where the plot should be rendered
+	 * @param plotDataAttr: object containing info on data size (x and y min/max)
 	 */
-    var drawPlot = function(plotData, model, plotEl) {
+    var drawPlot = function(plotData, model, plotElName, plotDataAttr) {
 		
-    	// data that you want to plot, I've used separate arrays for x and y values
-    	var xdata = [5, 10, 15, 20],
-    	    ydata = [3, 17, 4, 6];
-
-    	// size and margins for the chart
-    	var margin = {top: 20, right: 15, bottom: 60, left: 60}
-    	  , width = 200 - margin.left - margin.right
-    	  , height = 200 - margin.top - margin.bottom;
-
-    	// x and y scales, I've used linear here but there are other options
-    	// the scales translate data values to pixel values for you
-    	var x = d3.scale.linear()
-    	          .domain([0, d3.max(xdata)])  // the range of the values to plot
-    	          .range([ 0, width ]);        // the pixel range of the x-axis
-
-    	var y = d3.scale.linear()
-    	          .domain([0, d3.max(ydata)])
-    	          .range([ height, 0 ]);
-
-    	// the chart object, includes all margins
-    	var chart = d3.select(plotEl)
-    	.append('svg:svg')
-    	.attr('width', width + margin.right + margin.left)
-    	.attr('height', height + margin.top + margin.bottom)
-    	.attr('class', 'chart')
-
-    	// the main object where the chart and axis will be drawn
-    	var main = chart.append('g')
-    	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-    	.attr('width', width)
-    	.attr('height', height)
-    	.attr('class', 'main')   
-
-    	// draw the x axis
-    	var xAxis = d3.svg.axis()
-    	.scale(x)
-    	.orient('bottom');
-
-    	main.append('g')
-    	.attr('transform', 'translate(0,' + height + ')')
-    	.attr('class', 'main axis date')
-    	.call(xAxis);
-
-    	// draw the y axis
-    	var yAxis = d3.svg.axis()
-    	.scale(y)
-    	.orient('left');
-
-    	main.append('g')
-    	.attr('transform', 'translate(0,0)')
-    	.attr('class', 'main axis date')
-    	.call(yAxis);
-
-    	// draw the graph object
-    	var g = main.append("svg:g"); 
-
-    	g.selectAll("scatter-dots")
-    	  .data(ydata)  // using the values in the ydata array
-    	  .enter().append("svg:circle")  // create a new circle for each value
-    	      .attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
-    	      .attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
-    	      .attr("r", 10) // radius of circle
-    	      .style("opacity", 0.6); // opacity of circle
-	    
-	    
+		var brushOn = true;
+		var scatterPlot = new ScatterPlots();
+		scatterPlotOptions.names.body = plotElName;
+        scatterPlot.init(scatterPlotOptions, plotData, plotDataAttr, brushOn, false);            
+        //scatterPlot.jointBrushCallback(scatterPlotBrushCallBack);
+        //scatterPlot.jointClickCallback(scatterPlotClickCallBack);
+        $("#study-view-scatter-plot-header").css('display', 'none');   
     }
 }

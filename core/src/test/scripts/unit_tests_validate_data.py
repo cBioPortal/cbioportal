0a99c778597d6f8bbcf2e7cfd212597ac63f9847
Fedde Schaeffer <fedde@thehyve.nl>
2016-01-08 18:22:39 +0100

    Add first unit tests for clinical attribute header validations

--- core/src/test/scripts/unit_tests_validate_data.py
+++ core/src/test/scripts/unit_tests_validate_data.py
@@ -87,3 +87,83 @@ class ValidateDataTester(unittest.TestCase):
         validator.validate()
         # again, we expect no errors or warnings
         self.assertEqual(0, len(logger.handlers[0].buffer))
+
+
+class LogBufferTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.handlers.BufferingHandler(capacity=1e6)
+        self.logger.handlers = [handler]
+
+    def tearDown(self):
+        self.logger.handlers = []
+
+    def get_log_records(self):
+        '''Get the log records written to the logger since the last call.'''
+        recs = self.logger.handlers[0].buffer
+        self.logger.handlers[0].flush()
+        return recs
+
+    @staticmethod
+    def print_log_records(record_list):
+        '''Pretty-print a list of log records to standard output.'''
+        formatter = validateData.LogfileStyleFormatter()
+        for record in record_list:
+            print formatter.format(record)
+
+
+class DataFileTestCase(LogBufferTestCase):
+
+    def setUp(self):
+        super(DataFileTestCase, self).setUp()
+        self.orig_study_dir = validateData.STUDY_DIR
+        validateData.STUDY_DIR = 'test_data'
+
+    def tearDown(self):
+        super(DataFileTestCase, self).tearDown()
+        validateData.STUDY_DIR = self.orig_study_dir
+
+    def validate(self, data_filename, ValidatorClass, extra_meta_fields=None):
+        meta_dict = {'data_file_path': data_filename}
+        if extra_meta_fields is not None:
+            meta_dict.update(extra_meta_fields)
+        validator = ValidatorClass(hugo_entrez_map, self.logger, meta_dict)
+        validator.validate()
+        return self.get_log_records()
+
+
+class ClinicalColumnDefsTestCase(DataFileTestCase):
+
+    '''Tests for validations of the column definitions in a clinical file.'''
+
+    def test_correct_definitions(self):
+        record_list = self.validate('data_clin_coldefs_correct.txt',
+                                    validateData.ClinicalValidator)
+        self.assertEqual(len(record_list), 2)
+        for record in record_list:
+            self.assertEqual(record.levelno, logging.INFO)
+
+    def test_wrong_displayname(self):
+        record_list = self.validate('data_clin_coldefs_wrong_display_name.txt',
+                                    validateData.ClinicalValidator)
+        self.assertEqual(len(record_list), 3)
+        self.assertEqual(record_list[1].levelno, logging.ERROR)
+        self.assertEqual(record_list[1].column_number, 4)
+        self.assertEqual(record_list[1].line_number, 1)
+
+    def test_unknown_attribute(self):
+        record_list = self.validate('data_clin_coldefs_unknown_attribute.txt',
+                                    validateData.ClinicalValidator)
+        self.assertEqual(len(record_list), 2)
+        for record in record_list:
+            self.assertEqual(record.levelno, logging.INFO)
+
+    def test_invalid_priority(self):
+        record_list = self.validate('data_clin_coldefs_invalid_priority.txt',
+                                    validateData.ClinicalValidator)
+        self.assertEqual(len(record_list), 3)
+        self.assertEqual(record_list[1].levelno, logging.ERROR)
+        self.assertEqual(record_list[1].column_number, 4)
+        self.assertEqual(record_list[1].line_number, 5)

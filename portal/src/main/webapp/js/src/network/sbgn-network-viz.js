fa29d4bf4ed64dd75bc9de5cd650d57157b7ca63
eightbit <istemi.bahceci@gmail.com>
2016-01-22 00:37:50 +0200

    cose-bilkent is replaced with the latest version

--- portal/src/main/webapp/js/src/network/sbgn-network-viz.js
+++ portal/src/main/webapp/js/src/network/sbgn-network-viz.js
@@ -12,13 +12,20 @@ var sbgnStyleSheet = cytoscape.stylesheet()
   'border-color': '#555',
   'background-color': '#f6f6f6',
   'font-size': 11,
-  'shape': 'data(sbgnclass)',
-  'background-opacity': '0.5',
-  'padding-relative': 20
+  //          'shape': 'data(sbgnclass)',
+  'background-opacity': '0.5'
+})
+.selector("node[sbgnclass]")
+.css({
+  'shape': 'data(sbgnclass)'
 })
 .selector("node[sbgnclass='complex']")
 .css({
-  'background-color': '#F4F3EE'
+  'background-color': '#F4F3EE',
+  'expanded-collapsed': 'expanded',
+  'text-valign': 'bottom',
+  'text-halign': 'center',
+  'font-size': '16'
 })
 .selector("node[sbgnclass='compartment']")
 .css({
@@ -27,9 +34,14 @@ var sbgnStyleSheet = cytoscape.stylesheet()
   'content': 'data(sbgnlabel)',
   'text-valign': 'bottom',
   'text-halign': 'center',
-  'font-size': '16'
+  'font-size': '16',
+  'expanded-collapsed': 'expanded'
 })
-.selector("node[sbgnclass!='complex'][sbgnclass!='compartment'][sbgnclass!='submap']")
+.selector("node[sbgnclass='submap']")
+.css({
+  'expanded-collapsed': 'expanded'
+})
+.selector("node[sbgnclass][sbgnclass!='complex'][sbgnclass!='compartment'][sbgnclass!='submap']")
 .css({
   'width': 'data(sbgnbbox.w)',
   'height': 'data(sbgnbbox.h)'
@@ -38,132 +50,132 @@ var sbgnStyleSheet = cytoscape.stylesheet()
 .css({
   'border-color': '#d67614',
   'target-arrow-color': '#000',
-  'text-outline-color': '#000'
-})
-.selector("node:active")
-.css({
-  'background-opacity': '0.7',
-  'overlay-color': '#d67614',
-  'overlay-padding': '14'
-})
-.selector("edge")
-.css({
-  'line-color': '#555',
-  'target-arrow-fill': 'hollow',
-  'source-arrow-fill': 'hollow',
-  'width': 1.5,
-  'target-arrow-color': '#555',
-  'source-arrow-color': '#555',
-  'target-arrow-shape': 'data(sbgnclass)'
-})
-.selector("edge[sbgnclass='inhibition']")
-.css({
-  'target-arrow-fill': 'filled'
-})
-.selector("edge[sbgnclass='consumption']")
-.css({
-  'target-arrow-shape': 'none',
-  'source-arrow-shape': 'data(sbgnclass)',
-  'line-style': 'consumption'
-})
-.selector("edge[sbgnclass='production']")
-.css({
-  'target-arrow-fill': 'filled',
-  'line-style': 'production'
-})
-.selector("edge:selected")
-.css({
-  'line-color': '#d67614',
-  'source-arrow-color': '#d67614',
-  'target-arrow-color': '#d67614'
-})
-.selector("edge:active")
-.css({
-  'background-opacity': '0.7',
-  'overlay-color': '#d67614',
-  'overlay-padding': '8'
-})
-.selector("core")
-.css({
-  'selection-box-color': '#d67614',
-  'selection-box-opacity': '0.2',
-  'selection-box-border-color': '#d67614'
-}); // end of sbgnStyleSheet
+  'text-outline-color': '#000'})
+  .selector("node:active")
+  .css({
+    'background-opacity': '0.7', 'overlay-color': '#d67614',
+    'overlay-padding': '14'
+  })
+  .selector("edge")
+  .css({
+    'line-color': '#555',
+    'target-arrow-fill': 'hollow',
+    'source-arrow-fill': 'hollow',
+    'width': 1.5,
+    'target-arrow-color': '#555',
+    'source-arrow-color': '#555',
+    'target-arrow-shape': 'data(sbgnclass)'
+  })
+  .selector("edge[sbgnclass='inhibition']")
+  .css({
+    'target-arrow-fill': 'filled'
+  })
+  .selector("edge[sbgnclass='consumption']")
+  .css({
+    'target-arrow-shape': 'none',
+    'source-arrow-shape': 'data(sbgnclass)',
+    'line-style': 'consumption'
+  })
+  .selector("edge[sbgnclass='production']")
+  .css({
+    'target-arrow-fill': 'filled',
+    'line-style': 'production'
+  })
+  .selector("edge:selected")
+  .css({
+    'line-color': '#d67614',
+    'source-arrow-color': '#d67614',
+    'target-arrow-color': '#d67614'
+  })
+  .selector("edge:active")
+  .css({
+    'background-opacity': '0.7', 'overlay-color': '#d67614',
+    'overlay-padding': '8'
+  })
+  .selector("core")
+  .css({
+    'selection-box-color': '#d67614',
+    'selection-box-opacity': '0.2', 'selection-box-border-color': '#d67614'
+  })
 
-function popUpSBGNView()
-{
-  //Get node ids from URL !!
-  var container = $('#sbgnCanvas');
-  var urlString = window.location.search.substring(1).split("&");
-  var nodeA = urlString[0];
-  var nodeB = urlString[1];
-
-  //Construct PC2 url from pop up url !
-  var pc2URL = "http://www.pathwaycommons.org/pc2/";
-  var format = "graph?format=SBGN";
-  var kind = "&kind=PATHSBETWEEN";
-  var sourceA = "&source="+nodeA;
-  var sourceB = "&source="+nodeB;
-  var pc2URL = pc2URL + format + kind + sourceA + sourceB;
-
-  //Add loading spinner
-  container.append('<i class="fa fa-spinner fa-5x fa-spin"></i>');
-
-  $.get(pc2URL, function(data)
+  function popUpSBGNView()
   {
-    //Remove loading spinner !
-    container.empty();
+    //Get node ids from URL !!
+    var container = $('#sbgnCanvas');
+    var urlString = window.location.search.substring(1).split("&");
+    var nodeA = urlString[0];
+    var nodeB = urlString[1];
 
-    //Convert incoming SBGNML string to json
-    var graphData = sbgnmlToJson.convert(data)
-    var positionMap = {};
+    //Construct PC2 url from pop up url !
+    var pc2URL = "http://www.pathwaycommons.org/pc2/";
+    var format = "graph?format=SBGN";
+    var kind = "&kind=PATHSBETWEEN";
+    var sourceA = "&source="+nodeA;
+    var sourceB = "&source="+nodeB;
+    var pc2URL = pc2URL + format + kind + sourceA + sourceB;
 
-    //add position information to data for preset layout
-    for (var i = 0; i < graphData.nodes.length; i++) {
-      var xPos = graphData.nodes[i].data.sbgnbbox.x;
-      var yPos = graphData.nodes[i].data.sbgnbbox.y;
-      positionMap[graphData.nodes[i].data.id] = {'x': xPos, 'y': yPos};
-    }
+    //Add loading spinner
+    container.append('<i class="fa fa-spinner fa-5x fa-spin"></i>');
 
-    var cy = window.cy = cytoscape(
+    $.get(pc2URL, function(data)
     {
-      container: document.getElementById('sbgnCanvas'),
-      elements:graphData,
-      style: sbgnStyleSheet,
-      layout:
-      {
-        name: 'preset',
-        positions: positionMap
-      },
-      showOverlay: false,
-      minZoom: 0.125,
-      maxZoom: 16,
-      boxSelectionEnabled: true,
-      motionBlur: true,
-      wheelSensitivity: 0.1,
-      ready: function ()
-      {
-        //refreshPaddings();
+      //Remove loading spinner !
+      container.empty();
 
-        var panProps = ({
-          fitPadding: 10,
-        });
-        container.cytoscapePanzoom(panProps);
+      //Convert incoming SBGNML string to json
+      var graphData = sbgnmlToJson.convert(data)
+      var positionMap = {};
 
-        cy.on('mouseover', 'node', function (event) {
-        });
-
-        cy.on('cxttap', 'node', function (event)
-        {
-        });
-
-        cy.on('tap', 'node', function (event)
-        {
-        });
-
+      //add position information to data for preset layout
+      for (var i = 0; i < graphData.nodes.length; i++) {
+        var xPos = graphData.nodes[i].data.sbgnbbox.x;
+        var yPos = graphData.nodes[i].data.sbgnbbox.y;
+        positionMap[graphData.nodes[i].data.id] = {'x': xPos, 'y': yPos};
       }
-    });
-  });
+
+      var cy = window.cy = cytoscape(
+        {
+          container: document.getElementById('sbgnCanvas'),
+          elements:graphData,
+          style: sbgnStyleSheet,
+          layout:
+          {
+            /*name: 'preset',
+            positions: positionMap*/
+            name: "cose-bilkent",
+            animate: false,
+            randomize: true,
+            fit: true
+          },
+          showOverlay: false,
+          minZoom: 0.125,
+          maxZoom: 16,
+          boxSelectionEnabled: true,
+          motionBlur: true,
+          wheelSensitivity: 0.1,
+          ready: function ()
+          {
+            //refreshPaddings();
+
+            var panProps = ({
+              fitPadding: 10,
+            });
+            container.cytoscapePanzoom(panProps);
+
+            cy.on('mouseover', 'node', function (event) {
+            });
+
+            cy.on('cxttap', 'node', function (event)
+            {
+            });
+
+            cy.on('tap', 'node', function (event)
+            {
+            });
+
+          }
+        });
+      });
 
 
-}
+    }

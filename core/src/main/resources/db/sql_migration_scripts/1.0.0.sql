86a802ea2508c05cac90df8c6878e9da9d856197
Zachary Heins <zackheins@gmail.com>
2016-01-28 09:07:55 -0500

    Semicolon in mysql

--- core/src/main/resources/db/sql_migration_scripts/1.0.0.sql
+++ core/src/main/resources/db/sql_migration_scripts/1.0.0.sql
@@ -6,5 +6,5 @@ DROP TABLE micro_rna;
 DROP TABLE micro_rna_alteration;
 DROP TABLE mutation_frequency;
 ALTER TABLE sample_list_list CHANGE PATIENT_ID SAMPLE_ID INT(11);
-CREATE TABLE version (VERSION_NUMBER VARCHAR(8))
+CREATE TABLE version (VERSION_NUMBER VARCHAR(8));
 INSERT into version values ("1.0.0");

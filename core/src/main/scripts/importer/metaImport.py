2ba6975d9f7ba995b674be77bb1867afffb0a722
Fedde Schaeffer <fedde@thehyve.nl>
2016-01-29 16:13:12 +0100

    Flush log messages after validating in metaImport.py

--- core/src/main/scripts/importer/metaImport.py
+++ core/src/main/scripts/importer/metaImport.py
@@ -9,6 +9,7 @@ __author__ = 'priti'
 
 import sys
 import argparse
+import logging
 
 import validateData
 import cbioportalImporter
@@ -66,7 +67,14 @@ if __name__ == '__main__':
 
     # Validate the study directory.
     print >> sys.stderr, "Starting validation...\n\n\n"
-    exitcode = validateData.main_validate(args)
+    try:
+        exitcode = validateData.main_validate(args)
+    finally:
+        # make sure all log messages are flushed
+        validator_logger = logging.getLogger(validateData.__name__)
+        for log_handler in validator_logger.handlers:
+            log_handler.close()
+        validator_logger.handlers = []
 
     # Depending on validation results, load the study or notify the user
     print >> sys.stderr, "#" * 71

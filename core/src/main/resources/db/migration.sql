e6206da42b6bd277b2ae32dcb41ef904549f015b
Zack Heins <zackheins@gmail.com>
2016-02-09 15:22:58 -0500

    Update versions in sql migrate

--- core/src/main/resources/db/migration.sql
+++ core/src/main/resources/db/migration.sql
@@ -1,6 +1,6 @@
 ##version: 1.0.0
 CREATE TABLE info (DB_SCHEMA_VERSION VARCHAR(8));
-INSERT INTO info VALUES ("1");
+INSERT INTO info VALUES ("1.0.0");
 
 ##version: 1.1.0
 CREATE TABLE sample_list LIKE patient_list;
@@ -11,4 +11,4 @@ DROP TABLE micro_rna;
 DROP TABLE micro_rna_alteration;
 DROP TABLE mutation_frequency;
 ALTER TABLE sample_list_list CHANGE PATIENT_ID SAMPLE_ID INT(11);
-UPDATE info SET DB_SCHEMA_VERSION="2";
+UPDATE info SET DB_SCHEMA_VERSION="1.1.0";

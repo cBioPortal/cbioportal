25b90676d947ffb6e3257f40e7eb39d19c03ebdc
Zachary Heins <zackheins@gmail.com>
2016-02-15 13:17:53 -0500

    remove drop statments

--- core/src/main/resources/db/migration.sql
+++ core/src/main/resources/db/migration.sql
@@ -7,8 +7,5 @@ CREATE TABLE sample_list LIKE patient_list;
 INSERT sample_list SELECT * FROM patient_list;
 CREATE TABLE sample_list_list LIKE patient_list_list;
 INSERT sample_list_list SELECT * FROM patient_list_list;
-DROP TABLE micro_rna;
-DROP TABLE micro_rna_alteration;
-DROP TABLE mutation_frequency;
 ALTER TABLE sample_list_list CHANGE PATIENT_ID SAMPLE_ID INT(11);
 UPDATE info SET DB_SCHEMA_VERSION="1.1.0";

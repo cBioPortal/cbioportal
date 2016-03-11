c07af52d6445c57c28f67d9c2ee946d47c0f454f
yichaoS <yichao@cbio.mskcc.org>
2016-03-11 16:05:38 -0500

    get rid of the ghost LinkOut

--- core/src/main/java/org/mskcc/cbio/portal/servlet/QueryBuilder.java
+++ core/src/main/java/org/mskcc/cbio/portal/servlet/QueryBuilder.java
@@ -78,24 +78,16 @@ public class QueryBuilder extends HttpServlet {
     public static final String SET_OF_CASE_IDS = "set_of_case_ids";
     public static final String CLINICAL_PARAM_SELECTION = "clinical_param_selection";
     public static final String GENE_LIST = "gene_list";
-    public static final String RAW_GENE_STR = "raw_gene_str";
     public static final String ACTION_NAME = "Action";
-    public static final String OUTPUT = "output";
-    public static final String FORMAT = "format";
-    public static final String PLOT_TYPE = "plot_type";
-    public static final String OS_SURVIVAL_PLOT = "os_survival_plot";
-    public static final String DFS_SURVIVAL_PLOT = "dfs_survival_plot";
     public static final String XDEBUG = "xdebug";
     public static final String ACTION_SUBMIT = "Submit";
     public static final String STEP1_ERROR_MSG = "step1_error_msg";
     public static final String STEP2_ERROR_MSG = "step2_error_msg";
     public static final String STEP3_ERROR_MSG = "step3_error_msg";
     public static final String STEP4_ERROR_MSG = "step4_error_msg";
-    public static final String MERGED_PROFILE_DATA_INTERNAL = "merged_profile_data";
     public static final String PROFILE_DATA_SUMMARY = "profile_data_summary";
     public static final String WARNING_UNION = "warning_union";
     public static final String DOWNLOAD_LINKS = "download_links";
-    public static final String NETWORK = "network";
     public static final String HTML_TITLE = "html_title";
     public static final String TAB_INDEX = "tab_index";
     public static final String TAB_DOWNLOAD = "tab_download";
@@ -118,7 +110,6 @@ public class QueryBuilder extends HttpServlet {
     public static final String DB_ERROR = "db_error";
     private static final String DB_CONNECT_ERROR = ("An error occurred while trying to connect to the database." +
                                                     "  This could happen if the database does not contain any cancer studies.");
-
 
     private static Log LOG = LogFactory.getLog(QueryBuilder.class);
 
@@ -169,6 +160,7 @@ public class QueryBuilder extends HttpServlet {
     protected void doPost(HttpServletRequest httpServletRequest,
                           HttpServletResponse httpServletResponse) throws ServletException,
             IOException {
+        
         XDebug xdebug = new XDebug( httpServletRequest );
         xdebug.startTimer();
 
@@ -191,14 +183,10 @@ public class QueryBuilder extends HttpServlet {
         HashSet<String> geneticProfileIdSet = getGeneticProfileIds(httpServletRequest, xdebug);
 
         //  Get User Defined Gene List
-	    // we need the raw gene list...
 	    String geneList = httpServletRequest.getParameter(GENE_LIST);
-
-	    if (httpServletRequest instanceof XssRequestWrapper)
-	    {
+	    if (httpServletRequest instanceof XssRequestWrapper) {
 		    geneList = ((XssRequestWrapper)httpServletRequest).getRawParameter(GENE_LIST);
 	    }
-
         geneList = servletXssUtil.getCleanInput(geneList);
 
         //  Get all Cancer Types
@@ -247,7 +235,6 @@ public class QueryBuilder extends HttpServlet {
          
             httpServletRequest.setAttribute(XDEBUG_OBJECT, xdebug);
             
-            // Get the db version
             String dbPortalVersion = GlobalProperties.getDbVersion();
             String dbVersion = DaoInfo.getVersion();
             LOG.info("version - "+dbPortalVersion);
@@ -257,7 +244,7 @@ public class QueryBuilder extends HttpServlet {
                 httpServletRequest.setAttribute(DB_ERROR, "Current DB Version: " + dbVersion + "<br/>" + "Portal DB Version: " + dbPortalVersion);
             }
 
-            boolean errorsExist = validateForm(action, profileList, geneticProfileIdSet, geneList,
+            boolean errorsExist = validateForm(action, profileList, geneticProfileIdSet,
                                                sampleSetId, sampleIds, httpServletRequest);
             if (action != null && action.equals(ACTION_SUBMIT) && (!errorsExist)) {
 
@@ -357,8 +344,6 @@ public class QueryBuilder extends HttpServlet {
         request.setAttribute(GENE_LIST, geneList);
         
         request.setAttribute(PATIENT_CASE_SELECT, patientCaseSelect);
-
-        xdebug.logMsg(this, "Using gene list geneList.toString():  " + geneList.toString());
         
         HashSet<String> setOfSampleIds = null;
         
@@ -452,7 +437,6 @@ public class QueryBuilder extends HttpServlet {
                continue;
             } 
          
-            xdebug.logMsg(this, "Getting data for:  " + profile.getProfileName());
             GetProfileData remoteCall =
               new GetProfileData(profile, geneList, StringUtils.join(setOfSampleIds, " "));
             ProfileData pData = remoteCall.getProfileData();
@@ -460,18 +444,6 @@ public class QueryBuilder extends HttpServlet {
                 remoteCall.getRawContent());
             downloadLinkSet.add(downloadLink);
             warningUnion.addAll(remoteCall.getWarnings());
-            if( pData == null ){
-               System.err.println( "pData == null" );
-            }else{
-               if( pData.getGeneList() == null ){
-                  System.err.println( "pData.getValidGeneList() == null" );
-               }
-            }
-            if (pData != null) {
-                xdebug.logMsg(this, "Got number of genes:  " + pData.getGeneList().size());
-                xdebug.logMsg(this, "Got number of cases:  " + pData.getCaseIdList().size());
-            }
-            xdebug.logMsg(this, "Number of warnings received:  " + remoteCall.getWarnings().size());
             profileDataList.add(pData);
 
             //  Optionally, get Extended Mutation Data.
@@ -505,53 +477,22 @@ public class QueryBuilder extends HttpServlet {
             xdebug.logMsg(this, "Merging Profile Data");
             ProfileMerger merger = new ProfileMerger(profileDataList);
             ProfileData mergedProfile = merger.getMergedProfile();
-
-            xdebug.logMsg(this, "Merged Profile, Number of genes:  "
-                    + mergedProfile.getGeneList().size());
-            ArrayList<String> mergedProfileGeneList = mergedProfile.getGeneList();
-            for (String currentGene:  mergedProfileGeneList) {
-                xdebug.logMsg(this, "Merged Profile Gene:  " + currentGene);
-            }
-            xdebug.logMsg(this, "Merged Profile, Number of cases:  "
-                    + mergedProfile.getCaseIdList().size());
-            request.setAttribute(MERGED_PROFILE_DATA_INTERNAL, mergedProfile);
             request.setAttribute(WARNING_UNION, warningUnion);
 
-            String output = request.getParameter(OUTPUT);
-            String format = request.getParameter(FORMAT);
             double zScoreThreshold = ZScoreUtil.getZScore(geneticProfileIdSet, profileList, request);
             double rppaScoreThreshold = ZScoreUtil.getRPPAScore(request);
             request.setAttribute(Z_SCORE_THRESHOLD, zScoreThreshold);
             request.setAttribute(RPPA_SCORE_THRESHOLD, rppaScoreThreshold);
 
-            if (output != null) {
-				if (output.equals("text")) {
-                    outputPlainText(response, mergedProfile, theOncoPrintSpecParserOutput,
-                            zScoreThreshold, rppaScoreThreshold);
-                }
-            } else {
-
-                // Store download links in session (for possible future retrieval).
-                request.getSession().setAttribute(DOWNLOAD_LINKS, downloadLinkSet);
-                RequestDispatcher dispatcher =
-                        getServletContext().getRequestDispatcher("/WEB-INF/jsp/visualize.jsp");
-                dispatcher.forward(request, response);
-            }
+            // Store download links in session (for possible future retrieval).
+            request.getSession().setAttribute(DOWNLOAD_LINKS, downloadLinkSet);
+            RequestDispatcher dispatcher =
+                    getServletContext().getRequestDispatcher("/WEB-INF/jsp/visualize.jsp");
+            dispatcher.forward(request, response);
         } else {
             ShowData.showDataAtSpecifiedIndex(servletContext, request,
                     response, 0, xdebug);
         }
-    }
-
-    private void outputPlainText(HttpServletResponse response, ProfileData mergedProfile,
-            ParserOutput theOncoPrintSpecParserOutput, double zScoreThreshold, double rppaScoreThreshold) throws IOException {
-        response.setContentType("text/plain");
-        ProfileDataSummary dataSummary = new ProfileDataSummary( mergedProfile,
-                theOncoPrintSpecParserOutput.getTheOncoPrintSpecification(), zScoreThreshold, rppaScoreThreshold );
-        PrintWriter writer = response.getWriter();
-        writer.write("" + dataSummary.getPercentCasesAffected());
-        writer.flush();
-        writer.close();
     }
 
 	private void redirectStudyUnavailable(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -567,7 +508,7 @@ public class QueryBuilder extends HttpServlet {
     private boolean validateForm(String action,
                                 ArrayList<GeneticProfile> profileList,
                                  HashSet<String> geneticProfileIdSet,
-                                 String geneList, String sampleSetId, String sampleIds,
+                                 String sampleSetId, String sampleIds,
                                  HttpServletRequest httpServletRequest) throws DaoException {
         boolean errorsExist = false;
         String tabIndex = httpServletRequest.getParameter(QueryBuilder.TAB_INDEX);
@@ -638,8 +579,6 @@ public class QueryBuilder extends HttpServlet {
                 	}
                 }
 
-                //errorsExist = validateGenes(geneList, httpServletRequest, errorsExist);
-
                 //  Additional validation rules
                 //  If we have selected mRNA Expression Data Check Box, but failed to
                 //  select an mRNA profile, this is an error.
@@ -665,22 +604,10 @@ public class QueryBuilder extends HttpServlet {
                 }
             }
         } 
-        if( errorsExist ){
-           httpServletRequest.setAttribute( GENE_LIST, geneList );
-       }
+
         return errorsExist;
     }
 
-//    private boolean validateGenes(String geneList, HttpServletRequest httpServletRequest,
-//            boolean errorsExist) throws DaoException {
-//        try {
-//            GeneValidator geneValidator = new GeneValidator(geneList);
-//        } catch (GeneValidationException e) {
-//            errorsExist = true;
-//            httpServletRequest.setAttribute(STEP4_ERROR_MSG, e.getMessage());
-//        }
-//        return errorsExist;
-//    }
     
     private void forwardToErrorPage(HttpServletRequest request, HttpServletResponse response,
                                     String userMessage, XDebug xdebug)

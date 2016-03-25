c2af44c7e534f05e157fe3e2ea6e233567c7b2ad
pieter <pieter@thehyve.nl>
2016-03-25 14:54:54 +0100

    changes to DB for 1.2.0

--- core/src/main/resources/db/migration.sql
+++ core/src/main/resources/db/migration.sql
@@ -9,3 +9,9 @@ CREATE TABLE sample_list_list LIKE patient_list_list;
 INSERT sample_list_list SELECT * FROM patient_list_list;
 ALTER TABLE sample_list_list CHANGE PATIENT_ID SAMPLE_ID INT(11);
 UPDATE info SET DB_SCHEMA_VERSION="1.1.0";
+
+##version: 1.2.0
+ALTER TABLE cna_event AUTO_INCREMENT=1;
+UPDATE info SET DB_SCHEMA_VERSION="1.2.0";
+ALTER TABLE mutation add UNIQUE KEY `UQ_MUTATION_EVENT_ID_GENETIC_PROFILE_ID_SAMPLE_ID` (`MUTATION_EVENT_ID`,`GENETIC_PROFILE_ID`,`SAMPLE_ID`) COMMENT 'Constraint to block duplicated mutation entries.';
+ALTER TABLE sample_profile add UNIQUE KEY `UQ_SAMPLE_ID_GENETIC_PROFILE_ID` (`SAMPLE_ID`,`GENETIC_PROFILE_ID`) COMMENT 'Constraint to allow each sample only once in each profile.';

5052fb61beeb496ae57636300b3f6a551b793240
pieter <pieter@thehyve.nl>
2016-04-07 11:31:39 +0200

    no longer allowing negative entrez id

--- core/src/main/java/org/mskcc/cbio/portal/scripts/ImportExtendedMutationData.java
+++ core/src/main/java/org/mskcc/cbio/portal/scripts/ImportExtendedMutationData.java
@@ -249,7 +249,7 @@ public class ImportExtendedMutationData{
                                 
 				CanonicalGene gene = null;
 				if (record.getGivenEntrezGeneId().trim().length() > 0) {
-					if (!record.getGivenEntrezGeneId().matches("-?[0-9]+")) {
+					if (!record.getGivenEntrezGeneId().matches("[0-9]+")) {
 		            	ProgressMonitor.logWarning("Ignoring line with invalid Entrez_Id " + record.getGivenEntrezGeneId());
 		            	entriesSkipped++;
 				    	continue;

4f7d7a6f53465ab29a6a488193f71e52c579e7cb
Marcin Stachniuk <stachnim@users.noreply.github.com>
2016-04-18 21:10:33 +0200

    Update test_make_screenshots.sh
    Remove unnecessary upload of image

--- test/end-to-end/test_make_screenshots.sh
+++ test/end-to-end/test_make_screenshots.sh
@@ -21,9 +21,6 @@ git diff --quiet -- ${DIR}/screenshots/patient_view_lgg_ucsf_2014_case_id_P04.pn
 phantomjs --ignore-ssl-errors=true --web-security=false ${DIR}/make_screenshot.js 'http://localhost:8080/study.do?cancer_study_id=lgg_ucsf_2014' \
                               "${DIR}/screenshots/study_view_lgg_ucsf_2014.png" \
                               5000
-
-# upload the second image
-curl -F "clbin=@${DIR}/screenshots/study_view_lgg_ucsf_2014.png" https://clbin.com							  
 							  
 # make sure screenshot is still the same as the one in the repo, if not upload
 # the image

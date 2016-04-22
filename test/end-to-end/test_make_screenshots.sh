f3c6b50fd1861ae6471c51e9ca30a54e0f0ea88f
Ino de Bruijn <ino@ino.pm>
2016-04-22 14:58:41 -0400

    Add screenshot test documentation for devs
    - More verbose output of the screenshot test
    - Add documentation to CONTRIBUTING.md about how to fix failing screenshot test
    - Increase screenshot timout to 10s and screenshot test start to 20s delay
    
    Signed-off-by: Ino de Bruijn <ino@ino.pm>

--- test/end-to-end/test_make_screenshots.sh
+++ test/end-to-end/test_make_screenshots.sh
@@ -1,25 +1,64 @@
 #!/usr/bin/env bash
-# halt on error
-set -e
 
-# make sure screenshot is still the same as the one in the repo, if not upload
-# the image
-upload_screenshot_if_different() {
-    git diff --quiet -- $1 || \
-        (echo "screenshot differs see:" && curl -F "clbin=@$1" https://clbin.com && exit 1)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+compare_image() {
+    local url=${1}
+    local png=${2}
+    local TIMEOUT=10000
+
+    phantomjs --ignore-ssl-errors=true make_screenshot.js $url $png $TIMEOUT && \
+        git diff --quiet -- $png
+    return $?
 }
+
+upload_image() {
+    local png=${1}
+
+    curl -s -F "clbin=@$png" https://clbin.com
+}
+
+# screenshots and the urls where they were taken
+declare -A screenshot_urls=(
+    ["screenshots/patient_view_lgg_ucsf_2014_case_id_P04.png"]="http://localhost:8080/case.do?cancer_study_id=lgg_ucsf_2014&case_id=P04"
+    ["screenshots/study_view_lgg_ucsf_2014.png"]="http://localhost:8080/study.do?cancer_study_id=lgg_ucsf_2014"
+)
 
 # dir of bash script http://stackoverflow.com/questions/59895
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# move to test dir
+cd ${DIR}
 
-# patient view screenshot
-phantomjs --ignore-ssl-errors=true ${DIR}/make_screenshot.js 'http://localhost:8080/case.do?cancer_study_id=lgg_ucsf_2014&case_id=P04' \
-                              "${DIR}/screenshots/patient_view_lgg_ucsf_2014_case_id_P04.png" \
-                              5000
-upload_screenshot_if_different ${DIR}/screenshots/patient_view_lgg_ucsf_2014_case_id_P04.png
+# make screenshots and compare against image in repo
+screenshot_error_count=0
+screenshots_failed=()
+echo "Running screenshot tests"
+for png in "${!screenshot_urls[@]}"; do
+    url=${screenshot_urls[$png]}
+    echo -e "Checking ${DIR}/${png} at $url..."
+    compare_image $url $png
+    # check exit code
+    if [[ $? -gt 0 ]]; then
+        echo -e "$RED FAILED${NC}"
+        screenshot_error_count=$(($screenshot_error_count + 1))
+        screenshots_failed=( ${screenshots_failed[$@]} $png )
+    else
+        echo -e "$GREEN SUCCESS${NC}"
+    fi
+done
 
-# studie view screenshot
-phantomjs --ignore-ssl-errors=true --web-security=false ${DIR}/make_screenshot.js 'http://localhost:8080/study.do?cancer_study_id=lgg_ucsf_2014' \
-                              "${DIR}/screenshots/study_view_lgg_ucsf_2014.png" \
-                              5000
-upload_screenshot_if_different ${DIR}/screenshots/study_view_lgg_ucsf_2014.png
+# show dev how to download failing test screenshots
+if [[ $screenshot_error_count -gt 0 ]]; then
+    echo -e "${RED}${screenshot_error_count} SCREENSHOT TESTS FAILED!${NC}"
+    echo -e "FOR STEPS TO SEE IMAGE DIFF ${RED}READ CONTRIBUTING.md${NC}"
+    echo "TO DOWNLOAD FAILING SCREENSHOTS TO LOCAL REPO ROOT RUN:"
+    for png in "${screenshots_failed[$@]}"; do
+        echo "curl '"$(upload_image $png)"' > test/end-to-end/${png}"
+    done
+    exit 1
+else
+    echo -e "${GREEN}SCREENSHOT TESTS SUCCEEDED${NC}"
+    exit 0
+fi

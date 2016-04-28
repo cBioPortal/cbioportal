c94109430b5a11b8e791685062f7074761364e65
Robert Sheridan <sheridan@cbio.mskcc.org>
2016-04-28 11:28:44 -0400

    Revert "Drop dead code"
    This reverts commit 6f6efb7088e3937683f36bfcbbbb40411b6f4519.

--- portal/src/main/webapp/js/lib/mutationMapper.js
+++ portal/src/main/webapp/js/lib/mutationMapper.js
@@ -6528,6 +6528,7 @@ var MutationModel = Backbone.Model.extend({
 		this.proteinPosStart = attributes.proteinPosStart;
 		this.proteinPosEnd = attributes.proteinPosEnd;
 		this.mutationCount = attributes.mutationCount;
+		this.specialGeneData = attributes.specialGeneData;
 		this.keyword = attributes.keyword;
 		this.cna = attributes.cna;
 		this.myCancerGenome = attributes.myCancerGenome;

56b9151d81c42382aa4696905819e058ae575f2b
Fedde Schaeffer <fedde@thehyve.nl>
2016-05-17 09:12:42 +0200

    Fully remove the ConsoleUtil.quitWithUsageLine function

--- core/src/main/java/org/mskcc/cbio/portal/util/ConsoleUtil.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/ConsoleUtil.java
@@ -116,32 +116,6 @@ public class ConsoleUtil {
         DecimalFormat formatter = new DecimalFormat("#,###,###.###");
         return formatter.format(mBytes) + " MB";
     }
-    
-    
-	/**
-	 * Simple utility method to quit a command line script with helpful message and 
-	 * extra usageLine info.
-	 * 
-	 * @param msg: a final error/exit message
-	 * @param usageLine: usage line intro for the current cmd (no need to include 
-	 * 		description of the parameters as these are found in the give parser and
-	 * 		are automatically printed out as well 
-	 * @param parser: the parser that parsed the given cmd line options
-	 */
-    public static void quitWithUsageLine(String msg, String usageLine, OptionParser parser)
-    {
-        if( null != msg ){
-            System.err.println( msg );
-        }
-        System.err.println( usageLine );
-        try {
-            parser.printHelpOn(System.err);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //use 2 for command line syntax errors:
-        System.exit(2);
-    }
 
     /**
      * Default method to be used when Importer class main method expects only 'data' and 'meta' as mandatory options 
@@ -226,31 +200,32 @@ public class ConsoleUtil {
 		OptionSpec<Void> help = parser.accepts( "help", "print this help info" );
 		parser.accepts( "data", "profile data file" ).withRequiredArg().describedAs( "data_file.txt" ).ofType( String.class );
 		parser.accepts( "study", "cancer study identifier" ).withRequiredArg().describedAs( "e.g. brca_tcga" ).ofType( String.class );
-		
-		String usageLine = description + "\nCommand line usage:\n";
-		
+
+        String progName = "importScript";
+
 		OptionSet options = null;
 		try {
 			options = parser.parse( args );
 		} catch (OptionException e) {
-			quitWithUsageLine(e.getMessage(), usageLine, parser);
+            throw new UsageException(progName, description, parser,
+                    e.getMessage());
 		}
-		  
+
 		if( options.has( help ) ){
-			quitWithUsageLine("", usageLine, parser);
+		    throw new UsageException(progName, description, parser);
 		}
 		//these extra checks are needed, since withRequiredArg above only indicated that the option 
 		//has a mandatory argument but does not make the option itself mandatory.
 		if(!options.has("data")) {
-			quitWithUsageLine("Error: 'data' argument required.", usageLine, parser);
+            throw new UsageException(progName, description, parser,
+                    "Error: 'data' argument required.");
 		}
-		
+
 		if(!options.has("study")) {
-			quitWithUsageLine("Error: 'study' argument required.", usageLine, parser);
+            throw new UsageException(progName, description, parser,
+                    "Error: 'study' argument required.");
 		}
-		
+
 		return options;
 	}
-    
 }
-

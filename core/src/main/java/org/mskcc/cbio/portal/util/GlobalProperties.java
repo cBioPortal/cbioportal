2be537c626cf48e212c486c5e7e2f7376ed57083
zheins <zackheins@gmail.com>
2016-06-27 16:47:41 -0400

    Allow tabs to be disabled on the visualize.jsp page
    Signed-off-by: zheins <zackheins@gmail.com>

--- core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
@@ -192,6 +192,8 @@ public class GlobalProperties {
     
     public static final String DB_VERSION = "db.version";
     
+    public static final String DISABLED_TABS = "disabled_tabs";
+    
     private static Log LOG = LogFactory.getLog(GlobalProperties.class);
     private static Properties properties = initializeProperties();
 
@@ -715,6 +717,17 @@ public class GlobalProperties {
         return version;
     }
     
+    public static List<String> getDisabledTabs() {
+        String disabledTabs = "";
+        try {
+            disabledTabs = properties.getProperty(DISABLED_TABS).trim();
+        }
+        catch (NullPointerException e) {}
+        
+        String[] tabs = disabledTabs.split("\\|");
+        return (tabs.length > 0 && disabledTabs.length() > 0) ? Arrays.asList(tabs) : new ArrayList<String>();
+    }
+
     public static void main(String[] args)
     {
         System.out.println(getAppVersion());

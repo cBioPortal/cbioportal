23e21ad86d359996050579004dc509fd62e330d8
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2016-07-12 16:24:48 -0400

    change action method back to post

--- portal/src/main/webapp/js/src/study-view/util/StudyViewBoilerplate.js
+++ portal/src/main/webapp/js/src/study-view/util/StudyViewBoilerplate.js
@@ -256,7 +256,7 @@ var StudyViewBoilerplate ={
         // Build query button
         _form
             .attr({
-                method: "get",
+                method: "post",
                 action: "index.do",
                 target: "_blank"
             })

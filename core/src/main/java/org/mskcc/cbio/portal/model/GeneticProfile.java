8f1fde9bb5d3f33d86ec511a99454ea0b534044f
Fedde Schaeffer <fedde@thehyve.nl>
2016-07-20 10:11:07 +0200

    Read optional flags in metadata files when loading
    Allowing particular loader classes to parse and use them as appropriate
    for their data types.

--- core/src/main/java/org/mskcc/cbio/portal/model/GeneticProfile.java
+++ core/src/main/java/org/mskcc/cbio/portal/model/GeneticProfile.java
@@ -32,6 +32,8 @@
 
 package org.mskcc.cbio.portal.model;
 
+import java.util.Properties;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
@@ -47,6 +49,7 @@ public class GeneticProfile {
     private String profileDescription;
     private String targetLine;
     private boolean showProfileInAnalysisTab;
+    private Properties otherMetadataFields;
 
     public GeneticProfile() {
       super();
@@ -54,7 +57,7 @@ public class GeneticProfile {
 
    public GeneticProfile(String stableId, int cancerStudyId, GeneticAlterationType geneticAlterationType,
 						 String datatype, String profileName, String profileDescription, boolean showProfileInAnalysisTab) {
-      super();
+      this();
       this.stableId = stableId;
       this.cancerStudyId = cancerStudyId;
       this.geneticAlterationType = geneticAlterationType;
@@ -135,10 +138,33 @@ public class GeneticProfile {
     public void setShowProfileInAnalysisTab(boolean showProfileInAnalysisTab) {
         this.showProfileInAnalysisTab = showProfileInAnalysisTab;
     }
-    
+
+    /**
+     * Stores metadata fields only recognized in particular data file types.
+     *
+     * @param fields  a properties instance holding the keys and values
+     */
+    public void setOtherMetadataFields(Properties fields) {
+        this.otherMetadataFields = fields;
+    }
+
+    /**
+     * Retrieves metadata fields specific to certain data file types.
+     *
+     * @param fieldname  the name of the field to retrieve
+     * @return  the value of the field or null
+     */
+    public String getOtherMetaDataField(String fieldname) {
+        if (otherMetadataFields == null) {
+            return null;
+        } else {
+            return otherMetadataFields.getProperty(fieldname);
+        }
+    }
+
     @Override
     public String toString() {
        return ToStringBuilder.reflectionToString(this);
     }
-    
+
 }

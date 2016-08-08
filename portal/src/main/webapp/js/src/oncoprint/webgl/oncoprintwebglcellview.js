39b3a268cecd5fa218ddcb03d692283e5c0a1711
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2016-08-08 17:54:40 -0400

    put extractRGBA in different file in oncoprint, and fix GradientRuleSet color range

--- portal/src/main/webapp/js/src/oncoprint/webgl/oncoprintwebglcellview.js
+++ portal/src/main/webapp/js/src/oncoprint/webgl/oncoprintwebglcellview.js
@@ -1,5 +1,6 @@
 var gl_matrix = require('gl-matrix');
 var svgfactory = require('./svgfactory.js');
+var extractRGBA = require('./extractrgba.js');
 
 // TODO: antialiasing
 
@@ -32,24 +33,6 @@ var getWebGLCanvasContext = function (view) {
     }
 };
 
-var extractRGBA = function (str) {
-    var ret = [0, 0, 0, 1];
-    if (str[0] === "#") {
-	// hex, convert to rgba
-	var r = parseInt(str[1] + str[2], 16);
-	var g = parseInt(str[3] + str[4], 16);
-	var b = parseInt(str[5] + str[6], 16);
-	str = 'rgba('+r+','+g+','+b+',1)';
-    }
-    var match = str.match(/^[\s]*rgba\([\s]*([0-9]+)[\s]*,[\s]*([0-9]+)[\s]*,[\s]*([0-9]+)[\s]*,[\s]*([0-9.]+)[\s]*\)[\s]*$/);
-    if (match && match.length === 5) {
-	ret = [parseFloat(match[1]) / 255,
-	    parseFloat(match[2]) / 255,
-	    parseFloat(match[3]) / 255,
-	    parseFloat(match[4])];
-    }
-    return ret;
-};
 var createShaderProgram = function (view, vertex_shader, fragment_shader) {
     var program = view.ctx.createProgram();
     view.ctx.attachShader(program, vertex_shader);

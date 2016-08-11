0c28be2a43a2dcad0dd098ce641339308e769043
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2016-08-11 14:11:59 -0400

    find gene by hugo symbol
    find gene list by hugo symbols
    
    add length

---
+++ service/src/main/java/org/cbioportal/service/impl/GeneServiceImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cbioportal.service.impl;
+
+import java.util.List;
+import org.cbioportal.model.Gene;
+import org.cbioportal.persistence.GeneRepository;
+import org.cbioportal.service.GeneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author jiaojiao
+ */
+@Service
+public class GeneServiceImpl implements GeneService{
+    @Autowired
+    private GeneRepository geneRepository;
+    public Gene getGeneByHugoSymbol(String hugoSymbol){
+        return geneRepository.getGeneByHugoSymbol(hugoSymbol);
+    }
+    
+    public List<Gene> getGeneListByHugoSymbols(List<String> hugoSymbols){
+        return geneRepository.getGeneListByHugoSymbols(hugoSymbols);
+    }
+}

271b3b95da55f41940e9fba5f79d3e8e27c38321
JiaoJiao123 <jiaojiaowanghere@gmail.com>
2016-08-16 16:28:54 -0400

    add gene controller seperately

---
+++ web/src/main/java/org/cbioportal/web/GeneController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cbioportal.web;
+
+import java.util.List;
+import org.cbioportal.model.Gene;
+import org.cbioportal.service.GeneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jiaojiao
+ */
+@RestController
+public class GeneController {
+    @Autowired
+    private GeneService geneService;
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/geneListByHugoSymbols")
+    public List<Gene> getGeneListByHugoSymbols(@RequestParam(required = true) List<String> hugoSymbols){
+        return geneService.getGeneListByHugoSymbols(hugoSymbols);
+    }
+}

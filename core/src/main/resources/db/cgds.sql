10545384311d074bfa86c3a43babcfd3a494cdc2
Robert Sheridan <sheridan@cbio.mskcc.org>
2016-10-19 12:10:56 -0400

    correct comment syntax in mysql scripts

--- core/src/main/resources/db/cgds.sql
+++ core/src/main/resources/db/cgds.sql
@@ -25,7 +25,7 @@
 --
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
--------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
 --
 -- Copyright (c) 2016 Memorial Sloan-Kettering Cancer Center.
 --

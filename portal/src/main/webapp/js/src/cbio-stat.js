6ea0998c2f42ae24f37b4866d3268ba98611bbba
Sun <suny1@lski1780.mskcc.org>
2016-10-21 15:30:17 -0400

    installed mocha and chai; set up spec for testing cbio statistics (cbio-stat.js); module exports from cbio-stat

--- portal/src/main/webapp/js/src/cbio-stat.js
+++ portal/src/main/webapp/js/src/cbio-stat.js
@@ -1,54 +1,58 @@
+var _ = require('underscore')._;
+
 if (cbio === undefined) {
     var cbio = {};
 }
 
-cbio.stat = (function () {
+cbio.stat = (function() {});
 
-    /*
-     *
-     * From TCGA:
-     * A z-score for a sample indicates the number of standard deviations away 
-     * from the mean of expression in the reference.  The formula is : 
-     * z = (expression in tumor sample - mean expression in reference sample) / 
-     * standard deviation of expression in reference sample
-     * The reference here is all the diploid samples under a study.
-     * 
-     * @param _ref: mrna expression data of all diploid samples under queried study
-     * @param _input: mrna expression data of all queried samples under queried study
-     * @return _zscoreArr: array of zscore of the _input array
-     * 
-     */
-    function zscore(_ref, _input) {
+// mean (µ)
+cbio.stat.prototype.mean = function(_inputArr) {
+    var _sum = 0;
+    _.each(_inputArr, function(_inputElem) {
+        _sum += _inputElem;
+    });
+    return _sum / _inputArr.length;
+}
 
-        // mean (µ)
-        var _refSum = 0;
-        _.each(_ref, function (_num) {
-            _refSum += _num;
-        });
-        var _mean = _refSum / _ref.length;
+// standard deviation (σ)
+cbio.stat.prototype.stDev = function(_inputArr) {
+    var _mean = cbio.stat.prototype.mean(_inputArr);
+    var _squaredRef = _.map(_inputArr, function (_inputElem) {
+        return Math.pow((_inputElem - _mean), 2);
+    });
+    var _squaredRefSum = 0;
+    _.each(_squaredRef, function (_num_stDev) {
+        _squaredRefSum += _num_stDev;
+    });
+    return Math.sqrt(_squaredRefSum / _inputArr.length - 1);
+}
 
-        // standard deviation (σ)
-        var _squaredRef = _.map(_ref, function (_num) {
-            return Math.pow((_num - _mean), 2);
-        });
-        var _squaredRefSum = 0;
-        _.each(_squaredRef, function (_num_stDev) {
-            _squaredRefSum += _num_stDev;
-        });
-        var _stDev = Math.sqrt(_squaredRefSum / _ref.length - 1);
+// z score
+/*
+ * From TCGA:
+ * A z-score for a sample indicates the number of standard deviations away 
+ * from the mean of expression in the reference.  The formula is : 
+ * z = (expression in tumor sample - mean expression in reference sample) / 
+ * standard deviation of expression in reference sample
+ * The reference here is all the diploid samples under a study.
+ * 
+ * @param _ref: mrna expression data of all diploid samples under queried study
+ * @param _input: mrna expression data of all queried samples under queried study
+ * @return _zscoreArr: array of zscore of the _input array
+ */
 
-        // z = (x - u) / σ
-        var _zscoreArr = [];
-        _.each(_input, function (_num) {
-            _zscoreArr.push((_num - _mean) / _stDev);
-        });
+cbio.stat.prototype.zscore = function(_refArr, _inputArr) {
+    var _refMean = cbio.stat.prototype.mean(_refArr);
+    var _refStDev = cbio.stat.prototype.stDev(_refArr);
 
-        return _zscoreArr;
-    }
+    // z = (x - u) / σ
+    var _zscoreArr = [];
+    _.each(_inputArr, function (_inputElem) {
+        _zscoreArr.push((_inputElem - _refMean) / _refStDev);
+    });
+    
+    return _zscoreArr;
+}
 
-    return {
-        zscore: zscore
-    };
-
-})();
-
+module.exports = cbio.stat;

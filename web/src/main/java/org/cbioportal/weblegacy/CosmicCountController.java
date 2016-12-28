da7d2bf8383749a2648c3d9e2a30b6291bdb5b42
shivanshsoni <shivanshsoni23@gmail.com>
2016-12-28 20:13:58 +0530

    just shorten the code
    make the code small and easy to understand

--- web/src/main/java/org/cbioportal/weblegacy/CosmicCountController.java
+++ web/src/main/java/org/cbioportal/weblegacy/CosmicCountController.java
@@ -1,10 +1,7 @@
 package org.cbioportal.weblegacy;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 

d886dbc55c177e7fa3df6abd539633b9dd53adf7
Peter Kok <peter@thehyve.nl>
2017-01-12 13:38:36 +0100

    Workaround for maximum url length

--- portal/src/main/webapp/js/src/civic/civicservice.js
+++ portal/src/main/webapp/js/src/civic/civicservice.js
@@ -1,55 +1,101 @@
 
 function CivicService() {
     
+    // All results are stored here
+    var civicGenes = {};
+    
+    function getCivicGenesBatch(ids) {
+        return $.ajax({
+            type: 'GET',
+            url: 'api-legacy/proxy/civic/genes/' + ids,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: {
+                identifier_type: 'entrez_symbol'
+            }
+        }).then(function(result) {
+            if (_.isString(result)) {
+                result = $.parseJSON(result);
+            }
+
+            // Wrap the result in an Array if it is not already one
+            if (!(result instanceof Array)) {
+                result = [result];
+            }
+            if (result) {
+                result.forEach(function(record) {
+                    var civicGene = {
+                        id: record.id,
+                        name: record.name,
+                        description: record.description,
+                        url: 'https://civic.genome.wustl.edu/#/events/genes/'
+                        + record.id + '/summary',
+                        variants: {}
+                    };
+                    civicGenes[record.name] = civicGene;
+                    if (record.variants && record.variants.length > 0) {
+                        var variants = civicGene.variants;
+                        record.variants.forEach(function(variant) {
+                            variants[variant.name] = {
+                                id: variant.id,
+                                name: variant.name,
+                                geneId: civicGene.id
+                            };
+                        });
+                    }
+                });
+            }
+            return civicGenes;
+        });
+    }
+    
+    
     var service = {
         
         getCivicGenes: function(geneSymbols) {
-            var ids = geneSymbols.join(',');
-            return $.ajax({
-                type: 'GET',
-                url: 'api-legacy/proxy/civic/genes/' + ids,
-                dataType: 'json',
-                contentType: 'application/json',
-                data: {
-                    identifier_type: 'entrez_symbol'
-                }
-            }).then(function(result) {
-                if (_.isString(result)) {
-                    result = $.parseJSON(result);
+
+            // Assemble a list of promises, each of which will retrieve a batch of genes
+            var promises = [];
+            var ids = '';
+            geneSymbols.forEach(function(geneSymbol) {
+                // Check if we already have it in the cache
+                if (civicGenes.hasOwnProperty(geneSymbol)) {
+                    return;
                 }
 
-                var civicGenes = {};
-                // Wrap the result in an Array if it is not already one
-                if (!(result instanceof Array)) {
-                    result = [result];
-                } 
-                if (result) {
-                    result.forEach(function(record) {
-                        var civicGene = {
-                            id: record.id,
-                            name: record.name,
-                            description: record.description,
-                            url: 'https://civic.genome.wustl.edu/#/events/genes/'
-                            + record.id + '/summary',
-                            variants: {}
-                        };
-                        civicGenes[record.name] = civicGene;
-                        if (record.variants && record.variants.length > 0) {
-                            var variants = civicGene.variants;
-                            record.variants.forEach(function(variant) {
-                                variants[variant.name] = {
-                                    id: variant.id,
-                                    name: variant.name,
-                                    geneId: civicGene.id
-                                };
-                            });
-                        }
-                    });
+                // Add the symbol to the list
+                if (ids.length > 0) {
+                    ids += ',';
                 }
+                ids += geneSymbol;
+
+                // To prevent the request from growing too large, we send it off
+                // when it reaches this limit and start a new one
+                if (ids.length >= 1900) {
+                    promises.push(getCivicGenesBatch(ids));
+                    ids = '';
+                }
+            });
+            if (ids.length > 0) {
+                promises.push(getCivicGenesBatch(ids));
+            }
+
+            // We're explicitly waiting for all promises to finish (done or fail).
+            // We are wrapping them in another promise separately, to make sure we also 
+            // wait in case one of the promises fails and the other is still busy.
+            var mainPromise = $.when.apply($, $.map(promises, function(promise) {
+                var wrappingDeferred = $.Deferred();
+                promise.always(function (result) {
+                    wrappingDeferred.resolve();
+                });
+                return wrappingDeferred.promise();
+            }));
+
+            return mainPromise.then(function() {
                 return civicGenes;
             });
         },
-        
+
         getMatchingCivicVariants: function(civicVariants, proteinChange) {
             var matchingCivicVariants = [];
             

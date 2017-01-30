65959692e20837cbdfb55c679acd9db1960465e1
Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>
2017-01-30 15:35:49 -0500

    Handle webgl unavailable gracefuly
    Signed-off-by: Abeshouse, Adam A./Sloan Kettering Institute <abeshoua@mskcc.org>

--- portal/src/main/webapp/js/src/oncoprint/setup.js
+++ portal/src/main/webapp/js/src/oncoprint/setup.js
@@ -12,6 +12,15 @@ var stringListUnique = function(list) {
 };
 
 var utils = {
+    'isWebGLAvailable': function() {
+	var canvas = document.createElement("canvas");
+	var gl = canvas.getContext("webgl")
+	  || canvas.getContext("experimental-webgl");
+	return (gl && gl instanceof WebGLRenderingContext);
+    },
+    'getUnavailableMessageHTML': function() {
+	return 'Oncoprint cannot be displayed because <a href="https://get.webgl.org/">your browser does not support WebGL</a>.';
+    },
     'timeoutSeparatedLoop': function (array, loopFn) {
 	// loopFn is function(elt, index, array) {
 	var finished_promise = new $.Deferred();
@@ -414,6 +423,12 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
     $('#oncoprint #everything').show();
     $('#oncoprint #oncoprint-diagram-toolbar-buttons').show();
     
+    if (!utils.isWebGLAvailable()) {
+	$(ctr_selector).append("<p>"+utils.getUnavailableMessageHTML()+"</p>");
+	$(toolbar_selector).hide();
+	return;
+    }
+    
     $(ctr_selector).css({'position':'relative'});
     
     var LoadingBar = (function() {
@@ -463,6 +478,8 @@ window.CreateCBioPortalOncoprintWithToolbar = function (ctr_selector, toolbar_se
 	    'DOWNLOADING_MSG': 'Downloading data..'
 	};
     })();
+    
+    LoadingBar.hide();
     
     var oncoprint = new window.Oncoprint(ctr_selector, 1050);
     var toolbar_fade_out_timeout;
@@ -2276,6 +2293,13 @@ window.CreateOncoprinterWithToolbar = function (ctr_selector, toolbar_selector) 
     $('#oncoprint #everything').show();
     $('#oncoprint #oncoprint-diagram-toolbar-buttons').show();
     
+    if (!utils.isWebGLAvailable()) {
+	$(ctr_selector).append("<p>"+utils.getUnavailableMessageHTML()+"</p>");
+	$(toolbar_selector).hide();
+	$("#inner-container").hide();
+	return;
+    }
+    
     $(ctr_selector).css({'position':'relative'});
     
     var LoadingBar = (function() {
@@ -2329,6 +2353,7 @@ window.CreateOncoprinterWithToolbar = function (ctr_selector, toolbar_selector) 
     LoadingBar.hide();
     
     var oncoprint = new window.Oncoprint(ctr_selector, 1050);
+   
     var toolbar_fade_out_timeout;
     $(toolbar_selector).css({'visibility':'visible'});
     $(ctr_selector).add(toolbar_selector).on("mouseover", function(evt) {

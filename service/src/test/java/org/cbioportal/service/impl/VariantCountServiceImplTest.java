04526698c5460ccff77bba50fbf0bed229b10f70
Ersin Ciftci <arseniusthegreat@gmail.com>
2017-07-12 14:51:15 -0400

    Use mutation_count_by_keyword table in Variant Counts API
    Use the pre-calculated table in Variant Counts API, instead of dynamically
    calculating it.

--- service/src/test/java/org/cbioportal/service/impl/VariantCountServiceImplTest.java
+++ service/src/test/java/org/cbioportal/service/impl/VariantCountServiceImplTest.java
@@ -6,6 +6,7 @@ import org.cbioportal.model.MutationSampleCountByGene;
 import org.cbioportal.model.MutationSampleCountByKeyword;
 import org.cbioportal.model.VariantCount;
 import org.cbioportal.model.meta.MutationMeta;
+import org.cbioportal.persistence.VariantCountRepository;
 import org.cbioportal.service.GeneticProfileService;
 import org.cbioportal.service.MutationService;
 import org.junit.Test;
@@ -16,6 +17,7 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,6 +27,8 @@ public class VariantCountServiceImplTest extends BaseServiceImplTest {
     private VariantCountServiceImpl variantCountService;
     
     @Mock
+    private VariantCountRepository variantCountRepository;
+    @Mock
     private MutationService mutationService;
     @Mock
     private GeneticProfileService geneticProfileService;
@@ -32,48 +36,26 @@ public class VariantCountServiceImplTest extends BaseServiceImplTest {
     @Test
     public void fetchVariantCounts() throws Exception {
 
-        List<MutationSampleCountByGene> mutationSampleCountByGeneList = new ArrayList<>();
-        MutationSampleCountByGene mutationSampleCountByGene = new MutationSampleCountByGene();
-        mutationSampleCountByGene.setEntrezGeneId(ENTREZ_GENE_ID);
-        mutationSampleCountByGene.setSampleCount(3);
-        mutationSampleCountByGeneList.add(mutationSampleCountByGene);
-
         GeneticProfile geneticProfile = new GeneticProfile();
         geneticProfile.setGeneticAlterationType(GeneticProfile.GeneticAlterationType.MUTATION_EXTENDED);
         Mockito.when(geneticProfileService.getGeneticProfile(GENETIC_PROFILE_ID)).thenReturn(geneticProfile);
-        
-        List<Integer> entrezGeneIds = new ArrayList<>();
-        entrezGeneIds.add(ENTREZ_GENE_ID);
-
-        Mockito.when(mutationService.getSampleCountByEntrezGeneIdsAndSampleIds(GENETIC_PROFILE_ID, null, entrezGeneIds))
-            .thenReturn(mutationSampleCountByGeneList);
-        
-        List<MutationSampleCountByKeyword> mutationSampleCountByKeywordList = new ArrayList<>();
-        MutationSampleCountByKeyword mutationSampleCountByKeyword = new MutationSampleCountByKeyword();
-        mutationSampleCountByKeyword.setKeyword(KEYWORD);
-        mutationSampleCountByKeyword.setSampleCount(2);
-        mutationSampleCountByKeywordList.add(mutationSampleCountByKeyword);
-        
-        List<String> keywords = new ArrayList<>();
-        keywords.add(KEYWORD);
-        
-        Mockito.when(mutationService.getSampleCountByKeywords(GENETIC_PROFILE_ID, keywords))
-            .thenReturn(mutationSampleCountByKeywordList);
 
         MutationMeta mutationMeta = new MutationMeta();
         mutationMeta.setSampleCount(5);
         Mockito.when(mutationService.fetchMetaMutationsInGeneticProfile(GENETIC_PROFILE_ID, null, null))
             .thenReturn(mutationMeta);
         
-        List<VariantCount> result = variantCountService.fetchVariantCounts(GENETIC_PROFILE_ID, entrezGeneIds, keywords);
+        List<VariantCount> expectedVariantCounts = new ArrayList<>();
+        VariantCount variantCount = new VariantCount();
+        expectedVariantCounts.add(variantCount);
+        
+        Mockito.when(variantCountRepository.fetchVariantCounts(GENETIC_PROFILE_ID, Arrays.asList(ENTREZ_GENE_ID), 
+            Arrays.asList(KEYWORD))).thenReturn(expectedVariantCounts);
+        
+        List<VariantCount> result = variantCountService.fetchVariantCounts(GENETIC_PROFILE_ID, 
+            Arrays.asList(ENTREZ_GENE_ID), Arrays.asList(KEYWORD));
 
-        Assert.assertEquals(1, result.size());
-        VariantCount variantCount = result.get(0);
-        Assert.assertEquals(GENETIC_PROFILE_ID, variantCount.getGeneticProfileId());
-        Assert.assertEquals(ENTREZ_GENE_ID, variantCount.getEntrezGeneId());
-        Assert.assertEquals(KEYWORD, variantCount.getKeyword());
-        Assert.assertEquals((Integer) 5, variantCount.getNumberOfSamples());
-        Assert.assertEquals((Integer) 3, variantCount.getNumberOfSamplesWithMutationInGene());
-        Assert.assertEquals((Integer) 2, variantCount.getNumberOfSamplesWithKeyword());
+        Assert.assertEquals(expectedVariantCounts, result);
+        Assert.assertEquals((Integer) 5, result.get(0).getNumberOfSamples());
     }
 }

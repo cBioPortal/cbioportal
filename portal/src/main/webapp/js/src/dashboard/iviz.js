2a5684a4557688230ec3ceb95aaa82cc35fbfd4c
Hongxin Zhang <jackson.zhang.828@gmail.com>
2017-07-25 17:51:28 -0400

    Update

--- portal/src/main/webapp/js/src/dashboard/iviz.js
+++ portal/src/main/webapp/js/src/dashboard/iviz.js
@@ -2355,50 +2355,8 @@ var iViz = (function(_, $, cbio, QueryByGeneUtil, QueryByGeneTextArea) {
  */
 
 /**
- * Priorities
- *
- * The priority system is represented with a final score.
- * The higher the final (numeric) score, the higher priority assigned.
- *
- * The default score is 1.
- *
- * To promote certain chart in study view, please increase priority in the database to a certain number.
- * The higher the score, the higher priority it will be displayed in the study view.
- * If you want to hide chart, please set the priority to 0.
- * For combination chart, as long as one of the clinical attributes has been set to 0, it will be hidden.
- *
- * Currently, we preassigned priority to few charts, but as long as you assign a priority in the database except than 1,
- * these preassigned priorities will be overwritten.
- *
- * CANCER_TYPE:          3000,
- * CANCER_TYPE_DETAILED: 2000
- *
- * Overall survival plot:                400 (This is combination of OS_MONTH and OS_STATUS),
- * Disease Free Survival Plot:           300 (This is combination of DFS_MONTH and DFS_STATUS),
- * Mutation Count vs. CNA Scatter Plot:  200
- *
- * Mutated Genes Table:          90,
- * CNA Genes Table:              80,
- * study_id:                     70,
- * With Mutation Data Pie Chart: 60,
- * With CNA Data Pie Chart:      50,
- * # of Samples Per Patient:     40,
- * Mutation Count Bar Chart:     30,
- * CNA Bar Chart:                20
- *
- * GENDER, SEX:  9,
- * AGE:          8
- * 
- * 
- * Please note:
- * Priority is not the sole factor determining which chart will be displayed first.
- * A layout algorithm in study view also makes a minor adjustment on the layout.
- * The algorithm tries to fit all charts into a 2 by 2 matrix (Mutated Genes Table occupies 2 by 2 space).
- * When a chart can not be fitted in the first matrix, the second matrixed will be generated.
- * And the second matrix will have lower priority than the first one.
- * If later chart can fit into the first matrix, then its priority will be promoted.
-
- * Please see https://github.com/cBioPortal/cbioportal/pull/2689 for an example.
+ * Please see Study-View.md under cBioPortal repository for more information 
+ * about priority and layout.
  */
 
 

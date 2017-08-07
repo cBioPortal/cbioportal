0ee6878b0786c2b843f4f9435aa7265cc5935dff
emsw.bandara <emsw.bandara@gmail.com>
2017-08-07 19:45:23 +0530

    modified ClinicalTrialsController to send trial info on API call

--- web/src/main/java/org/cbioportal/web/ClinicalTrialsController.java
+++ web/src/main/java/org/cbioportal/web/ClinicalTrialsController.java
@@ -3,6 +3,11 @@ package org.cbioportal.web;
 import io.swagger.annotations.Api;
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.cbioportal.web.config.annotation.PublicApi;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,16 +15,30 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -41,48 +60,133 @@ public class ClinicalTrialsController {
 
 
     @RequestMapping(value = "/molecularmatch", method = RequestMethod.POST)
-    public HashMap<String, Integer> getMolecularMatchClinicalTrials(@RequestParam String filters, HttpMethod method) throws URISyntaxException {
+    public String getMolecularMatchClinicalTrials(@RequestParam String filters, HttpMethod method) throws 
+        URISyntaxException {
 
         String apiKey = "539188c9-0516-47c4-b3a6-98b4b5524dc8";
+
+
         Map payload = new HashedMap();
         payload.put("apiKey", apiKey);
         payload.put("filters", filters);
 
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            URI uri = new URI(molecularMatchURL);
-
-            JSONObject resp = restTemplate.postForObject(uri, payload, JSONObject.class);
-
             JSONArray filtersArr = (JSONArray) new JSONParser().parse(filters);
+            JSONObject jsonObject = new JSONObject();
+            //put trial count
+            //put best 5 trials and their details
+            
             HashMap<String, Integer> trialCount = new HashMap<>();
+//            RestTemplate restTemplate = new RestTemplate();
+            URI uri = new URI(molecularMatchURL);
+//
+//            JSONObject resp = restTemplate.postForObject(uri, payload, JSONObject.class);
+
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+            SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+            CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+            HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+            requestFactory.setHttpClient(httpClient);
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+             TrustManager[] UNQUESTIONING_TRUST_MANAGER = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers(){
+                        return null;
+                    }
+                    public void checkClientTrusted( X509Certificate[] certs, String authType ){}
+                    public void checkServerTrusted( X509Certificate[] certs, String authType ){}
+                }
+            };
+            final SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init( null, UNQUESTIONING_TRUST_MANAGER, null );
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            
+            JSONObject map = new JSONObject();
+            map.put("apiKey", apiKey);
+            map.put("filters", filtersArr);
+
+            HttpEntity<JSONObject> request = new HttpEntity<JSONObject>(map, headers);
+
+           // ResponseEntity<JSONObject> response = restTemplate.postForEntity( uri, request , String.class );
+
+            ResponseEntity<JSONObject> respEntity = restTemplate.exchange(uri, HttpMethod.POST, request, JSONObject.class);
+
+            JSONObject resp = respEntity.getBody();
+
+           
 
             ArrayList<LinkedHashMap<String, ArrayList<String>>> trials = (ArrayList<LinkedHashMap<String, ArrayList<String>>>) resp.get("trials");
 
+            JSONArray response = new JSONArray();
             for (Object obj : filtersArr) {
                 String facet = (String) ((JSONObject) obj).get("facet");
                 if (StringUtils.equals(facet, "MUTATION")) {
-                    String searchMutation = (String) ((JSONObject) obj).get("term");
+                    String searchMutation = ((String) ((JSONObject) obj).get("term")).split(" ")[0];
 
+                    JSONObject object = new JSONObject();
+                    JSONArray searchResults = new JSONArray();
                     for (LinkedHashMap<String, ArrayList<String>> trial : trials) {
                         ArrayList<String> mutations = trial.get("molecularAlterations");
                         if (mutations.contains(searchMutation)) {
+                            JSONObject topTrial = new JSONObject();
                             if (trialCount.containsKey(searchMutation)) {
                                 int count = trialCount.get(searchMutation);
                                 trialCount.put(searchMutation, count + 1);
+                                if(count <= 5){
+                                    topTrial.put("id", trial.get("id"));
+                                    topTrial.put("briefTitle", trial.get("briefTitle"));
+                                    topTrial.put("phase", trial.get("phase"));
+                                    
+                                    searchResults.add(topTrial);
+                                }
                             } else {
                                 trialCount.put(searchMutation, 1);
+                                topTrial.put("id", trial.get("id"));
+                                topTrial.put("briefTitle", trial.get("briefTitle"));
+                                topTrial.put("phase", trial.get("phase"));
+                                searchResults.add(topTrial);
                             }
                         }
                     }
+                    object.put("mutation", searchMutation);                
+                    if(trialCount.get(searchMutation) != null){
+                        object.put("count", trialCount.get(searchMutation));
+                    }
+                    else{
+                        object.put("count", 0);
+                    }
+                    object.put("trials", searchResults);
+                    response.add(object);
                 }
             }
-            return trialCount;
+            
+            return response.toJSONString();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
     }
+
 }
 

d076e69f9bff94ed1cfdc84a2d801675b8e30f48
Kelsey Zhu <kzhu@uhnresearch.ca>
2017-08-11 12:04:30 -0400

    add a new genetic profile type of copy number segment and entrez_gene_id filtering when join reference_genome_gene table

--- model/src/main/java/org/cbioportal/model/MolecularProfile.java
+++ model/src/main/java/org/cbioportal/model/MolecularProfile.java
@@ -21,7 +21,8 @@ public class MolecularProfile implements Serializable {
         PROTEIN_LEVEL,
         PROTEIN_ARRAY_PROTEIN_LEVEL,
         PROTEIN_ARRAY_PHOSPHORYLATION,
-        GENESET_SCORE
+        GENESET_SCORE,
+        COPY_NUMBER_SEGMENT
     }
 
     private Integer molecularProfileId;

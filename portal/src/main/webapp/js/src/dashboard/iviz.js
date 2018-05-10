c34c1b742f881be59057057629ae5011e50130b8
Hongxin Zhang <hongxin@cbio.mskcc.org>
2018-05-10 10:16:03 -0400

    Fix the study linkout issue when saving the VS
    This solves issue #4199
    
    Signed-off-by: Hongxin Zhang <hongxin@cbio.mskcc.org>

--- portal/src/main/webapp/js/src/dashboard/iviz.js
+++ portal/src/main/webapp/js/src/dashboard/iviz.js
@@ -9145,6 +9145,9 @@ window.LogRankTest = (function(jStat) {
                   $('.origin-studies-frame [data-toggle="collapse"]').click(function(a, b) {
                     $($(this).attr('data-target')).collapse('toggle');
                   });
+                  $('.origin-studies-frame .panel-title a').click(function() {
+                    window.open($(this).attr('href'));
+                  });
                 }
                 self_.showDialog(tooltip);
                 self_.hideLoading(tooltip);

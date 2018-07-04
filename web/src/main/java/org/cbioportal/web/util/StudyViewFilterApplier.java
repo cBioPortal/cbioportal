d68d3f6254d5d9afd246572df8c75b1a6a865dc7
Onur Sumer <s.onur.sumer@gmail.com>
2018-07-03 20:55:18 -0400

    added bar chart API endpoints for clinical data

--- web/src/main/java/org/cbioportal/web/util/StudyViewFilterApplier.java
+++ web/src/main/java/org/cbioportal/web/util/StudyViewFilterApplier.java
@@ -2,53 +2,56 @@ package org.cbioportal.web.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.apache.commons.collections.map.MultiKeyMap;
-import org.cbioportal.model.ClinicalData;
 import org.cbioportal.model.DiscreteCopyNumberData;
 import org.cbioportal.model.Mutation;
-import org.cbioportal.model.Patient;
 import org.cbioportal.model.Sample;
-import org.cbioportal.service.ClinicalDataService;
 import org.cbioportal.service.DiscreteCopyNumberService;
 import org.cbioportal.service.MolecularProfileService;
 import org.cbioportal.service.MutationService;
-import org.cbioportal.service.PatientService;
 import org.cbioportal.service.SampleService;
-import org.cbioportal.web.parameter.ClinicalDataEqualityFilter;
-import org.cbioportal.web.parameter.ClinicalDataType;
-import org.cbioportal.web.parameter.CopyNumberGeneFilter;
-import org.cbioportal.web.parameter.CopyNumberGeneFilterElement;
-import org.cbioportal.web.parameter.MutationGeneFilter;
-import org.cbioportal.web.parameter.Projection;
-import org.cbioportal.web.parameter.SampleIdentifier;
-import org.cbioportal.web.parameter.StudyViewFilter;
+import org.cbioportal.web.parameter.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StudyViewFilterApplier {
 
-    @Autowired
+    
     private SampleService sampleService;
-    @Autowired
-    private PatientService patientService;
-    @Autowired
-    private ClinicalDataService clinicalDataService;
-    @Autowired
+    
     private MutationService mutationService;
-    @Autowired
+    
     private DiscreteCopyNumberService discreteCopyNumberService;
-    @Autowired
+    
     private MolecularProfileService molecularProfileService;
+    
+    private ClinicalDataEqualityFilterApplier clinicalDataEqualityFilterApplier;
+    
+    private ClinicalDataIntervalFilterApplier clinicalDataIntervalFilterApplier;
+    
+    private StudyViewFilterUtil studyViewFilterUtil;
+
+    @Autowired
+    public StudyViewFilterApplier(SampleService sampleService, 
+                                  MutationService mutationService, 
+                                  DiscreteCopyNumberService discreteCopyNumberService, 
+                                  MolecularProfileService molecularProfileService, 
+                                  ClinicalDataEqualityFilterApplier clinicalDataEqualityFilterApplier, 
+                                  ClinicalDataIntervalFilterApplier clinicalDataIntervalFilterApplier, 
+                                  StudyViewFilterUtil studyViewFilterUtil) 
+    {
+        this.sampleService = sampleService;
+        this.mutationService = mutationService;
+        this.discreteCopyNumberService = discreteCopyNumberService;
+        this.molecularProfileService = molecularProfileService;
+        this.clinicalDataEqualityFilterApplier = clinicalDataEqualityFilterApplier;
+        this.clinicalDataIntervalFilterApplier = clinicalDataIntervalFilterApplier;
+        this.studyViewFilterUtil = studyViewFilterUtil;
+    }
 
     Function<Sample, SampleIdentifier> sampleToSampleIdentifier = new Function<Sample, SampleIdentifier>() {
         
@@ -70,7 +73,7 @@ public class StudyViewFilterApplier {
         if (studyViewFilter != null && studyViewFilter.getSampleIdentifiers() != null && !studyViewFilter.getSampleIdentifiers().isEmpty()) {
             List<String> studyIds = new ArrayList<>();
             List<String> sampleIds = new ArrayList<>();
-            extractStudyAndSampleIds(studyViewFilter.getSampleIdentifiers(), studyIds, sampleIds);
+            studyViewFilterUtil.extractStudyAndSampleIds(studyViewFilter.getSampleIdentifiers(), studyIds, sampleIds);
             sampleIdentifiers = sampleService.fetchSamples(studyIds, sampleIds, Projection.ID.name()).stream()
                 .map(sampleToSampleIdentifier).collect(Collectors.toList());;
         } else {
@@ -80,8 +83,14 @@ public class StudyViewFilterApplier {
         
         List<ClinicalDataEqualityFilter> clinicalDataEqualityFilters = studyViewFilter.getClinicalDataEqualityFilters();
         if (clinicalDataEqualityFilters != null) {
-            sampleIdentifiers = filterClinicalData(sampleIdentifiers, clinicalDataEqualityFilters, ClinicalDataType.SAMPLE);
-            sampleIdentifiers = filterClinicalData(sampleIdentifiers, clinicalDataEqualityFilters, ClinicalDataType.PATIENT);
+            sampleIdentifiers = equalityFilterClinicalData(sampleIdentifiers, clinicalDataEqualityFilters, ClinicalDataType.SAMPLE);
+            sampleIdentifiers = equalityFilterClinicalData(sampleIdentifiers, clinicalDataEqualityFilters, ClinicalDataType.PATIENT);
+        }
+
+        List<ClinicalDataIntervalFilter> clinicalDataIntervalFilters = studyViewFilter.getClinicalDataIntervalFilters();
+        if (clinicalDataIntervalFilters != null) {
+            sampleIdentifiers = intervalFilterClinicalData(sampleIdentifiers, clinicalDataIntervalFilters, ClinicalDataType.SAMPLE);
+            sampleIdentifiers = intervalFilterClinicalData(sampleIdentifiers, clinicalDataIntervalFilters, ClinicalDataType.PATIENT);
         }
 
         List<MutationGeneFilter> mutatedGenes = studyViewFilter.getMutatedGenes();
@@ -89,7 +98,7 @@ public class StudyViewFilterApplier {
             for (MutationGeneFilter molecularProfileGeneFilter : mutatedGenes) {
                 List<String> studyIds = new ArrayList<>();
                 List<String> sampleIds = new ArrayList<>();
-                extractStudyAndSampleIds(sampleIdentifiers, studyIds, sampleIds);
+                studyViewFilterUtil.extractStudyAndSampleIds(sampleIdentifiers, studyIds, sampleIds);
                 List<Mutation> mutations = mutationService.getMutationsInMultipleMolecularProfiles(molecularProfileService
                     .getFirstMutationProfileIds(studyIds, sampleIds), sampleIds, molecularProfileGeneFilter.getEntrezGeneIds(), 
                     Projection.ID.name(), null, null, null, null);
@@ -109,7 +118,7 @@ public class StudyViewFilterApplier {
                 
                 List<String> studyIds = new ArrayList<>();
                 List<String> sampleIds = new ArrayList<>();
-                extractStudyAndSampleIds(sampleIdentifiers, studyIds, sampleIds);
+                studyViewFilterUtil.extractStudyAndSampleIds(sampleIdentifiers, studyIds, sampleIds);
                 List<Integer> ampEntrezGeneIds = copyNumberGeneFilter.getAlterations().stream().filter(a -> 
                     a.getAlteration() == 2).map(CopyNumberGeneFilterElement::getEntrezGeneId).collect(Collectors.toList());
                 List<DiscreteCopyNumberData> ampCNAList = new ArrayList<>();
@@ -142,109 +151,24 @@ public class StudyViewFilterApplier {
 
         return sampleIdentifiers;
     }
-
-    private List<SampleIdentifier> filterClinicalData(List<SampleIdentifier> sampleIdentifiers,
-                                                      List<ClinicalDataEqualityFilter> clinicalDataEqualityFilters, 
-                                                      ClinicalDataType filterClinicalDataType) {
-        
-        List<ClinicalDataEqualityFilter> attributes = clinicalDataEqualityFilters.stream()
+    
+    private List<SampleIdentifier> intervalFilterClinicalData(List<SampleIdentifier> sampleIdentifiers,
+                                                              List<ClinicalDataIntervalFilter> clinicalDataIntervalFilters, 
+                                                              ClinicalDataType filterClinicalDataType)
+    {
+        List<ClinicalDataIntervalFilter> attributes = clinicalDataIntervalFilters.stream()
             .filter(c-> c.getClinicalDataType().equals(filterClinicalDataType)).collect(Collectors.toList());
-        List<ClinicalData> clinicalDataList = new ArrayList<>();
-        if (!attributes.isEmpty() && !sampleIdentifiers.isEmpty()) {
-            List<String> studyIds = new ArrayList<>();
-            List<String> sampleIds = new ArrayList<>();
-            extractStudyAndSampleIds(sampleIdentifiers, studyIds, sampleIds);
-            List<Patient> patients = patientService.getPatientsOfSamples(studyIds, sampleIds);
-            List<String> patientIds = patients.stream().map(Patient::getStableId).collect(Collectors.toList());
-            List<String> studyIdsOfPatients = patients.stream().map(Patient::getCancerStudyIdentifier).collect(Collectors.toList());
-            clinicalDataList = clinicalDataService.fetchClinicalData(filterClinicalDataType.equals(ClinicalDataType.PATIENT) ? 
-                studyIdsOfPatients : studyIds, filterClinicalDataType.equals(ClinicalDataType.PATIENT) ? patientIds : sampleIds, 
-                attributes.stream().map(ClinicalDataEqualityFilter::getAttributeId).collect(Collectors.toList()), 
-                filterClinicalDataType.name(), Projection.SUMMARY.name());
-
-            clinicalDataList.forEach(c -> {
-                if (c.getAttrValue().toUpperCase().equals("NAN") || c.getAttrValue().toUpperCase().equals("N/A")) {
-                    c.setAttrValue("NA");
-                }
-            });
-
-            MultiKeyMap clinicalDataMap = new MultiKeyMap();
-            for (ClinicalData clinicalData : clinicalDataList) {
-                if (filterClinicalDataType.equals(ClinicalDataType.PATIENT)) {
-                    if (clinicalDataMap.containsKey(clinicalData.getPatientId(), clinicalData.getStudyId())) {
-                        ((List<ClinicalData>)clinicalDataMap.get(clinicalData.getPatientId(), clinicalData.getStudyId())).add(clinicalData);
-                    } else {
-                        List<ClinicalData> clinicalDatas = new ArrayList<>();
-                        clinicalDatas.add(clinicalData);
-                        clinicalDataMap.put(clinicalData.getPatientId(), clinicalData.getStudyId(), clinicalDatas);
-                    }
-                } else {
-                    if (clinicalDataMap.containsKey(clinicalData.getSampleId(), clinicalData.getStudyId())) {
-                        ((List<ClinicalData>)clinicalDataMap.get(clinicalData.getSampleId(), clinicalData.getStudyId())).add(clinicalData);
-                    } else {
-                        List<ClinicalData> clinicalDatas = new ArrayList<>();
-                        clinicalDatas.add(clinicalData);
-                        clinicalDataMap.put(clinicalData.getSampleId(), clinicalData.getStudyId(), clinicalDatas);
-                    }
-                }
-            }
-
-            List<String> ids = new ArrayList<>();
-            List<String> studyIdsOfIds = new ArrayList<>();
-            int index = 0;
-            for (String entityId : filterClinicalDataType.equals(ClinicalDataType.PATIENT) ? patientIds : sampleIds) {
-                String studyId = filterClinicalDataType.equals(ClinicalDataType.PATIENT) ? studyIdsOfPatients.get(index) : studyIds.get(index);
-                int count = 0;
-                for (ClinicalDataEqualityFilter s : attributes) {
-                    List<ClinicalData> entityClinicalData = (List<ClinicalData>)clinicalDataMap.get(entityId, studyId);
-                    if (entityClinicalData != null) {
-                        Optional<ClinicalData> clinicalData = entityClinicalData.stream().filter(c -> c.getAttrId()
-                            .equals(s.getAttributeId())).findFirst();
-                        if (clinicalData.isPresent() && s.getValues().contains(clinicalData.get().getAttrValue())) {
-                            count++;
-                        } else if (!clinicalData.isPresent() && s.getValues().contains("NA")) {
-                            count++;
-                        }
-                    } else if (s.getValues().contains("NA")) {
-                        count++;
-                    }
-                }
-                if (count == attributes.size()) {
-                    ids.add(entityId);
-                    studyIdsOfIds.add(studyId);
-                }
-                index++;
-            }
-
-            if (filterClinicalDataType.equals(ClinicalDataType.PATIENT)) {
-                if (!ids.isEmpty()) {
-                    List<Sample> samples = sampleService.getSamplesOfPatientsInMultipleStudies(studyIdsOfIds, ids, 
-                        Projection.ID.name());
-                    ids = samples.stream().map(Sample::getStableId).collect(Collectors.toList());
-                    studyIdsOfIds = samples.stream().map(Sample::getCancerStudyIdentifier).collect(Collectors.toList());
-                }
-            }
-            
-            Set<String> idsSet = new HashSet<>(ids);
-            idsSet.retainAll(new HashSet<>(sampleIds));
-            List<SampleIdentifier> newSampleIdentifiers = new ArrayList<>();
-            for (int i = 0; i < ids.size(); i++) {
-                SampleIdentifier sampleIdentifier = new SampleIdentifier();
-                sampleIdentifier.setSampleId(ids.get(i));
-                sampleIdentifier.setStudyId(studyIdsOfIds.get(i));
-                newSampleIdentifiers.add(sampleIdentifier);
-            }
-            return newSampleIdentifiers;
-        }
-
-        return sampleIdentifiers;
+        
+        return clinicalDataIntervalFilterApplier.apply(sampleIdentifiers, attributes, filterClinicalDataType);
     }
 
-    private void extractStudyAndSampleIds(List<SampleIdentifier> sampleIdentifiers, List<String> studyIds, List<String> sampleIds) {
-        
-        for (SampleIdentifier sampleIdentifier : sampleIdentifiers) {
-            studyIds.add(sampleIdentifier.getStudyId());
-            sampleIds.add(sampleIdentifier.getSampleId());
-        }
+    private List<SampleIdentifier> equalityFilterClinicalData(List<SampleIdentifier> sampleIdentifiers, 
+                                                              List<ClinicalDataEqualityFilter> clinicalDataEqualityFilters, 
+                                                              ClinicalDataType filterClinicalDataType) 
+    {
+        List<ClinicalDataEqualityFilter> attributes = clinicalDataEqualityFilters.stream()
+            .filter(c-> c.getClinicalDataType().equals(filterClinicalDataType)).collect(Collectors.toList());
+
+        return clinicalDataEqualityFilterApplier.apply(sampleIdentifiers, attributes, filterClinicalDataType);
     }
 }

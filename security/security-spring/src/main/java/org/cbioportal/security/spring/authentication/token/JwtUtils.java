b5158e794295a80f87dc77428ec68084f32088f2
Robert Sheridan <sheridan@cbio.mskcc.org>
2018-09-18 13:28:24 -0400

    Create JwtUtils, to do token packing / unpacking - importat jjwt to security module - properties added to portal.properties for jwt policies/credentials - validation of token is done though exception propagation (InvalidDataAccessTokenException) - java application utility available from JwtUtils which generates a new signature key

---
+++ security/security-spring/src/main/java/org/cbioportal/security/spring/authentication/token/JwtUtils.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2018 Memorial Sloan-Kettering Cancer Center.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS
+ * FOR A PARTICULAR PURPOSE. The software and documentation provided hereunder
+ * is on an "as is" basis, and Memorial Sloan-Kettering Cancer Center has no
+ * obligations to provide maintenance, support, updates, enhancements or
+ * modifications. In no event shall Memorial Sloan-Kettering Cancer Center be
+ * liable to any party for direct, indirect, special, incidental or
+ * consequential damages, including lost profits, arising out of the use of this
+ * software and its documentation, even if Memorial Sloan-Kettering Cancer
+ * Center has been advised of the possibility of such damage.
+ */
+
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.cbioportal.security.spring.authentication.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.PrematureJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import java.util.*;
+import javax.crypto.SecretKey;
+import org.apache.commons.logging.*;
+import org.cbioportal.security.exception.InvalidDataAccessTokenException;
+import org.springframework.beans.factory.annotation.Value;
+
+
+class JwtUtils {
+
+    private byte[] decodedSecretKey;
+    @Value("${jwt.secret.key}")
+    private void setDecodedSecretKey(String secretKey) {
+        decodedSecretKey = Decoders.BASE64.decode(secretKey);
+    }
+
+    @Value("${jwt.ttl_seconds}")
+    private int jwtTtlSeconds;
+
+    @Value("${jwt.issuer}")
+    private String jwtIssuer;
+
+    public String createToken(String subject) {
+        return createToken(subject, null);
+    }
+
+    public String createToken(String subject, Date notValidBefore) {
+        if (subject == null || subject.trim().length() == 0) {
+            throw new IllegalArgumentException("subject cannot be empty");
+        }
+        Calendar calendar = Calendar.getInstance();
+        Date creationDate = calendar.getTime();
+        calendar.add(Calendar.SECOND, jwtTtlSeconds);
+        Date expirationDate = calendar.getTime();
+        Date notBeforeDate = notValidBefore == null ? creationDate : notValidBefore;
+        String jws = Jwts.builder()
+            .setIssuer(jwtIssuer)
+            .setSubject(subject)
+            .setIssuedAt(creationDate)
+            .setExpiration(expirationDate)
+            .setNotBefore(expirationDate)
+            .signWith(SignatureAlgorithm.HS256, decodedSecretKey).compact();
+        return jws;
+    }
+
+    public void validate(String token) throws InvalidDataAccessTokenException {
+        Map<String, Object> properties = extractProperties(token);
+    }
+
+    public String extractSubject(String token) throws InvalidDataAccessTokenException {
+        Map<String, Object> properties = extractProperties(token);
+        return properties.get(Claims.SUBJECT).toString();
+    }
+
+    public Date extractExpirationDate(String token) throws InvalidDataAccessTokenException {
+        Map<String, Object> properties = extractProperties(token);
+        return (Date) properties.get(Claims.EXPIRATION);
+    }
+
+    public Map<String, Object> extractProperties(String token) throws InvalidDataAccessTokenException {
+        Claims claims = null;
+        try {
+            Jws<Claims> jwsClaims = Jwts.parser()
+                .setSigningKey(decodedSecretKey)
+                .parseClaimsJws(token);
+            claims = jwsClaims.getBody();
+            if (!claims.getIssuer().equals(jwtIssuer)) {
+                throw new InvalidDataAccessTokenException("unexpected token issuer: " + claims.getIssuer());
+            }
+        } catch (SignatureException e) {
+            throw new InvalidDataAccessTokenException("signature not valid");
+        } catch (ExpiredJwtException e) {
+            throw new InvalidDataAccessTokenException("token has expired");
+        } catch (PrematureJwtException e) {
+            throw new InvalidDataAccessTokenException("token has not yet become valid");
+        }
+        return claims;
+    }
+
+    public static String createNewSecretKey() {
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256); // TODO: consider other Algorithms .. or parameterizing this choice
+        String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        return encodedKey;
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 1) {
+            usage();
+        }
+        if (args[0].equals("--make-key")) {
+            System.out.println("Creating new secret key for JWTS token signing:");
+            System.out.println(createNewSecretKey());
+        }
+    }
+
+    public static void usage() {
+        System.out.println("usage: JwtUtils --make-key");
+    }
+
+}

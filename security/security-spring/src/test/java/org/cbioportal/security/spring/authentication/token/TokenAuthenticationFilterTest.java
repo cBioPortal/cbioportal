1c19f94db34caadde57440b4b36f246cf9fa9d03
Avery Wang <wanga5@mskcc.org>
2018-11-21 15:45:19 -0500

    updated properties jwt.secret_key jwt.issuer to dat.jwt_secret_key dat.jwt_issuer

--- security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/TokenAuthenticationFilterTest.java
+++ security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/TokenAuthenticationFilterTest.java
@@ -51,9 +51,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource(
-    properties = { "jwt.secret_key = +NbopXzb/AIQNrVEGzxzP5CF42e5drvrXTQot3gfW/s=",
+    properties = { "dat.jwt_secret_key = +NbopXzb/AIQNrVEGzxzP5CF42e5drvrXTQot3gfW/s=",
                     "dat.ttl_seconds = 2",
-                    "jwt.issuer = org.cbioportal.mskcc.org",
+                    "dat.jwt_issuer = org.cbioportal.mskcc.org",
                     "dat.method = jwt"
     },
     inheritLocations = false

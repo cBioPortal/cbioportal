4b178bdde668677c8da1b802f646ee4c48ffe81d
Ino de Bruijn <ino@ino.pm>
2018-12-06 16:24:01 -0500

    Heroku fixes
    - use public_test on heroku
    - remove cleaning step in core. Doesn't seem to be necessary anymore and it's
      giving some conflicts with maven-external-version-plugin

--- version.sh
+++ version.sh
@@ -2,8 +2,8 @@
 if [ -n "$PROJECT_VERSION" ]; then
     # use env variable PROJECT_VERSION if defined
     echo $PROJECT_VERSION
-elif ! [ -x "$(command -v git)" ]; then
-    # git is not installed, 
+elif ! [ -x "$(command -v git)" ] || ! [ -d .git ]; then
+	# git is not installed, or not in a git repo
     echo '0-unknown-version'
 else
 	# get human readable commit id from git

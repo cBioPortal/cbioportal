8adb7764d045661457b5a2cb00c8da648da3dcd1
Ino de Bruijn <ino@ino.pm>
2018-12-07 13:53:11 -0500

    maven-external-version - fetch all tags

--- version.sh
+++ version.sh
@@ -6,6 +6,8 @@ elif ! [ -x "$(command -v git)" ] || ! [ -d .git ]; then
 	# git is not installed, or not in a git repo
     echo '0-unknown-version'
 else
+	# fetch all tags silently
+	git fetch --tags --quiet > /dev/null 2> /dev/null
 	# get human readable commit id from git
 	git describe --tags --always --dirty | sed 's/^v//'
 fi

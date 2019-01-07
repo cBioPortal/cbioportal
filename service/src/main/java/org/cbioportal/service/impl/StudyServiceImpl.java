f1ee34884fb689372e86fe9fea7a84bfc8f5769b
Ersin Ciftci <arseniusthegreat@gmail.com>
2019-01-07 13:43:30 -0500

    Search in the primary site of the studies in quick search

--- service/src/main/java/org/cbioportal/service/impl/StudyServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/StudyServiceImpl.java
@@ -2,8 +2,10 @@ package org.cbioportal.service.impl;
 
 import org.cbioportal.model.CancerStudy;
 import org.cbioportal.model.CancerStudyTags;
+import org.cbioportal.model.TypeOfCancer;
 import org.cbioportal.model.meta.BaseMeta;
 import org.cbioportal.persistence.StudyRepository;
+import org.cbioportal.service.CancerTypeService;
 import org.cbioportal.service.StudyService;
 import org.cbioportal.service.exception.StudyNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
 @Service
@@ -19,6 +22,8 @@ public class StudyServiceImpl implements StudyService {
 
     @Autowired
     private StudyRepository studyRepository;
+    @Autowired
+    private CancerTypeService cancerTypeService;
     @Value("${authenticate:false}")
     private String AUTHENTICATE;
 
@@ -28,13 +33,32 @@ public class StudyServiceImpl implements StudyService {
                                            String sortBy, String direction) {
 
         List<CancerStudy> allStudies = studyRepository.getAllStudies(keyword, projection, pageSize, pageNumber, sortBy, direction);
+        if (keyword != null && (pageSize == null || allStudies.size() < pageSize)) {
+            List<CancerStudy> primarySiteMatchingStudies = findPrimarySiteMatchingStudies(keyword);
+            if (pageSize != null) {
+                int toIndex = primarySiteMatchingStudies.size() > pageSize - allStudies.size() ? 
+                    pageSize - allStudies.size() : primarySiteMatchingStudies.size();
+                primarySiteMatchingStudies = primarySiteMatchingStudies.subList(0, toIndex);
+            }
+            for (CancerStudy cancerStudy : primarySiteMatchingStudies) {
+                if (!allStudies.stream().anyMatch(c -> c.getCancerStudyIdentifier().equals(cancerStudy.getCancerStudyIdentifier())))
+                allStudies.add(cancerStudy);
+            }
+        }
         // copy the list before returning so @PostFilter doesn't taint the list stored in the mybatis second-level cache
         return (AUTHENTICATE.equals("false")) ? allStudies : new ArrayList<CancerStudy>(allStudies);
     }
 
     @Override
     public BaseMeta getMetaStudies(String keyword) {
-        return studyRepository.getMetaStudies(keyword);
+        if (keyword == null) {
+            return studyRepository.getMetaStudies(keyword);
+        }
+        else {
+            BaseMeta baseMeta = new BaseMeta();
+            baseMeta.setTotalCount(getAllStudies(keyword, "SUMMARY", null, null, null, null).size());
+            return baseMeta;
+        }
     }
 
     @Override
@@ -70,5 +94,27 @@ public class StudyServiceImpl implements StudyService {
     public List<CancerStudyTags> getTagsForMultipleStudies(List<String> studyIds) {
 
         return studyRepository.getTagsForMultipleStudies(studyIds);
+    }
+
+    private List<CancerStudy> findPrimarySiteMatchingStudies(String keyword) {
+
+        List<CancerStudy> matchingStudies = new ArrayList<>();
+
+        List<String> matchingCancerTypes = new ArrayList<>();
+        for (Map.Entry<String, TypeOfCancer> entry : cancerTypeService.getPrimarySiteMap().entrySet()) {
+            if (entry.getValue().getTypeOfCancerId().toLowerCase().contains(keyword.toLowerCase()) || 
+                entry.getValue().getName().toLowerCase().contains(keyword.toLowerCase())) {
+                matchingCancerTypes.add(entry.getKey());
+            }
+        }
+        if (!matchingCancerTypes.isEmpty()) {
+            List<CancerStudy> allUnfilteredStudies = studyRepository.getAllStudies(null, "SUMMARY", null, null, null, null);
+            for (CancerStudy cancerStudy : allUnfilteredStudies) {
+                if (matchingCancerTypes.contains(cancerStudy.getTypeOfCancerId())) {
+                    matchingStudies.add(cancerStudy);
+                }
+            }
+        }
+        return matchingStudies;
     }
 }

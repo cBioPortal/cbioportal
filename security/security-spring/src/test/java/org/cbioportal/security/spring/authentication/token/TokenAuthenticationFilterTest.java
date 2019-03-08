12ecf2b2daf8042e387a3b48d3a6c5ddd3df9dcb
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2019-03-08 10:15:43 -0500

    Authenticating users via Data Access Tokens
    This PR contains the complete development efforts for supporting token-based authentication allowing users to access data programmatically from protected instances of the cBioPortal.
    
    **General**
    1. Data Access Tokens properties update
        - Add new property for DAT revocation default value
        - Fallback on default value for revoking access tokens on issuance of new token if parameter not provided
    2. Make DAT implementation configurable via portal.properties
        - DAT implementation method used is now configurable via `/portal.properties:dat.method`
        - Supported values for `dat.method` are `uuid`,`jwt`, or `none` (default)
        - Throw `AccessDeniedException` is user tries to interact with DAT endpoints on an unauthenticated portal.
    3. Data Access Token documentation
        - Added documentation for Data Access Token:
            - obtaining and usage
            - implementation / configuration
    4. Upgraded Spring security version to 5.0.7.RELEASE
    
    **Web/Service/Persistence**
    1. Web API udpates for data access tokens:
        - Authenticates any request with an authorization bearer header
        - Added isValid() function to DAT service layer
        - Add POST Web API endpoint to create new tokens for authenticated user session
        - Migrated DAT endpoint to internal API
    2. Create JwtUtils, to do token packing / unpacking (JWT data access token implementation)
        - Imported jjwt to security module
        - Validation of token is done though exception propagation (InvalidDataAccessTokenException)
        - Java application utility available from JwtUtils which generates a new signature key
    3. UUID implementation for data access tokens
        - Additional data members in DataAccessToken class
        - Repository layer which interacts with sql database through mybatis
            - Get all data access tokens (given a username)
            - Get a data access token (given a token identifier)
            - Insert a new record
            - Delete a record associated with a token identifier
            - Delete all records associated with a username
    4. UUID service implementation
        - Creating a data access token will delete oldest token if user already has max number of tokens allowed
    5. Added exceptions to handle errors when user exceeds max number of tokens allowed, or tokens not found (MaxNumberTokensExceedException, TokenNotFoundException)
    
    **Unit tests**
    1. Added unit tests for TokenAuthenticationFilter, JwtUtils, DataAccessTokenController, UuidDataAccessTokenServiceImpl, DataAccessTokenMyBatisRepository
        - Introduced DataAccessToken model for both persistence and API response [JSON mapping]
        - JwtUtils.createToken() now returns DataAccessToken class instance (full info)
        - Added service for retrieving full token info (getDataAccessTokenInfo) from plain token text
        - Added service/endpoint for revoking a single token
        - JWT services throw UnsupportedOperation for all methods involving persistence (revoke, retrieve)
        - Edited testContextDatabase.xml to include a transaction manager (needed for state-altering database operations i.e insert)
        - Added test applicationContext-security.xml (including filters, authentication manager, success handler)
    
    **Database**
    1. Update sql migration.sql and cgds.sql scripts
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>
    Co-authored-by: Manda Wilson <1458628+mandawilson@users.noreply.github.com>
    Co-authored-by: Avery Wang <18199796+averyniceday@users.noreply.github.com>
    Co-authored-by: Benjamin Gross <benjamin.gross@gmail.com>
    Co-authored-by: Robert Sheridan <sheridan@cbio.mskcc.org>

---
+++ security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/TokenAuthenticationFilterTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2018 Memorial Sloan-Kettering Cancer Center.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS
+ * FOR A PARTICULAR PURPOSE. The software and documentation provided hereunder
+ * is on an "as is" basis, and Memorial Sloan-Kettering Cancer Center has no
+ * obligations to provide maintenance, support, updates, enhancements or
+ * modifications. In no event shall Memorial Sloan-Kettering Cancer Center be
+ * liable to any party for direct, indirect, special, incidental or
+ * consequential damages, including lost profits, arising out of the use of this
+ * software and its documentation, even if Memorial Sloan-Kettering Cancer
+ * Center has been advised of the possibility of such damage.
+ */
+
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.cbioportal.security.spring.authentication.token;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cbioportal.service.util.JwtUtils;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(
+    properties = { "dat.jwt.secret_key = +NbopXzb/AIQNrVEGzxzP5CF42e5drvrXTQot3gfW/s=",
+                    "dat.ttl_seconds = 2",
+                    "dat.method = jwt"
+    },
+    inheritLocations = false
+)
+@ContextConfiguration(classes=TokenAuthenticationFilterTestConfiguration.class)
+@RunWith(SpringRunner.class)
+public class TokenAuthenticationFilterTest {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+    @Autowired
+    private TokenAuthenticationFilter tokenAuthenticationFilter;
+
+    private static final long TEST_TOKEN_EXPIRATION_MILLISECONDS = 2000L;
+
+    private static final Log LOG = LogFactory.getLog(TokenAuthenticationFilterTest.class);
+
+    // TODO: test requiresValidation() function maybe
+
+    @Test
+    public void testAttemptAuthentication_success() {
+        String token = jwtUtils.createToken(TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT).getToken();
+        LOG.debug("testAttemptAuthentication_success() token = " + token);
+        Mockito.reset(request);
+        Mockito.when(request.getHeader(Matchers.anyString())).thenReturn("Bearer " + token);
+        // response object is autowired above
+        Authentication authentication = tokenAuthenticationFilter.attemptAuthentication(request, response);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        // userDetails is mocked and hard coded to return TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT
+        // but we will only get TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT if the token is valid and
+        // the authority provider (not mocked) successfully authenticated
+        // if the token is invalid or the authentication provider fails then exceptions are thrown
+        if (userDetails == null || userDetails.getUsername() == null || !userDetails.getUsername().equalsIgnoreCase(TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT)) {
+            Assert.fail("principal username returned by authentication filter (" + userDetails == null ? "null" : userDetails.getUsername() + ") does not match token user : (" + TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT +")");
+        }
+        Mockito.reset(request);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAttemptAuthentication_nullToken() {
+        Mockito.reset(request);
+        Mockito.when(request.getHeader(Matchers.anyString())).thenReturn(null);
+        // response object is autowired above
+        tokenAuthenticationFilter.attemptAuthentication(request, response);
+        // make sure we call Mockito.reset(request) in other methods
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void testAttemptAuthentication_expiredToken() throws InterruptedException {
+        String token = jwtUtils.createToken(TokenAuthenticationFilterTestConfiguration.TEST_SUBJECT).getToken();
+        LOG.debug("testAttemptAuthentication_expiredToken() token = " + token);
+        Mockito.reset(request);
+        Mockito.when(request.getHeader(Matchers.anyString())).thenReturn("Bearer " + token);
+        Thread.sleep(TEST_TOKEN_EXPIRATION_MILLISECONDS + 10L); // NOTE: sleep time must be adequate to allow created token to expire
+        // response object is autowired above
+        tokenAuthenticationFilter.attemptAuthentication(request, response);
+        // make sure we call Mockito.reset(request) in other methods
+    }
+
+}

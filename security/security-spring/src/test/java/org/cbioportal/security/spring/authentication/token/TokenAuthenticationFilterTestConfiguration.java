56792ef6b63ddf60f1cd8eadf73fd93f099a9134
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2019-03-08 10:15:43 -0500

    Authenticating users via Data Access Tokens
    This PR contains the complete development efforts for supporting token-based authentication allowing users to access data programmatically from protected instances of the cBioPortal.
    
    **General**
    1. Data Access Tokens properties update
        - Add new property for DAT revocation default value
        - Fallback on default value for revoking access tokens on issuance of new token if parameter not provided
    2. Make DAT implementation configurable via portal.properties
        - DAT implementation method used is now configurable via `/portal.properties:dat.method`
        - Supported values for `dat.method` are `uuid`,`jwt`, or `none` (default)
        - Throw `AccessDeniedException` is user tries to interact with DAT endpoints on an unauthenticated portal.
    3. Data Access Token documentation
        - Added documentation for Data Access Token:
            - obtaining and usage
            - implementation / configuration
    4. Upgraded Spring security version to 5.0.7.RELEASE
    
    **Web/Service/Persistence**
    1. Web API udpates for data access tokens:
        - Authenticates any request with an authorization bearer header
        - Added isValid() function to DAT service layer
        - Add POST Web API endpoint to create new tokens for authenticated user session
        - Migrated DAT endpoint to internal API
    2. Create JwtUtils, to do token packing / unpacking (JWT data access token implementation)
        - Imported jjwt to security module
        - Validation of token is done though exception propagation (InvalidDataAccessTokenException)
        - Java application utility available from JwtUtils which generates a new signature key
    3. UUID implementation for data access tokens
        - Additional data members in DataAccessToken class
        - Repository layer which interacts with sql database through mybatis
            - Get all data access tokens (given a username)
            - Get a data access token (given a token identifier)
            - Insert a new record
            - Delete a record associated with a token identifier
            - Delete all records associated with a username
    4. UUID service implementation
        - Creating a data access token will delete oldest token if user already has max number of tokens allowed
    5. Added exceptions to handle errors when user exceeds max number of tokens allowed, or tokens not found (MaxNumberTokensExceedException, TokenNotFoundException)
    
    **Unit tests**
    1. Added unit tests for TokenAuthenticationFilter, JwtUtils, DataAccessTokenController, UuidDataAccessTokenServiceImpl, DataAccessTokenMyBatisRepository
        - Introduced DataAccessToken model for both persistence and API response [JSON mapping]
        - JwtUtils.createToken() now returns DataAccessToken class instance (full info)
        - Added service for retrieving full token info (getDataAccessTokenInfo) from plain token text
        - Added service/endpoint for revoking a single token
        - JWT services throw UnsupportedOperation for all methods involving persistence (revoke, retrieve)
        - Edited testContextDatabase.xml to include a transaction manager (needed for state-altering database operations i.e insert)
        - Added test applicationContext-security.xml (including filters, authentication manager, success handler)
    
    **Database**
    1. Update sql migration.sql and cgds.sql scripts
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>
    Co-authored-by: Manda Wilson <1458628+mandawilson@users.noreply.github.com>
    Co-authored-by: Avery Wang <18199796+averyniceday@users.noreply.github.com>
    Co-authored-by: Benjamin Gross <benjamin.gross@gmail.com>
    Co-authored-by: Robert Sheridan <sheridan@cbio.mskcc.org>

---
+++ security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/TokenAuthenticationFilterTestConfiguration.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2018 Memorial Sloan-Kettering Cancer Center.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS
+ * FOR A PARTICULAR PURPOSE. The software and documentation provided hereunder
+ * is on an "as is" basis, and Memorial Sloan-Kettering Cancer Center has no
+ * obligations to provide maintenance, support, updates, enhancements or
+ * modifications. In no event shall Memorial Sloan-Kettering Cancer Center be
+ * liable to any party for direct, indirect, special, incidental or
+ * consequential damages, including lost profits, arising out of the use of this
+ * software and its documentation, even if Memorial Sloan-Kettering Cancer
+ * Center has been advised of the possibility of such damage.
+ */
+
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.cbioportal.security.spring.authentication.token;
+
+import org.cbioportal.security.spring.authentication.googleplus.PortalUserDetailsService;
+import org.cbioportal.service.DataAccessTokenService;
+import org.cbioportal.service.DataAccessTokenServiceFactory;
+import org.cbioportal.service.impl.JwtDataAccessTokenServiceImpl;
+import org.cbioportal.service.util.JwtUtils;
+
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class TokenAuthenticationFilterTestConfiguration {
+
+    public static final String TEST_SUBJECT = "testSubject";
+
+    @Bean ProviderManager authenticationManager() {
+        List<AuthenticationProvider> authenticationProviderList = new ArrayList<AuthenticationProvider>();
+        PortalUserDetailsService userDetailsService = Mockito.mock(PortalUserDetailsService.class);
+        UserDetails userDetails = Mockito.mock(UserDetails.class);
+        // See: https://github.com/spring-projects/spring-security/blob/master/core/src/main/java/org/springframework/security/authentication/dao/AbstractUserDetailsAuthenticationProvider.java#L350
+        Mockito.when(userDetails.isAccountNonLocked()).thenReturn(true);
+        Mockito.when(userDetails.isEnabled()).thenReturn(true);
+        Mockito.when(userDetails.isAccountNonExpired()).thenReturn(true);
+        Mockito.when(userDetails.isCredentialsNonExpired()).thenReturn(true);
+        Mockito.when(userDetails.getUsername()).thenReturn(TEST_SUBJECT);
+        Mockito.when(userDetailsService.loadUserByUsername(Matchers.anyString())).thenReturn(userDetails);
+        TokenUserDetailsAuthenticationProvider authenticationProvider = new TokenUserDetailsAuthenticationProvider(userDetailsService);
+        authenticationProviderList.add(authenticationProvider);
+        return new ProviderManager(authenticationProviderList);
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        TokenAuthenticationFilter filter = new TokenAuthenticationFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        return filter;
+    }
+
+    @Bean
+    public ServiceLocatorFactoryBean tokenServiceFactory() {
+        ServiceLocatorFactoryBean factoryBean = new ServiceLocatorFactoryBean();
+        factoryBean.setServiceLocatorInterface(DataAccessTokenServiceFactory.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataAccessTokenServiceFactory dataAccessTokenServiceFactory() {
+        DataAccessTokenServiceFactory factory = Mockito.mock(DataAccessTokenServiceFactory.class);
+        Mockito.when(factory.getDataAccessTokenService(Matchers.anyString())).thenReturn(tokenService());
+        return factory;
+    }
+
+    @Bean
+    public DataAccessTokenService tokenService() {
+        return new JwtDataAccessTokenServiceImpl();
+    }
+
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils();
+    }
+
+    @Bean
+    public HttpServletRequest request() {
+        return Mockito.mock(HttpServletRequest.class);
+    }
+
+    @Bean
+    public HttpServletResponse response() {
+        return Mockito.mock(HttpServletResponse.class);
+    }
+}
---
+++ web/src/test/java/org/cbioportal/web/DataAccessTokenControllerTest.java
@@ -0,0 +1,246 @@
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.cbioportal.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.http.HttpSession;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import org.cbioportal.model.DataAccessToken;
+import org.cbioportal.service.DataAccessTokenService;
+import org.cbioportal.service.exception.TokenNotFoundException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/applicationContext-web.xml", "/applicationContext-security-test.xml"})
+@WebAppConfiguration
+public class DataAccessTokenControllerTest {
+
+    public static final String MOCK_USER = "MOCK_USER";
+    public static final String MOCK_PASSWORD = "MOCK_PASSWORD";
+    public static final String VALID_TOKEN_STRING = "VALID_TOKEN";
+    public static final String NONEXISTENT_TOKEN_STRING = "NONEXISTENT_TOKEN";
+    public static final String NOT_FOUND_ERROR_MESSAGE = "Specified token cannot be found";
+    public static final DataAccessToken MOCK_TOKEN_INFO = new DataAccessToken(VALID_TOKEN_STRING);
+
+    public String receivedArgument = null;
+
+    @Bean
+    public DataAccessTokenService tokenService() {
+        DataAccessTokenService tokenService = Mockito.mock(DataAccessTokenService.class);
+        return tokenService;
+    }
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private DataAccessTokenService tokenService;
+
+    @Autowired
+    private FilterChainProxy filterChainProxy;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).addFilter(filterChainProxy).build();
+    }
+
+    public void resetReceivedArgument() {
+        this.receivedArgument = null;
+    }
+
+    private HttpSession getSession(String user, String password) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.post("/j_spring_security_check")
+            .param("j_username", user)
+            .param("j_password", password))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andReturn()
+        .getRequest()
+        .getSession();
+    }
+    /* Tests mapping for GET /data-access-tokens/{token}
+     * Test for valid token - checks returned response type is 200 success
+     */
+    @Test
+    public void getTokenInfoForValidTokenTest() throws Exception {
+        Mockito.when(tokenService.getDataAccessTokenInfo(VALID_TOKEN_STRING)).thenReturn(MOCK_TOKEN_INFO);
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/data-access-tokens/" + VALID_TOKEN_STRING)
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+    }
+
+    /* Tests mapping for GET /data-access-tokens/{token}
+     * Test for nonexistent token - checks returned response type is 404 not found
+     * Checks response for correct error message
+     */
+    @Test
+    public void getTokenInfoForNonexistentTokenTest() throws Exception {
+        Mockito.doThrow(new TokenNotFoundException()).when(tokenService).getDataAccessTokenInfo(NONEXISTENT_TOKEN_STRING);
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/data-access-tokens/" + NONEXISTENT_TOKEN_STRING)
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isNotFound())
+            .andReturn();
+        if (!result.getResponse().getContentAsString().contains(NOT_FOUND_ERROR_MESSAGE)) {
+            Assert.fail("Returned response did not contain expected error message. Expected response: '" + NOT_FOUND_ERROR_MESSAGE + "' Returned response: '" + result.getResponse().getContentAsString() + "'");
+        }
+    }
+
+    /* Tests mapping for DELETE /data-access-tokens/{token}
+     * Test that proper service method was called
+     */
+    @Test
+    public void revokeValidTokenTest() throws Exception {
+        resetReceivedArgument();
+        Answer<Void> tokenServiceRevokeTokenAnswer = new Answer<Void>() {
+            public Void answer(InvocationOnMock revokeTokenInvocation) {
+                receivedArgument = (String)revokeTokenInvocation.getArguments()[0];
+                return null;
+            }
+        };
+        Mockito.doAnswer(tokenServiceRevokeTokenAnswer).when(tokenService).revokeDataAccessToken(Matchers.anyString());
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/data-access-tokens/" + VALID_TOKEN_STRING)
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        if (!receivedArgument.equals(VALID_TOKEN_STRING)) {
+            Assert.fail("Unexpected argument passed to service class. Expected argument: " + VALID_TOKEN_STRING + " Received argument: " + receivedArgument);
+        }
+    }
+
+    /* Tests mapping for GET /data-access-tokens/{token}
+     * Test for nonexistent token - checks returned response type is 404 not found
+     * Checks response for correct error message
+     */
+    @Test
+    public void revokeNonexistentTokenTest() throws Exception {
+        resetReceivedArgument();
+        Mockito.doThrow(new TokenNotFoundException()).when(tokenService).revokeDataAccessToken(NONEXISTENT_TOKEN_STRING);;
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/data-access-tokens/" + NONEXISTENT_TOKEN_STRING)
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isNotFound())
+            .andReturn();
+        if (!result.getResponse().getContentAsString().contains(NOT_FOUND_ERROR_MESSAGE)) {
+            Assert.fail("Returned response did not contain expected error message. Expected response: '" + NOT_FOUND_ERROR_MESSAGE + "' Returned response: '" + result.getResponse().getContentAsString() + "'");
+        }
+    }
+
+    /* Tests mapping for POST /data-access-tokens
+     * Tests for 201 (CREATED) response code
+     */
+    @Test
+    public void createTokenValidUserTest() throws Exception {
+        Mockito.when(tokenService.createDataAccessToken(Matchers.anyString())).thenReturn(MOCK_TOKEN_INFO);
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/data-access-tokens")
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andReturn();
+    }
+
+    /* Tests mapping for DELETE /data-access-tokens
+     * Checks response status code is 200 success
+     * Checks that correct username argument is passed to service class
+     */
+    @Test
+    public void revokeAllTokensForUserTest() throws Exception {
+        resetReceivedArgument();
+        Answer<Void> tokenServiceRevokeAllTokensAnswer = new Answer<Void>() {
+            public Void answer(InvocationOnMock revokeAllTokensInvocation) {
+                receivedArgument = (String)revokeAllTokensInvocation.getArguments()[0];
+                return null;
+            }
+        };
+        Mockito.doAnswer(tokenServiceRevokeAllTokensAnswer).when(tokenService).revokeAllDataAccessTokens(Matchers.anyString());
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/data-access-tokens")
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        if(!receivedArgument.equals(MOCK_USER)) {
+            Assert.fail("Unexpected argument passed to service class. Expected argument: " + MOCK_USER + " Received argument: " + receivedArgument);
+        }
+    }
+
+    /* Tests mapping for GET /data-access-tokens
+     * Checks response status code is 200 success
+     * Checks that correct username argument is passed to service class
+     */
+    @Test
+    public void getAllTokensForUserTest() throws Exception {
+        resetReceivedArgument();
+        Answer<Void> tokenServiceGetAllTokensAnswer = new Answer<Void>() {
+            public Void answer(InvocationOnMock getAllTokensInvocation) {
+                receivedArgument = (String)getAllTokensInvocation.getArguments()[0];
+                return null;
+            }
+        };
+        Mockito.doAnswer(tokenServiceGetAllTokensAnswer).when(tokenService).getAllDataAccessTokens(Matchers.anyString());
+        HttpSession session = getSession(MOCK_USER, MOCK_PASSWORD);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/data-access-tokens")
+            .session((MockHttpSession) session)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+        if(!receivedArgument.equals(MOCK_USER)) {
+            Assert.fail("Unexpected argument passed to service class. Expected argument: " + MOCK_USER + " Received argument: " + receivedArgument);
+        }
+    }
+}

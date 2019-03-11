4701ed0aac85972a3f1bb4b0fa8201c9943400db
averyniceday <wanga5@mskcc.org>
2019-03-11 13:59:51 -0700

    Expand Enrichment API to take Multiple Molecular Profile Ids (#5733)
    * new MultipleStudiesEnrichmentFilter (to be passed into the web layer)
    new model (Entity - this can be renamed) - multipleStudiesEnrichmentFilter contains two seperate lists of entities
    each entity contains a molecularProfileId, and entityId (patient/sampleId)
    commented out a bunch of different unit tests so code will compile (just to see if the model shows up correctly on swagger)
    renaming data members / started looking at service class
    * Updates to avery/enrichment-api
    count of samples in each set profiled for gene)
    * Update Service test classes for Mutation/CN enrichment
    * Renamed Entity to MolecularProfileCase
    - Updated relevant getters and setters to reflect the renaming of this java class.
    - Renamed variables to be consistent with updated java class name.
    * fixed small typo
    * fix for unit tests
    * Renamed AlterationCount, MolecularProfileCase
    * fixed naming
    Signed-off-by: Angelica Ochoa <aochoa4230@gmail.com>

--- service/src/main/java/org/cbioportal/service/impl/MutationEnrichmentServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/MutationEnrichmentServiceImpl.java
@@ -5,6 +5,7 @@ import org.cbioportal.model.MolecularProfile;
 import org.cbioportal.model.Mutation;
 import org.cbioportal.model.MutationCountByGene;
 import org.cbioportal.model.Sample;
+import org.cbioportal.model.MolecularProfileCaseIdentifier;
 import org.cbioportal.service.MolecularProfileService;
 import org.cbioportal.service.MutationEnrichmentService;
 import org.cbioportal.service.MutationService;
@@ -14,8 +15,7 @@ import org.cbioportal.service.util.AlterationEnrichmentUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,33 +31,45 @@ public class MutationEnrichmentServiceImpl implements MutationEnrichmentService 
     private AlterationEnrichmentUtil alterationEnrichmentUtil;
 
     @Override
-    public List<AlterationEnrichment> getMutationEnrichments(String molecularProfileId, List<String> alteredIds,
-                                                             List<String> unalteredIds, String enrichmentType)
+    public List<AlterationEnrichment> getMutationEnrichments(List<MolecularProfileCaseIdentifier> molecularProfileCaseSet1,
+            List<MolecularProfileCaseIdentifier> molecularProfileCaseSet2,
+            String enrichmentType)
         throws MolecularProfileNotFoundException {
 
-        List<String> allIds = new ArrayList<>(alteredIds);
-        allIds.addAll(unalteredIds);
-        List<MutationCountByGene> mutationCountByGeneListFromRepo;
-        List<Mutation> mutations;
-        
+        List<MolecularProfileCaseIdentifier> allIds = new ArrayList<>(molecularProfileCaseSet1);
+        allIds.addAll(molecularProfileCaseSet2);
+        List<MutationCountByGene> mutationCountByGeneListFromRepo = new ArrayList<>();
+        List<Mutation> mutations = new ArrayList<>();
+
+        Map<String, List<String>> allMolecularProfileIdToCaseMap = alterationEnrichmentUtil.mapMolecularProfileIdToCaseId(allIds);
+        Map<String, List<String>> group1MolecularProfileIdToCaseMap = alterationEnrichmentUtil.mapMolecularProfileIdToCaseId(molecularProfileCaseSet1);
+
+        // get mutation count by gene list for set 1 ids
         if (enrichmentType.equals("SAMPLE")) {
-            mutationCountByGeneListFromRepo = mutationService.getSampleCountByEntrezGeneIdsAndSampleIds(molecularProfileId, 
-                allIds, null);
-            mutations = mutationService.fetchMutationsInMolecularProfile(molecularProfileId, alteredIds, null, null, 
-                "ID", null, null, null, null);
+            for (String molecularProfileId : allMolecularProfileIdToCaseMap.keySet()) {
+                mutationCountByGeneListFromRepo.addAll(mutationService.getSampleCountByEntrezGeneIdsAndSampleIds(molecularProfileId,
+                    allMolecularProfileIdToCaseMap.get(molecularProfileId), null));
+            }
+            for (String molecularProfileId : group1MolecularProfileIdToCaseMap.keySet()) {
+                mutations.addAll(mutationService.fetchMutationsInMolecularProfile(molecularProfileId, group1MolecularProfileIdToCaseMap.get(molecularProfileId), null, null,
+                    "ID", null, null, null, null));
+            }
         } else {
-            mutationCountByGeneListFromRepo = mutationService.getPatientCountByEntrezGeneIdsAndSampleIds(molecularProfileId,
-                allIds, null);
-            MolecularProfile molecularProfile = molecularProfileService.getMolecularProfile(molecularProfileId);
-            List<Sample> sampleList = sampleService.getAllSamplesOfPatientsInStudy(
-                molecularProfile.getCancerStudyIdentifier(), alteredIds, "ID");
-            mutations = mutationService.fetchMutationsInMolecularProfile(molecularProfileId,
-                sampleList.stream().map(Sample::getStableId).collect(Collectors.toList()), null, null, "ID", null, null,
-                null, null);
+            for (String molecularProfileId : allMolecularProfileIdToCaseMap.keySet()) {
+                mutationCountByGeneListFromRepo.addAll(mutationService.getPatientCountByEntrezGeneIdsAndSampleIds(molecularProfileId,
+                    allMolecularProfileIdToCaseMap.get(molecularProfileId), null));
+            }
+            for (String molecularProfileId : group1MolecularProfileIdToCaseMap.keySet()) {
+                MolecularProfile molecularProfile = molecularProfileService.getMolecularProfile(molecularProfileId);
+                List<Sample> sampleList = sampleService.getAllSamplesOfPatientsInStudy(molecularProfile.getCancerStudyIdentifier(), group1MolecularProfileIdToCaseMap.get(molecularProfileId), "ID");
+                mutations.addAll(mutationService.fetchMutationsInMolecularProfile(molecularProfileId,
+                    sampleList.stream().map(Sample::getStableId).collect(Collectors.toList()), null, null, "ID", null, null,
+                    null, null));
+            }
         }
 
         List<MutationCountByGene> mutationCountByGeneList = new ArrayList<MutationCountByGene>(mutationCountByGeneListFromRepo);
-        return alterationEnrichmentUtil.createAlterationEnrichments(alteredIds.size(), unalteredIds.size(),
+        return alterationEnrichmentUtil.createAlterationEnrichments(molecularProfileCaseSet1.size(), molecularProfileCaseSet2.size(),
             mutationCountByGeneList, mutations, enrichmentType);
     }
 }
--- web/src/main/java/org/cbioportal/web/CopyNumberEnrichmentController.java
+++ web/src/main/java/org/cbioportal/web/CopyNumberEnrichmentController.java
@@ -3,25 +3,27 @@ package org.cbioportal.web;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import java.util.Collection;
 import org.cbioportal.model.AlterationEnrichment;
 import org.cbioportal.service.CopyNumberEnrichmentService;
 import org.cbioportal.service.exception.MolecularProfileNotFoundException;
 import org.cbioportal.web.config.annotation.InternalApi;
 import org.cbioportal.web.parameter.CopyNumberEnrichmentEventType;
-import org.cbioportal.web.parameter.EnrichmentFilter;
+import org.cbioportal.web.parameter.MultipleStudiesEnrichmentFilter;
 import org.cbioportal.web.parameter.EnrichmentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -35,23 +37,25 @@ public class CopyNumberEnrichmentController {
     @Autowired
     private CopyNumberEnrichmentService copyNumberEnrichmentService;
 
-    @PreAuthorize("hasPermission(#molecularProfileId, 'MolecularProfileId', 'read')")
-    @RequestMapping(value = "/molecular-profiles/{molecularProfileId}/copy-number-enrichments/fetch",
+    @PreAuthorize("hasPermission(#involvedCancerStudies, 'Collection<CancerStudyId>', 'read')")
+    @RequestMapping(value = "/copy-number-enrichments/fetch",
         method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Fetch copy number enrichments in a molecular profile")
     public ResponseEntity<List<AlterationEnrichment>> fetchCopyNumberEnrichments(
-        @ApiParam(required = true, value = "Molecular Profile ID e.g. acc_tcga_mutations")
-        @PathVariable String molecularProfileId,
+        @ApiIgnore // prevent reference to this attribute in the swagger-ui interface
+        @RequestAttribute(required = false, value = "involvedCancerStudies") Collection<String> involvedCancerStudies,
+        @ApiIgnore // prevent reference to this attribute in the swagger-ui interface. this attribute is needed for the @PreAuthorize tag above.
+        @RequestAttribute(required = false, value = "interceptedMultiStudyEnrichmentFilter") MultipleStudiesEnrichmentFilter interceptedMultiStudyEnrichmentFilter,
         @ApiParam("Type of the copy number event")
         @RequestParam(defaultValue = "HOMDEL") CopyNumberEnrichmentEventType copyNumberEventType,
         @ApiParam("Type of the enrichment e.g. SAMPLE or PATIENT")
         @RequestParam(defaultValue = "SAMPLE") EnrichmentType enrichmentType,
-        @ApiParam(required = true, value = "List of altered and unaltered Sample/Patient IDs")
-        @Valid @RequestBody EnrichmentFilter enrichmentFilter) throws MolecularProfileNotFoundException {
-
-        return new ResponseEntity<>(copyNumberEnrichmentService.getCopyNumberEnrichments(molecularProfileId,
-            enrichmentFilter.getAlteredIds(), enrichmentFilter.getUnalteredIds(),
-            copyNumberEventType.getAlterationTypes(), enrichmentType.name()), HttpStatus.OK);
+        @ApiParam(required = true, value = "List of entities")
+        @Valid @RequestBody(required = false) MultipleStudiesEnrichmentFilter multipleStudiesEnrichmentFilter) throws MolecularProfileNotFoundException {
+        return new ResponseEntity<>(
+                copyNumberEnrichmentService.getCopyNumberEnrichments(interceptedMultiStudyEnrichmentFilter.getMolecularProfileCaseSet1(),
+                        interceptedMultiStudyEnrichmentFilter.getMolecularProfileCaseSet2(), copyNumberEventType.getAlterationTypes(), enrichmentType.name()),
+                HttpStatus.OK);
     }
 }

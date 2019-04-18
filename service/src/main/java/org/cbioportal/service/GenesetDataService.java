fa203a8057fb3188def47d50ed504f043f9deda6
Ersin Ciftci <arseniusthegreat@gmail.com>
2019-04-18 16:08:01 -0400

    Apply the co-exp optimization to the new code

--- service/src/main/java/org/cbioportal/service/GenesetDataService.java
+++ service/src/main/java/org/cbioportal/service/GenesetDataService.java
@@ -2,6 +2,7 @@ package org.cbioportal.service;
 
 import java.util.List;
 
+import org.cbioportal.model.GenesetMolecularAlteration;
 import org.cbioportal.model.GenesetMolecularData;
 import org.cbioportal.service.exception.MolecularProfileNotFoundException;
 import org.cbioportal.service.exception.SampleListNotFoundException;
@@ -10,5 +11,7 @@ public interface GenesetDataService {
 
     List<GenesetMolecularData> fetchGenesetData(String geneticProfileId, List<String> sampleIds, List<String> genesetIds) throws MolecularProfileNotFoundException;
 
-	List<GenesetMolecularData> fetchGenesetData(String geneticProfileId, String sampleListId, List<String> genesetIds) throws MolecularProfileNotFoundException, SampleListNotFoundException;
+    List<GenesetMolecularData> fetchGenesetData(String geneticProfileId, String sampleListId, List<String> genesetIds) throws MolecularProfileNotFoundException, SampleListNotFoundException;
+    
+    List<GenesetMolecularAlteration> getGenesetAlterations(String molecularProfileId, List<String> genesetIds) throws MolecularProfileNotFoundException;
 }

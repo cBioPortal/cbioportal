15bd62ac9821f560ebc514422a07d3350d0959af
Karthik Kalletla <kalletlakarthik@gmail.com>
2019-05-07 14:20:07 -0400

    Allow overlapping samples in clinical-data-enrichments api

--- web/src/main/java/org/cbioportal/web/parameter/GroupFilter.java
+++ web/src/main/java/org/cbioportal/web/parameter/GroupFilter.java
@@ -1,13 +1,9 @@
 package org.cbioportal.web.parameter;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Size;
 
 public class GroupFilter implements Serializable {
@@ -15,17 +11,6 @@ public class GroupFilter implements Serializable {
     @Size(min = 2)
     @Valid
     private List<Group> groups;
-
-    @AssertTrue
-    private boolean isGroupsContainsDistinctSamples() {
-
-        List<SampleIdentifier> sampleIdentifiers = groups.stream().flatMap(x -> x.getSampleIdentifiers().stream())
-                .collect(Collectors.toList());
-
-        Set<SampleIdentifier> uniqSampleIdentifiers = new HashSet<>(sampleIdentifiers);
-
-        return uniqSampleIdentifiers.size() == sampleIdentifiers.size();
-    }
 
     public List<Group> getGroups() {
         return groups;

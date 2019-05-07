d169d5eb3c3d6fe2772286342c877f4a646e1dd2
Benjamin Gross <benjamin.gross@gmail.com>
2019-05-07 16:02:54 -0400

    Drop MyBatis caching and use EHCache instead - drop MyBatis configurations - option to enable/disable use of Ehcache (portal.properties.EXAMPLE defaults to disabled) - initial configuration options for EHCache, with some portal.properties settings     - ehcache.xml ehcache settings file available (can be edited to support heap-only or disk-only or hybrid configuration)     - disk persistence set to false in all cache configurations     - expiry policy set to none     - max-object-graph-size set to Long.MAX_VALUE (from 10000000) - mark repository functions as @Cacheable - enable cache event logging - track cache statistics - two caches available     - StaticRepositoryCacheOne - holds only some GeneRepository call values and sized approximately to hold all genes in actual use (30MB)     - GeneralRepositoryCache - holds all other cached repository calls - update cbioportal docs to explain configuration options Add /api/cacheStatistics endpoint for reporting cache content / sizes - endpoint can be enabled/disabled via portal.properties - adjust GlobalExceptionHandler to return a 404 NotFound status for requests on an invalid cache alias - store requested cache name in CacheNotFoundException - add unit tests for cache statistics controller     - mock CacheStatisticsService to throw CacheNotFoundException when requests for unknown cache alias are made     - test keysInCache endpoint and keyCountsPerClass endpoints with valid and invalid cache aliases Error fixed in cgds.sql (order of DROP statements must follow foreign key constraints) Other notes - xalan dependency excluded due to xml parsing errors - old core/src/main/resources/ehcache.xml config file dropped
    Co-authored-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>
    Co-authored-by: Avery Wang <18199796+averyniceday@users.noreply.github.com>
    Co-authored-by: Benjamin Gross <benjamin.gross@gmail.com>
    Co-authored-by: Manda Wilson <1458628+mandawilson@users.noreply.github.com>
    Co-authored-by: Robert Sheridan <7747489+sheridancbio@users.noreply.github.com>

---
+++ web/src/test/java/org/cbioportal/web/CacheStatsControllerTest.java
@@ -0,0 +1,105 @@
+package org.cbioportal.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.cbioportal.service.CacheStatisticsService;
+import org.cbioportal.service.exception.CacheNotFoundException;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("/applicationContext-web-test.xml")
+@Configuration
+public class CacheStatsControllerTest {
+
+    public static final String VALID_CACHE_ALIAS = "GeneralRepositoryCache";
+
+    public static final String INVALID_CACHE_ALIAS = "InvalidCacheForTesting";
+
+    @Autowired
+    private WebApplicationContext wac;
+    
+    private MockMvc mockMvc;
+
+    @Autowired
+    public CacheStatisticsService cacheStatisticsService;
+    
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+    
+    @Bean
+    public static CacheStatisticsService cacheStatisticsService() throws CacheNotFoundException {
+        CacheStatisticsService cacheStatisticsServiceMock = Mockito.mock(CacheStatisticsService.class);
+        Mockito.when(cacheStatisticsServiceMock.getKeyCountsPerClass(Mockito.anyString())).thenAnswer(new Answer<List<String>>() {
+            public List<String> answer(InvocationOnMock invocation) throws CacheNotFoundException {
+                Object[] args = invocation.getArguments();
+                String cacheAlias = (String)args[0];
+                if (VALID_CACHE_ALIAS.equals(cacheAlias)) {
+                    return new ArrayList<String>();
+                } 
+                throw new CacheNotFoundException(cacheAlias);
+            }
+        });
+        Mockito.when(cacheStatisticsServiceMock.getKeysInCache(Mockito.anyString())).thenAnswer(new Answer<List<String>>() {
+            public List<String> answer(InvocationOnMock invocation) throws CacheNotFoundException {
+                Object[] args = invocation.getArguments();
+                String cacheAlias = (String)args[0];
+                if (VALID_CACHE_ALIAS.equals(cacheAlias)) {
+                    return new ArrayList<String>();
+                } 
+                throw new CacheNotFoundException(cacheAlias);
+            }
+        });
+        return cacheStatisticsServiceMock;
+    }
+
+    @Test
+    public void testGetKeysInCache() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + VALID_CACHE_ALIAS + "/keysInCache")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testGetKeysInInvalidCache() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + INVALID_CACHE_ALIAS + "/keysInCache")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testGetKeyCountsPerClass() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + VALID_CACHE_ALIAS + "/keyCountsPerClass")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testGetKeyCountsPerClassInvalidCache() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/" + INVALID_CACHE_ALIAS + "/keyCountsPerClass")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+}

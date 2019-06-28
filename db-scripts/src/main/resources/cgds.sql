3f29ddee78382241d4245dfd58b735b35df27894
Ino de Bruijn <ino@ino.pm>
2019-06-28 18:18:04 -0400

    revert cgds sql

--- db-scripts/src/main/resources/cgds.sql
+++ db-scripts/src/main/resources/cgds.sql
@@ -82,6 +82,7 @@ DROP TABLE IF EXISTS `genetic_profile`;
 DROP TABLE IF EXISTS `uniprot_id_mapping`;
 DROP TABLE IF EXISTS `gene_alias`;
 DROP TABLE IF EXISTS `geneset_gene`;
+DROP TABLE IF EXISTS `reference_genome_gene`;
 DROP TABLE IF EXISTS `gene`;
 DROP TABLE IF EXISTS `sample_list_list`;
 DROP TABLE IF EXISTS `sample_list`;

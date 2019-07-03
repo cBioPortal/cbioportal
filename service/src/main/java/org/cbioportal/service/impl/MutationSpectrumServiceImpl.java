bdcd5c7d2de55c45aa75351065618838cd179fdc
Andrew <atc397@nyu.edu>
2019-07-03 18:22:36 -0400

    Added functions

--- service/src/main/java/org/cbioportal/service/impl/MutationSpectrumServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/MutationSpectrumServiceImpl.java
@@ -15,32 +15,32 @@ import java.util.stream.Collectors;
 
 @Service
 public class MutationSpectrumServiceImpl implements MutationSpectrumService {
-    
+
     @Autowired
     private MutationService mutationService;
-    
+
     @Override
-    public List<MutationSpectrum> getMutationSpectrums(String molecularProfileId, String sampleListId) 
+    public List<MutationSpectrum> getMutationSpectrums(String molecularProfileId, String sampleListId)
         throws MolecularProfileNotFoundException {
 
-        List<Mutation> mutations = mutationService.getMutationsInMolecularProfileBySampleListId(molecularProfileId, 
-            sampleListId, null, true, "SUMMARY", null, null, null, null);
-        
+        List<Mutation> mutations = mutationService.getMutationsInMolecularProfileBySampleListId(molecularProfileId,
+            sampleListId, null, true, "SUMMARY", null, null, null, null, null);
+
         return createMutationSpectrums(molecularProfileId, mutations);
     }
 
     @Override
-    public List<MutationSpectrum> fetchMutationSpectrums(String molecularProfileId, List<String> sampleIds) 
+    public List<MutationSpectrum> fetchMutationSpectrums(String molecularProfileId, List<String> sampleIds)
         throws MolecularProfileNotFoundException {
-        
-        List<Mutation> mutations = mutationService.fetchMutationsInMolecularProfile(molecularProfileId, sampleIds, null, 
+
+        List<Mutation> mutations = mutationService.fetchMutationsInMolecularProfile(molecularProfileId, sampleIds, null,
             true, "SUMMARY", null, null, null, null);
-        
+
         return createMutationSpectrums(molecularProfileId, mutations);
     }
 
     private List<MutationSpectrum> createMutationSpectrums(String molecularProfileId, List<Mutation> mutations) {
-        
+
         Map<String, List<Mutation>> mutationMap = mutations.stream().collect(Collectors.groupingBy(
             Mutation::getSampleId));
 
@@ -53,27 +53,27 @@ public class MutationSpectrumServiceImpl implements MutationSpectrumService {
             mutationSpectrum.setSampleId(sampleId);
             mutationSpectrum.setPatientId(mutationsInSample.get(0).getPatientId());
             mutationSpectrum.setStudyId(mutationsInSample.get(0).getStudyId());
-            mutationSpectrum.setCtoA(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "A") 
+            mutationSpectrum.setCtoA(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "A")
                 || checkSpectrum(m, "G", "T")).count()));
-            mutationSpectrum.setCtoG(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "G") 
+            mutationSpectrum.setCtoG(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "G")
                 || checkSpectrum(m, "G", "C")).count()));
-            mutationSpectrum.setCtoT(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "T") 
+            mutationSpectrum.setCtoT(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "C", "T")
                 || checkSpectrum(m, "G", "A")).count()));
-            mutationSpectrum.setTtoA(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "A") 
+            mutationSpectrum.setTtoA(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "A")
                 || checkSpectrum(m, "A", "T")).count()));
-            mutationSpectrum.setTtoC(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "C") 
+            mutationSpectrum.setTtoC(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "C")
                 || checkSpectrum(m, "A", "G")).count()));
-            mutationSpectrum.setTtoG(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "G") 
+            mutationSpectrum.setTtoG(Math.toIntExact(mutationsInSample.stream().filter(m -> checkSpectrum(m, "T", "G")
                 || checkSpectrum(m, "A", "C")).count()));
             mutationSpectrums.add(mutationSpectrum);
         }
 
         return mutationSpectrums;
     }
-    
+
     private boolean checkSpectrum(Mutation mutation, String referenceAllele, String tumorSeqAllele) {
-        
-        return mutation.getReferenceAllele().equals(referenceAllele) && 
+
+        return mutation.getReferenceAllele().equals(referenceAllele) &&
             mutation.getTumorSeqAllele().equals(tumorSeqAllele);
     }
 }

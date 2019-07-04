ea51357d40b4f867ea868c6c1611f08f906e566c
Andrew <atc397@nyu.edu>
2019-07-03 22:44:37 -0400

    Added functions

--- service/src/main/java/org/cbioportal/service/impl/MutationServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/MutationServiceImpl.java
@@ -59,7 +59,8 @@ public class MutationServiceImpl implements MutationService {
         List<Mutation> mutationList = mutationRepository.getMutationsInMolecularProfileByOncogenicity(molecularProfileId,
             sampleListId, entrezGeneIds, snpOnly, projection, pageSize, pageNumber, sortBy, direction, oncogenicity);
 
-        mutationList.forEach(mutation -> chromosomeCalculator.setOncogenicity("oncogenicity"));
+        mutationList.forEach(mutation -> chromosomeCalculator.setChromosome(mutation.getGene()));
+        mutationList.forEach(mutation -> mutation.setOncogenicity("Mutation"));
         return mutationList;
     }
 

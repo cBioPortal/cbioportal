4fb750f4b3c6adeb6bf89b263568c4f304c0310e
drose <doori.rose@gmail.com>
2019-08-01 14:07:58 -0400

    add util classes

--- persistence/persistence-spark/src/test/java/org/cbioportal/persistence/spark/SampleSparkRepositoryTest.java
+++ persistence/persistence-spark/src/test/java/org/cbioportal/persistence/spark/SampleSparkRepositoryTest.java
@@ -2,6 +2,7 @@ package org.cbioportal.persistence.spark;
 
 import org.apache.spark.sql.*;
 import org.cbioportal.model.Sample;
+import org.cbioportal.persistence.spark.util.ParquetLoader;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,6 +32,9 @@ public class SampleSparkRepositoryTest {
     @Mock
     private SparkSession spark;
 
+    @Mock
+    private ParquetLoader parquetLoader;
+    
     @InjectMocks
     private SampleSparkRepository sampleSparkRepository;
 
@@ -38,7 +42,6 @@ public class SampleSparkRepositoryTest {
 
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this);
 
         ds = mock(Dataset.class);
         DataFrameReader dfr = mock(DataFrameReader.class);
@@ -47,32 +50,62 @@ public class SampleSparkRepositoryTest {
         when(dfr.parquet(any(Seq.class))).thenReturn(ds);
         when(spark.sql(anyString())).thenReturn(ds);
         when(ds.withColumn(anyString(), any(Column.class))).thenReturn(ds);
+        when(parquetLoader.loadStudyFiles(any(SparkSession.class), anySet(), anyString(), anyBoolean())).thenReturn(ds);
     }
 
     @Test
     public void testFetchSamplesSummary() {
 
-        List<Row> res = Arrays.asList(RowFactory.create("patientId1", "sampleId1", "type1", "studyId"),
-            RowFactory.create("patientId2", "TCGA-AC-A6IX-06", "type2", "studyId"));
+        List<Row> res = Arrays.asList(
+            RowFactory.create("patientId", "TCGA-AC-A6IX-01", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-02", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-03", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-04", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-06", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-10", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-11", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "TCGA-AC-A6IX-19", "msk_impact_2017", "type"),
+            RowFactory.create("patientId", "sampleId", "msk_impact_2017", "Solid Tissues Normal"),
+            RowFactory.create("patientId", "sampleId", "msk_impact_2017", "type"));
+
         when(ds.collectAsList()).thenReturn(res);
 
         List<Sample> result = sampleSparkRepository.fetchSamples(Arrays.asList("msk_impact_2017"), null, "SUMMARY");
 
-        Assert.assertEquals(2, result.size());
-        Assert.assertEquals(Sample.SampleType.PRIMARY_SOLID_TUMOR, result.get(0).getSampleType());
-        Assert.assertEquals(Sample.SampleType.METASTATIC, result.get(1).getSampleType());
+        Assert.assertEquals(10, result.size());
+        Sample sample1 = result.get(0);
+        Assert.assertEquals(Sample.SampleType.PRIMARY_SOLID_TUMOR, sample1.getSampleType());
+        Assert.assertEquals("msk_impact_2017", sample1.getCancerStudyIdentifier());
+        
+        Sample sample2 = result.get(1);
+        Assert.assertEquals(Sample.SampleType.RECURRENT_SOLID_TUMOR, sample2.getSampleType());
+        Assert.assertEquals("msk_impact_2017", sample2.getCancerStudyIdentifier());
+        Assert.assertEquals("TCGA-AC-A6IX-02", sample2.getStableId());
+        
+        Assert.assertEquals(Sample.SampleType.PRIMARY_BLOOD_TUMOR, result.get(2).getSampleType());
+        Assert.assertEquals(Sample.SampleType.RECURRENT_BLOOD_TUMOR, result.get(3).getSampleType());
+        Assert.assertEquals(Sample.SampleType.METASTATIC, result.get(4).getSampleType());
+        Assert.assertEquals(Sample.SampleType.BLOOD_NORMAL, result.get(5).getSampleType());
+        Assert.assertEquals(Sample.SampleType.SOLID_NORMAL, result.get(6).getSampleType());
+        Assert.assertEquals(Sample.SampleType.PRIMARY_SOLID_TUMOR, result.get(7).getSampleType());
+        Assert.assertEquals(Sample.SampleType.SOLID_NORMAL, result.get(8).getSampleType());
+        Assert.assertEquals(Sample.SampleType.PRIMARY_SOLID_TUMOR, result.get(9).getSampleType());
     }
 
     @Test
     public void testFetchSamplesId() {
 
-        List<Row> res = Arrays.asList(RowFactory.create("patientId", "sampleId", "studyId"));
+        List<Row> res = Arrays.asList(RowFactory.create("P-1000", "P-1000-01", "msk_impact_2017"));
         when(ds.collectAsList()).thenReturn(res);
         when(ds.where(any(Column.class))).thenReturn(ds);
         when(ds.col(anyString())).thenReturn(mock(Column.class));
 
-        List<Sample> result = sampleSparkRepository.fetchSamples(Arrays.asList("msk_impact_2017"), Arrays.asList("P-1000"), "ID");
+        List<Sample> result = sampleSparkRepository.fetchSamples(Arrays.asList("msk_impact_2017"), Arrays.asList("P-1000-01"), "ID");
 
         Assert.assertEquals(1, result.size());
+        Sample sample = result.get(0);
+        Assert.assertEquals("msk_impact_2017", sample.getCancerStudyIdentifier());
+        Assert.assertEquals("P-1000-01", sample.getStableId());
+        Assert.assertNull(sample.getSampleType());
     }
 }

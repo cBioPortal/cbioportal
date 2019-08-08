166665550060d1b7bfcd811dd4e74131269046c1
Robert Sheridan <sheridan@cbio.mskcc.org>
2019-08-08 12:39:22 -0400

    add unit tests for cache statistics controller - mock CacheStatisticsService to throw CacheNotFoundException when requests for unknown cache alias are made - store requested cache name in CacheNotFoundException - test keysInCache endpoint and keyCountsPerClass endpoints with valid and invalid cache aliases adjust GlobalExceptionHandler to return a 404 NotFound status for requests on an invalid cache alias
    Co-authored-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- web/src/test/java/org/cbioportal/web/SampleListControllerTest.java
+++ web/src/test/java/org/cbioportal/web/SampleListControllerTest.java
@@ -4,7 +4,6 @@ import org.cbioportal.model.CancerStudy;
 import org.cbioportal.model.SampleList;
 import org.cbioportal.model.meta.BaseMeta;
 import org.cbioportal.service.SampleListService;
-import org.cbioportal.service.CacheStatisticsService;
 import org.cbioportal.service.exception.SampleListNotFoundException;
 import org.cbioportal.web.parameter.HeaderKeyConstants;
 import org.hamcrest.Matchers;
@@ -71,11 +70,6 @@ public class SampleListControllerTest {
     @Bean
     public SampleListService sampleListService() {
         return Mockito.mock(SampleListService.class);
-    }
-
-    @Bean
-    public CacheStatisticsService cacheStatisticsService() {
-        return Mockito.mock(CacheStatisticsService .class);
     }
 
     @Before

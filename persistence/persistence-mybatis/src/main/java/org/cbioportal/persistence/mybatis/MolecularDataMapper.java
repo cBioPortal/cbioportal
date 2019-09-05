d356f6b357d6b58c22b4f0b9ab87232c367fea5a
Benjamin Gross <benjamin.gross@gmail.com>
2019-09-05 15:03:34 -0400

    Revert of cursor code

--- persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/MolecularDataMapper.java
+++ persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/MolecularDataMapper.java
@@ -6,7 +6,6 @@ import org.cbioportal.model.GenesetMolecularAlteration;
 import org.cbioportal.model.TreatmentMolecularAlteration;
 
 import java.util.List;
-import org.apache.ibatis.cursor.Cursor;
 
 public interface MolecularDataMapper {
 
@@ -14,9 +13,6 @@ public interface MolecularDataMapper {
 
     List<GeneMolecularAlteration> getGeneMolecularAlterations(String molecularProfileId, List<Integer> entrezGeneIds,
                                                               String projection);
-
-    Cursor<GeneMolecularAlteration> getGeneMolecularAlterationsIter(String molecularProfileId, List<Integer> entrezGeneIds,
-                                                                    String projection);
 
     List<GeneMolecularAlteration> getGeneMolecularAlterationsInMultipleMolecularProfiles(List<String> molecularProfileIds, 
                                                                                          List<Integer> entrezGeneIds, String projection);

681d1a278daea54ca8d94b44fc01a4c1dc7cfc3e
gaofei zhao <zgfilx@163.com>
2019-10-23 15:14:33 -0400

    Add Cacheable annotations

--- persistence/persistence-api/src/main/java/org/cbioportal/persistence/GenericAssayRepository.java
+++ persistence/persistence-api/src/main/java/org/cbioportal/persistence/GenericAssayRepository.java
@@ -4,14 +4,19 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.cbioportal.model.meta.GenericAssayMeta;
+import org.springframework.cache.annotation.Cacheable;
 
 public interface GenericAssayRepository {
 
+    @Cacheable(cacheNames = "GeneralRepositoryCache", condition = "@cacheEnabledConfig.getEnabled()")
     List<GenericAssayMeta> getGenericAssayMeta(List<String> stableIds);
 
+    @Cacheable(cacheNames = "GeneralRepositoryCache", condition = "@cacheEnabledConfig.getEnabled()")
     List<String> getGenericAssayStableIdsByMolecularIds(List<String> molecularProfileIds);
 
+    @Cacheable(cacheNames = "GeneralRepositoryCache", condition = "@cacheEnabledConfig.getEnabled()")
     int getGeneticEntityIdByStableId(String stableId);
 
+    @Cacheable(cacheNames = "GeneralRepositoryCache", condition = "@cacheEnabledConfig.getEnabled()")
     List<HashMap<String, String>> getGenericAssayMetaPropertiesMap(int geneticEntityId);
 }

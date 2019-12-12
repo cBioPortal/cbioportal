7767a4cb68bf2fb9fcb28fb8acdc17fdffdcbd51
Luke Sikina <lucas@ds.dfci.harvard.edu>
2019-12-12 16:08:01 -0500

    Memoize full genome response - Create primitive caching for problematic endpoint - To be removed when Ehcache works - Necessary if we are going to allow the frontend to use this endpoint again

--- web/src/main/java/org/cbioportal/web/StaticDataTimestampController.java
+++ web/src/main/java/org/cbioportal/web/StaticDataTimestampController.java
@@ -2,7 +2,6 @@ package org.cbioportal.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.cbioportal.model.TableTimestampPair;
 import org.cbioportal.service.StaticDataTimestampService;
 import org.cbioportal.web.config.annotation.InternalApi;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +20,14 @@ import java.util.*;
 @Validated
 @Api(tags = "Timestamps", description = " ")
 public class StaticDataTimestampController {
-    private static final List<String> tables = Arrays.asList("gene", "reference_genome_gene");
+    public static final List<String> TIMESTAMP_TABLES = Arrays.asList("gene", "reference_genome_gene");
     @Autowired
     private StaticDataTimestampService service;
     
     @RequestMapping(value = "/timestamps", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get the last time each static resource was updated")
     public ResponseEntity<Map<String, String>> getAllTimestamps() {
-        return new ResponseEntity<>(service.getTimestamps(tables), HttpStatus.OK);
+        return new ResponseEntity<>(service.getTimestamps(TIMESTAMP_TABLES), HttpStatus.OK);
     }
     
 }

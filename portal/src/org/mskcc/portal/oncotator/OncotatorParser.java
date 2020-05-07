848a7f5ffcbffff453930557bf3f0710ef43f398
Niklas Reimer <niklas@nr205.de>
2020-05-07 14:38:46 +0200

    fix jackson dependent stuff

--- portal/src/org/mskcc/portal/oncotator/OncotatorParser.java
+++ portal/src/org/mskcc/portal/oncotator/OncotatorParser.java
@@ -1,7 +1,7 @@
 package org.mskcc.portal.oncotator;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 

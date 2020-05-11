0ea47f3efd12e0463485e6065257bdefe5b4b51b
Benjamin Gross <benjamin.gross@gmail.com>
2020-05-11 12:35:31 -0400

    Unit test failure fixes

--- core/src/main/java/org/mskcc/cbio/portal/dao/DaoTextCache.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoTextCache.java
@@ -42,10 +42,10 @@ import java.util.Date;
 import org.mskcc.cbio.portal.util.CacheUtil;
 
 public class DaoTextCache
-{	
+{
 	/**
 	 * Generates an MD5 key for the given text.
-	 * 
+	 *
 	 * @param text	text to hashed
 	 * @return		an MD5 key corresponding to the given text
 	 */
@@ -53,21 +53,21 @@ public class DaoTextCache
 	{
 		return CacheUtil.md5sum(text);
 	}
-	
+
 	/**
 	 * Inserts the given key and text pair to the database.
-	 *  
+	 *
 	 * @param key			key value
 	 * @param text			text value
 	 * @return
-	 * @throws DaoException	if an entity already exists with the same key 
+	 * @throws DaoException	if an entity already exists with the same key
 	 */
 	public int cacheText(String key, String text) throws DaoException
 	{
 		Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try
         {
 			con = JdbcUtil.getDbConnection(DaoTextCache.class);
@@ -76,7 +76,6 @@ public class DaoTextCache
 			        		+ "VALUES (?,?,NOW())");
 			pstmt.setString(1, key);
 			pstmt.setString(2, text);
-			
 			return pstmt.executeUpdate();
         }
         catch (SQLException e)
@@ -88,10 +87,10 @@ public class DaoTextCache
             JdbcUtil.closeAll(DaoTextCache.class, con, pstmt, rs);
         }
 	}
-	
+
 	/**
 	 * Retrieves the text corresponding to the given key form the DB.
-	 * 
+	 *
 	 * @param key	cache key
 	 * @return
 	 * @throws DaoException
@@ -101,7 +100,7 @@ public class DaoTextCache
     	Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try
         {
             con = JdbcUtil.getDbConnection(DaoTextCache.class);
@@ -109,12 +108,10 @@ public class DaoTextCache
                     "WHERE HASH_KEY=?");
             pstmt.setString(1, key);
             rs = pstmt.executeQuery();
-            
             if (rs.next())
             {
                 return rs.getString("TEXT");
             }
-            
             return null;
         }
         catch (SQLException e)
@@ -126,10 +123,10 @@ public class DaoTextCache
             JdbcUtil.closeAll(DaoTextCache.class, con, pstmt, rs);
         }
     }
-    
+
     /**
      * Deletes all records in the table.
-     * 
+     *
      * @throws DaoException
      */
     public void deleteAllKeys() throws DaoException
@@ -137,7 +134,7 @@ public class DaoTextCache
     	Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try
         {
             con = JdbcUtil.getDbConnection(DaoTextCache.class);
@@ -147,42 +144,41 @@ public class DaoTextCache
         catch (SQLException e)
         {
             throw new DaoException(e);
-        } 
+        }
         finally
         {
             JdbcUtil.closeAll(DaoTextCache.class, con, pstmt, rs);
         }
     }
-    
+
     /**
      * Remove records older than the specified date.
-     * 
+     *
      * @param date	threshold date
-     * @throws DaoException 
+     * @throws DaoException
      */
     public void purgeOldKeys(Date date) throws DaoException
     {
     	Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        
+
         try
         {
             con = JdbcUtil.getDbConnection(DaoTextCache.class);
             pstmt = con.prepareStatement("DELETE FROM text_cache " +
             		"WHERE `DATE_TIME_STAMP` <= ?");
-            
             // create date_time_stamp string using the given date
             // (java.sql package does not have a proper "datetime" type support)
             SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-            
+            formatter.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
             pstmt.setString(1, formatter.format(date));
             pstmt.executeUpdate();
         }
         catch (SQLException e)
         {
             throw new DaoException(e);
-        } 
+        }
         finally
         {
             JdbcUtil.closeAll(DaoTextCache.class, con, pstmt, rs);

dc00865a4584a9bbc9231d4a5aa0861ca73b33d9
tweep <tweep@users.noreply.github.com>
2020-05-15 08:08:08 -0700

    Use environment perl instead of hard-coded system perl  (#7503)
    Don't use system perl - make this flexible and use the environment perl. This helps users who don't have a perl in /usr/bin/perl

--- core/src/main/scripts/calculateCoExpression.pl
+++ core/src/main/scripts/calculateCoExpression.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 require "../scripts/env.pl";
 
 system ("$JAVA_HOME/bin/java -Xmx2048M -XX:-UseGCOverheadLimit -cp $cp -DPORTAL_HOME='$portalHome' org.mskcc.cbio.portal.scripts.CalculateCoexpression @ARGV");
--- core/src/main/scripts/importTimelineData.pl
+++ core/src/main/scripts/importTimelineData.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 require "../scripts/env.pl";
 
 exec("$JAVA_HOME/bin/java -Xmx1524M -cp $cp -DPORTAL_HOME='$portalHome' org.mskcc.cbio.portal.scripts.ImportTimelineData @ARGV");

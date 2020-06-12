8da05bb64b982956bef8393ab8f512fafa75eb9b
Benjamin Gross <benjamin.gross@gmail.com>
2020-06-12 14:44:25 -0400

    Final PR to drop business module from codebase.

--- business/src/main/java/org/mskcc/cbio/portal/repository/MutationalSignatureRepository.java
+++
@@ -1,9 +0,0 @@
-package org.mskcc.cbio.portal.repository;
-
-import java.util.List;
-import org.mskcc.cbio.portal.model.SNPCount;
-
-public interface MutationalSignatureRepository {
-	List<SNPCount> getSNPCounts(String geneticProfileStableId, List<String> sampleStableIds);
-	List<SNPCount> getSNPCounts(String geneticProfileStableId);
-}

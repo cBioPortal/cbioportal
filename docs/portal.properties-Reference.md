a421b2798325215ca8edba790a2cc2b9d978c2e2
Manda Wilson <wilson@cbio.mskcc.org>
2020-07-17 17:59:54 +0100

    Added spring profiles for redis and ehcache (#7696)
    * updated ip addresses for redis servers
    * changed ttl to 0 and added comments
    * added spring profiles for redis and ehcache

--- docs/portal.properties-Reference.md
+++ docs/portal.properties-Reference.md
@@ -197,7 +197,7 @@ google_analytics_profile_id
 
 The portal supports password authentication via Google+. Before you start you need to setup a google account that will own the authentication API. Follow https://developers.google.com/identity/sign-in/web/devconsole-project to get clientID and secret. Fill it in portal.properties:
 ```
-googleplus.consumer.key=195047654890-499gl89hj65j8d2eorqe0jvjnfaxcln0.apps.googleusercontent.com 
+googleplus.consumer.key=195047654890-499gl89hj65j8d2eorqe0jvjnfaxcln0.apps.googleusercontent.com
 googleplus.consumer.secret=2jCfg4SPWdGfXF44WC588dK
 ```
 (note: these are just examples, you need to get your own) You will also need to go to "Google+ API" and click Enable button. In case of problems make sure to enable DEBUG logging for org.springframework.social and org.springframework.security.web.authentication.
@@ -269,11 +269,11 @@ oncoprint.defaultview=sample
 ```
 
 # Custom annotation of driver and passenger mutations
-cBioPortal supports 2 formats to add custom annotations for driver and passenger mutations. 
+cBioPortal supports 2 formats to add custom annotations for driver and passenger mutations.
 1. **cbp_driver**: This will define whether a mutation is a driver or not.
-2. **cbp_driver_tiers**: This can be used to define multiple classes of driver mutations. 
+2. **cbp_driver_tiers**: This can be used to define multiple classes of driver mutations.
 
-These data formats are described in the [cBioPortal MAF specifications](File-Formats.md#extending-the-maf-format). 
+These data formats are described in the [cBioPortal MAF specifications](File-Formats.md#extending-the-maf-format).
 
 #### Enabling custom annotations in the OncoPrint
 To enable functionality for one or both types of custom annotations, enter values for the following properties. These labels will appear in the OncoPrint's "Mutation color" menu.
@@ -293,7 +293,7 @@ If you want to disable the automatic selection of OncoKB and hotspots as annotat
 ```
 oncoprint.oncokb.default=true|false
 oncoprint.hotspots.default=true|false
-``` 
+```
 
 #### Automatic hiding of variants of unknown significance (VUS)
 By default, the selection box to hide VUS mutations is unchecked. If you want to automatically hide VUS, set this property to `true`. Default is `false`.
@@ -344,22 +344,22 @@ This gene set will add the following in the query box:
 # Ehcache Settings
 cBioPortal is supported on the backend with Ehcache. The configuration, size, and location of these caches are configurable from within portal.properties through the following properties.
 
-The cache type is set using `ehcache.cache_type`. Valid values are `none`, `heap` (heap-only), `disk` (disk-only), and `hybrid` (disk + heap). By default, `ehcache.cache_type` is set to `none` which disables the cache. When the cache is disabled, no responses will be stored in the cache. 
+The cache type is set using `persistence.cache_type`. Valid values are `no-cache`, `redis` (redis), `ehache-heap` (ehcache heap-only), `ehache-disk` (ehcache disk-only), and `ehache-hybrid` (ehcache disk + heap). By default, `persistence.cache_type` is set to `no-cache` which disables the cache. When the cache is disabled, no responses will be stored in the cache.
 ```
-ehcache.cache_type=[none or heap or disk or hybrid]
+persistence.cache_type=[no-cache or ehache-heap or ehcache-disk or ehcache-hybrid or redis]
 ```
 
-Ehcache initializes caches using a template found in an Ehcache xml configuration file. When caching is enabled, set `ehcache.xml_configuration` to the name of the Ehcache xml configuration file. The default provided is `ehcache.xml`; to change the cache template, directly edit this file. Alternatively, you can create your own Ehcache xml configuration file, place it under `/persistence/persistence-api/src/main/resources/` and set `ehcache.xml_configuration` to `/[Ehcache xml configuration filename]`.  
+Ehcache initializes caches using a template found in an Ehcache xml configuration file. When caching is enabled, set `ehcache.xml_configuration` to the name of the Ehcache xml configuration file. The default provided is `ehcache.xml`; to change the cache template, directly edit this file. Alternatively, you can create your own Ehcache xml configuration file, place it under `/persistence/persistence-api/src/main/resources/` and set `ehcache.xml_configuration` to `/[Ehcache xml configuration filename]`.
 ```
 ehcache.xml_configuration=
 ```
 
-If the cache is configured to use disk resources, users must make a directory available and set it with the `ehcache.persistence_path` property. Ehcache will create separate directories under the provided path for each cache defined in the ehcache.xml_configuration file. 
+If the cache is configured to use disk resources, users must make a directory available and set it with the `ehcache.persistence_path` property. Ehcache will create separate directories under the provided path for each cache defined in the ehcache.xml_configuration file.
 ```
 ehcache.persistence_path=[location on the disk filesystem where Ehcache can write the cache to /tmp/]
 ```
 
-Cache size must be set for heap and/or disk depending on which are in use; Ehcache requires disk size to be greater than heap size in a hybrid configuration. Zero is not a supported size and will cause an exception. Units are in megabytes. Default values are provided. The general repository cache is specified to use 1024MB of heap and 4096MB of disk. The static repository cache is specified to use 30MB of heap and 32MB of disk. For installations with increased traffic or data, cache sizes can be increased to further improve performance. 
+Cache size must be set for heap and/or disk depending on which are in use; Ehcache requires disk size to be greater than heap size in a hybrid configuration. Zero is not a supported size and will cause an exception. Units are in megabytes. Default values are provided. The general repository cache is specified to use 1024MB of heap and 4096MB of disk. The static repository cache is specified to use 30MB of heap and 32MB of disk. For installations with increased traffic or data, cache sizes can be increased to further improve performance.
 ```
 ehcache.general_repository_cache.max_mega_bytes_heap=
 ehcache.general_repository_cache.max_mega_bytes_local_disk=
@@ -389,7 +389,7 @@ For a list of counts of keys in cache per repository class:
 /api/[name of cache]/keyCountsPerClass
 ```
 
-**WARNING**: It must be noted that since cache statistics endpoint returns data on cache keys, the endpoint may expose otherwise hidden database query parameters such as sample identifiers, study names, etc. Generally, it is recommended that the endpoint only be turned on during cache-related development for testing. Deployers of a protected portal where users only have authorities to a subset of studies should carefully consider whether or not to turn on the cache statistics endpoint, as it does not filter the results. 
+**WARNING**: It must be noted that since cache statistics endpoint returns data on cache keys, the endpoint may expose otherwise hidden database query parameters such as sample identifiers, study names, etc. Generally, it is recommended that the endpoint only be turned on during cache-related development for testing. Deployers of a protected portal where users only have authorities to a subset of studies should carefully consider whether or not to turn on the cache statistics endpoint, as it does not filter the results.
 
 For more information on Ehcache, refer to the official documentation [here](https://www.ehcache.org/documentation/3.7/index.html)
 
@@ -397,7 +397,7 @@ For more information on how Ehcache is implemented in cBioPortal refer to the [C
 
 # Enable GSVA functionality
 
-[GSVA functionality](https://github.com/cBioPortal/cbioportal/blob/master/docs/File-Formats.md#gene-set-data) can be enabled by uncommenting this line (and making sure it is set to `true`): 
+[GSVA functionality](https://github.com/cBioPortal/cbioportal/blob/master/docs/File-Formats.md#gene-set-data) can be enabled by uncommenting this line (and making sure it is set to `true`):
 ```
 skin.show_gsva=true
 ```
--- persistence/persistence-api/src/main/java/org/cbioportal/persistence/util/CustomRedisCachingProvider.java
+++ persistence/persistence-api/src/main/java/org/cbioportal/persistence/util/CustomRedisCachingProvider.java
@@ -33,12 +33,8 @@
 package org.cbioportal.persistence.util;
 
 import java.io.*;
-import java.net.URL;
 import java.util.*;
-import org.cbioportal.persistence.CacheEnabledConfig;
 import org.redisson.Redisson;
-import org.redisson.api.RKeys;
-import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.cache.CacheConfig;
@@ -51,26 +47,8 @@ public class CustomRedisCachingProvider {
 
     private static final Logger LOG = LoggerFactory.getLogger(CustomRedisCachingProvider.class);
 
-    @Value("${ehcache.xml_configuration:/ehcache.xml}")
-    private String xmlConfigurationFile;
-
-    @Value("${ehcache.cache_type:none}")
+    @Value("${persistence.cache_type:no-cache}")
     private String cacheType;
-
-    @Value("${ehcache.general_repository_cache.max_mega_bytes_heap:1024}")
-    private Integer generalRepositoryCacheMaxMegaBytes;
-
-    @Value("${ehcache.static_repository_cache_one.max_mega_bytes_heap:30}")
-    private Integer staticRepositoryCacheOneMaxMegaBytes;
-
-    @Value("${ehcache.persistence_path:/tmp/}")
-    private String persistencePath;
-
-    @Value("${ehcache.general_repository_cache.max_mega_bytes_local_disk:4096}")
-    private Integer generalRepositoryCacheMaxMegaBytesLocalDisk;
-
-    @Value("${ehcache.static_repository_cache_one.max_mega_bytes_local_disk:32}")
-    private Integer staticRepositoryCacheOneMaxMegaBytesLocalDisk;
 
     @Value("${app.name:cbioportal}")
     private String appName;
@@ -78,9 +56,9 @@ public class CustomRedisCachingProvider {
     public RedissonClient getRedissionClient() {
         Config config = new Config();
         config.useMasterSlaveServers()
-                .setMasterAddress("redis://100.96.35.3:6379")
-                .addSlaveAddress("redis://100.96.22.54:6379")
-                .addSlaveAddress("redis://100.96.34.8:6379")
+                .setMasterAddress("redis://100.96.35.44:6379")
+                .addSlaveAddress("redis://100.96.39.22:6379")
+                .addSlaveAddress("redis://100.96.13.106:6379")
                 .setPassword("PASSWORD_GOES_HERE");
         RedissonClient redissonClient = Redisson.create(config);
         LOG.error("Created Redisson Client: " + redissonClient);
@@ -92,236 +70,18 @@ public class CustomRedisCachingProvider {
         RedissonSpringCacheManager toReturn = null;
         
         Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
-        // create "testMap" cache with ttl = 24 hours and maxIdleTime = 12 hours
-        config.put(appName + "GeneralRepositoryCache", new CacheConfig(24*60*60*1000, 12*60*60*1000));
-        config.put(appName + "StaticRepositoryCacheOne", new CacheConfig(24*60*60*1000, 12*60*60*1000));
-        toReturn = new RedissonSpringCacheManager(redissonClient, config);
-        LOG.info("Created Redisson CacheManager: " + toReturn);
-
-        LOG.error("KEYS BEFORE INSERT:");
-        RKeys keys = redissonClient.getKeys();
-        Iterable<String> allKeys = keys.getKeys();
-        for (String key : allKeys) {
-            LOG.error("  key found: " + key);
-        }
-        LOG.error("INSERTING NEW TEST KEY");
-        RBucket<String> bucket = redissonClient.getBucket("testBucket");
-        bucket.set("testBucketValue");
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-        }
-        
-        LOG.error("KEYS AFTER INSERT:");
-        keys = redissonClient.getKeys();
-        allKeys = keys.getKeys();
-        for (String key : allKeys) {
-            LOG.error("  key found: " + key);
-        }
-
-/*
-        try {
-            if (CacheEnabledConfig.enableCache(cacheType)) {
-                detectCacheConfigurationErrorsAndLog();
-                LOG.info("Caching is enabled, using '" + xmlConfigurationFile + "' for configuration");
-                XmlConfiguration xmlConfiguration = new XmlConfiguration(getClass().getResource(xmlConfigurationFile));
-
-                // initilize configurations specific to each individual cache (by template)
-                // to add new cache - create cache configuration with its own resource pool + template
-                ResourcePoolsBuilder generalRepositoryCacheResourcePoolsBuilder = ResourcePoolsBuilder.newResourcePoolsBuilder();
-                ResourcePoolsBuilder staticRepositoryCacheOneResourcePoolsBuilder = ResourcePoolsBuilder.newResourcePoolsBuilder();
-
-                // Set up heap resources as long as not disk-only
-                if (!cacheType.equalsIgnoreCase(CacheEnabledConfig.DISK)) {
-                    generalRepositoryCacheResourcePoolsBuilder = generalRepositoryCacheResourcePoolsBuilder.heap(generalRepositoryCacheMaxMegaBytes, MemoryUnit.MB);
-                    staticRepositoryCacheOneResourcePoolsBuilder = staticRepositoryCacheOneResourcePoolsBuilder.heap(staticRepositoryCacheOneMaxMegaBytes, MemoryUnit.MB);
-                }
-                // Set up disk resources as long as not heap-only
-                // will default to using /tmp -- let Ehcache throw exception if persistence path is invalid (locked or otherwise)
-                if (!cacheType.equalsIgnoreCase(CacheEnabledConfig.HEAP)) {
-                    generalRepositoryCacheResourcePoolsBuilder = generalRepositoryCacheResourcePoolsBuilder.disk(generalRepositoryCacheMaxMegaBytesLocalDisk, MemoryUnit.MB);
-                    staticRepositoryCacheOneResourcePoolsBuilder = staticRepositoryCacheOneResourcePoolsBuilder.disk(staticRepositoryCacheOneMaxMegaBytesLocalDisk, MemoryUnit.MB);
-                }
-
-                CacheConfiguration<Object, Object> generalRepositoryCacheConfiguration = xmlConfiguration.newCacheConfigurationBuilderFromTemplate("RepositoryCacheTemplate",
-                        Object.class, Object.class, generalRepositoryCacheResourcePoolsBuilder)
-                    .withSizeOfMaxObjectGraph(Long.MAX_VALUE)
-                    .withSizeOfMaxObjectSize(Long.MAX_VALUE, MemoryUnit.B)
-                    .build();
-                CacheConfiguration<Object, Object> staticRepositoryCacheOneConfiguration = xmlConfiguration.newCacheConfigurationBuilderFromTemplate("RepositoryCacheTemplate",
-                        Object.class, Object.class, staticRepositoryCacheOneResourcePoolsBuilder)
-                    .withSizeOfMaxObjectGraph(Long.MAX_VALUE)
-                    .withSizeOfMaxObjectSize(Long.MAX_VALUE, MemoryUnit.B)
-                    .build();
-
-                // places caches in a map which will be used to create cache manager
-                Map<String, CacheConfiguration<?, ?>> caches = new HashMap<>();
-                caches.put("GeneralRepositoryCache", generalRepositoryCacheConfiguration);
-                caches.put("StaticRepositoryCacheOne", staticRepositoryCacheOneConfiguration);
-
-                Configuration configuration = null;
-                if (cacheType.equalsIgnoreCase(CacheEnabledConfig.HEAP)) {
-                    configuration = new DefaultConfiguration(caches, this.getDefaultClassLoader());
-                } else { // add persistence configuration if cacheType is either disk-only or hybrid
-                    File persistenceFile = new File(persistencePath);
-                    configuration = new DefaultConfiguration(caches, this.getDefaultClassLoader(), new DefaultPersistenceConfiguration(persistenceFile));
-                }
-
-                toReturn = this.getCacheManager(this.getDefaultURI(), configuration);
-            } else {
-                LOG.info("Caching is disabled");
-                // we can not really disable caching,
-                // we can not make a cache of 0 objects,
-                // and we can not make a heap of memory size 0, so make a tiny heap
-                CacheConfiguration<Object, Object> generalRepositoryCacheConfiguration = CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class,
-                    Object.class,
-                    ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1, MemoryUnit.B)).build();
-                CacheConfiguration<Object, Object> staticRepositoryCacheOneConfiguration = CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class,
-                    Object.class,
-                    ResourcePoolsBuilder.newResourcePoolsBuilder().heap(1, MemoryUnit.B)).build();
-
-                Map<String, CacheConfiguration<?, ?>> caches = new HashMap<>();
-                caches.put("GeneralRepositoryCache", generalRepositoryCacheConfiguration);
-                caches.put("StaticRepositoryCacheOne", staticRepositoryCacheOneConfiguration);
-
-                Configuration configuration = new DefaultConfiguration(caches, this.getDefaultClassLoader());
-
-                toReturn = this.getCacheManager(this.getDefaultURI(), configuration);
-            }
-        }
-        catch (Exception e) {
-            LOG.error(e.getClass().getName() + ": " + e.getMessage());
-            StringWriter stackTrace = new StringWriter();
-            e.printStackTrace(new PrintWriter(stackTrace));
-            LOG.error(stackTrace.toString());
-        }
-*/
-        return toReturn;
+        /*
+            create cache with ttl and maxIdleTime
+            ttl - - time to live for key\value entry in milliseconds. If 0 then time to live doesn't affect entry expiration.
+            maxIdleTime - - max idle time for key\value entry in milliseconds.
+            if maxIdleTime and ttl params are equal to 0 then entry stores infinitely.
+            we rely on the server having maxmemory set and maxmemory-policy set to allkeys-lru or allkeys-lfu
+            NOTE: for some reason having ttl and maxIdleTime set to 0 caused the portal to not start up even
+            with allkeys-lfu eviction policy, so added maxIdleTime back.
+        */
+        config.put(appName + "GeneralRepositoryCache", new CacheConfig(0, 12*60*60*1000));
+        config.put(appName + "StaticRepositoryCacheOne", new CacheConfig(0, 12*60*60*1000));
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
 
-    public void detectCacheConfigurationErrorsAndLog() {
-        String MESSAGE_PREFIX = "Errors detected during configuration of Ehcache:";
-        StringBuffer messages = new StringBuffer(MESSAGE_PREFIX);
-        boolean usesHeap = false;
-        boolean usesDisk = false;
-        switch (this.cacheType.trim().toLowerCase()) {
-            case "none":
-                break;
-            case "heap":
-                usesHeap = true;
-                break;
-            case "disk":
-                usesDisk = true;
-                break;
-            case "hybrid":
-                usesHeap = true;
-                usesDisk = true;
-                break;
-            default:
-                messages.append("\n  property ehcache.cache_type has value (")
-                        .append(cacheType)
-                        .append(") which is not a recognized value");
-        }
-        if (usesDisk || usesHeap) {
-            if (xmlConfigurationFile == null || xmlConfigurationFile.trim().length() == 0) {
-                messages.append("\n  property ehcache.xml_configuration is required but is unset");
-            } else {
-                URL configFileURL = getClass().getResource(xmlConfigurationFile);
-                if (configFileURL == null) {
-                    messages.append("\n  property ehcache.xml_configuration has value (")
-                            .append(xmlConfigurationFile)
-                            .append(") but this resource is not available to the classloader");
-                } else {
-                    boolean readable = false;
-                    try {
-                        InputStream configFileInputStream = configFileURL.openStream();
-                        configFileInputStream.read();
-                        configFileInputStream.close();
-                        readable = true;
-                    } catch (IOException e) {
-                    }
-                    if (!readable) {
-                        messages.append("\n  property ehcache.xml_configuration has value (")
-                                .append(xmlConfigurationFile)
-                                .append(") but an attempt to read from this resource failed");
-                    }
-                }
-            }
-        }
-        if (usesDisk) {
-            if (generalRepositoryCacheMaxMegaBytesLocalDisk == null) {
-                messages.append("\n  property ehcache.general_repository_cache.max_mega_bytes_local_disk is required to be set, but has no value");
-            } else {
-                if (generalRepositoryCacheMaxMegaBytesLocalDisk <= 0) {
-                    messages.append("\n  property ehcache.general_repository_cache.max_mega_bytes_local_disk must be greater than zero but is not");
-                }
-            }
-            if (staticRepositoryCacheOneMaxMegaBytesLocalDisk == null) {
-                messages.append("\n  property ehcache.static_repository_cache_one.max_mega_bytes_local_disk is required to be set, but has no value");
-            } else {
-                if (staticRepositoryCacheOneMaxMegaBytesLocalDisk <= 0) {
-                    messages.append("\n  property ehcache.static_repository_cache_one.max_mega_bytes_local_disk must be greater than zero but is not");
-                }
-            }
-            if (persistencePath == null || persistencePath.trim().length() == 0) {
-                messages.append("\n  property ehcache.persistence_path is required when using a disk resource but is unset");
-            } else {
-                File persistenceDirectory = new File(persistencePath);
-                boolean accessible = false;
-                try {
-                    if (persistenceDirectory.isDirectory() && persistenceDirectory.canWrite()) {
-                        accessible = true;
-                    }
-                } catch (SecurityException e) {
-                }
-                if (!accessible) {
-                    messages.append("\n  property ehcache.persistence_path has value (")
-                            .append(persistencePath)
-                            .append(") but this path does not exist or is not an accessible directory");
-                }
-            }
-        }
-        if (usesHeap) {
-            if (generalRepositoryCacheMaxMegaBytes == null) {
-                messages.append("\n  property ehcache.general_repository_cache.max_mega_bytes_heap is required to be set, but has no value");
-            } else {
-                if (generalRepositoryCacheMaxMegaBytes <= 0) {
-                    messages.append("\n  property ehcache.general_repository_cache.max_mega_bytes_heap must be greater than zero but is not");
-                }
-            }
-            if (staticRepositoryCacheOneMaxMegaBytes == null) {
-                messages.append("\n  property ehcache.static_repository_cache_one.max_mega_bytes_heap is required to be set, but has no value");
-            } else {
-                if (staticRepositoryCacheOneMaxMegaBytes <= 0) {
-                    messages.append("\n  property ehcache.static_repository_cache_one.max_mega_bytes_heap must be greater than zero but is not");
-                }
-            }
-        }
-        if (usesHeap && usesDisk) {
-            if (generalRepositoryCacheMaxMegaBytesLocalDisk != null
-                    && generalRepositoryCacheMaxMegaBytes != null
-                    && generalRepositoryCacheMaxMegaBytesLocalDisk <= generalRepositoryCacheMaxMegaBytes) {
-                messages.append("\n  property ehcache.general_repository_cache.max_mega_bytes_heap must be set to a value less than the value of ");
-                messages.append("property ehcache.general_repository_cache.max_mega_bytes_local_disk, however ");
-                messages.append(generalRepositoryCacheMaxMegaBytes);
-                messages.append(" is not less than ");
-                messages.append(generalRepositoryCacheMaxMegaBytesLocalDisk);
-            }
-            if (staticRepositoryCacheOneMaxMegaBytesLocalDisk != null
-                    && staticRepositoryCacheOneMaxMegaBytes != null
-                    && staticRepositoryCacheOneMaxMegaBytesLocalDisk <= staticRepositoryCacheOneMaxMegaBytes) {
-                messages.append("\n  property ehcache.static_repository_cache_one.max_mega_bytes_heap must be set to a value less than the value of ");
-                messages.append("property ehcache.static_repository_cache_one.max_mega_bytes_local_disk, however ");
-                messages.append(staticRepositoryCacheOneMaxMegaBytes);
-                messages.append(" is not less than ");
-                messages.append(staticRepositoryCacheOneMaxMegaBytesLocalDisk);
-            }
-        }
-        if (messages.length() > MESSAGE_PREFIX.length()) {
-            LOG.error(messages.toString());
-            LOG.error("because of Ehcache configuration errors, it is likely that an exception will be thrown during startup. Recent observed exceptions contain the string"
-                    + " \"Provider org.redisson.jcache.JCachingProvider not a subtype\" even though the problem is in the Ehcache configuration settings.");
-        }
-    }
 }

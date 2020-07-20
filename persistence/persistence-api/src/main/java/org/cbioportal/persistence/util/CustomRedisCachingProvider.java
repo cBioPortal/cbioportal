36c400aeb5b499f3a91174f13cf723a3975e86f0
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2020-07-20 21:58:15 +0100

    started work on getting redis JCache API working

--- persistence/persistence-api/src/main/java/org/cbioportal/persistence/util/CustomRedisCachingProvider.java
+++ persistence/persistence-api/src/main/java/org/cbioportal/persistence/util/CustomRedisCachingProvider.java
@@ -34,11 +34,17 @@ package org.cbioportal.persistence.util;
 
 import java.io.*;
 import java.util.*;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.Configuration;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.jcache.configuration.RedissonConfiguration;
+import org.redisson.jcache.JCachingProvider;
 import org.redisson.spring.cache.CacheConfig;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -65,11 +71,10 @@ public class CustomRedisCachingProvider {
         return redissonClient;
     }
 
-    public RedissonSpringCacheManager getCacheManager(RedissonClient redissonClient) {
+    public CacheManager getCacheManager(RedissonClient redissonClient) {
 
-        RedissonSpringCacheManager toReturn = null;
-        
-        Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
+        //RedissonSpringCacheManager toReturn = null;
+        //Map<String, CacheConfig> caches = new HashMap<String, CacheConfig>();
         /*
             create cache with ttl and maxIdleTime
             ttl - - time to live for key\value entry in milliseconds. If 0 then time to live doesn't affect entry expiration.
@@ -79,9 +84,30 @@ public class CustomRedisCachingProvider {
             NOTE: for some reason having ttl and maxIdleTime set to 0 caused the portal to not start up even
             with allkeys-lfu eviction policy, so added maxIdleTime back.
         */
-        config.put(appName + "GeneralRepositoryCache", new CacheConfig(0, 12*60*60*1000));
-        config.put(appName + "StaticRepositoryCacheOne", new CacheConfig(0, 12*60*60*1000));
-        return new RedissonSpringCacheManager(redissonClient, config);
+        //caches.put(appName + "GeneralRepositoryCache", new CacheConfig(0, 12*60*60*1000));
+        //caches.put(appName + "StaticRepositoryCacheOne", new CacheConfig(0, 12*60*60*1000));
+        //return new RedissonSpringCacheManager(redissonClient, config);
+
+        MutableConfiguration<String, String> jcacheConfig = new MutableConfiguration<>();
+        Configuration<String, String> config = RedissonConfiguration.fromInstance(redissonClient, jcacheConfig);
+        CachingProvider redisCachingProvider = null;
+        for (CachingProvider cachingProvider : Caching.getCachingProviders()) {
+            LOG.debug("CachingProvider: " + cachingProvider);
+            if (cachingProvider instanceof JCachingProvider) {
+                redisCachingProvider = cachingProvider;
+                break;
+            }
+        }
+        if (redisCachingProvider == null) {
+            // this should never happen, no one should try to create this bean (calling this method)
+            // unless we have loaded the Redis libraries
+            LOG.error("Failed to find a Redis caching provider");
+            return null;
+        }
+        CacheManager manager = redisCachingProvider.getCacheManager();
+        manager.createCache("TEST" + appName + "GeneralRepositoryCache", config);
+        manager.createCache("TEST" + appName + "StaticRepositoryCacheOne", config);
+        return manager;
     }
 
 }

690061c370a84d7265ffe9f90c0124ad1cda2c83
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2020-07-23 18:16:46 -0400

    move files to cbioportal-docker-compose repo

--- docker/compose/config/init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cat ${SCRIPT_DIR}/../../../src/main/resources/portal.properties.EXAMPLE | \
-    sed 's/db.host=.*/db.host=cbioportal_database:3306/g' | \
-    sed 's|db.connection_string=.*|db.connection_string=jdbc:mysql://cbioportal_database:3306/|g' \
-> portal.properties
--- docker/compose/init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for d in config data study; do
-    cd $d; ./init.sh
-    cd ..
-done

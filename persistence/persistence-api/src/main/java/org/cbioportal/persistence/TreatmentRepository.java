violethaze74/Bioinformatic-Portal

Study View Treatments API
This commit creates two new endpoints to support the new treatments
charts in the study view. It also adds additional logic to the
StudyViewFilterApplier to help it filter by treatment. The commit
also fixes two endpoints that 500ed when given a filter that results
in 0 samples.

`/treatments/patient` returns a list of PatientTreatmentRows. Each
patient treatment rows represents the number of patients that have
recieved a specific treatment.

`/treatments/sample` returns a list of SampleTreatmentRows. Each
sample treatment row represents either the number of samples
that were taken after a patient recieved a specific treatment,
or the number of samples that were taken before a patient recieved
a specific treatment.

The StudyViewFilter gained two new fields: sampleTreatmentFilters
and patientTreatmentFilters. These fileds correspond to the previously
described rows and filter out samples as you would expect.

persistence/persistence-api/src/main/java/org/cbioportal/persistence/TreatmentRepository.java
@@ -1,0 +1,16 @@
+package org.cbioportal.persistence;
+
+import org.cbioportal.model.ClinicalEventSample;
+import org.cbioportal.model.Treatment;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface TreatmentRepository {
+    public Map<String, List<Treatment>> getTreatmentsByPatient(List<String> sampleIds, List<String> studyIds);
+    
+    public Map<String, List<ClinicalEventSample>> getSamplesByPatient(List<String> sampleIds, List<String> studyIds);
+
+    Set<String> getAllUniqueTreatments(List<String> sampleIds, List<String> studyIds);
+}

persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/TreatmentMyBatisRepository.java
@@ -1,0 +1,41 @@
+package org.cbioportal.persistence.mybatis;
+
+import org.cbioportal.model.ClinicalEventSample;
+import org.cbioportal.model.Treatment;
+import org.cbioportal.persistence.TreatmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Repository
+public class TreatmentMyBatisRepository implements TreatmentRepository {
+    @Autowired
+    private TreatmentMapper treatmentMapper;
+    
+    @Override
+    public Map<String, List<Treatment>> getTreatmentsByPatient(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllTreatments(sampleIds, studyIds)
+            .stream()
+            .sorted(Comparator.comparingInt(Treatment::getStart))
+            .collect(groupingBy(Treatment::getPatientId));
+    }
+
+    @Override
+    public Map<String, List<ClinicalEventSample>> getSamplesByPatient(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllSamples(sampleIds, studyIds)
+            .stream()
+            .distinct()
+            .collect(groupingBy(ClinicalEventSample::getPatientId));
+    }
+
+    @Override
+    public Set<String> getAllUniqueTreatments(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllUniqueTreatments(sampleIds, studyIds);
+    }
+}

cBioPortal/cbioportal

persistence/persistence-api/src/main/java/org/cbioportal/persistence/TreatmentRepository.java
@@ -1,0 +1,18 @@
+package org.cbioportal.persistence;
+
+import org.cbioportal.model.ClinicalEventSample;
+import org.cbioportal.model.Treatment;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface TreatmentRepository {
+    public Map<String, List<Treatment>> getTreatmentsByPatientId(List<String> sampleIds, List<String> studyIds);
+    
+    public Map<String, List<ClinicalEventSample>> getSamplesByPatientId(List<String> sampleIds, List<String> studyIds);
+
+    Set<String> getAllUniqueTreatments(List<String> sampleIds, List<String> studyIds);
+
+    Integer getTreatmentCount(List<String> samples, List<String> studies);
+}

persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/TreatmentMyBatisRepository.java
@@ -1,0 +1,45 @@
+package org.cbioportal.persistence.mybatis;
+
+import org.cbioportal.model.ClinicalEventSample;
+import org.cbioportal.model.Treatment;
+import org.cbioportal.persistence.TreatmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Repository
+public class TreatmentMyBatisRepository implements TreatmentRepository {
+    @Autowired
+    private TreatmentMapper treatmentMapper;
+    
+    @Override
+    public Map<String, List<Treatment>> getTreatmentsByPatientId(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllTreatments(sampleIds, studyIds)
+            .stream()
+            .collect(groupingBy(Treatment::getPatientId));
+    }
+
+    @Override
+    public Map<String, List<ClinicalEventSample>> getSamplesByPatientId(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllSamples(sampleIds, studyIds)
+            .stream()
+            .distinct()
+            .collect(groupingBy(ClinicalEventSample::getPatientId));
+    }
+
+    @Override
+    public Set<String> getAllUniqueTreatments(List<String> sampleIds, List<String> studyIds) {
+        return treatmentMapper.getAllUniqueTreatments(sampleIds, studyIds);
+    }
+
+    @Override
+    public Integer getTreatmentCount(List<String> samples, List<String> studies) {
+        return treatmentMapper.getTreatmentCount(samples, studies);
+    }
+}
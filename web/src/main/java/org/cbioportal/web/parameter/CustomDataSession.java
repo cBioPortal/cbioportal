8a2f7e87fa082fd78fd5d7894dd20131606d98af
Karthik Kalletla <kalletlakarthik@gmail.com>
2020-09-01 09:02:49 -0400

    Add new api's to support saving custom data charts

--- web/src/main/java/org/cbioportal/web/parameter/CustomDataSession.java
+++ web/src/main/java/org/cbioportal/web/parameter/CustomDataSession.java
@@ -6,29 +6,30 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.cbioportal.session_service.domain.Session;
 import org.cbioportal.session_service.domain.SessionType;
+import org.cbioportal.web.CustomAttributeWithData;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class VirtualStudy extends Session {
+public class CustomDataSession extends Session {
 
-    private final Log LOG = LogFactory.getLog(VirtualStudy.class);
-    private VirtualStudyData data;
+    private final Log LOG = LogFactory.getLog(CustomDataSession.class);
+    private CustomAttributeWithData data;
 
     @Override
     public void setData(Object data) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            this.data = mapper.readValue(mapper.writeValueAsString(data), VirtualStudyData.class);
+            this.data = mapper.readValue(mapper.writeValueAsString(data), CustomAttributeWithData.class);
         } catch (IOException e) {
             LOG.error(e);
         }
     }
 
     @Override
-    public VirtualStudyData getData() {
+    public CustomAttributeWithData getData() {
         return data;
     }
 

293d3d0a5c57d0227865e645797e9455d91279ca
Pim van Nierop <pim@thehyve.nl>
2021-01-27 09:54:49 +0100

    Add GitHub action for integration tests

--- test/integration/integration_test_oncokb_import.sh
+++ test/integration/integration_test_oncokb_import.sh
@@ -3,13 +3,13 @@
 # Test OncoKB import against local portal info dump (requires no Internet) and
 # live oncokb.org instance (requires Internet and OncoKB to be up-and-running)
 
-cd /cbioportal/core/src/main/scripts/importer
-python ./metaImport.py \
-  --study_directory=/cbioportal/core/src/test/scripts/test_data/study_oncokb_import \
-  --portal_info_dir=/cbioportal/core/src/test/scripts/test_data/api_json_system_tests \
+cd "$PORTAL_SOURCE_DIR"/core/src/main/scripts/importer
+python3 ./metaImport.py \
+  --study_directory="$PORTAL_SOURCE_DIR"/core/src/test/scripts/test_data/study_oncokb_import \
+  --portal_info_dir="$PORTAL_SOURCE_DIR"/core/src/test/scripts/test_data/api_json_system_tests \
   --import_oncokb \
   --skip_db_import &&
-cd /cbioportal/core/src/test/scripts/test_data/study_oncokb_import &&
+cd "$PORTAL_SOURCE_DIR"/core/src/test/scripts/test_data/study_oncokb_import &&
 test -e data_cna_pd_annotations.txt &&
 test -e ONCOKB_IMPORT_BACKUP_data_mutations_extended.maf &&
 test -e ONCOKB_IMPORT_BACKUP_meta_cna_discrete.txt &&

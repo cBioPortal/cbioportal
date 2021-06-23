8830ea4f8adcf9ec66bdbddfc5f22bb4473783c6
Gaofei Zhao <gzd23@mail.missouri.edu>
2021-06-23 13:57:47 -0400

    address comments

--- service/src/main/java/org/cbioportal/service/impl/GenericAssayServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/GenericAssayServiceImpl.java
@@ -48,8 +48,10 @@ public class GenericAssayServiceImpl implements GenericAssayService {
         if (molecularProfileIds != null) {
             List<String> distinctMolecularProfileIds = molecularProfileIds.stream().distinct().sorted().collect(Collectors.toList());
             if (distinctMolecularProfileIds.size() > 0) {
-                List<String> stableIdsInMolecularProfiles = genericAssayRepository.getGenericAssayStableIdsByMolecularIds(distinctMolecularProfileIds);
-                allStableIds.addAll(stableIdsInMolecularProfiles);
+                // fetch one profile at a time to improve cache performace for multiple profiles query
+                for (String distinctMolecularProfileId : distinctMolecularProfileIds) {
+                    allStableIds.addAll(genericAssayRepository.getGenericAssayStableIdsByMolecularIds(Arrays.asList(distinctMolecularProfileId)));
+                }
             }
             // if stableIds and molecularProfileIds both exist, find the common
             if (stableIds != null) {
@@ -74,14 +76,14 @@ public class GenericAssayServiceImpl implements GenericAssayService {
         if (distinctStableIds.size() > 0) {
             List<GenericAssayMeta> metaData = genericAssayRepository.getGenericAssayMeta(distinctStableIds);
             // just return stable_id if projection is ID
-            if (projection == "ID") {
+            if (projection.equals("ID")) {
                 for (GenericAssayMeta meta : metaData) {
                     metaResults.add(new GenericAssayMeta(meta.getStableId()));
                 }
             } else {
-                List<GenericAssayAdditionalProperty> additionalProperties = genericAssayRepository.getGenericAssayAdditionalproperties(distinctStableIds);
                 Map<String, List<GenericAssayAdditionalProperty>> additionalPropertiesGroupedByStableId =
-                    additionalProperties.stream().collect(Collectors.groupingBy(GenericAssayAdditionalProperty::getStableId));
+                    genericAssayRepository.getGenericAssayAdditionalproperties(distinctStableIds).stream()
+                        .collect(Collectors.groupingBy(GenericAssayAdditionalProperty::getStableId));
                 for (GenericAssayMeta meta : metaData) {
                     String stableId = meta.getStableId();
                     HashMap<String, String> map = new HashMap<>();

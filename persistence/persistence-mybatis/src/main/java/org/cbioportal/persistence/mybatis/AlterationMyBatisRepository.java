57ba0da5688f65edea94f1f575c7f3f000db06a4
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-07-21 14:56:52 -0400

    Workaround for fusions structural variant genes imported as mutations
    FIXES:
    - [x] expand def'n of what genomic profiles fall under types of "structural variant" data
    - [x] identify other areas of code that affect population of study view "Genomic Profile Sample Counts: Molecular Profiles" table and "Structural Variant" genes tables // ONCOPRINT does not use the same endpoint. This table is rendering fine for the MSKARCHER study fusions imported as "_mutations"
    - [x] fix oncoprint rendering - samples are not showing up with genomic alterations for genes which have structural variants
    - [x] fix mutation table rendering - genomic alteration events are not rendering in table for genes which have structural variants
    
    **Describe changes proposed in this pull request:**
    
    **Implement workaround for populating Structural Variant Genes table in Study View page**
    - Pull structural variant genes for genomic profiles of genetic alteration type (molecular alteration type)= "MUTATION_EXTENDED" and datatype = "FUSION"
      - Endpoints affected:
        - `/structuralvariants-genes/fetch`
        - `/filtered-samples/fetch`
    
    **Structural Variant Genes table renders now for this type of scenario**
    ![image](https://user-images.githubusercontent.com/15623749/126648556-9b96e04c-28cd-42e6-9268-41fdb123c668.png)
    
    **Implement workaround for populating OncoPrint fusion events that are imported as a "_mutations" profile w/genetic alteration type "STRUCTURAL_VARIANT" and datatype "FUSION"**
    
    _Note: This is a necessary change to accommodate `mskarcher` which has a single genomic profile (fusions) being imported as "_mutations" so that they show up in the mutation table and are linked to the appropriate gene panel._
    
    If you are really curious as to all the steps taken during debugging/troubleshooting this issue then please refer to the attachment `mskarcher_debugging.txt` for this PR. It is literally just a log of trials and errors so it's messy but contains all of my notes.
    [mskarcher_debugging.txt](https://github.com/cBioPortal/cbioportal/files/6865407/mskarcher_debugging.txt)
    
    Fixed OncoPrint screen shot:
    ![image](https://user-images.githubusercontent.com/15623749/126713987-1bc7d491-abf4-4941-8d84-79cea1cce82d.png)
    
      - Endpoints affected:
        - `/structural-variants/fetch`
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/AlterationMyBatisRepository.java
+++ persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/AlterationMyBatisRepository.java
@@ -58,6 +58,18 @@ public class AlterationMyBatisRepository implements AlterationRepository {
             .stream()
             .collect(Collectors.groupingBy(e -> profileTypeByProfileId.getOrDefault(e.getMolecularProfileId(), null)));
 
+        // TODO: Remove once fusions are removed from mutation table
+        // if fusions were imported as a "mutations" profile then replace STRUCTURAL_VARIANT in
+        // groupedIdentifiersByProfileType map with MUTATION_EXTENDED
+        for (MolecularProfile profile : molecularProfileRepository.getMolecularProfiles(molecularProfileIds, "SUMMARY")) {
+            if (profile.getStableId().endsWith("mutations") && profile.getDatatype().equals("FUSION")) {
+                groupedIdentifiersByProfileType.put(MolecularAlterationType.MUTATION_EXTENDED,
+                        groupedIdentifiersByProfileType.get(MolecularAlterationType.STRUCTURAL_VARIANT));
+                groupedIdentifiersByProfileType.remove(MolecularAlterationType.STRUCTURAL_VARIANT);
+                break;
+            }
+        }
+
         return alterationCountsMapper.getSampleAlterationCounts(
             groupedIdentifiersByProfileType.get(MolecularAlterationType.MUTATION_EXTENDED),
             groupedIdentifiersByProfileType.get(MolecularAlterationType.COPY_NUMBER_ALTERATION),
@@ -137,7 +149,7 @@ public class AlterationMyBatisRepository implements AlterationRepository {
             || allAlterationsExcludedDriverTierAnnotation(alterationFilter)) {
             return Collections.emptyList();
         }
-        
+
         List<MolecularProfileCaseIdentifier> molecularProfileCaseInternalIdentifiers =
             alterationCountsMapper.getMolecularProfileCaseInternalIdentifier(molecularProfileCaseIdentifiers, "SAMPLE_ID");
 
@@ -175,7 +187,7 @@ public class AlterationMyBatisRepository implements AlterationRepository {
             alterationFilter.getSelectedTiers(),
             alterationFilter.getIncludeUnknownTier());
     }
-    
+
     private Select<Short> createCnaTypeList(final AlterationFilter alterationFilter) {
         if (alterationFilter.getCNAEventTypeSelect().hasNone())
             return Select.none();
@@ -198,12 +210,12 @@ public class AlterationMyBatisRepository implements AlterationRepository {
     private boolean allAlterationsExcludedMutationStatus(AlterationFilter alterationFilter) {
         return !alterationFilter.getIncludeGermline() && !alterationFilter.getIncludeSomatic() && !alterationFilter.getIncludeUnknownStatus();
     }
-    
+
     private boolean allAlterationsExcludedDriverAnnotation(AlterationFilter alterationFilter) {
         return !alterationFilter.getIncludeDriver() && !alterationFilter.getIncludeVUS()
             && !alterationFilter.getIncludeUnknownOncogenicity();
     }
-    
+
     private boolean allAlterationsExcludedDriverTierAnnotation(AlterationFilter alterationFilter) {
         return alterationFilter.getSelectedTiers().hasNone()
             && !alterationFilter.getIncludeUnknownTier();

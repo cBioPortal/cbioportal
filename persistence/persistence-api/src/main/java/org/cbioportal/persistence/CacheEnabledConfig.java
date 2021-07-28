7ae1e6eb6d1c8ba6816835aebfddee86ffc29c15
Pim van Nierop <pim@thehyve.nl>
2021-07-28 14:09:24 +0200

    Remove obsolete import

--- persistence/persistence-api/src/main/java/org/cbioportal/persistence/CacheEnabledConfig.java
+++ persistence/persistence-api/src/main/java/org/cbioportal/persistence/CacheEnabledConfig.java
@@ -1,6 +1,5 @@
 package org.cbioportal.persistence;
 
-import org.cbioportal.utils.config.annotation.ConditionalOnProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;

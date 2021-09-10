4da4627ebc4b913e6667adc400ed4c1e5189dee3
Pim van Nierop <pim@thehyve.nl>
2021-09-10 13:30:17 +0200

    First commit

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Demo1Application.class, args);
+  }
+
+}

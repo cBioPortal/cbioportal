fbdcac415d33de8153f1356a4df9c73e4a4e1ae5
Pim van Nierop <pim@thehyve.nl>
2021-09-11 17:54:22 +0200

    Add main application

---
+++ core/src/main/java/org/cbioportal/PortalApplication.java
@@ -0,0 +1,12 @@
+package org.cbioportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
+}

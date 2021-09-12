08d9e4a8c33249c49a3b1991a7745e2fce987e15
Pim van Nierop <pim@thehyve.nl>
2021-09-12 11:39:06 +0200

    Working web application with db connection

--- core/src/main/java/org/cbioportal/PortalApplication.java
+++ core/src/main/java/org/cbioportal/PortalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
     }

7a2177a52cad95531ba0ef178be280035fb36a73
Pim van Nierop <pim@thehyve.nl>
2021-09-16 11:56:30 +0200

    Add required portal properties check

--- core/src/main/java/org/cbioportal/RequiredPropertiesCheck.java
+++ core/src/main/java/org/cbioportal/RequiredPropertiesCheck.java
@@ -1,13 +1,40 @@
 package org.cbioportal;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RequiredPropertiesCheck {
 
-    // Add all required portal.properties here. When no properties are found in Application context
-    // Spring will throw an error message stating the property that needs to be defined.
+    // --- Add all required portal.properties here.
 
-    @Value("${authenticate}")
+    @Value("${authenticate:not defined}")
     private String authenticate;
+
+    // ---
+
+    @PostConstruct
+    public void checkProperties() {
+
+        Field[] requiredProperties = RequiredPropertiesCheck.class.getDeclaredFields();
+        String missingFieldsString = Arrays.stream(requiredProperties)
+            .filter(field -> {
+                try {
+                    return "not defined".equals((String) field.get(this));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            })
+            .map(Field::getName)
+            .collect(Collectors.joining("', '"));
+        if (!missingFieldsString.isEmpty()) {
+            throw new IllegalStateException("TERMINATING -> define the following cBioPortal properties: '" + missingFieldsString + "'");
+        }
+    }
 
 }

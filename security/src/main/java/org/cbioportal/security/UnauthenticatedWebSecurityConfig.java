01b9c6232b0b9f4532b6066211d8f1b59bdf8d24
Pim van Nierop <pim@thehyve.nl>
2021-09-17 08:57:22 +0200

    Add security module

--- security/src/main/java/org/cbioportal/security/UnauthenticatedWebSecurityConfig.java
+++ security/src/main/java/org/cbioportal/security/UnauthenticatedWebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @ConditionalOnProperty(value = "authenticate", havingValue = "none")
-public class UnauthenticatedSecurityConfig extends WebSecurityConfigurerAdapter {
+public class UnauthenticatedWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

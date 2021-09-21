954454464915efb0012260035fe615e2baaa97dc
Pim van Nierop <pim@thehyve.nl>
2021-09-21 09:13:03 +0200

    Add OpenIDSecurityConfig

--- security/security-spring/src/main/java/org/cbioportal/security/spring/OpenIDSecurityConfig.java
+++ security/security-spring/src/main/java/org/cbioportal/security/spring/OpenIDSecurityConfig.java
@@ -1,6 +1,5 @@
 package org.cbioportal.security.spring;
 
-import org.cbioportal.security.spring.authentication.PortalSavedRequestAwareAuthenticationSuccessHandler;
 import org.cbioportal.security.spring.authentication.openID.PortalUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,33 +8,42 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 // add new chain after api-filter chain (at position -2), but before the default fallback chain 
 @Order(SecurityProperties.BASIC_AUTH_ORDER - 1)
 @ConditionalOnProperty(value = "authenticate", havingValue = "openid")
-public class OidcSecurityConfig extends WebSecurityConfigurerAdapter {
+public class OpenIDSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PortalUserDetailsService portalUserDetailsService;
-    
-    @Autowired
-    private PortalSavedRequestAwareAuthenticationSuccessHandler successHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
             .csrf().disable()
             .sessionManagement().sessionFixation().none()
             .and()
+            .antMatcher("/**")
             .openidLogin()
                 .loginPage("/login.jsp")
-                .authenticationUserDetailsService(portalUserDetailsService)
                 .failureUrl("/login.jsp?login_error=true")
+                .authenticationUserDetailsService(portalUserDetailsService)
+                .attributeExchange("https://www.google.com/.*")
+                    .attribute("email").type("http://axschema.org/contact/email").required(true).and()
+                    .attribute("firstname").type("http://axschema.org/namePerson/first").required(true).and()
+                    .attribute("lastname").type("http://axschema.org/namePerson/last").required(true).and()
+                .and()
                 .attributeExchange(".*yahoo.com.*")
-                    .attribute("email",);
-
+                    .attribute("email").type("http://axschema.org/contact/email").required(true).and()
+                    .attribute("fullname").type("http://axschema.org/namePerson").required(true).and()
+                .and()
+                .attributeExchange(".*myopenid.com.*")
+                    .attribute("email").type("http://schema.openid.net/contact/email").required(true).and()
+                    .attribute("fullname").type("http://schema.openid.net/namePerson").required(true).and()
+                .and()
+                .permitAll();
 
     }
 

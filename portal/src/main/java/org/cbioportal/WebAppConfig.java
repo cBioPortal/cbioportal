3ade2ff47cf0e6ed091b126284d4c3f44b721169
Pim van Nierop <pim@thehyve.nl>
2021-09-30 20:29:05 +0200

    Move webapp to static folder

--- portal/src/main/java/org/cbioportal/WebAppConfig.java
+++ portal/src/main/java/org/cbioportal/WebAppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebAppConfig implements WebMvcConfigurer {
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS =

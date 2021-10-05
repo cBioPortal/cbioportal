e41a85e4470bfb571a669a49d783efb0f5d9ed1e
Pim van Nierop <pim@thehyve.nl>
2021-10-05 09:25:13 +0200

    Fix path to static resources

--- portal/src/main/java/org/cbioportal/WebAppConfig.java
+++ portal/src/main/java/org/cbioportal/WebAppConfig.java
@@ -4,25 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebAppConfig implements WebMvcConfigurer {
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
-        {
-            "classpath:/META-INF/resources/",
-            "classpath:/resources/",
-            "classpath:/static/",
-            "classpath:/public/",
-            "classpath:/reactapp/",
-            "classpath:/",
-            "file:/opt/myfiles/"
-        };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
-        registry.addResourceHandler("/**")
-            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+        registry
+            .addResourceHandler("/images/**").addResourceLocations("classpath:/images/")
+            .setCachePeriod(3000);
+        registry
+            .addResourceHandler("/reactapp/**").addResourceLocations("classpath:/reactapp/")
+            .setCachePeriod(3000);
+        // TODO remove?
+        registry
+            .addResourceHandler("/webapp/**").addResourceLocations("classpath:/webapp/")
             .setCachePeriod(3000);
     }
 

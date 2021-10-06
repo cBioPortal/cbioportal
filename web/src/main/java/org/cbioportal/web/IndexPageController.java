7215aafb1f9d3d986fd24222062246dcf6a5c234
Pim van Nierop <pim@thehyve.nl>
2021-10-06 14:21:37 +0200

    Clean up IndexPageController

--- web/src/main/java/org/cbioportal/web/IndexPageController.java
+++ web/src/main/java/org/cbioportal/web/IndexPageController.java
@@ -5,11 +5,10 @@ import static org.cbioportal.service.FrontendPropertiesServiceImpl.FrontendPrope
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Enumeration;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.cbioportal.service.FrontendPropertiesService;
-import org.json.simple.JSONObject;
+import org.cbioportal.web.util.HttpRequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -21,6 +20,9 @@ public class IndexPageController {
 
     @Autowired
     private FrontendPropertiesService frontendPropertiesService;
+    
+    @Autowired
+    private HttpRequestUtils requestUtils;
 
     private ObjectMapper mapper = new ObjectMapper();
 
@@ -28,39 +30,20 @@ public class IndexPageController {
     public String showIndexPage(HttpServletRequest request, Authentication authentication, Model model)
         throws JsonProcessingException {
 
+        String baseUrl = requestUtils.getBaseUrl(request);
+        
         Map<String, String> properties = frontendPropertiesService.getFrontendProperties();
-        
-        String currentUrl = request.getRequestURL().toString();
-        String contextPath = request.getContextPath();
-        String baseURL = currentUrl.substring(0, currentUrl.length() - request.getRequestURI().length()) + contextPath;
-        baseURL = baseURL.replace("https://", "").replace("http://", "");
-        properties.put("base_url", baseURL);
-        
-        String useremail = authentication != null ? authentication.getName() : "anonymousUser";
-        properties.put("user_email_address", useremail);
+        properties.put("base_url", baseUrl);
+        properties.put("user_email_address", authentication != null ? authentication.getName() : "anonymousUser");
         
         model.addAttribute("propertiesJson", mapper.writeValueAsString(properties));
         model.addAttribute("frontendUrl", frontendPropertiesService.getFrontendProperty(FrontendProperty.frontendUrl));
-        model.addAttribute("baseUrl", baseURL);
-        model.addAttribute("contextPath", contextPath);
+        model.addAttribute("baseUrl", baseUrl);
+        model.addAttribute("contextPath", request.getContextPath());
         model.addAttribute("appVersion", frontendPropertiesService.getFrontendProperty(FrontendProperty.app_version));
-        model.addAttribute("postData", getPostData(request));
+        model.addAttribute("postData", requestUtils.getPostData(request));
+
         return "index";
-    }
-    
-    private String getPostData(HttpServletRequest request) {
-        // To support posted query data (when data would exceed URL length),
-        // write all post params to json on page where it can be consumed.
-        if (request.getMethod().equals("POST")) {
-            Enumeration<String> parameterNames = request.getParameterNames();
-            JSONObject paramsJson = new JSONObject();
-            while (parameterNames.hasMoreElements()) {
-                String parameterName = parameterNames.nextElement();
-                paramsJson.put(parameterName, request.getParameter(parameterName));
-            }
-            return paramsJson.toString();
-        }
-        return "{}";
     }
 
 }

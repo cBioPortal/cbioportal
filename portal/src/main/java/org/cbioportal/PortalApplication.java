bc6ef68373d9ec6ed45f5ed6da8f249a8becb212
Pim van Nierop <pim@thehyve.nl>
2021-10-11 14:46:52 +0200

    Disable MongDB autoconfiguration

--- portal/src/main/java/org/cbioportal/PortalApplication.java
+++ portal/src/main/java/org/cbioportal/PortalApplication.java
@@ -2,10 +2,15 @@ package org.cbioportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class
+})
 @PropertySources({
     @PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties"),
     @PropertySource(ignoreResourceNotFound = true, value = "classpath:maven.properties"),

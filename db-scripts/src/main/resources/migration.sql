e1fa7b68247f0d3a3f039f8e3a9c7861cd432200
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2021-11-15 12:39:03 -0500

    update migration.sql setting version
    update migration.sql setting version - adding missing line

--- db-scripts/src/main/resources/migration.sql
+++ db-scripts/src/main/resources/migration.sql
@@ -952,6 +952,7 @@ UPDATE `info` SET `DB_SCHEMA_VERSION`="2.12.9";
 -- all previous genetic_profile will be considered as sample level data
 -- so set 0 (false) as default value for PATIENT_LEVEL field
 ALTER TABLE `genetic_profile` ADD COLUMN `PATIENT_LEVEL` boolean DEFAULT 0;
+UPDATE `info` SET `DB_SCHEMA_VERSION`="2.12.10";
 
 ##version: 2.12.11
 DROP TABLE IF EXISTS pdb_uniprot_residue_mapping;

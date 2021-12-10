c881e5ec0f34cc5fa08e21cf10f0feb40c5f6b51
Pim van Nierop <pim@thehyve.nl>
2021-12-10 10:04:29 +0100

    Cosmetic code clean up

--- portal/src/main/java/org/cbioportal/PortalPropertiesValidation.java
+++ portal/src/main/java/org/cbioportal/PortalPropertiesValidation.java
@@ -20,6 +20,3 @@ public class PortalPropertiesValidation {
         return authenticate;
     }
 }
-
-
-
--- portal/src/main/java/org/cbioportal/SentryConfig.java
+++ portal/src/main/java/org/cbioportal/SentryConfig.java
@@ -3,7 +3,6 @@ package org.cbioportal;
 import io.sentry.spring.SentryExceptionResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
 // TODO move to different module (web)?
 @Configuration

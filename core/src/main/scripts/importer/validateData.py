cba99a6d9f48720ee7a362cbd8e77784144bee63
rmadupuri <ramyamadupuri99@gmail.com>
2022-08-17 12:58:04 -0400

    update accepted panel list

--- core/src/main/scripts/importer/validateData.py
+++ core/src/main/scripts/importer/validateData.py
@@ -3234,7 +3234,7 @@ class GenePanelMatrixValidator(Validator):
         # Check whether gene panel stable ids are in the database
         if self.portal.gene_panel_list is not None:
             for gene_panel_id in data:
-                if gene_panel_id not in self.portal.gene_panel_list and gene_panel_id not in ['NA','WGS','WXS','WGS/WXS']:
+                if gene_panel_id not in self.portal.gene_panel_list and gene_panel_id not in ['NA','WGS','WXS','WXS/WGS']:
                     self.logger.error('Gene panel ID is not in database. Please import this gene panel before loading '
                                     'study data.',
                                     extra={'line_number': self.line_number,

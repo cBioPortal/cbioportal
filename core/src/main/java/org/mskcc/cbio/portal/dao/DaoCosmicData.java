df670647593dce1765fc8caf940aeecd25d5b5a7
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2022-09-21 21:56:21 -0400

    Update DaoCosmicData.java
    Using 0 instead of -1 for mutPos as 0 is a valid value for position.

--- core/src/main/java/org/mskcc/cbio/portal/dao/DaoCosmicData.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoCosmicData.java
@@ -116,7 +116,7 @@ public class DaoCosmicData {
         
         Set<CosmicMutationFrequency> ret = new HashSet<CosmicMutationFrequency>();
         Pattern p = Pattern.compile("[0-9]+");
-        int mutPos = -1;
+        int mutPos = 0;
         for (CosmicMutationFrequency cmf : cmfs) {
             String aa = cmf.getAminoAcidChange();
             Matcher m = p.matcher(aa);

3755855e37e5773ea05162d81804109cd173d876
Prasanna Kumar Jagannathan <37613906+jagnathan@users.noreply.github.com>
2022-11-11 16:48:58 -0500

    Update DaoMutation.java
    The data files still use the old column prefix of ONCOTATOR instead of MUTATION

--- core/src/main/java/org/mskcc/cbio/portal/dao/DaoMutation.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/DaoMutation.java
@@ -799,11 +799,11 @@ public final class DaoMutation {
         event.setDbSnpRs(rs.getString("DB_SNP_RS"));
         event.setDbSnpValStatus(rs.getString("DB_SNP_VAL_STATUS"));
         event.setReferenceAllele(rs.getString("REFERENCE_ALLELE"));
-        event.setMutationRefseqMrnaId(rs.getString("MUTATION_REFSEQ_MRNA_ID"));
-        event.setMutationCodonChange(rs.getString("MUTATION_CODON_CHANGE"));
-        event.setMutationUniprotAccession(rs.getString("MUTATION_UNIPROT_ACCESSION"));
-        event.setMutationProteinPosStart(rs.getInt("MUTATION_PROTEIN_POS_START"));
-        event.setMutationProteinPosEnd(rs.getInt("MUTATION_PROTEIN_POS_END"));
+        event.setMutationRefseqMrnaId(rs.getString("ONCOTATOR_REFSEQ_MRNA_ID"));
+        event.setMutationCodonChange(rs.getString("ONCOTATOR_CODON_CHANGE"));
+        event.setMutationUniprotAccession(rs.getString("ONCOTATOR_UNIPROT_ACCESSION"));
+        event.setMutationProteinPosStart(rs.getInt("ONCOTATOR_PROTEIN_POS_START"));
+        event.setMutationProteinPosEnd(rs.getInt("ONCOTATOR_PROTEIN_POS_END"));
         event.setCanonicalTranscript(rs.getBoolean("CANONICAL_TRANSCRIPT"));
         event.setTumorSeqAllele(rs.getString("TUMOR_SEQ_ALLELE"));
         event.setKeyword(rs.getString("KEYWORD"));

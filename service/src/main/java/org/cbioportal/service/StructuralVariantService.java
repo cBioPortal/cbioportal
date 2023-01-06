91cb44c3e08edfeea469b6f916c83376f7bad954
Pim van Nierop <pim@thehyve.nl>
2023-01-06 10:29:43 +0100

    Support Gene1/Gene2 queries with /structural-variant/fetch endpoint

--- service/src/main/java/org/cbioportal/service/StructuralVariantService.java
+++ service/src/main/java/org/cbioportal/service/StructuralVariantService.java
@@ -27,12 +27,16 @@ import java.util.List;
 
 import org.cbioportal.model.GeneFilterQuery;
 import org.cbioportal.model.StructuralVariant;
+import org.cbioportal.model.StructuralVariantQuery;
 
 public interface StructuralVariantService {
 
-    List<StructuralVariant> fetchStructuralVariants(List<String> molecularProfileIds,
-                                                    List<String> sampleIds,
-                                                    List<Integer> entrezGeneIds);
+    List<StructuralVariant> fetchStructuralVariants(
+        List<String> molecularProfileIds,
+        List<String> sampleIds,
+        List<Integer> entrezGeneIds,
+        List<StructuralVariantQuery> structuralVariantQueries
+    );
 
     List<StructuralVariant> fetchStructuralVariantsByGeneQueries(List<String> molecularProfileIds,
                                                                  List<String> sampleIds,

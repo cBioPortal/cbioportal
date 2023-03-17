aa216b81f857ab61cb87be124901bcc5fe3e73a9
Pim van Nierop <pim@thehyve.nl>
2023-03-17 16:08:46 +0100

    Wip

--- persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatiscolumnstore/StudyViewMapper.java
+++ persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatiscolumnstore/StudyViewMapper.java
@@ -1,4 +1,7 @@
 package org.cbioportal.persistence.mybatiscolumnstore;
 
+import org.cbioportal.webparam.StudyViewFilter;
+
 public interface StudyViewMapper {
+    void filteredSamples(StudyViewFilter studyViewFilter);
 }

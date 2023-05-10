5fcaa637e1368575b2ce790b986254f8fbbf9895
pvannierop <pim@thehyve.nl>
2023-05-10 20:59:07 +0200

    Externalize clickhouse connection string config

--- core/src/main/java/org/mskcc/cbio/portal/dao/ClickhouseJdbcDataSource.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/ClickhouseJdbcDataSource.java
@@ -1,13 +1,18 @@
 package org.mskcc.cbio.portal.dao;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.mskcc.cbio.portal.util.GlobalProperties;
 
 public class ClickhouseJdbcDataSource extends BasicDataSource {
+    
     public ClickhouseJdbcDataSource() {
+        String connectionString = GlobalProperties.getProperty("db.clickhouse.connection_string");
+        String user = GlobalProperties.getProperty("db.user");
+        String password = GlobalProperties.getProperty("db.password");
         this.setDriverClassName("com.clickhouse.jdbc.ClickHouseDriver");
-        this.setUsername("cbio");
-        this.setPassword("P@ssword1");
-        this.setUrl("jdbc:ch://clickhouse:8123/cbioportal?user=cbio&zeroDateTimeBehavior=convertToNull&useSSL=false");
+        this.setUsername(user);
+        this.setPassword(password);
+        this.setUrl(connectionString);
         this.setJmxName("org.cbioportal:DataSource=clickhouse");
     }
 }

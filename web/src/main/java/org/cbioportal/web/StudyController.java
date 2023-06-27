cBioPortal/cbioportal

commit a57c4894bec3a701835f72dbb751b1f66fc4e5e6

fixed swagger page validation errors

web/src/main/java/org/cbioportal/web/StudyController.java
@@ -99,7 +99,8 @@ public class StudyController {
         @RequestParam(required = false) StudySortBy sortBy,
         @ApiParam("Direction of the sort")
         @RequestParam(defaultValue = "ASC") Direction direction,
-        Authentication authentication) {
+        @ApiParam(hidden = true, required = false)
+        @RequestParam(required = false) Authentication authentication) {
         
         // Only use this feature on the public portal and make sure it is never used
         // on portals using auth, as in auth setting, different users will have different

web/src/main/java/org/cbioportal/web/TreatmentController.java
@@ -57,7 +57,7 @@ public class TreatmentController {
     @RequestMapping(value = "/treatments/patient", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get all patient level treatments")
     public ResponseEntity<List<PatientTreatmentRow>> getAllPatientTreatments(
-        @ApiParam(required = false, defaultValue = "Treatment")
+        @ApiParam(required = false, defaultValue = "Agent")
         @RequestParam(name = "tier", required = false, defaultValue = "Agent")
         ClinicalEventKeyCode tier,
         
@@ -101,7 +101,7 @@ public class TreatmentController {
     @RequestMapping(value = "/treatments/sample", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get all sample level treatments")
     public ResponseEntity<List<SampleTreatmentRow>> getAllSampleTreatments(
-        @ApiParam(required = false, defaultValue = "Treatment")
+        @ApiParam(required = false, defaultValue = "Agent")
         @RequestParam(name = "tier", required = false, defaultValue = "Agent")
         ClinicalEventKeyCode tier,
 
@@ -144,7 +144,7 @@ public class TreatmentController {
     @RequestMapping(value = "/treatments/display-patient", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Should patient level treatments be displayed")
     public ResponseEntity<Boolean> getContainsTreatmentData(
-        @ApiParam(required = false, defaultValue = "Treatment")
+        @ApiParam(required = false, defaultValue = "Agent")
         @RequestParam(name = "tier", required = false, defaultValue = "Agent")
         ClinicalEventKeyCode tier,
         
@@ -161,7 +161,7 @@ public class TreatmentController {
     @RequestMapping(value = "/treatments/display-sample", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Should sample level treatments be displayed")
     public ResponseEntity<Boolean> getContainsSampleTreatmentData(
-        @ApiParam(required = false, defaultValue = "Treatment")
+        @ApiParam(required = false, defaultValue = "Agent")
         @RequestParam(name = "tier", required = false, defaultValue = "Agent")
         ClinicalEventKeyCode tier,
         

commit 2ef50ef0663dd8da65f8fa8b114af9f68fd313d4

Fix 10221 403 error; Unauthorized studies are displayed as authorized
Fix 10221  Unauthorized studies are displayed as authorized due to issue with using RequestParam

adding @RequestParam causes error HTTP 400 error as by default required = true, if set to required = false causes unauthorized studies to be shown as authorized

Swagger validation errors fixed via commit a57c4894bec3a701835f72dbb751b1f66fc4e5e6
caused this issue.

web/src/main/java/org/cbioportal/web/StudyController.java
@@ -99,9 +99,10 @@ public class StudyController {
         @RequestParam(required = false) StudySortBy sortBy,
         @ApiParam("Direction of the sort")
         @RequestParam(defaultValue = "ASC") Direction direction,
-        @ApiParam(hidden = true, required = false)
-        @RequestParam(required = false) Authentication authentication) {
-        
+        // adding @RequestParam causes error HTTP 400 error as by default required = true 
+        // if set to required = false causes unauthorized studies to be shown as authorized - see issue 10221
+        @ApiParam(hidden = true)
+        Authentication authentication) {
         // Only use this feature on the public portal and make sure it is never used
         // on portals using auth, as in auth setting, different users will have different
         // results.
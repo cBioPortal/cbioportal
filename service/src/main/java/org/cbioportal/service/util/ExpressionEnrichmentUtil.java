78ed82580ecf619700a4ed636126e1d5361a494a
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-06-28 10:30:46 -0400

    update backend

--- service/src/main/java/org/cbioportal/service/util/ExpressionEnrichmentUtil.java
+++ service/src/main/java/org/cbioportal/service/util/ExpressionEnrichmentUtil.java
@@ -1,31 +1,20 @@
 package org.cbioportal.service.util;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.inference.ChiSquareTest;
 import org.apache.commons.math3.stat.inference.OneWayAnova;
 import org.apache.commons.math3.stat.inference.TestUtils;
-import org.cbioportal.model.EnrichmentType;
-import org.cbioportal.model.ExpressionEnrichment;
-import org.cbioportal.model.GenericAssayEnrichment;
-import org.cbioportal.model.GenericAssayMolecularAlteration;
-import org.cbioportal.model.GenomicEnrichment;
-import org.cbioportal.model.GroupStatistics;
-import org.cbioportal.model.MolecularAlteration;
-import org.cbioportal.model.MolecularProfile;
-import org.cbioportal.model.MolecularProfileCaseIdentifier;
-import org.cbioportal.model.MolecularProfileSamples;
-import org.cbioportal.model.Sample;
+import org.cbioportal.model.*;
 import org.cbioportal.persistence.MolecularDataRepository;
 import org.cbioportal.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +30,10 @@ public class ExpressionEnrichmentUtil {
 
 	private static final double LOG2 = Math.log(2);
 	private static final String RNA_SEQ = "rna_seq";
-
+    private static final String POS = "true";
+    private static final String NEG = "false";
+    private static final String ALTERED = "1";
+    private static final String UNALTERED = "0";
 	public <T extends MolecularAlteration, S extends ExpressionEnrichment> List<S> getEnrichments(
 			MolecularProfile molecularProfile,
 			Map<String, List<MolecularProfileCaseIdentifier>> molecularProfileCaseSets, EnrichmentType enrichmentType,
@@ -63,12 +55,12 @@ public class ExpressionEnrichmentUtil {
 						.map(sampleIndex -> ma.getSplitValues()[sampleIndex])
 						.filter(a -> NumberUtils.isNumber(a))
 						.collect(Collectors.toList());
-
+  
 				// ignore group if there are less than 2 values
 				if (molecularDataValues.size() < 2) {
 					continue;
 				}
-
+ 
 				double[] values = getAlterationValues(molecularDataValues, molecularProfile.getStableId());
 
 				GroupStatistics groupStatistics = new GroupStatistics();
@@ -111,7 +103,144 @@ public class ExpressionEnrichmentUtil {
 		return expressionEnrichments;
 	}
 
-	private double[] getAlterationValues(List<String> molecularDataValues, String molecularProfileId) {
+    public <T extends MolecularAlteration, S extends ExpressionEnrichment> List<S> getGenericAssayCategoricalEnrichments(
+        MolecularProfile molecularProfile,
+        Map<String, List<MolecularProfileCaseIdentifier>> molecularProfileCaseSets, EnrichmentType enrichmentType,
+        Iterable<T> maItr) {
+        
+        List<S> expressionEnrichments = new ArrayList<>();
+        Map<String, Map<String, Integer>> groupCategoryStatistics = new HashMap<>();
+        Map<String, List<Integer>> groupIndicesMap = getGroupIndicesMap(molecularProfileCaseSets, enrichmentType,
+            molecularProfile);
+        
+        for (MolecularAlteration ma : maItr) {
+            List<GroupStatistics> groupsStatistics = new ArrayList<GroupStatistics>();
+            for (Entry<String, List<Integer>> group : groupIndicesMap.entrySet()) {
+                // Get the corresponding split values for the group
+                List<String> groupValues = group.getValue().stream()
+                    .map(sampleIndex -> ma.getSplitValues()[sampleIndex])
+                    .collect(Collectors.toList());
+                // Group and count the split values
+                Map<String, Integer> groupedSplitValues = groupValues.stream()
+                    .collect(Collectors.toMap(Function.identity(), v -> 1, Integer::sum));
+                
+                // ignore group if there are less than 2 values
+                if (groupValues.size() < 2) {
+                    continue;
+                }
+                
+                GroupStatistics groupStatistics = new GroupStatistics();
+                groupStatistics.setName(group.getKey());
+                groupsStatistics.add(groupStatistics);
+                groupCategoryStatistics.put(group.getKey(), groupedSplitValues);
+            }
+
+            // calculate p-value and add enrichment if atleast 2 groups have data
+            if (groupsStatistics.size() > 1) {
+                long[][] array = getCategoricalValues(groupCategoryStatistics);
+                double pValue;
+
+                ChiSquareTest chiSquareTest = new ChiSquareTest();
+                pValue = chiSquareTest.chiSquareTest(array);
+
+                // set p-value to 1 when the cases in all groups are altered
+                if (Double.isNaN(pValue)) {
+                    pValue = 1;
+                }
+                
+                S expressionEnrichment = null;
+                GenericAssayCategoricalEnrichment genericAssayCategoricalEnrichment = new GenericAssayCategoricalEnrichment();
+                genericAssayCategoricalEnrichment.setStableId(ma.getStableId());
+                expressionEnrichment = (S) genericAssayCategoricalEnrichment;
+
+                expressionEnrichment.setpValue(BigDecimal.valueOf(pValue));
+                expressionEnrichment.setGroupsStatistics(groupsStatistics);
+                expressionEnrichments.add(expressionEnrichment);
+            }
+        }
+        return expressionEnrichments;
+    }
+
+    public <T extends MolecularAlteration, S extends ExpressionEnrichment> List<S> getGenericAssayBinaryEnrichments(
+        MolecularProfile molecularProfile,
+        Map<String, List<MolecularProfileCaseIdentifier>> molecularProfileCaseSets, EnrichmentType enrichmentType,
+        Iterable<T> maItr) {
+        List<S> expressionEnrichments = new ArrayList<>();
+
+        Map<String, List<Integer>> groupIndicesMap = getGroupIndicesMap(molecularProfileCaseSets, enrichmentType,
+            molecularProfile);
+        // unaltered: 34 altered 12 1: tttf 2:ffft
+        for (MolecularAlteration ma : maItr) {
+            List<GenericAssayCountSummary> genericAssayCountSummaries = new ArrayList<>();
+            List<GroupStatistics> groupsStatistics = new ArrayList<GroupStatistics>();
+            // used for p-value calculation
+            List<double[]> groupedValues = new ArrayList<double[]>();
+
+            for (Entry<String, List<Integer>> group : groupIndicesMap.entrySet()) {
+                GenericAssayCountSummary genericAssayCountSummary = new GenericAssayCountSummary();
+                genericAssayCountSummary.setName(group.getKey());
+                // Get the corresponding split values for the group
+                List<String> groupValues = group.getValue().stream()
+                    .map(sampleIndex -> ma.getSplitValues()[sampleIndex])
+                    .collect(Collectors.toList());
+                // Group and count the split values
+                Map<String, Integer> groupedSplitValues = groupValues.stream()
+                    .collect(Collectors.toMap(Function.identity(), v -> 1, Integer::sum));
+
+                // get expression values to all the indices in the group, filter NA and map binary values
+                //unaltered:1:1,0 2:0,1 altered:1:1,1 2:0,0
+                List<String> molecularDataValues = group.getValue().stream()
+                    .map(sampleIndex -> ma.getSplitValues()[sampleIndex])
+                    .filter(StringUtils::isNotEmpty)
+                    .map(a -> a.equals(POS) ? ALTERED : UNALTERED)
+                    .collect(Collectors.toList());
+
+                // ignore group if there are less than 2 values
+                if (molecularDataValues.size() < 2) {
+                    continue;
+                }
+
+                double[] values = getAlterationValues(molecularDataValues, molecularProfile.getStableId());
+
+                GroupStatistics groupStatistics = new GroupStatistics();
+                double alteredMean = StatUtils.mean(values);
+                double alteredStandardDeviation = calculateStandardDeviation(values);
+
+                // ignore if mean or standard deviation are not numbers
+                if (Double.isNaN(alteredMean) || Double.isNaN(alteredStandardDeviation)) {
+                    continue;
+                }
+
+                groupedValues.add(values);
+                groupStatistics.setName(group.getKey());
+                groupStatistics.setMeanExpression(BigDecimal.valueOf(alteredMean));
+                groupStatistics.setStandardDeviation(BigDecimal.valueOf(alteredStandardDeviation));
+                groupsStatistics.add(groupStatistics);
+                genericAssayCountSummaries.add(genericAssayCountSummary);
+
+            }
+
+            // calculate p-value and add enrichment if atleast 2 groups have data
+            if (groupsStatistics.size() > 1) {
+                double pValue = calculatePValue(groupedValues);
+                if (Double.isNaN(pValue)) {
+                    continue;
+                }
+                S expressionEnrichment = null;
+                GenericAssayBinaryEnrichment genericAssayBinaryEnrichment = new GenericAssayBinaryEnrichment();
+                genericAssayBinaryEnrichment.setStableId(ma.getStableId());
+                genericAssayBinaryEnrichment.setCounts(genericAssayCountSummaries);
+                expressionEnrichment = (S) genericAssayBinaryEnrichment;
+                
+                expressionEnrichment.setpValue(BigDecimal.valueOf(pValue));
+                expressionEnrichment.setGroupsStatistics(groupsStatistics);
+                expressionEnrichments.add(expressionEnrichment);
+            }
+        }
+        return expressionEnrichments;
+    }
+
+    private double[] getAlterationValues(List<String> molecularDataValues, String molecularProfileId) {
 
 		if (molecularProfileId.contains(RNA_SEQ)) {
 			return molecularDataValues.stream().mapToDouble(d -> {
@@ -123,6 +252,36 @@ public class ExpressionEnrichmentUtil {
 			return molecularDataValues.stream().mapToDouble(g -> Double.parseDouble(g)).toArray();
 		}
 	}
+    
+    private long[][] getCategoricalValues(Map<String, Map<String, Integer>> groupCategoryStatistics) {
+            // Determine the number of rows and columns
+            int numRows = groupCategoryStatistics.size();
+            Set<String> allCategories = groupCategoryStatistics.values().stream()
+                .flatMap(innerMap -> innerMap.keySet().stream())
+                .collect(Collectors.toSet());
+            int numCols = allCategories.size();
+    
+            // Create the 2-dimensional long array
+            long[][] array = new long[numRows][numCols];
+    
+            // Iterate over the outer map (group -> categories)
+            List<String> groupKeys = new ArrayList<>(groupCategoryStatistics.keySet());
+            for (int row = 0; row < numRows; row++) {
+                String groupKey = groupKeys.get(row);
+                Map<String, Integer> innerMap = groupCategoryStatistics.get(groupKey);
+    
+                // Iterate over all categories
+                List<String> categoryKeys = new ArrayList<>(allCategories);
+                for (int col = 0; col < numCols; col++) {
+                    String categoryKey = categoryKeys.get(col);
+    
+                    // Get the count from the inner map, or set as zero if the category doesn't exist
+                    int count = innerMap.getOrDefault(categoryKey, 0);
+                    array[row][col] = count;
+                }
+            }
+            return array;
+    }
 
 	private double calculatePValue(List<double[]> alteredValues) {
 
@@ -229,4 +388,11 @@ public class ExpressionEnrichmentUtil {
 					.collect(Collectors.toMap(Sample::getStableId, x -> Arrays.asList(x.getInternalId())));
 		}
 	}
+    public static void main(String[] args) {
+        List<double[]> alteredValues = null;
+        alteredValues.add(new double[]{0, 0});
+        alteredValues.add(new double[]{0, 1});
+        double pValue = TestUtils.tTest(alteredValues.get(0), alteredValues.get(1));
+        System.out.println("Calculated p-value: " + pValue);
+    }
 }

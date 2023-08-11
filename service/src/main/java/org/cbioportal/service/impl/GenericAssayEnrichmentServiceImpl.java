fe2acc86748a3744733b06b664bdea7fc9f77923
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-08-11 11:05:28 -0400

    add inline comments

--- service/src/main/java/org/cbioportal/service/impl/GenericAssayEnrichmentServiceImpl.java
+++ service/src/main/java/org/cbioportal/service/impl/GenericAssayEnrichmentServiceImpl.java
@@ -47,28 +47,36 @@ public class GenericAssayEnrichmentServiceImpl implements GenericAssayEnrichment
         Map<String, List<MolecularProfileCaseIdentifier>> molecularProfileCaseSets, EnrichmentType enrichmentType)
         throws MolecularProfileNotFoundException {
 
+        // Validate and fetch molecular profile
         MolecularProfile molecularProfile = getAndValidateMolecularProfile(molecularProfileId, true);
+
+        // Get the molecular alterations for the provided profile
         Iterable<GenericAssayMolecularAlteration> maItr = molecularDataRepository
             .getGenericAssayMolecularAlterationsIterable(molecularProfile.getStableId(), null, "SUMMARY");
 
+        // Filter the case sets based on molecular profile
         Map<String, List<MolecularProfileCaseIdentifier>> filteredMolecularProfileCaseSets = filterMolecularProfileCaseSets(molecularProfile, molecularProfileCaseSets);
 
+        // Obtain binary enrichments from the utility
         List<GenericAssayBinaryEnrichment> genericAssayBinaryEnrichments = expressionEnrichmentUtil.getGenericAssayBinaryEnrichments(molecularProfile,
             filteredMolecularProfileCaseSets, enrichmentType, maItr);
 
+        // Calculate q-values for enrichments
         calcQValues(genericAssayBinaryEnrichments);
 
+        // Extract stable IDs from binary enrichments
         List<String> getGenericAssayStableIds = genericAssayBinaryEnrichments.stream()
             .map(GenericAssayEnrichment::getStableId).collect(Collectors.toList());
 
+        // Fetch metadata of generic assays by their stable IDs
         Map<String, GenericAssayMeta> genericAssayMetaByStableId = getGenericAssayMetaByStableId(getGenericAssayStableIds, molecularProfileId);
 
+        // Assign meta properties to each enrichment
         return genericAssayBinaryEnrichments.stream().map(enrichmentDatum -> {
             enrichmentDatum.setGenericEntityMetaProperties(
                 genericAssayMetaByStableId.get(enrichmentDatum.getStableId()).getGenericEntityMetaProperties());
             return enrichmentDatum;
         }).collect(Collectors.toList());
-
     }
 
     @Override
@@ -111,8 +119,10 @@ public class GenericAssayEnrichmentServiceImpl implements GenericAssayEnrichment
                                           List<MolecularProfile.MolecularAlterationType> validMolecularAlterationTypes, 
                                           boolean isBinary) throws MolecularProfileNotFoundException {
         if (!validMolecularAlterationTypes.contains(molecularProfile.getMolecularAlterationType())) {
+            // Check alteration type
             throw new MolecularProfileNotFoundException(molecularProfile.getStableId());
         }
+        // Check datatype for binary or categorical
         if(isBinary) {
             if(!molecularProfile.getDatatype().equals("BINARY")) {
                 throw new MolecularProfileNotFoundException(molecularProfile.getStableId());
@@ -126,6 +136,8 @@ public class GenericAssayEnrichmentServiceImpl implements GenericAssayEnrichment
 
     private Map<String, List<MolecularProfileCaseIdentifier>> filterMolecularProfileCaseSets(MolecularProfile molecularProfile, Map<String, List<MolecularProfileCaseIdentifier>> molecularProfileCaseSets) {
         if (BooleanUtils.isTrue(molecularProfile.getPatientLevel())) {
+            // If patient level, filter duplicates by patient id
+            // For now we only support sample level for samples
             List<String> sampleIds = molecularProfileCaseSets.values().stream().flatMap(Collection::stream).map(MolecularProfileCaseIdentifier::getCaseId).collect(Collectors.toList());
             List<String> studyIds = Collections.nCopies(sampleIds.size(), molecularProfile.getCancerStudyIdentifier());
             List<Sample> samples = sampleService.fetchSamples(studyIds, sampleIds, "ID");
@@ -156,10 +168,11 @@ public class GenericAssayEnrichmentServiceImpl implements GenericAssayEnrichment
     }
 
     private <T extends GenericAssayEnrichment> void calcQValues(List<T> enrichments) {
+        // Sort enrichments by pValue
         Collections.sort(enrichments, GenericAssayEnrichment::compare);
         BigDecimal[] pValues = enrichments.stream().map(T::getpValue).toArray(BigDecimal[]::new);
         BigDecimal[] qValues = fisherExactTestCalculator.calcqValue(pValues);
-
+        // Assign q-values to enrichments
         for (int i = 0; i < enrichments.size(); i++) {
             enrichments.get(i).setqValue(qValues[i]);
         }

6c4d73e94d0f923e04ffea3296422ae5b76288d6
haynescd <haynescd@chop.edu>
2023-09-04 18:17:35 -0400

    Update Persistence Config to update classpath for mybatis

--- persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/config/PersistenceConfig.java
+++ persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/config/PersistenceConfig.java
@@ -2,13 +2,19 @@ package org.cbioportal.persistence.mybatis.config;
 
 import org.cbioportal.model.Sample;
 import org.cbioportal.persistence.mybatis.typehandler.SampleTypeTypeHandler;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@MapperScan("org.cbioportal.persistence.mybatis")
+import javax.sql.DataSource;
+
+
 @Configuration
+@MapperScan("org.cbioportal.persistence.mybatis")
 public class PersistenceConfig {
 
     // This is the only way I was able to register the SampleType TypeHandler to MyBatis.
@@ -22,6 +28,18 @@ public class PersistenceConfig {
                 configuration.getTypeHandlerRegistry().register(Sample.SampleType.class, new SampleTypeTypeHandler());
             }
         };
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        // Specify the location of your mapper XML files in the 'mappers' directory
+        Resource[] resources = new PathMatchingResourcePatternResolver()
+            .getResources("classpath:org/cbioportal/persistence/mybatis/*.xml");
+
+        sessionFactory.setMapperLocations(resources);
+        return sessionFactory;
     }
 
 }

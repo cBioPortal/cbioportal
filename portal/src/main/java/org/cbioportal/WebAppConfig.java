04cbc20f2d7043096cd93a79338790b5c5df4def
haynescd <haynescd@chop.edu>
2023-09-27 09:44:49 -0400

    Add mutation_mapper to redirect ruleset

--- portal/src/main/java/org/cbioportal/WebAppConfig.java
+++ portal/src/main/java/org/cbioportal/WebAppConfig.java
@@ -37,6 +37,7 @@ public class WebAppConfig implements WebMvcConfigurer {
         registry.addViewController("/patient**").setViewName(SINGLE_PAGE_APP_ROOT);
         registry.addViewController("/study/*").setViewName(SINGLE_PAGE_APP_ROOT);
         registry.addViewController("/mutation_mapper/*").setViewName(SINGLE_PAGE_APP_ROOT);
+        registry.addViewController("/mutation_mapper").setViewName(SINGLE_PAGE_APP_ROOT);
         registry.addViewController("/index.do/*").setViewName(SINGLE_PAGE_APP_ROOT);
         registry.addViewController("/case.do/*").setViewName(SINGLE_PAGE_APP_ROOT);
         registry.addViewController("/loading/*").setViewName(SINGLE_PAGE_APP_ROOT);

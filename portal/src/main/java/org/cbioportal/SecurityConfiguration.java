a1a23a7936ac64facc9a1423a3fd1f63ea2a6011
haynescd <haynescd@chop.edu>
2023-10-11 16:35:42 -0400

    Upgrade to spring security 5.8

--- portal/src/main/java/org/cbioportal/SecurityConfiguration.java
+++ portal/src/main/java/org/cbioportal/SecurityConfiguration.java
@@ -1,16 +1,21 @@
 package org.cbioportal;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/").permitAll();
-        http.csrf().disable();
+public class SecurityConfiguration {
+   //TODO: Remove this... this is strictly for testing Spring-boot updates 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/").permitAll()
+            .and()
+            .csrf()
+            .disable();
+        return http.build();
     }
 }

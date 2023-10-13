d3ebe5de2c708c3a0d5a4fe15e16e8a93ef3e4f9
haynescd <haynescd@chop.edu>
2023-10-12 23:04:48 -0400

    Fix Swagger Docs

--- web/src/main/java/org/cbioportal/web/config/SwaggerConfig.java
+++ web/src/main/java/org/cbioportal/web/config/SwaggerConfig.java
@@ -21,7 +21,8 @@ public class SwaggerConfig {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
             .group("public")
-            .addOpenApiMethodFilter(method -> method.isAnnotationPresent(PublicApi.class))
+            .addOpenApiMethodFilter(method -> method.getDeclaringClass().isAnnotationPresent(PublicApi.class))
+            .pathsToMatch("/api/**")
             .build();
     }
 
@@ -29,49 +30,10 @@ public class SwaggerConfig {
     public GroupedOpenApi internalApi() {
         return GroupedOpenApi.builder()
             .group("internal")
-            .addOpenApiMethodFilter(method -> method.isAnnotationPresent(InternalApi.class))
+            .addOpenApiMethodFilter(method -> method.getDeclaringClass().isAnnotationPresent(InternalApi.class))
             .build();
     }
     
-//    @Bean
-//    public Docket publicApi() {
-//        Docket d = new Docket(DocumentationType.SWAGGER_2)
-//            .select()
-//            .apis(RequestHandlerSelectors.withClassAnnotation(PublicApi.class))
-//            .build()
-//            .useDefaultResponseMessages(false)
-//            .apiInfo(publicApiInfo());
-//
-//        d.tags(
-//            new Tag(PublicApiTags.CANCER_TYPES, "", 0),
-//            new Tag(PublicApiTags.STUDIES, "", 1),
-//            new Tag(PublicApiTags.PATIENTS, "", 2),
-//            new Tag(PublicApiTags.SAMPLES, "", 3),
-//            new Tag(PublicApiTags.SAMPLE_LISTS, "", 4),
-//            new Tag(PublicApiTags.CLINICAL_ATTRIBUTES, "", 5),
-//            new Tag(PublicApiTags.CLINICAL_DATA, "", 6),
-//            new Tag(PublicApiTags.MOLECULAR_DATA, "", 7),
-//            new Tag(PublicApiTags.MOLECULAR_PROFILES, "", 8),
-//            new Tag(PublicApiTags.MUTATIONS, "", 9),
-//            new Tag(PublicApiTags.DISCRETE_COPY_NUMBER_ALTERATIONS, "", 10),
-//            new Tag(PublicApiTags.COPY_NUMBER_SEGMENTS, "", 11),
-//            new Tag(PublicApiTags.GENES, "", 12),
-//            new Tag(PublicApiTags.GENE_PANELS, "", 13),
-//            new Tag(PublicApiTags.GENERIC_ASSAYS, "", 14),
-//            new Tag(PublicApiTags.GENERIC_ASSAY_DATA, "", 15),
-//            new Tag(PublicApiTags.INFO, "", 16)
-//        );
-
-//    @Bean
-//    public Docket internalApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).groupName("internal")
-//            .select()
-//            .apis(RequestHandlerSelectors.withClassAnnotation(InternalApi.class))
-//            .build()
-//            .useDefaultResponseMessages(false)
-//            .apiInfo(internalApiInfo());
-//    }
-
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()

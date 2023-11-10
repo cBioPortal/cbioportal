b2ddd012b42ed3e4c9ab67fc8f79d820fb1c5eae
haynescd <haynescd@chop.edu>
2023-11-10 17:23:14 -0500

    Update core jar

--- test/integration/test_load_study.sh
+++ test/integration/test_load_study.sh
@@ -15,11 +15,12 @@ run_in_service() {
 # load panels
 echo "Testing the loading of gene panels..."
 run_in_service cbioportal "cd /core/scripts/ && perl importGenePanel.pl --data \
-                          /cbioportal/test/integration/test_data/study_es_0/data_gene_panel_testpanel1.txt"
+                          /cbioportal/test/test_data/study_es_0/data_gene_panel_testpanel1.txt"
+echo "Next"
 run_in_service cbioportal "cd /core/scripts/ && perl importGenePanel.pl --data \
-                          /cbioportal/test/integration/test_data/study_es_0/data_gene_panel_testpanel2.txt"
-run_in_service cbioportal "cd /core/scripts/ && echo yes | ./importGenesetData.pl --data /cbioportal/test/integration/test_data/genesets/study_es_0_genesets.gmt --new-version msigdb_7.5.1 --supp /cbioportal/test/integration/test_data/genesets/study_es_0_supp-genesets.txt"
-run_in_service cbioportal "cd /core/scripts/ && ./importGenesetHierarchy.pl --data /cbioportalc/test/integration/test_data/genesets/study_es_0_tree.yaml"
+                          /cbioportal/test/test_data/study_es_0/data_gene_panel_testpanel2.txt"
+run_in_service cbioportal "cd /core/scripts/ && echo yes | ./importGenesetData.pl --data /cbioportal/test/test_data/genesets/study_es_0_genesets.gmt --new-version msigdb_7.5.1 --supp /cbioportal/test/test_data/genesets/study_es_0_supp-genesets.txt"
+run_in_service cbioportal "cd /core/scripts/ && ./importGenesetHierarchy.pl --data /cbioportalc/test/test_data/genesets/study_es_0_tree.yaml"
 
 # dump portal info
 echo "Testing the dump of local portal info directory..."
@@ -27,10 +28,10 @@ run_in_service cbioportal 'cd core/scripts/ && perl dumpPortalInfo.pl /cbioporta
 
 # validate study_es_0 using local portal info directory
 echo "Testing validation based on local portalinfo..."
-run_in_service cbioportal 'validateData.py -v -p /cbioportal/portalinfo -s /cbioportal/test/integration/test_data/study_es_0/'
+run_in_service cbioportal 'validateData.py -v -p /cbioportal/portalinfo -s /cbioportal/test/test_data/study_es_0/'
 
 # load study_es_0 using API validation
 echo "Testing loading of study with API validation..."
-run_in_service cbioportal 'metaImport.py -v -u http://cbioportal-container:8080 -o -s /cbioportal/test/integration/test_data/study_es_0/'
+run_in_service cbioportal 'metaImport.py -v -u http://cbioportal-container:8080 -o -s /cbioportal/test/test_data/study_es_0/'
 
 exit 0

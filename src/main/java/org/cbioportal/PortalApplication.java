d58d217e0360ca039a4671fd88c180dfa140e8c4
haynescd <haynescd@chop.edu>
2023-11-28 12:39:09 -0500

    Add dep for spring boot http Redis session

--- src/main/java/org/cbioportal/PortalApplication.java
+++ src/main/java/org/cbioportal/PortalApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 

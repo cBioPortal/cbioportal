434b0a75f8eb27e9535757a2cc6fdc490fdaf052
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2023-12-11 16:47:09 -0600

    Add static fields to method security config

--- src/main/java/org/cbioportal/security/config/MethodSecurityConfig.java
+++ src/main/java/org/cbioportal/security/config/MethodSecurityConfig.java
@@ -7,31 +7,31 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
 @EnableMethodSecurity
 @ConditionalOnExpression("{'oauth2','saml','optional_oauth2'}.contains('${authenticate}')")
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityConfig {
     @Value("${app.name:}")
-    private String appName;
+    private static String appName;
 
     @Value("${filter_groups_by_appname:true}")
-    private String doFilterGroupsByAppName;
+    private static String doFilterGroupsByAppName;
 
     @Value("${always_show_study_group:}")
-    private String alwaysShowCancerStudyGroup;
+    private static String alwaysShowCancerStudyGroup;
 
     @Qualifier("staticRefCacheMapUtil")
     @Autowired
-    private CacheMapUtil cacheMapUtil;
+    private static CacheMapUtil cacheMapUtil;
     
-    @Override
-    protected MethodSecurityExpressionHandler createExpressionHandler() {
+    @Bean
+    static MethodSecurityExpressionHandler createExpressionHandler() {
         DefaultMethodSecurityExpressionHandler expressionHandler =
             new DefaultMethodSecurityExpressionHandler();
         expressionHandler.setPermissionEvaluator(new CancerStudyPermissionEvaluator(appName, doFilterGroupsByAppName, alwaysShowCancerStudyGroup, cacheMapUtil));

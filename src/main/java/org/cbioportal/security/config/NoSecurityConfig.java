cb16072e030fd8741bef550e4b0d909c91d45980
haynescd <haynescd@chop.edu>
2024-01-02 15:12:23 -0500

    Add Cors Configuration to CbioPortal

--- src/main/java/org/cbioportal/security/config/NoSecurityConfig.java
+++ src/main/java/org/cbioportal/security/config/NoSecurityConfig.java
@@ -3,6 +3,7 @@ package org.cbioportal.security.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,11 +15,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class NoSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.authorizeHttpRequests(auth -> auth
+        return http
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/").permitAll()
                 .anyRequest().permitAll()
             )
-            .csrf(AbstractHttpConfigurer::disable)
             .build();
     }
 }

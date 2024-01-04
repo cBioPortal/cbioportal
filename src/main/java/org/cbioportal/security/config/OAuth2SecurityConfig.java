01a31957cb99c43f916a3462035351a95e8ec5e4
haynescd <haynescd@chop.edu>
2024-01-04 10:26:58 -0500

    Fix Oauth config

--- src/main/java/org/cbioportal/security/config/OAuth2SecurityConfig.java
+++ src/main/java/org/cbioportal/security/config/OAuth2SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;

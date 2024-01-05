ed92a2b30c31e0e13a0246b873389484bc85df8d
Charles Haynes <33608920+haynescd@users.noreply.github.com>
2024-01-05 09:34:20 -0500

    Fix CacheMapUtil configurations (#10513)
    * Fix CacheMapUtil configurations
    
    * fix InActiveCacheMapUtil
    
    * Fix Oauth config

--- src/main/java/org/cbioportal/persistence/cachemaputil/StaticRefCacheMapUtil.java
+++ src/main/java/org/cbioportal/persistence/cachemaputil/StaticRefCacheMapUtil.java
@@ -39,12 +39,14 @@ import org.cbioportal.model.SampleList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 // Instantiate when user authorization is active and spring-managed implementation is not needed
+@ConditionalOnExpression("${security.method_authorization_enabled:false} and !${cache.cache-map-utils.spring-managed:false}")
 public class StaticRefCacheMapUtil implements CacheMapUtil {
 
     private static final Logger LOG = LoggerFactory.getLogger(StaticRefCacheMapUtil.class);

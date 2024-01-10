ad6c0f8c5f81d26bee3e29e89f0a033e701635a0
pvannierop <pim@thehyve.nl>
2024-01-10 14:19:42 +0100

    Update security config files

---
+++ src/main/java/org/cbioportal/security/config/ApiSecurityConfig.java
@@ -0,0 +1,98 @@
+package org.cbioportal.security.config;
+
+import org.cbioportal.security.token.RestAuthenticationEntryPoint;
+import org.cbioportal.security.token.TokenAuthenticationFilter;
+import org.cbioportal.security.token.TokenAuthenticationSuccessHandler;
+import org.cbioportal.service.DataAccessTokenService;
+import org.cbioportal.utils.config.annotation.ConditionalOnProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+
+@Configuration
+@ConditionalOnProperty(name = "authenticate", havingValue = {"false", "noauthsessionservice"}, isNot = true)
+public class ApiSecurityConfig {
+
+    // Add security filter chains that handle calls to the API endpoints.
+    // Different chains are added for the '/api' and legacy '/webservice.do' paths.
+    // Both are able to handle API tokens provided in the request.
+    // see: "Creating and Customizing Filter Chains" @ https://spring.io/guides/topicals/spring-security-architecture
+
+    @Autowired(required = false)
+    private DataAccessTokenService tokenService;
+    
+    @Autowired(required = false)
+    private AuthenticationProvider tokenAuthenticationProvider;
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            // FIXME - csrf should be enabled
+            .csrf(AbstractHttpConfigurer::disable)
+            // This filter chain only grabs requests to the '/api' path.
+            .securityMatcher("/api/**", "/webservice.do")
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                    "/api/swagger-resources/**",
+                    "/api/swagger-ui.html",
+                    "/api/health",
+                    "/api/cache/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(sessionManagement -> sessionManagement.sessionFixation().none())
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(restAuthenticationEntryPoint())
+            );
+        // When dat.method is not 'none' and a tokenService bean is present,
+        // the apiTokenAuthenticationFilter is added to the filter chain.
+        if (tokenService != null) {
+            http.apply(ApiTokenFilterDsl.tokenFilterDsl(tokenService));
+        }
+        return http.build();
+    }
+    
+    @Autowired
+    public AuthenticationManagerBuilder buildAuthenticationManager(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        if (tokenAuthenticationProvider != null) {
+            authenticationManagerBuilder.authenticationProvider(tokenAuthenticationProvider);
+        }
+        return authenticationManagerBuilder;
+    }
+    
+    @Bean
+    public RestAuthenticationEntryPoint restAuthenticationEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
+    
+}
+
+
+class ApiTokenFilterDsl extends AbstractHttpConfigurer<ApiTokenFilterDsl, HttpSecurity> {
+
+    private final DataAccessTokenService tokenService;
+
+    public ApiTokenFilterDsl(DataAccessTokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+    
+    @Override
+    public void configure(HttpSecurity http) {
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+        TokenAuthenticationSuccessHandler tokenAuthenticationSuccessHandler = new TokenAuthenticationSuccessHandler();
+        TokenAuthenticationFilter filter = new TokenAuthenticationFilter("/**", authenticationManager, tokenService);
+        filter.setAuthenticationSuccessHandler(tokenAuthenticationSuccessHandler);
+        http.addFilterAfter(filter, SecurityContextPersistenceFilter.class);
+    }
+    
+    public static ApiTokenFilterDsl tokenFilterDsl(DataAccessTokenService tokenService) {
+        return new ApiTokenFilterDsl(tokenService);
+    }
+    
+}
--- src/main/java/org/cbioportal/security/config/Saml2SecurityConfig.java
+++ src/main/java/org/cbioportal/security/config/Saml2SecurityConfig.java
@@ -1,13 +1,14 @@
 package org.cbioportal.security.config;
 
 import org.cbioportal.security.util.GrantedAuthorityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,8 +16,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.authentication.logout.OpenSaml4LogoutRequestResolver;
+import org.springframework.security.saml2.provider.service.web.authentication.logout.Saml2RelyingPartyInitiatedLogoutSuccessHandler;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Collection;
@@ -24,28 +31,51 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
+@ConditionalOnProperty(value = "authenticate", havingValue = "saml")
 public class Saml2SecurityConfig {
-    @Bean
-    @ConditionalOnProperty(value = "authenticate", havingValue = "saml")
-    public SecurityFilterChain samlFilterChain(HttpSecurity http) throws Exception {
-        OpenSaml4AuthenticationProvider authenticationProvider = new OpenSaml4AuthenticationProvider();
-        authenticationProvider.setResponseAuthenticationConverter(rolesConverter());
+    
+    @Value("${saml.logout.url:/saml/logout}")
+    private String logoutUrl;
 
-        return http.authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/health", "/login", "/images/**").permitAll()
-                                .anyRequest().authenticated())
-                .exceptionHandling(eh ->
-                        eh.defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), AntPathRequestMatcher.antMatcher("/api/**")))
-                .saml2Login(saml2 -> saml2
-                        .authenticationManager(new ProviderManager(authenticationProvider)))
-                .logout(logout -> logout.logoutSuccessUrl("/login?logout_success"))
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .build();
+    @Autowired(required = false)
+    private RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder builder) {
+        OpenSaml4AuthenticationProvider saml4AuthenticationProvider = new OpenSaml4AuthenticationProvider();
+        saml4AuthenticationProvider.setResponseAuthenticationConverter(rolesConverter());
+        builder.authenticationProvider(saml4AuthenticationProvider);
     }
-
+    
+    @Bean
+    public SecurityFilterChain samlFilterChain(HttpSecurity http) throws Exception {
+        DefaultSecurityFilterChain build = http
+            // FIXME - csrf should be enabled
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                auth.requestMatchers("/api/health", "/images/**", "/js/**").permitAll()
+                    .anyRequest().authenticated())
+            .exceptionHandling(eh ->
+                eh.defaultAuthenticationEntryPointFor(
+                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), AntPathRequestMatcher.antMatcher("/api/**")
+                )
+            )
+            .saml2Login(withDefaults())
+            // NOTE: I did not get the official .saml2Logout() DSL to work as
+            // described at https://docs.spring.io/spring-security/reference/6.1/servlet/saml2/logout.html
+            // Logout Service POST Binding URL: http://localhost:8080/logout/saml2/slo
+            .logout(logout -> logout
+                .logoutUrl(logoutUrl)
+                .logoutSuccessHandler(logoutSuccessHandler())
+            )
+            .build();
+        return build;
+    }
+    
     private Converter<OpenSaml4AuthenticationProvider.ResponseToken, Saml2Authentication> rolesConverter() {
 
         Converter<OpenSaml4AuthenticationProvider.ResponseToken, Saml2Authentication> delegate =
@@ -64,4 +94,15 @@ public class Saml2SecurityConfig {
             return new Saml2Authentication(principal, authentication.getSaml2Response(), mappedAuthorities);
         };
     }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        // Perform logout at the SAML2 IDP
+        DefaultRelyingPartyRegistrationResolver relyingPartyRegistrationResolver =
+            new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrationRepository);
+        OpenSaml4LogoutRequestResolver logoutRequestResolver =
+            new OpenSaml4LogoutRequestResolver(relyingPartyRegistrationResolver);
+        return new Saml2RelyingPartyInitiatedLogoutSuccessHandler(logoutRequestResolver);
+    }
+    
 }

36c25b4b504f5dbf533f18f82304964e86cf77fc
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2024-01-22 07:08:20 -0600

    Update StudyViewServiceClickHouseImpl.java
    Change it to be just the mysql impl extension

--- src/main/java/org/cbioportal/service/impl/clickhouse/StudyViewServiceClickHouseImpl.java
+++ src/main/java/org/cbioportal/service/impl/clickhouse/StudyViewServiceClickHouseImpl.java
@@ -13,6 +13,8 @@ import org.cbioportal.model.GenomicDataCountItem;
 import org.cbioportal.persistence.MolecularDataRepository;
 import org.cbioportal.service.StudyViewService;
 import org.cbioportal.service.exception.StudyNotFoundException;
+import org.cbioportal.service.impl.mysql.SampleServiceMySQLImpl;
+import org.cbioportal.service.impl.mysql.StudyViewServiceMySQLImpl;
 import org.cbioportal.web.parameter.StudyViewFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -20,58 +22,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Profile("clickhouse")
-public class StudyViewServiceClickHouseImpl implements StudyViewService {
+public class StudyViewServiceClickHouseImpl extends StudyViewServiceMySQLImpl {
 	
 	@Autowired
 	private MolecularDataRepository molecularDataRepository;
-
-	@Override
-	public List<GenomicDataCount> getGenomicDataCounts(List<String> studyIds, List<String> sampleIds) {
-		return null;
-	}
-
-	@Override
-	public List<AlterationCountByGene> getMutationAlterationCountByGenes(List<String> studyIds, List<String> sampleIds,
-			AlterationFilter annotationFilter) throws StudyNotFoundException {
-		// TODO Auto-generated method stub
-		
-		return null;
-	}
-
-	@Override
-	public List<AlterationCountByGene> getStructuralVariantAlterationCountByGenes(List<String> studyIds,
-			List<String> sampleIds, AlterationFilter annotationFilter) throws StudyNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<AlterationCountByStructuralVariant> getStructuralVariantAlterationCounts(List<String> studyIds,
-			List<String> sampleIds, AlterationFilter annotationFilters) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<CopyNumberCountByGene> getCNAAlterationCountByGenes(List<String> studyIds, List<String> sampleIds,
-			AlterationFilter annotationFilter) throws StudyNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<GenomicDataCountItem> getCNAAlterationCountsByGeneSpecific(List<String> studyIds,
-			List<String> sampleIds, List<Pair<String, String>> genomicDataFilters) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<GenericAssayDataCountItem> fetchGenericAssayDataCounts(List<String> sampleIds, List<String> studyIds,
-			List<String> stableIds, List<String> profileTypes) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public List<GenomicDataCount> getGenomicDataCounts(StudyViewFilter studyViewFilter,

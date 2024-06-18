1d1606b25d4e8ddf68fe42df3ee5b87b9e0197f0
Neel Kuila <neelk1123@gmail.com>
2024-06-18 16:19:22 -0400

    RFC80:density plot endpoint(#10837)
    * working poc
    
    * refactor logic into service, so clean
    
    * refactor for parameters builder, simplify min max logic, streamline service call
    
    * remove unused services and imports
    
    * remove more unused imports

---
+++ src/main/java/org/cbioportal/service/ClinicalDataDensityPlotService.java
@@ -0,0 +1,12 @@
+package org.cbioportal.service;
+
+import org.cbioportal.model.ClinicalData;
+import org.cbioportal.model.DensityPlotData;
+import org.cbioportal.web.util.DensityPlotParameters;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ClinicalDataDensityPlotService {
+    DensityPlotData getDensityPlotData(List<ClinicalData> filteredClinicalData, DensityPlotParameters densityPlotParameters);
+}

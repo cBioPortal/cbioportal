a03ec54679191a62ef6506d5d00671fec6b2313f
Neel Kuila <neelk1123@gmail.com>
2024-06-18 16:19:22 -0400

    RFC80:density plot endpoint(#10837)
    * working poc
    
    * refactor logic into service, so clean
    
    * refactor for parameters builder, simplify min max logic, streamline service call
    
    * remove unused services and imports
    
    * remove more unused imports

--- src/main/java/org/cbioportal/web/columnar/StudyViewColumnStoreController.java
+++ src/main/java/org/cbioportal/web/columnar/StudyViewColumnStoreController.java
@@ -1,12 +1,20 @@
 package org.cbioportal.web.columnar;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import org.cbioportal.model.AlterationCountByGene;
 import org.cbioportal.model.AlterationFilter;
+import org.cbioportal.model.ClinicalData;
 import org.cbioportal.model.ClinicalDataBin;
 import org.cbioportal.model.ClinicalDataCountItem;
+import org.cbioportal.model.DensityPlotData;
 import org.cbioportal.model.Sample;
+import org.cbioportal.service.ClinicalDataDensityPlotService;
 import org.cbioportal.service.StudyViewColumnarService;
-import org.cbioportal.service.StudyViewService;
 import org.cbioportal.service.exception.StudyNotFoundException;
 import org.cbioportal.web.columnar.util.NewStudyViewFilterUtil;
 import org.cbioportal.web.config.annotation.InternalApi;
@@ -15,6 +23,7 @@ import org.cbioportal.web.parameter.ClinicalDataCountFilter;
 import org.cbioportal.web.parameter.ClinicalDataFilter;
 import org.cbioportal.web.parameter.DataBinMethod;
 import org.cbioportal.web.parameter.StudyViewFilter;
+import org.cbioportal.web.util.DensityPlotParameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -29,7 +38,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,15 +52,20 @@ import java.util.stream.Collectors;
 public class StudyViewColumnStoreController {
     
     private final StudyViewColumnarService studyViewColumnarService;
-    private final StudyViewService studyViewService;
     private final ClinicalDataBinner clinicalDataBinner;
+    private final ClinicalDataDensityPlotService clinicalDataDensityPlotService;
     
     @Autowired
-    public StudyViewColumnStoreController(StudyViewColumnarService studyViewColumnarService, StudyViewService studyViewService, ClinicalDataBinner clinicalDataBinner) {
+    public StudyViewColumnStoreController(StudyViewColumnarService studyViewColumnarService, 
+                                          ClinicalDataBinner clinicalDataBinner,
+                                          ClinicalDataDensityPlotService clinicalDataDensityPlotService
+                                          ) {
         this.studyViewColumnarService = studyViewColumnarService;
-        this.studyViewService = studyViewService;
         this.clinicalDataBinner = clinicalDataBinner;
+        this.clinicalDataDensityPlotService = clinicalDataDensityPlotService;
     }
+    
+
     
     @PreAuthorize("hasPermission(#involvedCancerStudies, 'Collection<CancerStudyId>', T(org.cbioportal.utils.security.AccessLevel).READ)")
     @PostMapping(value = "/column-store/filtered-samples/fetch",
@@ -117,5 +133,62 @@ public class StudyViewColumnStoreController {
             true
         );
         return new ResponseEntity<>(clinicalDataBins, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasPermission(#involvedCancerStudies, 'Collection<CancerStudyId>', T(org.cbioportal.utils.security.AccessLevel).READ)")
+    @RequestMapping(value = "/column-store/clinical-data-density-plot/fetch", method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(description = "Fetch clinical data density plot bins by study view filter")
+    @ApiResponse(responseCode = "200", description = "OK",
+        content = @Content(schema = @Schema(implementation = DensityPlotData.class)))
+    @Validated
+    public ResponseEntity<DensityPlotData> fetchClinicalDataDensityPlot(
+        @Parameter(required = true, description = "Clinical Attribute ID of the X axis")
+        @RequestParam String xAxisAttributeId,
+        @Parameter(description = "Number of the bins in X axis")
+        @RequestParam(defaultValue = "50") Integer xAxisBinCount,
+        @Parameter(description = "Starting point of the X axis, if different than smallest value")
+        @RequestParam(required = false) BigDecimal xAxisStart,
+        @Parameter(description = "Starting point of the X axis, if different than largest value")
+        @RequestParam(required = false) BigDecimal xAxisEnd,
+        @Parameter(required = true, description = "Clinical Attribute ID of the Y axis")
+        @RequestParam String yAxisAttributeId,
+        @Parameter(description = "Number of the bins in Y axis")
+        @RequestParam(defaultValue = "50") Integer yAxisBinCount,
+        @Parameter(description = "Starting point of the Y axis, if different than smallest value")
+        @RequestParam(required = false) BigDecimal yAxisStart,
+        @Parameter(description = "Starting point of the Y axis, if different than largest value")
+        @RequestParam(required = false) BigDecimal yAxisEnd,
+        @Parameter(description="Use log scale for X axis")
+        @RequestParam(required = false, defaultValue = "false") Boolean xAxisLogScale,
+        @Schema(defaultValue = "false")
+        @Parameter(description="Use log scale for Y axis")
+        @RequestParam(required = false, defaultValue = "false") Boolean yAxisLogScale,
+        @Parameter(hidden = true) // prevent reference to this attribute in the swagger-ui interface
+        @RequestAttribute(required = false, value = "involvedCancerStudies") Collection<String> involvedCancerStudies,
+        @Parameter(hidden = true) // prevent reference to this attribute in the swagger-ui interface. this attribute is needed for the @PreAuthorize tag above.
+        @Valid @RequestAttribute(required = false, value = "interceptedStudyViewFilter") StudyViewFilter interceptedStudyViewFilter,
+        @Parameter(required = true, description = "Study view filter")
+        @RequestBody(required = false) StudyViewFilter studyViewFilter) {
+        
+        List<String> xyAttributeId = new ArrayList<>(Arrays.asList(xAxisAttributeId, yAxisAttributeId));
+        DensityPlotParameters densityPlotParameters = 
+            new DensityPlotParameters.Builder()
+            .xAxisAttributeId(xAxisAttributeId)
+            .yAxisAttributeId(yAxisAttributeId)
+            .xAxisBinCount(xAxisBinCount)
+            .yAxisBinCount(yAxisBinCount)
+            .xAxisStart(xAxisStart)
+            .yAxisStart(yAxisStart)
+            .xAxisEnd(xAxisEnd)
+            .yAxisEnd(yAxisEnd)
+            .xAxisLogScale(xAxisLogScale)
+            .yAxisLogScale(yAxisLogScale)
+            .build();
+        
+        List<ClinicalData> sampleClinicalDataList = studyViewColumnarService.getSampleClinicalData(interceptedStudyViewFilter, xyAttributeId);
+        DensityPlotData result = clinicalDataDensityPlotService.getDensityPlotData(sampleClinicalDataList, densityPlotParameters);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }

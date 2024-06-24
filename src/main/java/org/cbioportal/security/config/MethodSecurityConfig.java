8e2e889f54058577cdfda58341e34864fbcc95dc
Floris-Hyve <146722061+Floris-Hyve@users.noreply.github.com>
2024-06-24 16:54:02 +0200

    Add bean for CancerStudyPermissionEvaluator (#10825)

--- src/main/java/org/cbioportal/security/config/MethodSecurityConfig.java
+++ src/main/java/org/cbioportal/security/config/MethodSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -32,7 +33,12 @@ public class MethodSecurityConfig {
     public MethodSecurityExpressionHandler createExpressionHandler() {
         DefaultMethodSecurityExpressionHandler expressionHandler =
             new DefaultMethodSecurityExpressionHandler();
-        expressionHandler.setPermissionEvaluator(new CancerStudyPermissionEvaluator(appName, doFilterGroupsByAppName, alwaysShowCancerStudyGroup, cacheMapUtil));
+        expressionHandler.setPermissionEvaluator(cancerStudyPermissionEvaluator());
         return expressionHandler;
+    }
+    
+    @Bean
+    public CancerStudyPermissionEvaluator cancerStudyPermissionEvaluator() {
+        return new CancerStudyPermissionEvaluator(appName, doFilterGroupsByAppName, alwaysShowCancerStudyGroup, cacheMapUtil);
     }
 }

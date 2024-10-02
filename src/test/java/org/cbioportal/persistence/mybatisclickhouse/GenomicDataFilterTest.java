ed1165f6bed85fa779482393858c6f013fcafbf1
alisman <lisman.aaron@gmail.com>
2024-10-02 13:39:05 -0400

    Add support for NA in genomic-data-bin-counts (#11006)
    * Add NA for genomic-data-bin-counts
    
    * Filtering with NA for genomic-data-bin-counts
    
    * Fix when subqueries return empty for NA counts
    
    * Refinement and comments
    
    * Address comments
    
    * Fix intersection with parens (#11032)
    
    * Fix intersection with parens
    
    * Demo rfc80 poc genomic data filter tests (#11036)
    
    * add genomic data filter tests testing for missing NAs
    * add NA genomic data filter tests
    
    
    ---------
    
    Co-authored-by: Zhaoyuan (Ryan) Fu <fuzhaoyuan@gmail.com>

--- src/test/java/org/cbioportal/persistence/mybatisclickhouse/GenomicDataFilterTest.java
+++ src/test/java/org/cbioportal/persistence/mybatisclickhouse/GenomicDataFilterTest.java
@@ -1,9 +1,12 @@
 package org.cbioportal.persistence.mybatisclickhouse;
 
+import org.cbioportal.model.ClinicalDataCount;
 import org.cbioportal.model.GenomicDataCount;
 import org.cbioportal.model.GenomicDataCountItem;
 import org.cbioportal.persistence.helper.StudyViewFilterHelper;
 import org.cbioportal.persistence.mybatisclickhouse.config.MyBatisConfig;
+import org.cbioportal.web.parameter.DataFilterValue;
+import org.cbioportal.web.parameter.GenomicDataBinFilter;
 import org.cbioportal.web.parameter.GenomicDataFilter;
 import org.cbioportal.web.parameter.StudyViewFilter;
 import org.junit.Test;
@@ -107,5 +110,67 @@ public class GenomicDataFilterTest extends AbstractTestcontainers {
             .usingRecursiveComparison()
             .ignoringCollectionOrder()
             .isEqualTo(expectedMutationCountsByType);
+    }
+
+    @Test
+    public void getProteinExpressionCounts() {
+        // Testing combined study missing samples when one lacks a relevant genomic profile
+        StudyViewFilter studyViewFilter = new StudyViewFilter();
+        studyViewFilter.setStudyIds(List.of(STUDY_TCGA_PUB, STUDY_ACC_TCGA));
+
+        GenomicDataBinFilter genomicDataBinFilterRPPA = new GenomicDataBinFilter();
+        genomicDataBinFilterRPPA.setHugoGeneSymbol("AKT1");
+        genomicDataBinFilterRPPA.setProfileType("rppa");
+
+        List<ClinicalDataCount> actualRPPACounts1 = studyViewMapper.getGenomicDataBinCounts(StudyViewFilterHelper.build(studyViewFilter, null, null), List.of(genomicDataBinFilterRPPA));
+
+        ClinicalDataCount expectedRPPACount1 = new ClinicalDataCount();
+        expectedRPPACount1.setAttributeId("AKT1rppa");
+        expectedRPPACount1.setValue("0.7360");
+        expectedRPPACount1.setCount(1);
+        ClinicalDataCount expectedRPPACount2 = new ClinicalDataCount();
+        expectedRPPACount2.setAttributeId("AKT1rppa");
+        expectedRPPACount2.setValue("-0.8097");
+        expectedRPPACount2.setCount(1);
+        ClinicalDataCount expectedRPPACount3 = new ClinicalDataCount();
+        expectedRPPACount3.setAttributeId("AKT1rppa");
+        expectedRPPACount3.setValue("-0.1260");
+        expectedRPPACount3.setCount(1);
+        ClinicalDataCount expectedRPPACountNA = new ClinicalDataCount();
+        expectedRPPACountNA.setAttributeId("AKT1rppa");
+        expectedRPPACountNA.setValue("NA");
+        expectedRPPACountNA.setCount(16);
+
+        List<ClinicalDataCount> expectedRPPACounts1 = List.of(
+            expectedRPPACount1, expectedRPPACount2, expectedRPPACount3, expectedRPPACountNA
+        );
+        assertThat(actualRPPACounts1)
+            .usingRecursiveComparison()
+            .ignoringCollectionOrder()
+            .isEqualTo(expectedRPPACounts1);
+
+
+        // Testing NA filtering on combined study missing samples when one lacks a relevant genomic profile
+        // Make genomic data filter to put in study view filter
+        GenomicDataFilter genomicDataFilterRPPA = new GenomicDataFilter("AKT1", "rppa");
+        DataFilterValue dataFilterValue = new DataFilterValue();
+        dataFilterValue.setValue("NA");
+        genomicDataFilterRPPA.setValues(List.of(dataFilterValue));
+        studyViewFilter.setGenomicDataFilters(List.of(genomicDataFilterRPPA));
+
+        List<ClinicalDataCount> actualRPPACounts2 = studyViewMapper.getGenomicDataBinCounts(StudyViewFilterHelper.build(studyViewFilter, null, null), List.of(genomicDataBinFilterRPPA));
+
+        ClinicalDataCount expectedRPPACount = new ClinicalDataCount();
+        expectedRPPACount.setAttributeId("AKT1rppa");
+        expectedRPPACount.setValue("NA");
+        expectedRPPACount.setCount(16);
+
+        List<ClinicalDataCount> expectedRPPACounts2 = List.of(
+            expectedRPPACount
+        );
+        assertThat(actualRPPACounts2)
+            .usingRecursiveComparison()
+            .ignoringCollectionOrder()
+            .isEqualTo(expectedRPPACounts2);
     }
 }

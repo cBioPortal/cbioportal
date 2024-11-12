bd7320fa0a77e53b1a2fdb8bd86b81c6e693270f
alisman <lisman.aaron@gmail.com>
2024-11-11 21:58:23 -0500

    Respond to comments

--- src/main/java/org/cbioportal/service/impl/StudyViewColumnarServiceImpl.java
+++ src/main/java/org/cbioportal/service/impl/StudyViewColumnarServiceImpl.java
@@ -158,25 +158,26 @@ public class StudyViewColumnarServiceImpl implements StudyViewColumnarService {
         var result = studyViewRepository.getClinicalDataCounts(createContext(studyViewFilter), filteredAttributes);
         
         // fetch the samples by using the provided study view filter
-        List<Sample> filteredSamples = getFilteredSamples(studyViewFilter);
+        List<Sample> filteredSamples = studyViewRepository.getFilteredSamples(createContext(studyViewFilter));
 
         var resultWithAllAttributes = StudyViewColumnarServiceUtil.addClinicalDataCountsForMissingAttributes(result, filteredAttributes, filteredSamples);
 
-        resultWithAllAttributes.stream().forEach(attr -> {
-            Map<String, List<ClinicalDataCount>> countsPerType = attr.getCounts().stream()
-                .collect(Collectors.groupingBy(ClinicalDataCount::getValue));
-            List<ClinicalDataCount> res = countsPerType.entrySet().stream().map((entry)->{
-                ClinicalDataCount mergedCount = new ClinicalDataCount();
-                mergedCount.setAttributeId(attr.getAttributeId());
-                mergedCount.setValue(entry.getKey());
-                mergedCount.setCount(entry.getValue().stream().mapToInt(ClinicalDataCount::getCount).sum());
-                return mergedCount;
-            }).collect(Collectors.toList());
-            attr.setCounts(res);
-        });
+        var mergedResult = StudyViewColumnarServiceUtil.mergeClinicalDataCounts(resultWithAllAttributes);
         
+//        resultWithAllAttributes.stream().forEach(attr -> {
+//            Map<String, List<ClinicalDataCount>> countsPerType = attr.getCounts().stream()
+//                .collect(Collectors.groupingBy(ClinicalDataCount::getValue));
+//            List<ClinicalDataCount> res = countsPerType.entrySet().stream().map((entry)->{
+//                ClinicalDataCount mergedCount = new ClinicalDataCount();
+//                mergedCount.setAttributeId(attr.getAttributeId());
+//                mergedCount.setValue(entry.getKey());
+//                mergedCount.setCount(entry.getValue().stream().mapToInt(ClinicalDataCount::getCount).sum());
+//                return mergedCount;
+//            }).collect(Collectors.toList());
+//            attr.setCounts(res);
+//        });
         
-        return resultWithAllAttributes;
+        return mergedResult;
     }
     
     

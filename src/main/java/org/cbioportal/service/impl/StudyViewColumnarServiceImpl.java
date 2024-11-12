afa53bf248bbd5e5a623ffde4c4c6ee825329d0e
alisman <lisman.aaron@gmail.com>
2024-11-12 09:59:02 -0500

    Cleanup

--- src/main/java/org/cbioportal/service/impl/StudyViewColumnarServiceImpl.java
+++ src/main/java/org/cbioportal/service/impl/StudyViewColumnarServiceImpl.java
@@ -162,22 +162,8 @@ public class StudyViewColumnarServiceImpl implements StudyViewColumnarService {
 
         var resultWithAllAttributes = StudyViewColumnarServiceUtil.addClinicalDataCountsForMissingAttributes(result, filteredAttributes, filteredSamples);
 
-        var mergedResult = StudyViewColumnarServiceUtil.mergeClinicalDataCounts(resultWithAllAttributes);
+        return StudyViewColumnarServiceUtil.mergeClinicalDataCounts(resultWithAllAttributes);
         
-//        resultWithAllAttributes.stream().forEach(attr -> {
-//            Map<String, List<ClinicalDataCount>> countsPerType = attr.getCounts().stream()
-//                .collect(Collectors.groupingBy(ClinicalDataCount::getValue));
-//            List<ClinicalDataCount> res = countsPerType.entrySet().stream().map((entry)->{
-//                ClinicalDataCount mergedCount = new ClinicalDataCount();
-//                mergedCount.setAttributeId(attr.getAttributeId());
-//                mergedCount.setValue(entry.getKey());
-//                mergedCount.setCount(entry.getValue().stream().mapToInt(ClinicalDataCount::getCount).sum());
-//                return mergedCount;
-//            }).collect(Collectors.toList());
-//            attr.setCounts(res);
-//        });
-        
-        return mergedResult;
     }
     
     

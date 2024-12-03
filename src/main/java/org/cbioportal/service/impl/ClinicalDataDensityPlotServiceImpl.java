e7e7d7b2e99d5493992e2319c17203dfb93bb70e
alisman <lisman.aaron@gmail.com>
2024-12-03 18:08:25 -0500

    Fix unfiltered method call in cacheable condition

--- src/main/java/org/cbioportal/service/impl/ClinicalDataDensityPlotServiceImpl.java
+++ src/main/java/org/cbioportal/service/impl/ClinicalDataDensityPlotServiceImpl.java
@@ -26,7 +26,7 @@ public class ClinicalDataDensityPlotServiceImpl implements ClinicalDataDensityPl
 
     @Cacheable(
         cacheResolver = "staticRepositoryCacheOneResolver",
-        condition = "@cacheEnabledConfig.getEnabledClickhouse() && @studyViewFilterUtil.isUnfiltered(#studyViewFilter)"
+        condition = "@cacheEnabledConfig.getEnabledClickhouse() && @studyViewFilterUtil.isUnfilteredQuery(#studyViewFilter)"
     )
     @Override
     public DensityPlotData getDensityPlotData(List<ClinicalData> sampleClinicalData, DensityPlotParameters densityPlotParameters, StudyViewFilter studyViewFilter) {

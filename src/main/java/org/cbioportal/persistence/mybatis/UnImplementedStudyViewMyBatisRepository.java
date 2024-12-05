64376de3f1a2144ba576ce6b0c4d6d1e26bba410
Charles Haynes <haynescd@chop.edu>
2024-12-05 11:14:30 -0500

    Adress comments

--- src/main/java/org/cbioportal/persistence/mybatis/UnImplementedStudyViewMyBatisRepository.java
+++ src/main/java/org/cbioportal/persistence/mybatis/UnImplementedStudyViewMyBatisRepository.java
@@ -32,140 +32,143 @@ import java.util.Set;
 @Repository
 @ConditionalOnProperty(name = "clickhouse_mode", havingValue = "false", matchIfMissing = true)
 public class UnImplementedStudyViewMyBatisRepository implements StudyViewRepository {
+    
+    private static final String UNSUPPORTED_OPERATION = "StudyViewRepository Feature Not supported...  Contact cbio admin.";
+    
     @Override
     public List<Sample> getFilteredSamples(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<String> getFilteredStudyIds(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalData> getSampleClinicalData(StudyViewFilterContext studyViewFilterContext, List<String> attributeIds) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalData> getPatientClinicalData(StudyViewFilterContext studyViewFilterContext, List<String> attributeIds) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
 
     }
 
     @Override
     public List<AlterationCountByGene> getMutatedGenes(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<AlterationCountByGene> getStructuralVariantGenes(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<CopyNumberCountByGene> getCnaGenes(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalDataCountItem> getClinicalDataCounts(StudyViewFilterContext studyViewFilterContext, List<String> filteredAttributes) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<GenomicDataCount> getMolecularProfileSampleCounts(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public Map<String, ClinicalDataType> getClinicalAttributeDatatypeMap() {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<CaseListDataCount> getCaseListDataCountsPerStudy(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public Map<String, Integer> getTotalProfiledCounts(StudyViewFilterContext studyViewFilterContext, String alterationType, List<MolecularProfile> molecularProfiles) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalAttribute> getClinicalAttributes() {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalAttribute> getClinicalAttributesForStudies(List<String> studyIds) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getFilteredSamplesCount(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getFilteredPatientCount(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public Map<String, Set<String>> getMatchingGenePanelIds(StudyViewFilterContext studyViewFilterContext, String alterationType) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getTotalProfiledCountsByAlterationType(StudyViewFilterContext studyViewFilterContext, String alterationType) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getSampleProfileCountWithoutPanelData(StudyViewFilterContext studyViewFilterContext, String alterationType) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<ClinicalEventTypeCount> getClinicalEventTypeCounts(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<PatientTreatment> getPatientTreatments(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getTotalPatientTreatmentCount(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<SampleTreatment> getSampleTreatments(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public int getTotalSampleTreatmentCount(StudyViewFilterContext studyViewFilterContext) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<GenomicDataCountItem> getCNACounts(StudyViewFilterContext studyViewFilterContext, List<GenomicDataFilter> genomicDataFilters) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public List<GenericAssayDataCountItem> getGenericAssayDataCounts(StudyViewFilterContext studyViewFilterContext, List<GenericAssayDataFilter> genericAssayDataFilters) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
     public Map<String, Integer> getMutationCounts(StudyViewFilterContext studyViewFilterContext, GenomicDataFilter genomicDataFilter) {
-        throw new UnsupportedOperationException("StudyViewRepository Feature Not supported...  Contact cbio admin.");
+        throw new UnsupportedOperationException(UNSUPPORTED_OPERATION);
     }
 
     @Override
--- src/main/java/org/cbioportal/service/impl/AlterationCountServiceImpl.java
+++ src/main/java/org/cbioportal/service/impl/AlterationCountServiceImpl.java
@@ -25,7 +25,6 @@ import org.cbioportal.service.util.AlterationEnrichmentUtil;
 import org.cbioportal.web.parameter.Projection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;

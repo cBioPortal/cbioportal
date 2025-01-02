2c489b493f566a9854b55e0796b27ecdf968dce2
cbioportal import user <cbioportal_importer@pipelines.cbioportal.mskcc.org>
2025-01-02 12:56:21 -0500

    work on test code (wip)

--- src/test/java/org/cbioportal/service/impl/FederatedDataSourceServiceTest.java
+++ src/test/java/org/cbioportal/service/impl/FederatedDataSourceServiceTest.java
@@ -1,6 +1,8 @@
 package org.cbioportal.service.impl;
 
 import org.cbioportal.model.ClinicalAttribute;
+import org.cbioportal.model.ClinicalData;
+import org.cbioportal.model.ClinicalDataCount;
 import org.cbioportal.model.Sample;
 import org.cbioportal.persistence.ClinicalAttributeRepository;
 import org.cbioportal.persistence.ClinicalDataRepository;
@@ -9,6 +11,9 @@ import org.cbioportal.service.ClinicalAttributeService;
 import org.cbioportal.service.ClinicalDataService;
 import org.cbioportal.service.SampleService;
 import org.cbioportal.service.exception.FederationException;
+import org.cbioportal.web.parameter.ClinicalDataCountFilter;
+import org.cbioportal.web.parameter.ClinicalDataFilter;
+import org.cbioportal.web.parameter.StudyViewFilter;
 import org.cbioportal.web.util.ClinicalDataBinUtil;
 import org.cbioportal.web.util.ClinicalDataFetcher;
 import org.cbioportal.web.util.StudyViewFilterApplier;
@@ -21,12 +26,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -35,8 +43,11 @@ import static org.mockito.Mockito.*;
 public class FederatedDataSourceServiceTest extends BaseServiceImplTest {
     
     // NOTE: see ClinicalDataBinUtilTest for another example of a class where
-    // we are mocking out deeply nested dependencies, rather than direct subfields
+    // we are mocking out deeply nested dependencies (rather than direct subfields)
     // of the class under test.
+    
+    private final List<String> ALL_STUDIES = List.of(STUDY_ID, STUDY_ID_2, STUDY_ID_3);
+    private final List<String> VISIBLE_STUDIES = List.of(STUDY_ID, STUDY_ID_2);
     
     @Spy
     @InjectMocks
@@ -44,6 +55,9 @@ public class FederatedDataSourceServiceTest extends BaseServiceImplTest {
     
     // Injected fields -- real implementation
     // In order for @Spy / @InjectMocks to work, it's important that these are *classes*, not interfaces.
+    
+    @Spy
+    private List<String> visibleStudies = VISIBLE_STUDIES;
 
     @Spy
     @InjectMocks
@@ -133,49 +147,75 @@ public class FederatedDataSourceServiceTest extends BaseServiceImplTest {
 //            )
 //        ).thenReturn(...);
     }
+    
+    private List<ClinicalAttribute> mockPatientAttributes() {
 
+        var attrib1 = new ClinicalAttribute();
+        attrib1.setDisplayName("Sex");
+        attrib1.setDescription("Sex");
+        attrib1.setDatatype("STRING");
+        attrib1.setPatientAttribute(true);
+        attrib1.setPriority("1");
+        attrib1.setAttrId("SEX");
+        attrib1.setCancerStudyIdentifier(STUDY_ID);
+
+        var attrib2 = new ClinicalAttribute();
+        attrib2.setDisplayName("Age");
+        attrib2.setDescription("Age");
+        attrib2.setDatatype("NUMBER");
+        attrib2.setPatientAttribute(true);
+        attrib2.setPriority("1");
+        attrib2.setAttrId("AGE");
+        attrib2.setCancerStudyIdentifier(STUDY_ID);
+
+        var attrib3 = new ClinicalAttribute();
+        attrib3.setDisplayName("Sex");
+        attrib3.setDescription("Sex");
+        attrib3.setDatatype("STRING");
+        attrib3.setPatientAttribute(true);
+        attrib3.setPriority("1");
+        attrib3.setAttrId("SEX");
+        attrib3.setCancerStudyIdentifier(STUDY_ID_2);
+
+        return List.of(attrib1, attrib2, attrib3);
+    }
+    
+    private List<ClinicalAttribute> mockSampleAttributes() {
+
+        var attrib1 = new ClinicalAttribute();
+        attrib1.setDisplayName("Sample Type");
+        attrib1.setDescription("Sample Type");
+        attrib1.setDatatype("STRING");
+        attrib1.setPatientAttribute(false);
+        attrib1.setPriority("1");
+        attrib1.setAttrId("SAMPLE_TYPE");
+        attrib1.setCancerStudyIdentifier(STUDY_ID);
+
+        var attrib2 = new ClinicalAttribute();
+        attrib2.setDisplayName("Sample Type");
+        attrib2.setDescription("Sample Type");
+        attrib2.setDatatype("STRING");
+        attrib2.setPatientAttribute(false);
+        attrib2.setPriority("1");
+        attrib2.setAttrId("SAMPLE_TYPE");
+        attrib2.setCancerStudyIdentifier(STUDY_ID_2);
+        
+        return List.of(attrib1, attrib2);
+    }
+    
     private List<ClinicalAttribute> mockClinicalAttributes() {
-
-        var attrib11 = new ClinicalAttribute();
-        attrib11.setDisplayName("Sex");
-        attrib11.setDescription("Sex");
-        attrib11.setDatatype("STRING");
-        attrib11.setPatientAttribute(false);
-        attrib11.setPriority("1");
-        attrib11.setAttrId("SEX");
-        attrib11.setCancerStudyIdentifier(STUDY_ID);
-
-        var attrib21 = new ClinicalAttribute();
-        attrib21.setDisplayName("Age");
-        attrib21.setDescription("Age");
-        attrib21.setDatatype("NUMBER");
-        attrib21.setPatientAttribute(false);
-        attrib21.setPriority("1");
-        attrib21.setAttrId("AGE");
-        attrib21.setCancerStudyIdentifier(STUDY_ID);
-
-        var attrib12 = new ClinicalAttribute();
-        attrib12.setDisplayName("Sex");
-        attrib12.setDescription("Sex");
-        attrib12.setDatatype("STRING");
-        attrib12.setPatientAttribute(false);
-        attrib12.setPriority("1");
-        attrib12.setAttrId("SEX");
-        attrib12.setCancerStudyIdentifier(STUDY_ID_2);
-
-        var attrib22 = new ClinicalAttribute();
-        attrib22.setDisplayName("Age");
-        attrib22.setDescription("Age");
-        attrib22.setDatatype("NUMBER");
-        attrib22.setPatientAttribute(false);
-        attrib22.setPriority("1");
-        attrib22.setAttrId("AGE");
-        attrib22.setCancerStudyIdentifier(STUDY_ID_2);
-
-        return List.of(attrib11, attrib21, attrib12, attrib22);
+        var result = new ArrayList<ClinicalAttribute>();
+        result.addAll(mockPatientAttributes());
+        result.addAll(mockSampleAttributes());
+        return result;
+    }
+    
+    private List<String> mockUniqueCategories(ClinicalAttribute attrib) {
+        return List.of("Male", "Female");
     }
 
     private List<Sample> mockSamples() {
+        
         var sample1 = new Sample();
         sample1.setStableId(SAMPLE_ID1);
         sample1.setPatientStableId(PATIENT_ID_1);
@@ -194,18 +234,137 @@ public class FederatedDataSourceServiceTest extends BaseServiceImplTest {
         return List.of(sample1, sample2, sample3);
     }
     
+    private List<ClinicalDataCount> mockClinicalDataCounts(
+        InvocationOnMock invocation
+    ) {
+        List<String> studyIds = invocation.getArgument(0);
+        List<String> sampleIds = invocation.getArgument(1);
+        List<String> attributeIds = invocation.getArgument(2);
+        String clinicalDataType = invocation.getArgument(3);
+        String projection = invocation.getArgument(4);
+        
+        var rnd = new Random(0);
+        var result = new ArrayList<ClinicalDataCount>();
+        
+        if (clinicalDataType.equals("SAMPLE") && projection.equals("SUMMARY")) {
+            var sampleAttribs = mockSampleAttributes();
+            int upperBound = sampleIds.size();
+            
+            for (var attrib : sampleAttribs) {
+                for (var category : mockUniqueCategories(attrib)) {
+                    var cds = new ClinicalDataCount();
+                    cds.setAttributeId(attrib.getAttrId());
+                    cds.setValue(category);
+                    cds.setCount(rnd.nextInt(upperBound+1));
+                    result.add(cds);
+                }
+            }
+        } else if (clinicalDataType.equals("PATIENT") && projection.equals("SUMMARY")) {
+            var patientAttribs = mockPatientAttributes();
+            int upperBound = sampleIds.size();
+
+            for (var attrib : patientAttribs) {
+                for (var category : mockUniqueCategories(attrib)) {
+                    var cds = new ClinicalDataCount();
+                    cds.setAttributeId(attrib.getAttrId());
+                    cds.setValue(category);
+                    cds.setCount(rnd.nextInt(upperBound+1));
+                    result.add(cds);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Unsupported clinical datatype + projection");
+        }
+        
+        return result;
+    }
+    
+    private ClinicalDataCountFilter mockClinicalDataCountFilter() {
+        var svf = new StudyViewFilter();
+        // Even if the user requests to see all of the studies, we should only return a
+        // response for the studies that are actually visible
+        svf.setStudyIds(ALL_STUDIES);
+        
+        var requestedAttributes = new ArrayList<ClinicalDataFilter>();
+        for (var attrib : mockClinicalAttributes()) {
+            var cdf = new ClinicalDataFilter();
+            cdf.setAttributeId(attrib.getAttrId());
+            requestedAttributes.add(cdf);
+        }
+        
+        var filt = new ClinicalDataCountFilter();
+        filt.setStudyViewFilter(svf);
+        filt.setAttributes(requestedAttributes);
+        return filt;
+    }
+    
     @Test
     public void fetchClinicalAttributes() {
         List<ClinicalAttribute> attribs = mockClinicalAttributes();
         when(
             clinicalAttributeRepository.fetchClinicalAttributes(
-                /* studyIds */ any(),
-                /* projection */ any()
+                /* studyIds */ eq(visibleStudies),
+                /* projection */ eq("SUMMARY")
             )
         ).thenReturn(attribs);
         
-        var result = this.federatedDataSourceService.fetchClinicalAttributes();
+        var result = federatedDataSourceService.fetchClinicalAttributes();
 
+        // Verify results
         assertEquals(attribs, result);
+    }
+    
+    @Test
+    public void fetchClinicalDataCounts() {
+        List<ClinicalAttribute> attribs = mockClinicalAttributes();
+        List<String> attribIds = attribs.stream().map(ClinicalAttribute::getAttrId).collect(Collectors.toList());
+        List<Sample> samples = mockSamples();
+        List<String> sampleIds = samples.stream().map(Sample::getStableId).collect(Collectors.toList());
+        ClinicalDataCountFilter filter = mockClinicalDataCountFilter();
+
+        // When cross-checking list of attributes requested
+        when(
+            clinicalAttributeRepository.getClinicalAttributesByStudyIdsAndAttributeIds(
+                /* studyIds */ eq(visibleStudies),
+                /* attributeIds */ eq(attribIds)
+            )
+        ).thenReturn(attribs);
+
+        // When applying the study view filter to get a list of samples
+        when(
+            sampleRepository.fetchSamples(
+                /* studyIds */ eq(visibleStudies),
+                /* sampleIds */ eq(sampleIds),
+                /* projection */ eq("ID")
+            )
+        ).thenReturn(samples);
+
+        // When tallying the counts
+        when(
+            clinicalDataRepository.fetchClinicalDataCounts(
+                /* studyIds */ eq(visibleStudies),
+                /* sampleIds */ eq(sampleIds),
+                /* attributeIds */ eq(attribIds),
+                /* clinicalDataType */ any(), // "PATIENT" | "SAMPLE"
+                /* projection */ any() // "SUMMARY" | "DETAILED"
+            )
+        ).thenAnswer(this::mockClinicalDataCounts);
+
+        var result = federatedDataSourceService.fetchClinicalDataCounts(filter);
+        
+        System.out.println(result);
+
+//        // Verify results
+//        // TODO
+//        // Verify behavior
+//        // Even if the database contains other studies, we should only show the visible studies
+//        verify(studyViewFilterApplier).apply(
+//            argThat(filt -> filt.getStudyIds().equals(visibleStudies))
+//        );
+//        verify(clinicalDataService).fetchClinicalDataCounts(
+//            List.of(STUDY_ID, STUDY_ID_2),
+//            List.of(SAMPLE_ID1, SAMPLE_ID2, SAMPLE_ID3),
+//            List.of("SEX", "AGE")
+//        );
     }
 }

b36fc387c96cde63ebd626c4d2572c8274aadea6
Onur Sumer <s.onur.sumer@gmail.com>
2025-02-14 14:54:42 -0500

    reorganize files in favor of the clickhouse only clean architecture

--- src/main/java/org/cbioportal/domain/sample/Sample.java
+++ src/main/java/org/cbioportal/domain/sample/Sample.java
@@ -1,18 +1,96 @@
 package org.cbioportal.domain.sample;
 
-import org.cbioportal.legacy.model.Patient;
+import org.cbioportal.domain.patient.Patient;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-public record Sample (Integer internalId, String stableId, SampleType sampleType, Integer patientId,
-                      String patientStableId, Patient patient,
-                      String cancerStudyIdentifier, Boolean sequenced, Boolean copyNumberSegmentPresent,
-                      String uniqueSampleKey, String uniquePatientKey) implements Serializable {
+public record Sample (
+    Integer internalId,
+    String stableId,
+    SampleType sampleType,
+    Integer patientId, 
+    String patientStableId,
+    Patient patient,
+    String cancerStudyIdentifier,
+    Boolean sequenced,
+    Boolean copyNumberSegmentPresent, 
+    String uniqueSampleKey,
+    String uniquePatientKey
+) implements Serializable {
 
-    public Sample(Integer internalId, String stableId, String patientStableId, String cancerStudyIdentifier){
-        this(internalId,stableId,null,null,patientStableId,null, cancerStudyIdentifier, null, null,null, null);
+    // This constructor is intended for the ID projection
+    public Sample(Integer internalId, String stableId, String patientStableId, String cancerStudyIdentifier) {
+        this(
+            internalId,
+            stableId,
+            (SampleType) null,
+            null,
+            patientStableId,
+            null,
+            cancerStudyIdentifier,
+            null,
+            null,
+            null,
+            null
+        );
     }
+
+    // This constructor is intended for the SUMMARY projection
+    public Sample(
+        Integer internalId,
+        String stableId,
+        String sampleType,
+        Integer patientId,
+        String patientStableId,
+        String cancerStudyIdentifier,
+        String uniqueSampleKey,
+        String uniquePatientKey
+    ) {
+        this(
+            internalId,
+            stableId,
+            SampleType.fromString(sampleType),
+            patientId,
+            patientStableId,
+            null,
+            cancerStudyIdentifier,
+            null,
+            null,
+            uniqueSampleKey,
+            uniquePatientKey
+        );
+    }
+
+    // This constructor is intended for the DETAILED projection
+    public Sample(
+        Integer internalId,
+        String stableId,
+        String sampleType,
+        Integer patientId,
+        String patientStableId,
+        Patient patient,
+        String cancerStudyIdentifier,
+        Boolean sequenced,
+        Boolean copyNumberSegmentPresent,
+        String uniqueSampleKey,
+        String uniquePatientKey
+    ) {
+        this(
+            internalId,
+            stableId,
+            SampleType.fromString(sampleType),
+            patientId,
+            patientStableId,
+            patient,
+            cancerStudyIdentifier,
+            sequenced,
+            copyNumberSegmentPresent,
+            uniqueSampleKey,
+            uniquePatientKey
+        );
+    }
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

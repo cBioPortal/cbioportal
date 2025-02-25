bb78117589067967cb52a78690fc34a953a06b8c
Charles Haynes <33608920+haynescd@users.noreply.github.com>
2025-02-25 13:51:35 -0600

    Update records to extend Serializable (#11419)

--- src/main/java/org/cbioportal/domain/sample/Sample.java
+++ src/main/java/org/cbioportal/domain/sample/Sample.java
@@ -2,12 +2,13 @@ package org.cbioportal.domain.sample;
 
 import org.cbioportal.legacy.model.Patient;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 public record Sample (Integer internalId, String stableId, SampleType sampleType, Integer patientId,
                       String patientStableId, Patient patient,
                       String cancerStudyIdentifier, Boolean sequenced, Boolean copyNumberSegmentPresent,
-                      String uniqueSampleKey, String uniquePatientKey) {
+                      String uniqueSampleKey, String uniquePatientKey) implements Serializable {
 
     public Sample(Integer internalId, String stableId, String patientStableId, String cancerStudyIdentifier){
         this(internalId,stableId,null,null,patientStableId,null, cancerStudyIdentifier, null, null,null, null);

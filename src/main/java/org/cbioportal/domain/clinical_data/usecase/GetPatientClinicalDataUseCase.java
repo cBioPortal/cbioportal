3ef5db7e02d8556c3728cb06d3fa8ef0ae3164cf
Zhaoyuan (Ryan) Fu <fuzhaoyuan@gmail.com>
2025-02-27 13:33:48 -0500

    Basic structure

--- src/main/java/org/cbioportal/domain/clinical_data/usecase/GetPatientClinicalDataUseCase.java
+++ src/main/java/org/cbioportal/domain/clinical_data/usecase/GetPatientClinicalDataUseCase.java
@@ -3,18 +3,22 @@ package org.cbioportal.domain.clinical_data.usecase;
 import org.cbioportal.domain.clinical_data.repository.ClinicalDataRepository;
 import org.cbioportal.legacy.model.ClinicalData;
 import org.cbioportal.domain.studyview.StudyViewFilterContext;
+import org.cbioportal.legacy.web.parameter.ClinicalDataIdentifier;
+import org.cbioportal.legacy.web.parameter.ClinicalDataMultiStudyFilter;
+import org.cbioportal.shared.enums.ProjectionType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@Profile("clickhouse")
 /**
  * Use case for retrieving clinical data for a patient from the repository.
  * This class encapsulates the business logic for fetching clinical data based on
  * the provided study view filter context and filtered attributes.
  */
+@Service
+@Profile("clickhouse")
 public class GetPatientClinicalDataUseCase {
 
     private final ClinicalDataRepository clinicalDataRepository;
@@ -37,6 +41,30 @@ public class GetPatientClinicalDataUseCase {
      */
     public List<ClinicalData> execute(StudyViewFilterContext studyViewFilterContext, List<String> filteredAttributes) {
         return clinicalDataRepository.getPatientClinicalData(studyViewFilterContext, filteredAttributes);
+    }
+    
+    public List<ClinicalData> execute(ClinicalDataMultiStudyFilter clinicalDataMultiStudyFilter, List<String> attributeIds, ProjectionType projectionType) {
+        List<String> studyIds = new ArrayList<>();
+        List<String> patientIds = new ArrayList<>();
+        for (ClinicalDataIdentifier identifier : clinicalDataMultiStudyFilter.getIdentifiers()) {
+            studyIds.add(identifier.getStudyId());
+            patientIds.add(identifier.getEntityId());
+        }
+
+        // DETAILED level
+        if (projectionType == ProjectionType.DETAILED) {
+            return clinicalDataRepository.getPatientClinicalDataDetailed(studyIds, patientIds, attributeIds);
+        }
+
+        // ID or SUMMARY level
+        List<ClinicalData> clinicalDataList = clinicalDataRepository.getPatientClinicalDataSummary(studyIds, patientIds, attributeIds);
+        // ID level doesn't have attrValue
+        if (projectionType == ProjectionType.ID) {
+            for (ClinicalData clinicalData : clinicalDataList) {
+                clinicalData.setAttrValue(null);
+            }
+        }
+        return clinicalDataList;
     }
 }
 

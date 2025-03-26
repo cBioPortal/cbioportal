8c7db1fb27f22401198321055d196b4fa63f928b
Ruslan Forostianov <ruslan@se4.bio>
2025-03-26 14:56:06 +0100

    Port POC unit tests. Make them pass

---
+++ src/test/java/org/cbioportal/application/file/export/TSVUtilTest.java
@@ -0,0 +1,19 @@
+package org.cbioportal.application.file.export;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TSVUtilTest {
+    @Test
+    public void testComposeRow() {
+        List row = new ArrayList();
+        row.add("a");
+        row.add(null);
+        row.add("c");
+        assertEquals("a\t\tc\n", TSVUtil.composeRow(row));
+    }
+}

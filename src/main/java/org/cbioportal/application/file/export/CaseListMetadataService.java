dc08a49fc84d0151a531b3a32ef2ff06f4e40b69
Ruslan Forostianov <ruslan@se4.bio>
2025-03-26 11:31:18 +0100

    Enable case list export

---
+++ src/main/java/org/cbioportal/application/file/export/CaseListMetadataService.java
@@ -0,0 +1,19 @@
+package org.cbioportal.application.file.export;
+
+import org.cbioportal.application.file.export.mappers.CaseListMetadataMapper;
+import org.cbioportal.application.file.model.CaseListMetadata;
+
+import java.util.List;
+
+public class CaseListMetadataService {
+
+    private final CaseListMetadataMapper caseListMetadataMapper;
+
+    public CaseListMetadataService(CaseListMetadataMapper caseListMetadataMapper) {
+        this.caseListMetadataMapper = caseListMetadataMapper;
+    }
+
+    public List<CaseListMetadata> getCaseListsMetadata(String studyId) {
+        return caseListMetadataMapper.getCaseListsMetadata(studyId);
+    }
+}

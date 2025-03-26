5452da9b253fef2d2c56b0b247cc8b6bb01fbacb
Ruslan Forostianov <ruslan@se4.bio>
2025-03-26 15:25:28 +0100

    Port import-export functionality test

---
+++ test/integration/in_service_import_export_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# exit when any of these fails
+set -e
+
+echo "Importing of the test study with API validation..."
+metaImport.py -v -u http://cbioportal-container:8080 -o -s /cbioportal/test/test_data/study_es_0_import_export/
+
+echo "Exporting of the test study."
+curl -s http://cbioportal-container:8080/export/study/study_es_0_import_export.zip > study_es_0_import_export.zip \
+&& unzip study_es_0_import_export.zip -d ./output_study_es_0_import_export
+
+echo "Sort content of text files from both folders to make order during comparison unimportant."
+./cbioportal/test/integration/copy_and_sort.sh /cbioportal/test/test_data/study_es_0_import_export/ ./input_study_es_0_import_export_sorted/
+./cbioportal/test/integration/copy_and_sort.sh ./output_study_es_0_import_export/ ./output_study_es_0_import_export_sorted/
+
+echo "Comparing the original and exported studies."
+diff --recursive ./input_study_es_0_import_export_sorted/ ./output_study_es_0_import_export_sorted/
+
+exit 0

74c8c423be50a50fbab7f585122646010e0897ed
Ruslan Forostianov <ruslan@se4.bio>
2025-03-31 16:03:33 +0200

    Refactor the code

--- src/main/java/org/cbioportal/application/file/model/GenericDatatypeMetadata.java
+++ src/main/java/org/cbioportal/application/file/model/GenericDatatypeMetadata.java
@@ -2,6 +2,8 @@ package org.cbioportal.application.file.model;
 
 public interface GenericDatatypeMetadata extends StudyRelatedMetadata {
     String getGeneticAlterationType();
+
     String getDatatype();
+
     String getDataFilename();
 }

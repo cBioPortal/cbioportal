47dbca851f610d6ad6fd5a892ee4b674b0341b00
Ruslan Forostianov <ruslan@se4.bio>
2025-04-18 15:07:45 +0200

    Expand MRNA export support to Z-SCORE and DISCRETE

--- src/test/java/org/cbioportal/application/file/export/CaseListsExporterTests.java
+++ src/test/java/org/cbioportal/application/file/export/CaseListsExporterTests.java
@@ -9,11 +9,32 @@ import java.util.LinkedHashSet;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CaseListsExporterTests {
 
     String studyId = "STUDY_ID";
+    CaseListMetadataService caseListMetadataService = new CaseListMetadataService(null) {
+        @Override
+        public List<CaseListMetadata> getCaseListsMetadata(String studyId) {
+            var caseList1 = new CaseListMetadata();
+            caseList1.setCancerStudyIdentifier(studyId);
+            caseList1.setStableId(studyId + "_" + "stable_id_1");
+            caseList1.setName("Case List 1");
+            caseList1.setDescription("Description for Case List 1");
+            caseList1.setSampleIds(new LinkedHashSet<>(List.of("SAMPLE_1", "SAMPLE_2")));
+
+            var caseList2 = new CaseListMetadata();
+            caseList2.setCancerStudyIdentifier(studyId);
+            caseList2.setStableId("stable_id_2");
+            caseList2.setName("Case List 2");
+            caseList2.setDescription("Description for Case List 2");
+            caseList2.setSampleIds(new LinkedHashSet<>(List.of("SAMPLE_3", "SAMPLE_4")));
+
+            return List.of(caseList1, caseList2);
+        }
+    };
 
     @Test
     public void testNoCaseLists() {
@@ -27,7 +48,7 @@ public class CaseListsExporterTests {
 
         boolean exported = exporter.exportData(factory, studyId);
 
-        assertTrue("No case lists should be exported", !exported);
+        assertFalse("No case lists should be exported", exported);
         assertTrue("No files should be created", factory.getFileContents().isEmpty());
     }
 
@@ -58,25 +79,4 @@ public class CaseListsExporterTests {
                 + "case_list_ids: SAMPLE_3\tSAMPLE_4\n",
             fileContents.get("case_lists/cases_stable_id_2.txt").toString());
     }
-
-    CaseListMetadataService caseListMetadataService = new CaseListMetadataService(null) {
-        @Override
-        public List<CaseListMetadata> getCaseListsMetadata(String studyId) {
-            var caseList1 = new CaseListMetadata();
-            caseList1.setCancerStudyIdentifier(studyId);
-            caseList1.setStableId(studyId + "_" +"stable_id_1");
-            caseList1.setName("Case List 1");
-            caseList1.setDescription("Description for Case List 1");
-            caseList1.setSampleIds(new LinkedHashSet<>(List.of("SAMPLE_1", "SAMPLE_2")));
-
-            var caseList2 = new CaseListMetadata();
-            caseList2.setCancerStudyIdentifier(studyId);
-            caseList2.setStableId("stable_id_2");
-            caseList2.setName("Case List 2");
-            caseList2.setDescription("Description for Case List 2");
-            caseList2.setSampleIds(new LinkedHashSet<>(List.of("SAMPLE_3", "SAMPLE_4")));
-
-            return List.of(caseList1, caseList2);
-        }
-    };
 }

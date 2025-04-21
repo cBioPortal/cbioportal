534f79bf04496234ee646734edf0962d95a6cea2
alisman <lisman.aaron@gmail.com>
2025-04-21 16:14:34 -0400

    Fix sonar issue (un-used import)

--- src/test/java/org/cbioportal/legacy/service/util/AlterationCountServiceUtilTest.java
+++ src/test/java/org/cbioportal/legacy/service/util/AlterationCountServiceUtilTest.java
@@ -8,7 +8,6 @@ import org.cbioportal.legacy.model.Gistic;
 import org.cbioportal.legacy.model.GisticToGene;
 import org.cbioportal.legacy.model.MolecularProfile;
 import org.cbioportal.legacy.model.MutSig;
-import org.cbioportal.legacy.service.util.AlterationCountServiceUtil;
 import org.junit.Test;
 
 import java.math.BigDecimal;

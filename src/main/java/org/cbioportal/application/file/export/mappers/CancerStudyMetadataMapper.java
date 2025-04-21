5d0b43d451c8db424a7a51e3425a827b39f9c1cc
Ruslan Forostianov <ruslan@se4.bio>
2025-04-21 16:27:48 +0200

    Support export of cancer types
    We export the cancer type of the study with all its parents

--- src/main/java/org/cbioportal/application/file/export/mappers/CancerStudyMetadataMapper.java
+++ src/main/java/org/cbioportal/application/file/export/mappers/CancerStudyMetadataMapper.java
@@ -1,7 +1,12 @@
 package org.cbioportal.application.file.export.mappers;
 
 import org.cbioportal.application.file.model.CancerStudyMetadata;
+import org.cbioportal.application.file.model.CancerType;
+
+import java.util.List;
 
 public interface CancerStudyMetadataMapper {
     CancerStudyMetadata getCancerStudyMetadata(String studyId);
+
+    List<CancerType> getCancerTypeHierarchy(String studyId);
 }

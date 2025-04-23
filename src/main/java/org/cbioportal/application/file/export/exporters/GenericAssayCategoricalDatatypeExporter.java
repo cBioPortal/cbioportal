8df4d759bc5a6b494d45e47865803888e0e2bd64
Ruslan Forostianov <ruslan@se4.bio>
2025-04-23 15:22:06 +0200

    Support more generic assay data types

--- src/main/java/org/cbioportal/application/file/export/exporters/GenericAssayCategoricalDatatypeExporter.java
+++ src/main/java/org/cbioportal/application/file/export/exporters/GenericAssayCategoricalDatatypeExporter.java
@@ -3,14 +3,14 @@ package org.cbioportal.application.file.export.exporters;
 import org.cbioportal.application.file.export.services.GeneticProfileDataService;
 import org.cbioportal.application.file.export.services.GeneticProfileService;
 
-public class GenericAssayLimitValueDatatypeExporter extends GenericAssayDatatypeExporter {
-    public GenericAssayLimitValueDatatypeExporter(GeneticProfileService geneticProfileService, GeneticProfileDataService geneticProfileDataService) {
+public class GenericAssayCategoricalDatatypeExporter extends GenericAssayDatatypeExporter {
+    public GenericAssayCategoricalDatatypeExporter(GeneticProfileService geneticProfileService, GeneticProfileDataService geneticProfileDataService) {
         super(geneticProfileService, geneticProfileDataService);
     }
 
     @Override
     protected String getDatatype() {
-        return "LIMIT-VALUE";
+        return "CATEGORICAL";
     }
 
 }

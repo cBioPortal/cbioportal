14b497535004704d87802d694512357a8516ca3a
Onur Sumer <s.onur.sumer@gmail.com>
2025-04-30 12:54:35 -0400

    Clickhousify samples endpoint (#11393)
    * add sample column store controller, service, repository, and mapper classes
    
    * add sample_type, sequenced, and copy_number_segment_present to sample_derived
    
    * add fields required for detailed projection
    
    * add meta samples mappers
    
    * add remaining endpoints
    
    * reorganize files in favor of the clickhouse only clean architecture
    
    * reduce number of sample_derived columns by using join for the detailed projection
    
    * replace sample service with individual use cases
    
    * use ArrayType Handler wherever possible
    
    * add tests
    
    * add support for sample filter in hasPermission

--- src/main/java/org/cbioportal/domain/sample/repository/SampleRepository.java
+++ src/main/java/org/cbioportal/domain/sample/repository/SampleRepository.java
@@ -2,6 +2,8 @@ package org.cbioportal.domain.sample.repository;
 
 import org.cbioportal.domain.sample.Sample;
 import org.cbioportal.domain.studyview.StudyViewFilterContext;
+import org.cbioportal.legacy.model.meta.BaseMeta;
+import org.cbioportal.shared.enums.ProjectionType;
 
 import java.util.List;
 
@@ -27,5 +29,53 @@ public interface SampleRepository {
      * @return the total count of filtered samples
      */
     int getFilteredSamplesCount(StudyViewFilterContext studyViewFilterContext);
+
+    List<Sample> fetchSamples(List<String> studyIds, List<String> sampleIds, ProjectionType projection);
+
+    List<Sample> fetchSamplesBySampleListIds(List<String> sampleListIds, ProjectionType projection);
+
+    BaseMeta fetchMetaSamples(List<String> studyIds, List<String> sampleIds);
+
+    BaseMeta fetchMetaSamplesBySampleListIds(List<String> sampleListIds);
+
+    List<Sample> getAllSamplesInStudy(
+        String studyId,
+        ProjectionType projection,
+        Integer pageSize,
+        Integer pageNumber,
+        String sortBy,
+        String direction
+    );
+
+    BaseMeta getMetaSamplesInStudy(String studyId);
+
+    Sample getSampleInStudy(
+        String studyId,
+        String sampleId
+    );
+
+    List<Sample> getAllSamplesOfPatientInStudy(
+        String studyId,
+        String patientId,
+        ProjectionType projection,
+        Integer pageSize,
+        Integer pageNumber,
+        String sortBy,
+        String direction
+    );
+
+    BaseMeta getMetaSamplesOfPatientInStudy(String studyId, String patientId);
+
+    List<Sample> getAllSamples(
+        String keyword,
+        List<String> studyIds,
+        ProjectionType projection,
+        Integer pageSize,
+        Integer pageNumber,
+        String sort,
+        String direction
+    );
+
+    BaseMeta getMetaSamples(String keyword, List<String> studyIds);
 }
 

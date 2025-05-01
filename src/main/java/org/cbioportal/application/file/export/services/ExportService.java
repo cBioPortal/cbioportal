361b68af3efc9ba8b3f1a006c7a5a95b327b9df0
Ruslan Forostianov <ruslan@se4.bio>
2025-05-01 12:17:49 +0200

    Move check for presense of study into the service
    As a preparation to support of Virtual Study export

--- src/main/java/org/cbioportal/application/file/export/services/ExportService.java
+++ src/main/java/org/cbioportal/application/file/export/services/ExportService.java
@@ -2,10 +2,15 @@ package org.cbioportal.application.file.export.services;
 
 import org.cbioportal.application.file.export.exporters.ExportDetails;
 import org.cbioportal.application.file.export.exporters.Exporter;
+import org.cbioportal.application.file.model.CancerStudyMetadata;
 import org.cbioportal.application.file.utils.FileWriterFactory;
+import org.cbioportal.application.security.CancerStudyPermissionEvaluator;
+import org.cbioportal.legacy.utils.security.AccessLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -14,12 +19,25 @@ public class ExportService implements Exporter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExportService.class);
 
+    private final CancerStudyMetadataService cancerStudyMetadataService;
     private final List<Exporter> exporters;
+    private final CancerStudyPermissionEvaluator cancerStudyPermissionEvaluator;
 
     public ExportService(
+        CancerStudyMetadataService cancerStudyMetadataService,
+        CancerStudyPermissionEvaluator cancerStudyPermissionEvaluator,
         List<Exporter> exporters
     ) {
+        this.cancerStudyMetadataService = cancerStudyMetadataService;
+        this.cancerStudyPermissionEvaluator = cancerStudyPermissionEvaluator;
         this.exporters = exporters;
+    }
+
+    public boolean isStudyExportable(String studyId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CancerStudyMetadata cancerStudyMetadata = this.cancerStudyMetadataService.getCancerStudyMetadata(studyId);
+        return cancerStudyMetadata != null && (authentication == null || cancerStudyPermissionEvaluator == null ||
+            cancerStudyPermissionEvaluator.hasPermission(authentication, studyId, "CancerStudyId", AccessLevel.READ));
     }
 
     @Transactional

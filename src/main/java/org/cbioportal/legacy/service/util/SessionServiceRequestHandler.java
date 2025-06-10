4080dc508f1b02b93c49340436e37b294390c78e
Gulshan Anand <gulshan2052@gmail.com>
2025-06-10 23:24:16 +0530

    Refactored SessionServiceController to have all low-level http calls in SessionServiceHandler

--- src/main/java/org/cbioportal/legacy/service/util/SessionServiceRequestHandler.java
+++ src/main/java/org/cbioportal/legacy/service/util/SessionServiceRequestHandler.java
@@ -3,13 +3,17 @@ package org.cbioportal.legacy.service.util;
 import static org.cbioportal.legacy.utils.removeme.Session.*;
 
 import com.mongodb.BasicDBObject;
+import java.io.Serializable;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
-import org.cbioportal.legacy.web.parameter.VirtualStudy;
-import org.cbioportal.legacy.web.parameter.VirtualStudyData;
+import org.cbioportal.legacy.utils.removeme.Session;
+import org.cbioportal.legacy.web.parameter.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +30,10 @@ import org.springframework.web.client.RestTemplate;
 public class SessionServiceRequestHandler {
 
   private static final Logger LOG = LoggerFactory.getLogger(SessionServiceRequestHandler.class);
+
+  private static final String QUERY_OPERATOR_ALL = "$all";
+  private static final String QUERY_OPERATOR_SIZE = "$size";
+  private static final String QUERY_OPERATOR_AND = "$and";
 
   @Value("${session.service.url:}")
   private String sessionServiceURL;
@@ -161,5 +169,126 @@ public class SessionServiceRequestHandler {
         .put(
             sessionServiceURL + "/virtual_study/" + virtualStudy.getId(),
             new HttpEntity<>(virtualStudy.getData(), getHttpHeaders()));
+  }
+
+  public List<PageSettings> getPageSettingsForUser(
+      String username, Set<String> origin, String sessionPageName) {
+
+    List<BasicDBObject> basicDBObjects = new ArrayList<>();
+    basicDBObjects.add(
+        new BasicDBObject("data.owner", Pattern.compile(username, Pattern.CASE_INSENSITIVE)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_ALL, origin)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_SIZE, origin.size())));
+    basicDBObjects.add(new BasicDBObject("data.page", sessionPageName));
+
+    BasicDBObject queryDBObject = new BasicDBObject(QUERY_OPERATOR_AND, basicDBObjects);
+
+    ResponseEntity<List<PageSettings>> responseEntity =
+        new RestTemplate()
+            .exchange(
+                sessionServiceURL + Session.SessionType.settings + "/query/fetch",
+                HttpMethod.POST,
+                new HttpEntity<String>(queryDBObject.toString(), getHttpHeaders()),
+                new ParameterizedTypeReference<List<PageSettings>>() {});
+
+    return responseEntity.getBody();
+  }
+
+  public List<VirtualStudy> getVirtualStudiesForUser(String username, List<String> studyIds) {
+    // ignore origin studies order
+    // add $size to make sure origin studies is not a subset
+    List<BasicDBObject> basicDBObjects = new ArrayList<>();
+    basicDBObjects.add(
+        new BasicDBObject("data.users", Pattern.compile(username, Pattern.CASE_INSENSITIVE)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_ALL, studyIds)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_SIZE, studyIds.size())));
+
+    BasicDBObject queryDBObject = new BasicDBObject(QUERY_OPERATOR_AND, basicDBObjects);
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    HttpEntity<String> httpEntity = new HttpEntity<>(queryDBObject.toString(), getHttpHeaders());
+
+    ResponseEntity<List<VirtualStudy>> responseEntity =
+        restTemplate.exchange(
+            sessionServiceURL + Session.SessionType.group + "/query/fetch",
+            HttpMethod.POST,
+            httpEntity,
+            new ParameterizedTypeReference<List<VirtualStudy>>() {});
+
+    return responseEntity.getBody();
+  }
+
+  public List<CustomGeneList> getCustomGeneListsForUser(String username) {
+    BasicDBObject basicDBObject = new BasicDBObject();
+    basicDBObject.put("data.users", Pattern.compile(username, Pattern.CASE_INSENSITIVE));
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    HttpEntity<String> httpEntity = new HttpEntity<>(basicDBObject.toString(), getHttpHeaders());
+
+    ResponseEntity<List<CustomGeneList>> responseEntity =
+        restTemplate.exchange(
+            sessionServiceURL + Session.SessionType.custom_gene_list + "/query/fetch",
+            HttpMethod.POST,
+            httpEntity,
+            new ParameterizedTypeReference<List<CustomGeneList>>() {});
+
+    return responseEntity.getBody();
+  }
+
+  public List<CustomDataSession> getCustomDataSessionForUser(
+      String username, List<String> studyIds) {
+    List<BasicDBObject> basicDBObjects = new ArrayList<>();
+    basicDBObjects.add(
+        new BasicDBObject("data.users", Pattern.compile(username, Pattern.CASE_INSENSITIVE)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_ALL, studyIds)));
+    basicDBObjects.add(
+        new BasicDBObject("data.origin", new BasicDBObject(QUERY_OPERATOR_SIZE, studyIds.size())));
+
+    BasicDBObject queryDBObject = new BasicDBObject(QUERY_OPERATOR_AND, basicDBObjects);
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    HttpEntity<String> httpEntity = new HttpEntity<>(queryDBObject.toString(), getHttpHeaders());
+
+    ResponseEntity<List<CustomDataSession>> responseEntity =
+        restTemplate.exchange(
+            sessionServiceURL + Session.SessionType.custom_data + "/query/fetch",
+            HttpMethod.POST,
+            httpEntity,
+            new ParameterizedTypeReference<List<CustomDataSession>>() {});
+
+    return responseEntity.getBody();
+  }
+
+  public <T extends Serializable> ResponseEntity<Session> createSession(
+      SessionType type, T payload) {
+    RestTemplate restTemplate = new RestTemplate();
+    HttpEntity<?> httpEntity = new HttpEntity<>(payload, getHttpHeaders());
+
+    ResponseEntity<Session> responseEntity =
+        restTemplate.exchange(sessionServiceURL + type, HttpMethod.POST, httpEntity, Session.class);
+
+    return new ResponseEntity<>(responseEntity.getBody(), responseEntity.getStatusCode());
+  }
+
+  public <T extends Serializable> void updateUsers(SessionType type, String id, T payload) {
+    RestTemplate restTemplate = new RestTemplate();
+    HttpEntity<?> httpEntity = new HttpEntity<>(payload, getHttpHeaders());
+
+    restTemplate.put(sessionServiceURL + type + "/" + id, httpEntity);
+  }
+
+  public void updatePageSettings(SessionType type, String id, PageSettingsData body) {
+    RestTemplate restTemplate = new RestTemplate();
+    HttpEntity<Object> httpEntity = new HttpEntity<>(body, getHttpHeaders());
+
+    restTemplate.put(sessionServiceURL + type + "/" + id, httpEntity);
   }
 }

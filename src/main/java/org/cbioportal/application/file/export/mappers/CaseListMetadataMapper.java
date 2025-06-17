4c7f5bee3e548617099418d8aeb0ffcdec09545a
Ruslan Forostianov <ruslan@se4.bio>
2025-06-17 11:34:59 +0200

    Add javadoc to mappers of export fucntionality

--- src/main/java/org/cbioportal/application/file/export/mappers/CaseListMetadataMapper.java
+++ src/main/java/org/cbioportal/application/file/export/mappers/CaseListMetadataMapper.java
@@ -4,6 +4,14 @@ import java.util.List;
 import java.util.Set;
 import org.cbioportal.application.file.model.CaseListMetadata;
 
+/** Mapper interface for retrieving metadata for case lists. */
 public interface CaseListMetadataMapper {
+  /**
+   * Retrieves metadata for a specific case list.
+   *
+   * @param studyId the identifier of the study
+   * @param sampleIds filter for specific sample IDs within the case list
+   * @return a list of CaseListMetadata objects containing metadata for the specified case list
+   */
   List<CaseListMetadata> getCaseListsMetadata(String studyId, Set<String> sampleIds);
 }

8e2fe95571699a5a1cf2898be59d6082d9aaa239
Ruslan Forostianov <ruslan@se4.bio>
2025-06-23 17:11:43 +0200

    Introduce repositories layer

--- src/main/java/org/cbioportal/application/file/export/services/GenePanelMatrixService.java
+++ src/main/java/org/cbioportal/application/file/export/services/GenePanelMatrixService.java
@@ -2,29 +2,29 @@ package org.cbioportal.application.file.export.services;
 
 import java.util.List;
 import java.util.Set;
-import org.cbioportal.application.file.export.mappers.GenePanelMatrixMapper;
+import org.cbioportal.application.file.export.repositories.GenePanelMatrixRepository;
 import org.cbioportal.application.file.model.GenePanelMatrixItem;
 import org.cbioportal.application.file.utils.CloseableIterator;
-import org.cbioportal.application.file.utils.CursorAdapter;
 
 public class GenePanelMatrixService {
-  private final GenePanelMatrixMapper genePanelMatrixMapper;
+  private final GenePanelMatrixRepository genePanelMatrixRepository;
 
-  public GenePanelMatrixService(GenePanelMatrixMapper genePanelMatrixMapper) {
-    this.genePanelMatrixMapper = genePanelMatrixMapper;
+  public GenePanelMatrixService(GenePanelMatrixRepository genePanelMatrixRepository) {
+    this.genePanelMatrixRepository = genePanelMatrixRepository;
   }
 
   public boolean hasGenePanelMatrix(String studyId, Set<String> sampleIds) {
-    return genePanelMatrixMapper.hasGenePanelMatrix(studyId, sampleIds);
+    return genePanelMatrixRepository.hasGenePanelMatrix(studyId, sampleIds);
   }
 
   public CloseableIterator<GenePanelMatrixItem> getGenePanelMatrix(
       String studyId, Set<String> sampleIds) {
-    return new CursorAdapter<>(genePanelMatrixMapper.getGenePanelMatrix(studyId, sampleIds));
+    return genePanelMatrixRepository.getGenePanelMatrix(studyId, sampleIds);
   }
 
   public List<String> getDistinctGeneProfileIdsWithGenePanelMatrix(
       String studyId, Set<String> sampleIds) {
-    return genePanelMatrixMapper.getDistinctGeneProfileIdsWithGenePanelMatrix(studyId, sampleIds);
+    return genePanelMatrixRepository.getDistinctGeneProfileIdsWithGenePanelMatrix(
+        studyId, sampleIds);
   }
 }
--- src/main/java/org/cbioportal/application/file/export/services/StructuralVariantService.java
+++ src/main/java/org/cbioportal/application/file/export/services/StructuralVariantService.java
@@ -1,22 +1,20 @@
 package org.cbioportal.application.file.export.services;
 
 import java.util.Set;
-import org.cbioportal.application.file.export.mappers.SVMapper;
+import org.cbioportal.application.file.export.repositories.SVRepository;
 import org.cbioportal.application.file.model.StructuralVariant;
 import org.cbioportal.application.file.utils.CloseableIterator;
-import org.cbioportal.application.file.utils.CursorAdapter;
 
 public class StructuralVariantService {
 
-  private final SVMapper structuralVariantMapper;
+  private final SVRepository svRepository;
 
-  public StructuralVariantService(SVMapper structuralVariantMapper) {
-    this.structuralVariantMapper = structuralVariantMapper;
+  public StructuralVariantService(SVRepository svRepository) {
+    this.svRepository = svRepository;
   }
 
   public CloseableIterator<StructuralVariant> getStructuralVariants(
       String geneticDatatypeStableId, Set<String> sampleIds) {
-    return new CursorAdapter<>(
-        structuralVariantMapper.getStructuralVariants(geneticDatatypeStableId, sampleIds));
+    return svRepository.getStructuralVariants(geneticDatatypeStableId, sampleIds);
   }
 }

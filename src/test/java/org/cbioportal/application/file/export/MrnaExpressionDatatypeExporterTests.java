0897dab1137396600ea8d945ed471fbdfd3daa15
Ruslan Forostianov <ruslan@se4.bio>
2025-06-23 14:29:24 +0200

    Fix order of samples when exporting data for sample ids
    e.g. for Virtual Studies
    Before order of samples in header changed arbitrary!

--- src/test/java/org/cbioportal/application/file/export/MrnaExpressionDatatypeExporterTests.java
+++ src/test/java/org/cbioportal/application/file/export/MrnaExpressionDatatypeExporterTests.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import org.cbioportal.application.file.export.exporters.ExportDetails;
@@ -123,6 +124,68 @@ public class MrnaExpressionDatatypeExporterTests {
             Hugo_Symbol\tEntrez_Gene_Id\tSAMPLE_1\tSAMPLE_2
             GENE_SYMBOL\t12345\t1.23\t4.56
             """,
+        fileContents.get("data_mrna_expression_continuous_maf_stable_id.txt").toString());
+  }
+
+  @Test
+  public void testMrnaExpressionExport_SampleIdsOrder() {
+    var factory = new InMemoryFileWriterFactory();
+
+    GeneticProfileDataService geneticProfileDataService =
+        new GeneticProfileDataService(null) {
+          @Override
+          public CloseableIterator<GeneticProfileData> getData(String molecularProfileStableId) {
+            GeneticProfileData data = new GeneticProfileData();
+            var gene = new Gene();
+            gene.setHugoGeneSymbol("GENE_SYMBOL");
+            gene.setEntrezGeneId(12345);
+            data.setGene(gene);
+            GeneticEntity geneticEntity = new GeneticEntity();
+            geneticEntity.setGeneticEntityId(43210);
+            geneticEntity.setStableId("GENE1");
+            data.setGeneticEntity(geneticEntity);
+            data.setCommaSeparatedValues("1.23,4.56,");
+            return new SimpleCloseableIterator<>(List.of(data));
+          }
+
+          @Override
+          public List<String> getSampleStableIds(String molecularProfileStableId) {
+            return List.of("SAMPLE_1", "SAMPLE_2");
+          }
+        };
+
+    MrnaExpressionContinuousDatatypeExporter exporter =
+        new MrnaExpressionContinuousDatatypeExporter(
+            geneticProfileService, geneticProfileDataService);
+
+    ExportDetails exportDetails =
+        new ExportDetails(
+            "TEST_STUDY_ID",
+            null,
+            new LinkedHashSet<>(List.of("SAMPLE_2", "SAMPLE_1"))); // Different order
+    boolean exported = exporter.exportData(factory, exportDetails);
+
+    assertTrue(exported);
+    var fileContents = factory.getFileContents();
+    assertEquals(
+        Set.of(
+            "meta_mrna_expression_continuous_maf_stable_id.txt",
+            "data_mrna_expression_continuous_maf_stable_id.txt"),
+        fileContents.keySet());
+
+    assertEquals(
+        "cancer_study_identifier: TEST_STUDY_ID\n"
+            + "genetic_alteration_type: MRNA_EXPRESSION\n"
+            + "datatype: CONTINUOUS\n"
+            + "stable_id: MAF_STABLE_ID\n"
+            + "data_filename: data_mrna_expression_continuous_maf_stable_id.txt\n",
+        fileContents.get("meta_mrna_expression_continuous_maf_stable_id.txt").toString());
+
+    assertEquals(
+        """
+                Hugo_Symbol\tEntrez_Gene_Id\tSAMPLE_1\tSAMPLE_2
+                GENE_SYMBOL\t12345\t1.23\t4.56
+                """,
         fileContents.get("data_mrna_expression_continuous_maf_stable_id.txt").toString());
   }
 

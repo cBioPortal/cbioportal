8c60e33d706bfaa9376bd1065648599557b45a4d
gisetia <gisetia@gmail.com>
2025-06-24 17:52:44 +0200

    Add single-study virtual study integration test

--- test/integration/test_import_export.sh
+++ test/integration/test_import_export.sh
@@ -12,6 +12,13 @@ run_in_service() {
     return $?  # return the exit code of the last command
 }
 
+echo "Testing import and export of physical study."
 run_in_service cbioportal '/cbioportal/test/integration/in_service_import_export_test.sh'
+
+echo "Testing import and export of single-study virtual study."
+run_in_service cbioportal '/cbioportal/test/integration/in_service_single_virtual_import_export_test.sh'
+
+# echo "Testing import and export of multi-study virtual study."
+# run_in_service cbioportal '/cbioportal/test/integration/in_service_multi_virtual_import_export_test.sh'
 
 exit 0

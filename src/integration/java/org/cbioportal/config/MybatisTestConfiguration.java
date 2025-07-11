d07c9595219633d85483774a56667575decef4d6
Charles Haynes <33608920+haynescd@users.noreply.github.com>
2025-07-11 10:25:57 -0500

    :white_check_mark: Add e2e/integration testing (#11613)
    * Split integration test out
    
    * Add e2e test
    
    * Add E2E test workflow
    
    * Fix profile for e2e tests
    
    * update to run integration and e2e test
    
    * Fix env vars
    
    * fix sonar workflow

---
+++ src/integration/java/org/cbioportal/config/MybatisTestConfiguration.java
@@ -0,0 +1,41 @@
+package org.cbioportal.config;
+
+import java.io.IOException;
+
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ResourceLoader;
+
+@TestConfiguration
+@MapperScan(value = "org.cbioportal.infrastructure.repository.clickhouse")
+public class MybatisTestConfiguration {
+    @Bean
+    @ConfigurationProperties("spring.datasource.clickhouse")
+    public DataSourceProperties clickhouseDatSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return clickhouseDatSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlColumnarSessionFactory(
+            ResourceLoader resourceLoader, DataSource dataSource, ApplicationContext context)
+            throws IOException {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.addMapperLocations(
+                context.getResources("classpath:mappers/clickhouse/**/*.xml"));
+        return sessionFactory;
+    }
+
+}

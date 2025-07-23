c4731349c82a5f14cf51d19bcdb4ef84a6e45189
flelayo <franck.le-layo@gustaveroussy.fr>
2025-07-23 16:06:25 +0200

    Update skin_hide_download_controls property
    - Value must be changed if download_group is given

--- src/main/java/org/cbioportal/legacy/service/FrontendPropertiesServiceImpl.java
+++ src/main/java/org/cbioportal/legacy/service/FrontendPropertiesServiceImpl.java
@@ -1,10 +1,13 @@
 package org.cbioportal.legacy.service;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -287,9 +290,11 @@ public class FrontendPropertiesServiceImpl implements FrontendPropertiesService 
         return serverConfigProperties.get(property.getFrontendName());
     }
 
-    public Map<String, String> getFrontendProperties() {
+    public Map<String, String> getFrontendProperties(Authentication authentication) {
         // Make sure that requests work on individual instances of this data.
-        return cloneProperties();
+        Map<String,String> properties = cloneProperties();
+        updateShowDownloadButtonProperties(properties, authentication);
+        return properties;
     }
     
     private Map<String,String> cloneProperties() {
@@ -299,6 +304,16 @@ public class FrontendPropertiesServiceImpl implements FrontendPropertiesService 
                 (out, entry) -> out.put(entry.getKey(), entry.getValue()),
                 HashMap::putAll
             );
+    }
+    
+    private void updateShowDownloadButtonProperties(Map<String,String> properties, Authentication authentication) {
+        String downloadGroup = env.getProperty("download_group", "");
+        if(authentication != null && StringUtils.isNotEmpty(downloadGroup)) {
+            boolean userHasRole = authentication.getAuthorities().contains(new SimpleGrantedAuthority(downloadGroup));
+            if(userHasRole) {
+                properties.put("skin_hide_download_controls", "show");
+            }
+        }
     }
 
     /**

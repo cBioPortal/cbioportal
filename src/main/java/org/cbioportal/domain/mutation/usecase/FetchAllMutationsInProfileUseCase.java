8ea0ec04e85913af3f4eb93e7f0af436d26cee42
ejehleslie <ejehboss@gmail.com>
2025-08-02 09:26:12 -0400

    Working on the infrastructure layer

--- src/main/java/org/cbioportal/domain/mutation/usecase/FetchAllMutationsInProfileUseCase.java
+++ src/main/java/org/cbioportal/domain/mutation/usecase/FetchAllMutationsInProfileUseCase.java
@@ -2,10 +2,13 @@ package org.cbioportal.domain.mutation.usecase;
 
 import org.cbioportal.domain.mutation.repository.MutationRepository;
 import org.cbioportal.legacy.model.Mutation;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@Profile("clickhouse")
 public class FetchAllMutationsInProfileUseCase {
     private final MutationRepository mutationRepository;
 
@@ -22,8 +25,6 @@ public class FetchAllMutationsInProfileUseCase {
                                   String sortBy,
                                   String direction) {
         
-        //First validate the molecular profile exists then 
-        // return the list from repos with a list of mutations 
         return mutationRepository.getMutationsInMultipleMolecularProfiles(
             molecularProfileIds,
             sampleIds,

973af6cad197563037139e1020ffb967f8161221
ejehleslie <ejehboss@gmail.com>
2025-08-02 12:27:45 -0400

    finished up the mapper class for the infrastructure

--- src/main/java/org/cbioportal/infrastructure/repository/clickhouse/mutation/ClickhouseMutationDataMapper.java
+++ src/main/java/org/cbioportal/infrastructure/repository/clickhouse/mutation/ClickhouseMutationDataMapper.java
@@ -1,4 +1,23 @@
 package org.cbioportal.infrastructure.repository.clickhouse.mutation;
 
+import org.cbioportal.legacy.model.Mutation;
+import org.cbioportal.legacy.model.meta.MutationMeta;
+
+import java.util.List;
+
 public interface ClickhouseMutationDataMapper {
+
+    List<Mutation> getMutationsInMultipleMolecularProfiles(
+            List<String> molecularProfileIds,
+            List<String> sampleIds,
+            List<Integer> entrezGeneIds,
+            String projection,
+            Integer pageSize,
+            Integer pageNumber,
+            String sortBy,
+            String direction);
+
+    public MutationMeta getMetaMutationsInMultipleMolecularProfiles(List<String> molecularProfileIds, 
+                                                                    List<String> sampleIds, 
+                                                                    List<Integer> entrezGeneIds);
 }

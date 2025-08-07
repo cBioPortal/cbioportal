66e58220715c5394efe76cd284bd6fa78e9179b3
ejehleslie <ejehboss@gmail.com>
2025-08-07 12:40:39 -0400

    Finshed testing the utility class and usecase logic

--- src/main/java/org/cbioportal/shared/MutationSearchCriteria.java
+++ src/main/java/org/cbioportal/shared/MutationSearchCriteria.java
@@ -2,7 +2,6 @@ package org.cbioportal.shared;
 
 import org.cbioportal.legacy.web.parameter.Direction;
 import org.cbioportal.legacy.web.parameter.Projection;
-import org.cbioportal.legacy.web.parameter.sort.MutationSortBy;
 
 public record MutationSearchCriteria(Projection projection, Integer pageSize, Integer pageNumber, String sortBy, Direction direction ) {
 }

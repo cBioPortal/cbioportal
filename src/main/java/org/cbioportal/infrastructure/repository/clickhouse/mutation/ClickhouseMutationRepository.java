75272dc2d3aa758263fb5d5e6e4eea36152c0a10
ejehleslie <ejehboss@gmail.com>
2025-08-17 16:50:35 -0400

    Refractored the mapper class method to handle each projection with its seperate method

--- src/main/java/org/cbioportal/infrastructure/repository/clickhouse/mutation/ClickhouseMutationRepository.java
+++ src/main/java/org/cbioportal/infrastructure/repository/clickhouse/mutation/ClickhouseMutationRepository.java
@@ -5,18 +5,22 @@ import org.cbioportal.legacy.model.Mutation;
 import org.cbioportal.legacy.model.meta.MutationMeta;
 import org.cbioportal.legacy.persistence.mybatis.util.PaginationCalculator;
 import org.cbioportal.shared.MutationSearchCriteria;
+import org.cbioportal.shared.enums.ProjectionType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
 @Profile("clickhouse")
 public class ClickhouseMutationRepository implements MutationRepository {
     
-    private final ClickhouseMutationDataMapper mapper;
+    private final ClickhouseMutationMapper mapper;
     
 
-    public ClickhouseMutationRepository(ClickhouseMutationDataMapper clickhouseMutationMapper) {
+    public ClickhouseMutationRepository(ClickhouseMutationMapper clickhouseMutationMapper) {
         this.mapper = clickhouseMutationMapper;
     }
 
@@ -26,18 +30,44 @@ public class ClickhouseMutationRepository implements MutationRepository {
         List<String> sampleIds,
         List<Integer> entrezGeneIds,
         MutationSearchCriteria mutationSearchCriteria){
+        
         Integer Limit= mutationSearchCriteria.pageSize();
         Integer offset= PaginationCalculator.offset(mutationSearchCriteria.pageSize(),mutationSearchCriteria.pageNumber());
 
-        return  mapper.getMutationsInMultipleMolecularProfiles(molecularProfileIds,
-            sampleIds,
-            entrezGeneIds, 
-                false,
-            mutationSearchCriteria.projection().name(),
-                Limit,
-                offset,
-            mutationSearchCriteria.sortBy(),
-            mutationSearchCriteria.direction().name());
+        var projection=mutationSearchCriteria.projection();
+        return  switch (projection){
+            case ID->
+                    mapper.getMutationsInMultipleMolecularProfiles(molecularProfileIds,
+                            sampleIds,
+                            entrezGeneIds,
+                            false,
+                            mutationSearchCriteria.projection().name(),
+                            Limit,
+                            offset,
+                            mutationSearchCriteria.sortBy(),
+                            mutationSearchCriteria.direction().name());
+            case SUMMARY->
+                    mapper.getSummaryMutationsInMultipleMolecularProfiles(molecularProfileIds,
+                            sampleIds,
+                            entrezGeneIds,
+                            false,
+                            mutationSearchCriteria.projection().name(),
+                            Limit,
+                            offset,
+                            mutationSearchCriteria.sortBy(),
+                            mutationSearchCriteria.direction().name());
+            case DETAILED->
+                    mapper.getDetailedMutationsInMultipleMolecularProfiles(molecularProfileIds,
+                            sampleIds,
+                            entrezGeneIds,
+                            false,
+                            mutationSearchCriteria.projection().name(),
+                            Limit,
+                            offset,
+                            mutationSearchCriteria.sortBy(),
+                            mutationSearchCriteria.direction().name());
+            default -> new ArrayList<>();
+        };
     }
 
     @Override
--- src/main/java/org/cbioportal/shared/MutationSearchCriteria.java
+++ src/main/java/org/cbioportal/shared/MutationSearchCriteria.java
@@ -3,9 +3,9 @@ package org.cbioportal.shared;
 import org.cbioportal.legacy.web.parameter.Direction;
 import org.cbioportal.legacy.web.parameter.Projection;
 
-public record MutationSearchCriteria(Projection projection, 
+public record MutationSearchCriteria(Projection projection,
                                      Integer pageSize,
-                                     Integer pageNumber, 
+                                     Integer pageNumber,
                                      String sortBy,
                                      Direction direction ) {
 }

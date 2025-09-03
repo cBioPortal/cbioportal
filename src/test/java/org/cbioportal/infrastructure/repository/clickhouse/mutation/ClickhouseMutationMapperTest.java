a26d0238309842155c0fe74e0e19281c7634370a
ejehleslie <ejehboss@gmail.com>
2025-09-03 14:42:08 -0400

    Created dto's and maooers for data received from the clincal data mapper

---
+++ src/test/java/org/cbioportal/infrastructure/repository/clickhouse/mutation/ClickhouseMutationMapperTest.java
@@ -0,0 +1,73 @@
+package org.cbioportal.infrastructure.repository.clickhouse.mutation;
+
+import org.cbioportal.infrastructure.repository.clickhouse.AbstractTestcontainers;
+import org.cbioportal.infrastructure.repository.clickhouse.config.MyBatisConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@Import(MyBatisConfig.class)
+@DataJpaTest
+@DirtiesContext
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = AbstractTestcontainers.Initializer.class)
+public class ClickhouseMutationMapperTest {
+    
+    @Autowired private ClickhouseMutationMapper clickhouseMutationMapper;
+
+    @Test
+    public void getMutationsInMultipleMolecularProfiles() {
+        var molecularProfileIds = List.of("study_tcga_pub_mutations");
+        var sampleIds = List.of("tcga-a1-a0sh-01");
+        var entrezGeneIds = List.of(672);
+
+        var result = clickhouseMutationMapper.getMutationsInMultipleMolecularProfiles(
+            molecularProfileIds, sampleIds, entrezGeneIds, false, "ID", null, null, null, null);
+
+        assertEquals(2, result.size());
+        result.forEach(mutation -> {
+            assertEquals("study_tcga_pub_mutations", mutation.getMolecularProfileId());
+            assertEquals("tcga-a1-a0sh-01", mutation.getSampleId());
+            assertEquals((Integer) 672, mutation.getEntrezGeneId());
+        });
+        
+    }
+
+    @Test
+    public void getSummaryMutationsInMultipleMolecularProfiles() {
+        var molecularProfileIds = List.of("study_tcga_pub_mutations");
+        var sampleIds = List.of("tcga-a1-a0sh-01");
+        var entrezGeneIds = List.of(672);
+    }
+
+    @Test
+    public void getDetailedMutationsInMultipleMolecularProfiles() {
+        var molecularProfileIds = List.of("study_tcga_pub_mutations");
+        var sampleIds = List.of("tcga-a1-a0sh-01");
+        var entrezGeneIds = List.of(672);
+    }
+
+    @Test
+    public void getMetaMutationsInMultipleMolecularProfiles() {
+        var molecularProfileIds = List.of("study_tcga_pub_mutations");
+        var sampleIds = List.of("tcga-a1-a0sh-01");
+        var entrezGeneIds = List.of(672);
+
+        var result = clickhouseMutationMapper.getMetaMutationsInMultipleMolecularProfiles(
+            molecularProfileIds, sampleIds, entrezGeneIds, false);
+
+        assertEquals((Integer) 2, result.getTotalCount());
+        assertEquals((Integer) 1, result.getSampleCount());
+    }
+}

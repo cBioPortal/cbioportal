b161eaf963a9f90cafa8d0f8fdd13d537f1d3e9d
Gaofei Zhao <zhaogaofeimail@gmail.com>
2025-10-21 00:56:35 -0400

    Remove wrong profile from InfoServiceImpl.java

--- src/main/java/org/cbioportal/legacy/service/impl/InfoServiceImpl.java
+++ src/main/java/org/cbioportal/legacy/service/impl/InfoServiceImpl.java
@@ -4,11 +4,9 @@ import org.cbioportal.legacy.model.InfoDb;
 import org.cbioportal.legacy.persistence.InfoRepository;
 import org.cbioportal.legacy.service.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"legacy"})
 public class InfoServiceImpl implements InfoService {
 
   @Autowired private InfoRepository infoRepository;

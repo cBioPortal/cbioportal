7a89a904de4caea498858c6499d08ea68f783a2b
Onur Sumer <s.onur.sumer@gmail.com>
2025-11-03 14:22:17 -0500

    mark GeneMemoizer as deprecated and remove unused references

--- src/main/java/org/cbioportal/legacy/service/impl/GeneMemoizerServiceImpl.java
+++ src/main/java/org/cbioportal/legacy/service/impl/GeneMemoizerServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Deprecated
 public class GeneMemoizerServiceImpl implements GeneMemoizerService {
   @Autowired private StaticDataTimestampService timestampService;
 

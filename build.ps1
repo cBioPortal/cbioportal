8a0fa823ed3c0c8334662e78ac3f3919980f28b1
Purvansh Joshi <purvanshjoshi7534011576@gmail.com>
2026-02-19 08:47:05 +0530

    chore: remove unnecessary helper scripts and logs

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$env:JAVA_HOME = [Environment]::GetEnvironmentVariable('JAVA_HOME', 'User')
-$env:M2_HOME = [Environment]::GetEnvironmentVariable('M2_HOME', 'User')
-$gitBin = "C:\Program Files\Git\bin"
-$env:PATH = "$env:JAVA_HOME\bin;$env:M2_HOME\bin;$gitBin;$env:PATH"
-
-Write-Host "Running Maven with JAVA_HOME=$env:JAVA_HOME"
-mvn clean install "-DskipTests" "-Dmaven.antrun.skip=true" "-Dgit.commit.id.skip=true" "-Dmaven.gitcommitid.skip=true"

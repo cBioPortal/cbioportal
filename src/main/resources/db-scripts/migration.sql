8d6f77ac426864cc3d33eb8096f6a2cc5e922f79
James Ko <jamesqko@gmail.com>
2026-03-10 11:50:44 -0400

    drop table if exists

--- src/main/resources/db-scripts/migration.sql
+++ src/main/resources/db-scripts/migration.sql
@@ -1119,9 +1119,9 @@ FROM resource_study rs
 JOIN resource_definition rd
   ON rs.RESOURCE_ID = rd.RESOURCE_ID;
 
-DROP TABLE resource_patient;
-DROP TABLE resource_sample;
-DROP TABLE resource_study;
+DROP TABLE IF EXISTS resource_patient;
+DROP TABLE IF EXISTS resource_sample;
+DROP TABLE IF EXISTS resource_study;
 
 -- reminder : always set both the current DB_SCHEMA_VERSION and the current DERIVED_TABLE_SCHEMA_VERSION at each update
 ALTER TABLE `info` ADD COLUMN `GENE_TABLE_VERSION` varchar(24);
